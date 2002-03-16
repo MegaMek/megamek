54fdd2b8df27b6a1204ce251dc5939af24f1bbb7
bmazur <bmazur@users.sourceforge.net>
2002-03-16 22:21:49 +0000

    + center on unit works in new display + new display works when screen is bigger than board + highlight border appears under mech instead of on top (subtle) + initiative ties rolled off (up to 5 times--sort of a hack) + edges cleared in new display + charging attacker takes damage on normal front table + death from above implemented

---
+++ megamek/src/megamek/common/actions/DfaAttackAction.java
@@ -0,0 +1,48 @@
+/*
+ * MegaMek - Copyright (C) 2000-2002 Ben Mazur (bmazur@sev.org)
+ * 
+ *  This program is free software; you can redistribute it and/or modify it 
+ *  under the terms of the GNU General Public License as published by the Free 
+ *  Software Foundation; either version 2 of the License, or (at your option) 
+ *  any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *  for more details.
+ */
+
+/*
+ * DfaAttackAction.java
+ *
+ * Created on March 16, 2002, 11:43 AM
+ */
+
+package megamek.common.actions;
+
+import megamek.common.*;
+
+/**
+ *
+ * @author  Ben
+ * @version 
+ */
+public class DfaAttackAction extends AbstractAttackAction {
+
+    private Coords targetPos;
+
+    /** Creates new DfaAttackAction */
+    public DfaAttackAction(int entityId, int targetId, Coords targetPos) {
+        super(entityId, targetId);
+        this.targetPos = targetPos;
+    }
+    
+    public Coords getTargetPos() {
+        return targetPos;
+    }
+    
+    public void setTargetPos(Coords targetPos) {
+        this.targetPos = targetPos;
+    }
+
+}

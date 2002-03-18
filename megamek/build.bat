3fdc1df1146b7a64a46008fd4f49832fd54b1328
bmazur <bmazur@users.sourceforge.net>
2002-03-18 06:11:23 +0000

    fixed a bug with targetting the java compiler to version 1.1

--- megamek/build.bat
+++ megamek/build.bat
@@ -1,5 +1,5 @@
 rem uses an unholy combination of the Sun & Microsoft Java SDKs
 
-javac -O -target 1.1 -d classes megamek/*.java
+javac -target 1.1 -d classes -O megamek/*.java
 jexegen /out:MegaMek.exe /base:classes /main:megamek.MegaMek /r  *.class
 jar cmf manifest.txt MegaMek.jar -C classes .

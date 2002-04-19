237d818d38254d5d88dedbf1b7ba6ed7e3835ab4
bmazur <bmazur@users.sourceforge.net>
2002-04-19 14:11:20 +0000

    moved sun classes around

--- megamek/build.bat
+++ megamek/build.bat
@@ -1,6 +1,5 @@
 @rem uses an unholy combination of the Sun & Microsoft Java SDKs
 
-del /S /Q classes\megamek
-javac -target 1.1 -d classes -classpath classes -sourcepath . -O megamek/*.java
+javac -target 1.1 -d classes -O megamek/*.java
 jexegen /out:MegaMek.exe /base:classes /main:megamek.MegaMek /r  *.class
 jar cmf manifest.txt MegaMek.jar -C classes .

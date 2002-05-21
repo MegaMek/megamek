1072edb451ebf20c74e60b2ad161c167549a7b15
bmazur <bmazur@users.sourceforge.net>
2002-05-21 20:14:45 +0000

    VERSION, TIMESTAMP fields no longer final

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -26,8 +26,8 @@ import megamek.server.*;
 public class MegaMek
     implements WindowListener, ActionListener
 {
-    public static final String  VERSION = "0.25.5";
-    public static final long    TIMESTAMP = new File("timestamp").lastModified();
+    public static String    VERSION = "0.25.5";
+    public static long      TIMESTAMP = new File("timestamp").lastModified();
     
     public Frame            frame;
     

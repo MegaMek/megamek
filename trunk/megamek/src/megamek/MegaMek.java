35050c90f3b6f5c8ed18647121abaab03cb74a08
bmazur <bmazur@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2002-06-03 15:51:29 +0000

    0.26.0

--- trunk/megamek/src/megamek/MegaMek.java
+++ trunk/megamek/src/megamek/MegaMek.java
@@ -26,7 +26,7 @@ import megamek.server.*;
 public class MegaMek
     implements WindowListener, ActionListener
 {
-    public static String    VERSION = "0.25.7";
+    public static String    VERSION = "0.26.0";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
     
     public Frame            frame;

3bd055330827943b0d5f4eb8d2fa9afa2b874d76
bmazur <bmazur@users.sourceforge.net>
2002-06-13 16:13:06 +0000

    + added new bot code by Steven Hawkins <skh_mail@yahoo.com>

---
+++ megamek/src/megamek/client/bot/Strategy.java
@@ -0,0 +1,41 @@
+/*
+ * Strategy.java
+ *
+ * Created on May 19, 2002, 12:01 PM
+ */
+
+package megamek.client.bot;
+
+import megamek.common.*;
+import megamek.client.*;
+import megamek.client.bot.*;
+import megamek.server.*;
+
+/**
+ * Container for strategy modifiers -- will be expanded upon
+ * @author  Steve Hawkins
+ */
+public class Strategy {
+  
+  /* 0 full out retreat (damage shy)
+   * +inf full out attack (damage preference)
+   */
+  public double attack = 1;
+  
+  /* Modify attacks against me based upon this.
+   * i.e. how strongly do people want to attack me
+   */
+  public double target = 1;
+  
+  //not yet implemented
+  public double movement = 1;
+  
+  //where should I go?
+  Coords target_coords = null;
+  
+  /** Creates a new instance of Class */
+  public Strategy() {
+  }
+  
+}
+

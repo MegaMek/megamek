70a9002750d7a6bccb4bc137c1780c1063382743
bmazur <bmazur@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2002-07-20 01:15:13 +0000

    v 0.26.7

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -26,7 +26,7 @@ import megamek.server.*;
 public class MegaMek
     implements WindowListener, ActionListener
 {
-    public static String    VERSION = "0.26.6";
+    public static String    VERSION = "0.26.7";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
     
     public Frame            frame;

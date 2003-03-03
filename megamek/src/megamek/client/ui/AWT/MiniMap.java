2d73b3e6269719a1415f61dd46ccae385d6b1e79
zipp32 <zipp32@users.sourceforge.net>
2003-03-03 17:20:30 +0000

    Fixed resizing of hex grid and startup size of minimap window.

--- megamek/src/megamek/client/ui/AWT/MiniMap.java
+++ megamek/src/megamek/client/ui/AWT/MiniMap.java
@@ -32,25 +32,27 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
 
     private final static Color[] m_terrainColors = new Color[Terrain.SIZE];
 
-    private Image m_mapImage;
-    private BoardView1 m_bview;
-    private Game m_game;
-    private Dialog m_dialog;
-    private final double cos30 = 0.866;
-    private int          hexSide;
-    private final int    minHexSide = 3;
-    private int          hexSideByCos30;
-    private int          hexSideBySin30;
-    private int          minMargin = 3;
+    private Image        m_mapImage;
+    private BoardView1   m_bview;
+    private Game         m_game;
+    private Dialog       m_dialog;
+    private final int    margin = 6;
     private int          topMargin;
     private int          leftMargin;
-    private int          heightBufer;
+    private final int    buttonHeight = 14;
     private boolean      minimized = false;
-    private final int    unitSize = 6; //variable which define size of triangle for unit representation
-    private int          halfRoadWidth;
-    private int          halfRoadWidthByCos30;
-    private int          halfRoadWidthBySin30;
+    private int          heightBufer;
+    private final int    unitSize = 6;//variable which define size of triangle for unit representation
     private Vector       roadHexIndexes = new Vector();
+    private int          zoom = 1;
+    private int[]        hexSide = {3,6,8,10,12};
+    private int[]        hexSideByCos30 = {3,5,7,9,10};
+    private int[]        hexSideBySin30 = {2,3,4,5,6};
+    private int[]        halfRoadWidthByCos30 = {0,1,2,2,3};
+    private int[]        halfRoadWidthBySin30 = {0,1,1,1,2};
+    private int[]        halfRoadWidth        = {0,1,2,3,3};
+
+
     /**
      * Creates and lays out a new mech display.
      */
@@ -63,6 +65,7 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
         c.addGameListener(this);
         addMouseListener(this);
         addComponentListener(this);
+        m_dialog.setResizable(false);
     }
 
     public void update(Graphics g) {
@@ -93,24 +96,20 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
     }
 
     private void initializeMap() {
-        double heightFactor = (2*m_game.board.height+1)*cos30;
-        double widthFactor = (1.5 * m_game.board.width + 0.5);
+		int requiredWidth, requiredHeight;
+		int currentHexSide = hexSide[zoom];
+		int currentHexSideByCos30 = hexSideByCos30[zoom];
+		int currentHexSideBySin30 = hexSideBySin30[zoom];
+		topMargin = margin;
+		leftMargin = margin;
+		requiredWidth = m_game.board.width*(currentHexSide + currentHexSideBySin30) + currentHexSideBySin30 + 2*margin;
+		requiredHeight = (2*m_game.board.height + 1)*currentHexSideByCos30 + 2*margin + buttonHeight;
+        setSize(requiredWidth, requiredHeight);
+        m_dialog.pack();
         m_dialog.show();
-        hexSide = (int) ( Math.min((getSize().height - 14 - minMargin*2)/heightFactor, (getSize().width - minMargin*2)/widthFactor));
-        hexSide = Math.max(hexSide, minHexSide);
-        hexSideByCos30 = (int) Math.round(hexSide* cos30);
-        hexSideBySin30 = (int) Math.round(hexSide/2);
-        halfRoadWidth =  (int) (hexSide/4);
-        halfRoadWidthByCos30 = (int) (hexSide*cos30/4);
-        halfRoadWidthBySin30 = (int) (hexSide/8);
-        if ((halfRoadWidthByCos30 == 0) ||(halfRoadWidthBySin30 == 0)){
-            halfRoadWidth = 0;
-            halfRoadWidthByCos30 = 0;
-            halfRoadWidthBySin30 = 0;
-        }
-        leftMargin = (int)((getSize().width - m_game.board.width*(hexSide + hexSideBySin30) - hexSideBySin30)/2);
-        topMargin =(int)(((getSize().height - 14) - hexSideByCos30*(2*m_game.board.height + 1))/2);
-        m_mapImage = createImage(Math.max(1, getSize().width), Math.max(1, getSize().height)); // area must be > 0
+        m_mapImage = createImage(getSize().width,getSize().height);
+        if (getSize().width > requiredWidth) leftMargin = (int) ((getSize().width - requiredWidth)/2) + margin;
+        if (getSize().height > requiredHeight) topMargin = (int) ((getSize().height - requiredHeight)/2) + margin;
         drawMap();
     }
 
@@ -174,27 +173,53 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
         g.drawLine(getSize().width-1,getSize().height - 14,getSize().width-1,getSize().height);
         g.setColor(Color.yellow);
         g.fillPolygon(xPoints,yPoints,3);
+
+
+        //drawing "+" and "-" buttons
+        if (! minimized){
+			g.setColor(Color.black);
+			g.drawLine(14 - 1,getSize().height - 14, 14 - 1,getSize().height);
+			g.drawLine(getSize().width - 14 - 1,getSize().height - 14, getSize().width - 14 - 1,getSize().height);
+			g.setColor(Color.green.darker());
+			g.drawLine(14,getSize().height - 14, 14,getSize().height);
+			g.drawLine(getSize().width - 14 ,getSize().height - 14, getSize().width - 14,getSize().height);
+			if (zoom == 0){
+				g.setColor(Color.gray.brighter());
+		    } else {
+				g.setColor(Color.yellow);
+			}
+			g.fillRect(3,getSize().height - 14 + 6, 8, 2);
+			if (zoom == (hexSide.length - 1)){
+				g.setColor(Color.gray.brighter());
+		    } else {
+				g.setColor(Color.yellow);
+			}
+			g.fillRect(getSize().width - 14 + 3, getSize().height - 14 + 6, 8, 2);
+			g.fillRect(getSize().width - 14 + 6, getSize().height - 14 + 3, 2, 8);
+	    }
+
         g.setColor(oldColor);
+
     }
 
 
     private void paintCoord(Graphics g, int x, int y, boolean border) {
-        int baseX = x *(hexSide + hexSideBySin30) + leftMargin;
-        int baseY = (2*y + 1 + x%2)* hexSideByCos30 + topMargin;
+        int baseX = x *(hexSide[zoom] + hexSideBySin30[zoom]) + leftMargin;
+        int baseY = (2*y + 1 + x%2)* hexSideByCos30[zoom] + topMargin;
         int [] xPoints = new int[6];
         int [] yPoints = new int[6];
         xPoints[0] = baseX;
         yPoints[0] = baseY;
-        xPoints[1] = baseX + hexSideBySin30;
-        yPoints[1] = baseY + hexSideByCos30;
-        xPoints[2] = baseX + hexSideBySin30 + hexSide;
-        yPoints[2] = baseY + hexSideByCos30;
-        xPoints[3] = baseX + hexSideBySin30 + hexSide + hexSideBySin30;
+        xPoints[1] = baseX + hexSideBySin30[zoom];
+        yPoints[1] = baseY + hexSideByCos30[zoom];
+        xPoints[2] = xPoints[1] + hexSide[zoom];
+        yPoints[2] = yPoints[1];
+        xPoints[3] = xPoints[2] + hexSideBySin30[zoom];
         yPoints[3] = baseY;
-        xPoints[4] = baseX + hexSideBySin30 + hexSide;
-        yPoints[4] = baseY - hexSideByCos30;
-        xPoints[5] = baseX + hexSideBySin30;
-        yPoints[5] = baseY - hexSideByCos30;
+        xPoints[4] = xPoints[2];
+        yPoints[4] = baseY - hexSideByCos30[zoom];
+        xPoints[5] = xPoints[1];
+        yPoints[5] = yPoints[4];
         g.fillPolygon(xPoints,yPoints,6);
         if (border) {
             Color oldColor = g.getColor();
@@ -205,8 +230,8 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
     }
 
     private void paintUnit (Graphics g, int x, int y, boolean border) {
-        int baseX = x *(hexSide + hexSideBySin30) + leftMargin + hexSide;
-        int baseY = (2*y + 1 + x%2)* hexSideByCos30 + topMargin;
+        int baseX = x *(hexSide[zoom] + hexSideBySin30[zoom]) + leftMargin + hexSide[zoom];
+        int baseY = (2*y + 1 + x%2)* hexSideByCos30[zoom] + topMargin;
         int [] xPoints = new int[3];
         int [] yPoints = new int[3];
         xPoints[0] = baseX;
@@ -235,84 +260,84 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
             int[] hex = (int[])enum.nextElement();
             x = hex[0];
             y = hex[1];
-            baseX = x *(hexSide + hexSideBySin30) + leftMargin + hexSide;
-            baseY = (2*y + 1 + x%2)* hexSideByCos30 + topMargin;
+            baseX = x *(hexSide[zoom] + hexSideBySin30[zoom]) + leftMargin + hexSide[zoom];
+            baseY = (2*y + 1 + x%2)* hexSideByCos30[zoom] + topMargin;
             exits = hex[2];
             // Is there a North exit?
             if ( 0 != (exits & 0x0001) ) {
-                xPoints[0] = baseX - halfRoadWidth ;
+                xPoints[0] = baseX - halfRoadWidth[zoom] ;
                 yPoints[0] = baseY;
-                xPoints[1] = baseX - halfRoadWidth;
-                yPoints[1] = baseY - hexSideByCos30;
-                xPoints[2] = baseX + halfRoadWidth;
-                yPoints[2] = baseY - hexSideByCos30;
-                xPoints[3] = baseX + halfRoadWidth;
+                xPoints[1] = baseX - halfRoadWidth[zoom];
+                yPoints[1] = baseY - hexSideByCos30[zoom];
+                xPoints[2] = baseX + halfRoadWidth[zoom];
+                yPoints[2] = baseY - hexSideByCos30[zoom];
+                xPoints[3] = baseX + halfRoadWidth[zoom];
                 yPoints[3] = baseY;
                 g.drawPolygon(xPoints,yPoints,4);
                 g.fillPolygon(xPoints,yPoints,4);
             }
             // Is there a North-East exit?
             if ( 0 != (exits & 0x0002) ) {
-                xPoints[0] = baseX - halfRoadWidthBySin30;
-                yPoints[0] = baseY - halfRoadWidthByCos30;
-                xPoints[1] = (int) Math.round(baseX + 3*hexSide/4 - halfRoadWidthBySin30);
-                yPoints[1] = (int) Math.round(baseY - hexSideByCos30/2 - halfRoadWidthByCos30);
-                xPoints[2] = xPoints[1] + 2 * halfRoadWidthBySin30;
-                yPoints[2] = yPoints[1] + 2 * halfRoadWidthByCos30;
-                xPoints[3] = baseX + halfRoadWidthBySin30;
-                yPoints[3] = baseY + halfRoadWidthByCos30;
+                xPoints[0] = baseX - halfRoadWidthBySin30[zoom];
+                yPoints[0] = baseY - halfRoadWidthByCos30[zoom];
+                xPoints[1] = (int) Math.round(baseX + 3*hexSide[zoom]/4 - halfRoadWidthBySin30[zoom]);
+                yPoints[1] = (int) Math.round(baseY - hexSideByCos30[zoom]/2 - halfRoadWidthByCos30[zoom]);
+                xPoints[2] = xPoints[1] + 2 * halfRoadWidthBySin30[zoom];
+                yPoints[2] = yPoints[1] + 2 * halfRoadWidthByCos30[zoom];
+                xPoints[3] = baseX + halfRoadWidthBySin30[zoom];
+                yPoints[3] = baseY + halfRoadWidthByCos30[zoom];
                 g.drawPolygon(xPoints,yPoints,4);
                 g.fillPolygon(xPoints,yPoints,4);
             }
                   // Is there a South-East exit?
             if ( 0 != (exits & 0x0004) ) {
-                xPoints[0] = baseX + halfRoadWidthBySin30;
-                yPoints[0] = baseY - halfRoadWidthByCos30;
-                xPoints[1] = (int) Math.round(baseX + 3*hexSide/4 + halfRoadWidthBySin30);
-                yPoints[1] = (int) Math.round(baseY + hexSideByCos30/2 - halfRoadWidthByCos30);
-                xPoints[2] = xPoints[1] - 2 * halfRoadWidthBySin30;
-                yPoints[2] = yPoints[1] + 2 * halfRoadWidthByCos30;
-                xPoints[3] = baseX - halfRoadWidthBySin30;
-                yPoints[3] = baseY + halfRoadWidthByCos30;
+                xPoints[0] = baseX + halfRoadWidthBySin30[zoom];
+                yPoints[0] = baseY - halfRoadWidthByCos30[zoom];
+                xPoints[1] = (int) Math.round(baseX + 3*hexSide[zoom]/4 + halfRoadWidthBySin30[zoom]);
+                yPoints[1] = (int) Math.round(baseY + hexSideByCos30[zoom]/2 - halfRoadWidthByCos30[zoom]);
+                xPoints[2] = xPoints[1] - 2 * halfRoadWidthBySin30[zoom];
+                yPoints[2] = yPoints[1] + 2 * halfRoadWidthByCos30[zoom];
+                xPoints[3] = baseX - halfRoadWidthBySin30[zoom];
+                yPoints[3] = baseY + halfRoadWidthByCos30[zoom];
                 g.drawPolygon(xPoints,yPoints,4);
                 g.fillPolygon(xPoints,yPoints,4);
             }
             // Is there a South exit?
             if ( 0 != (exits & 0x0008) ) {
-                xPoints[0] = baseX + halfRoadWidth;
+                xPoints[0] = baseX + halfRoadWidth[zoom];
                 yPoints[0] = baseY;
-                xPoints[1] = baseX + halfRoadWidth;
-                yPoints[1] = baseY + hexSideByCos30;
-                xPoints[2] = baseX - halfRoadWidth;
-                yPoints[2] = baseY + hexSideByCos30;
-                xPoints[3] = baseX - halfRoadWidth;;
+                xPoints[1] = baseX + halfRoadWidth[zoom];
+                yPoints[1] = baseY + hexSideByCos30[zoom];
+                xPoints[2] = baseX - halfRoadWidth[zoom];
+                yPoints[2] = baseY + hexSideByCos30[zoom];
+                xPoints[3] = baseX - halfRoadWidth[zoom];
                 yPoints[3] = baseY;
                 g.drawPolygon(xPoints,yPoints,4);
                 g.fillPolygon(xPoints,yPoints,4);
             }
             // Is there a South-West exit?
             if ( 0 != (exits & 0x0010) ) {
-                xPoints[0] = baseX + halfRoadWidthBySin30;
-                yPoints[0] = baseY + halfRoadWidthByCos30;
-                xPoints[1] = (int) Math.round(baseX - 3*hexSide/4 + halfRoadWidthBySin30);
-                yPoints[1] = (int) Math.round(baseY + hexSideByCos30/2 + halfRoadWidthByCos30);
-                xPoints[2] = xPoints[1] - 2*halfRoadWidthBySin30;
-                yPoints[2] = yPoints[1] - 2*halfRoadWidthByCos30;
-                xPoints[3] = baseX - halfRoadWidthBySin30;
-                yPoints[3] = baseY - halfRoadWidthByCos30;
+                xPoints[0] = baseX + halfRoadWidthBySin30[zoom];
+                yPoints[0] = baseY + halfRoadWidthByCos30[zoom];
+                xPoints[1] = (int) Math.round(baseX - 3*hexSide[zoom]/4 + halfRoadWidthBySin30[zoom]);
+                yPoints[1] = (int) Math.round(baseY + hexSideByCos30[zoom]/2 + halfRoadWidthByCos30[zoom]);
+                xPoints[2] = xPoints[1] - 2*halfRoadWidthBySin30[zoom];
+                yPoints[2] = yPoints[1] - 2*halfRoadWidthByCos30[zoom];
+                xPoints[3] = baseX - halfRoadWidthBySin30[zoom];
+                yPoints[3] = baseY - halfRoadWidthByCos30[zoom];
                 g.drawPolygon(xPoints,yPoints,4);
                 g.fillPolygon(xPoints,yPoints,4);
             }
             // Is there a North-West exit?
             if ( 0 != (exits & 0x0020) ) {
-                xPoints[0] = baseX - halfRoadWidthBySin30;
-                yPoints[0] = baseY + halfRoadWidthByCos30;
-                xPoints[1] = (int) Math.round(baseX - 3*hexSide/4 - halfRoadWidthBySin30);
-                yPoints[1] = (int) Math.round(baseY - hexSideByCos30/2 + halfRoadWidthByCos30);
-                xPoints[2] = xPoints[1] + 2*halfRoadWidthBySin30;;
-                yPoints[2] = yPoints[1] - 2*halfRoadWidthByCos30;;
-                xPoints[3] = baseX + halfRoadWidthBySin30;
-                yPoints[3] = baseY - halfRoadWidthByCos30;
+                xPoints[0] = baseX - halfRoadWidthBySin30[zoom];
+                yPoints[0] = baseY + halfRoadWidthByCos30[zoom];
+                xPoints[1] = (int) Math.round(baseX - 3*hexSide[zoom]/4 - halfRoadWidthBySin30[zoom]);
+                yPoints[1] = (int) Math.round(baseY - hexSideByCos30[zoom]/2 + halfRoadWidthByCos30[zoom]);
+                xPoints[2] = xPoints[1] + 2*halfRoadWidthBySin30[zoom];
+                yPoints[2] = yPoints[1] - 2*halfRoadWidthByCos30[zoom];
+                xPoints[3] = baseX + halfRoadWidthBySin30[zoom];
+                yPoints[3] = baseY - halfRoadWidthByCos30[zoom];
                 g.drawPolygon(xPoints,yPoints,4);
                 g.fillPolygon(xPoints,yPoints,4);
             }
@@ -354,14 +379,14 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
     }
 
     private Coords translateCoords(int x, int y) {
-       int gridX = (int) (x / (hexSideBySin30 + hexSide));
-       int restX = x % (hexSideBySin30 + hexSide);
-       int gridY = (int) (y / (2*hexSideByCos30));
-       int restY = y % (2*hexSideByCos30);
-       restX = hexSideBySin30 + hexSide - restX;
-       restY -= hexSideByCos30;
-       if (hexSideBySin30*restX > hexSideByCos30*restY) gridX ++;
-       if (-hexSideBySin30*restX > hexSideByCos30*restY) gridY --;
+       int gridX = (int) (x / (hexSideBySin30[zoom] + hexSide[zoom]));
+       int restX = x % (hexSideBySin30[zoom] + hexSide[zoom]);
+       int gridY = (int) (y / (2*hexSideByCos30[zoom]));
+       int restY = y % (2*hexSideByCos30[zoom]);
+       restX = hexSideBySin30[zoom] + hexSide[zoom] - restX;
+       restY -= hexSideByCos30[zoom];
+       if (hexSideBySin30[zoom]*restX > hexSideByCos30[zoom]*restY) gridX ++;
+       if (-hexSideBySin30[zoom]*restX > hexSideByCos30[zoom]*restY) gridY --;
        if (gridY <0)gridY = 0;
        return new Coords(gridX, gridY);
     }
@@ -369,20 +394,31 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
     private void processMouseClick(int x, int y){
         if (y > (getSize().height - 14)){
 
-            if (minimized){
-                m_dialog.setResizable(true);
-                setSize(getSize().width, heightBufer);
-            }else{
-                heightBufer = getSize().height;
-                setSize(getSize().width, 14);
-                m_mapImage = createImage(Math.max(1, getSize().width), 14);
-                m_dialog.setResizable(false);
-            }
-            minimized = ! minimized;
-            m_dialog.pack();
-            drawMap();
+            if(x < 14){
+				if (zoom == 0) return;
+				zoom --;
+				initializeMap();
+		    }else if ( x> (getSize().width - 14)){
+				if (zoom == (hexSide.length - 1)) return;
+				zoom ++;
+				initializeMap();
+		    } else{
+                if (minimized){
+                    //m_dialog.setResizable(true);
+                    setSize(getSize().width, heightBufer);
+                    m_mapImage = createImage(getSize().width, heightBufer);
+                }else{
+                    heightBufer = getSize().height;
+                    setSize(getSize().width, 14);
+                    m_mapImage = createImage(Math.max(1, getSize().width), 14);
+                    //m_dialog.setResizable(false);
+                }
+                minimized = ! minimized;
+                m_dialog.pack();
+                drawMap();
+		  }
         }else{
-            if ((x < leftMargin) || (x > (getSize().width -  leftMargin)) || (y < topMargin) || (y > (getSize().height - topMargin))){
+            if ((x < margin) || (x > (getSize().width -  leftMargin)) || (y < topMargin) || (y > (getSize().height - topMargin - 14))){
                 return;
             } else {
                 m_bview.centerOnHex(translateCoords(x - leftMargin, y - topMargin));
@@ -453,7 +489,7 @@ implements BoardListener, MouseListener, ComponentListener, GameListener {
     }
 
     public void componentResized(ComponentEvent ce) {
-            if (!minimized) initializeMap();
+           // if (!minimized) initializeMap();
     }
 
 

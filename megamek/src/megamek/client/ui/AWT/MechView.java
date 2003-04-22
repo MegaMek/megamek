5c8d78e42210f311e2f34c5a506078017ce67f7e
suvarov454 <suvarov454@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2003-04-22 02:52:21 +0000

    + Added BAP, TAG, and AP Pods just to calc BV correctly, rules not implemented. + Target buildings for ignition. + 723043: Stop casting a Tank to a Mech to show C3 BV affects. + 724350: Updated mechset.txt for the new Infantry names. + 724351: Running away with someone else's Battle Armor squad shouldn't crash. + Add check for existing fires to Server#tryIgniteHex(). + Renamed Server#burn() to Server#ignite(), to match Inferno naming convention. + Only shots that miss Entities accidentally ignite fires. + Tooltip for weapons fire should use text value. + "/skip" command should handle NULL entities.

--- megamek/src/megamek/client/ui/AWT/MechView.java
+++ megamek/src/megamek/client/ui/AWT/MechView.java
@@ -65,9 +65,15 @@ public class MechView {
             sBasic.append( "Inner Sphere" );
         }
         sBasic.append("\n");
-        if (mech.hasC3M() || mech.hasC3S() || mech.hasC3i()) {
-            sBasic.append( "Linked c3 BV: ")
-                .append( ((Mech)mech).calculateBattleValue(true) );
+        if ( mech.hasC3M() || mech.hasC3S() || mech.hasC3i()) {
+            sBasic.append( "Linked c3 BV: ");
+            if ( isMech ) {
+                sBasic.append( ((Mech)mech).calculateBattleValue(true) );
+            } else {
+// TODO: Have C3 affect BV of tanks.
+//                sBasic.append( ((Tank)mech).calculateBattleValue(true) );
+                sBasic.append( ((Tank)mech).calculateBattleValue() );
+            }
         }
         sBasic.append("\n");
         sBasic.append( "Movement: " )

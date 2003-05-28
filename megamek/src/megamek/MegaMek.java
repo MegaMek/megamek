5806fed4384688b485dd6962c92fc08a74efb808
suvarov454 <suvarov454@users.sourceforge.net>
2003-05-28 12:03:57 +0000

    + Bug 743355, Data 742085: applied updated infantry.zip from Hopalong. + Bug 712311: use only lower case letters in the image file names.

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -27,7 +27,7 @@ import megamek.server.*;
 public class MegaMek
     implements WindowListener, ActionListener
 {
-    public static String    VERSION = "0.28.19";
+    public static String    VERSION = "0.28.20-dev";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
     
     public Frame            frame;

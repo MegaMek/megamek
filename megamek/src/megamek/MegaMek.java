641bf71b8fb52ed62a3b09af8bf911a59b88f02a
suvarov454 <suvarov454@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2003-06-25 21:47:22 +0000

    + Bug 760509: apply case-insensitive comparisons to default images, too.

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -27,7 +27,7 @@ import megamek.server.*;
 public class MegaMek
     implements WindowListener, ActionListener
 {
-    public static String    VERSION = "0.29.0";
+    public static String    VERSION = "0.29.0.1";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
 
     public Frame            frame;

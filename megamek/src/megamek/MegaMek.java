12d9eccedcd565713658a7ff478abe80ea7b529b
bmazur <bmazur@users.sourceforge.net>
2003-06-25 04:02:53 +0000

    release 0.29.0 :)

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -27,7 +27,7 @@ import megamek.server.*;
 public class MegaMek
     implements WindowListener, ActionListener
 {
-    public static String    VERSION = "0.29rc2-CVS";
+    public static String    VERSION = "0.29.0";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
 
     public Frame            frame;

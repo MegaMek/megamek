70745a4867607360bbd51e2179c8611af81132d3
oscarmm <oscarmm@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2003-06-28 17:38:37 +0000

    + New version

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -27,7 +27,7 @@ import megamek.server.*;
 public class MegaMek
     implements WindowListener, ActionListener
 {
-    public static String    VERSION = "0.29.1";
+    public static String    VERSION = "0.29.2-dev";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
 
     public Frame            frame;

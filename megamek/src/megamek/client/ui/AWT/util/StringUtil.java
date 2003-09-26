df425612fd9011d79ab80fc7582860466cf88b24
nalleolsson <nalleolsson@users.sourceforge.net>
2003-09-26 09:42:11 +0000

    Sorts camolist.

--- megamek/src/megamek/client/ui/AWT/util/StringUtil.java
+++ megamek/src/megamek/client/ui/AWT/util/StringUtil.java
@@ -15,6 +15,7 @@
 package megamek.client.util;
 
 import java.util.Vector;
+import com.sun.java.util.collections.Comparator;
 
 public class StringUtil {
 
@@ -42,4 +43,29 @@ public class StringUtil {
     	return v;
     }
 
+	public static Comparator stringComparator() {
+		return new Comparator() {
+			public int compare(java.lang.Object o1, java.lang.Object o2) {
+				String s1 = ((String) o1).toLowerCase();
+				String s2 = ((String) o2).toLowerCase();
+
+				for (int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
+					if (s1.charAt(i) < s2.charAt(i)) {
+						return -1;
+					} else if (s1.charAt(i) > s2.charAt(i)) {
+						return 1;
+					}
+				}
+
+				if (s1.length() < s2.length()) {
+					return -1;
+				} else if (s1.length() > s2.length()) {
+					return 1;
+				} else {
+					return 0;
+				}
+
+			}
+		};
+	}
 }

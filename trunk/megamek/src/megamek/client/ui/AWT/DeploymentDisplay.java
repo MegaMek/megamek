c1912123c7277c3250b4253a447c52bbc5a4484c
nalleolsson <nalleolsson@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2003-10-04 09:34:23 +0000

    New chat box.

--- trunk/megamek/src/megamek/client/ui/AWT/DeploymentDisplay.java
+++ trunk/megamek/src/megamek/client/ui/AWT/DeploymentDisplay.java
@@ -93,7 +93,7 @@ public class DeploymentDisplay
 
         // layout button grid
         panButtons = new Panel();
-        panButtons.setLayout(new GridLayout(2, 3));
+        panButtons.setLayout(new GridLayout(1, 6));
         panButtons.add(butTurn);
         panButtons.add(butLoad);
         panButtons.add(butNext);
@@ -116,12 +116,9 @@ public class DeploymentDisplay
         c.gridwidth = GridBagConstraints.REMAINDER;
         addBag(panStatus, gridbag, c);
 
-        c.gridwidth = 1;
-        c.weightx = 1.0;    c.weighty = 0.0;
-        addBag(client.cb.getComponent(), gridbag, c);
-
         c.gridwidth = GridBagConstraints.REMAINDER;
         c.weightx = 0.0;    c.weighty = 0.0;
+        c.ipady = 20;
         addBag(panButtons, gridbag, c);
 
         addKeyListener(this);

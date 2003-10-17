7674ffcf52bbde1425128c3bcffb4f838f75ca64
suvarov454 <suvarov454@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2003-10-17 01:29:18 +0000

    + Modified MegaMek.bat to be more friendly for Sun JRE.

--- megamek/MegaMek.bat
+++ megamek/MegaMek.bat
@@ -1,1 +1,10 @@
+@echo off
+if "%JAVA_HOME%"=="" goto MicrosoftJRE
+java -jar MegaMek.jar
+goto done
+
+:MicrosoftJRE
 start wjview /cp collections.jar;TinyXML.jar;MegaMek.jar megamek/MegaMek
+
+:done
+@echo on

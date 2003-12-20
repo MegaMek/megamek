4c48da5d77a392ff426d3d4645ee2d3d75cfdd51
suvarov454 <suvarov454@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2003-12-20 21:55:00 +0000

    + Bug 797649, Patch 862927: Stop accepting arbitrary block delimiters.

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -28,7 +28,7 @@ import megamek.server.*;
 public class MegaMek
     implements ActionListener
 {
-    public static String    VERSION = "0.29.19";
+    public static String    VERSION = "0.29.20-dev";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
 
     public Frame            frame;

e75299247e4ffe0a40b2d3f75f774dd966bb7490
nemchenk <nemchenk@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2004-01-17 22:24:17 +0000

    Didn't realise a release had been made :) Bumped the versions

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -28,7 +28,7 @@ import megamek.server.*;
 public class MegaMek
     implements ActionListener
 {
-    public static String    VERSION = "0.29.22";
+    public static String    VERSION = "0.29.23-dev";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
 
     public Frame            frame;

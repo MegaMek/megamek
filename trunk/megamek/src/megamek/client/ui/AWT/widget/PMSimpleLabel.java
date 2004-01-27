48c13369efddacecf2c3369353d218302dc48386
oscarmm <oscarmm@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2004-01-27 05:28:30 +0000

    Bug 849862: Heat Calculater no longer cropped

--- trunk/megamek/src/megamek/client/ui/AWT/widget/PMSimpleLabel.java
+++ trunk/megamek/src/megamek/client/ui/AWT/widget/PMSimpleLabel.java
@@ -51,14 +51,19 @@ public class PMSimpleLabel  implements PMLabel{
 	    color = c;
      }
      
-     public void setString(String s){
-     	string = s;
-     	width = fm.stringWidth(string);
-	    height = fm.getHeight();
-	    descent = fm.getMaxDescent();
-     	
-     }
-     
+    public void setString(String s){
+        string = s;
+        // The width use to just be the stringWidth, but this
+        //  sometimes caused cropping when setString was called.
+        //  The value of 140% was chosen by trial and error, and
+        //  may be incorrect.  In fact, this whole fix is
+        //  basically a kludge, since I don't know why it
+        //  is needed.
+        width = (int)Math.ceil(fm.stringWidth(string) * 1.4);
+        height = fm.getHeight();
+        descent = fm.getMaxDescent();
+    }
+
 
      /*
       * Set the color of the label of the font.

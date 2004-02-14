9c74f2c3eb0d6f568cbe69d4f754eb4bc57fd468
suvarov454 <suvarov454@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2004-02-14 04:12:47 +0000

    + Added static (decode|encode)Transporters to Entity. + Moved a bunch of Entity#set* methods into public access.

--- trunk/megamek/src/megamek/common/Entity.java
+++ trunk/megamek/src/megamek/common/Entity.java
@@ -1,5 +1,5 @@
 /*
- * MegaMek - Copyright (C) 2000-2003 Ben Mazur (bmazur@sev.org)
+ * MegaMek - Copyright (C) 2000,2001,2002,2003,2004 Ben Mazur (bmazur@sev.org)
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by the Free
@@ -19,6 +19,7 @@ import java.util.*;
 import java.awt.*;
 
 import megamek.common.actions.*;
+import megamek.common.util.StringUtil;
 
 /**
  * Entity is a master class for basically anything on the board except
@@ -292,7 +293,7 @@ public abstract class Entity
         return chassis;
     }
 
-    protected void setChassis(String chassis) {
+    public void setChassis(String chassis) {
         this.chassis = chassis;
     }
 
@@ -303,7 +304,7 @@ public abstract class Entity
         return techLevel;
     }
 
-    protected void setTechLevel(int techLevel) {
+    public void setTechLevel(int techLevel) {
         this.techLevel = techLevel;
     }
 
@@ -315,7 +316,7 @@ public abstract class Entity
         return year;
     }
 
-    protected void setYear(int year) {
+    public void setYear(int year) {
         this.year = year;
     }
 
@@ -336,7 +337,7 @@ public abstract class Entity
          }
     }
 
-    protected void setWeight(float weight) {
+    public void setWeight(float weight) {
         this.weight = weight;
     }
 
@@ -344,7 +345,7 @@ public abstract class Entity
         return omni;
     }
 
-    protected void setOmni(boolean omni) {
+    public void setOmni(boolean omni) {
         this.omni = omni;
     }
 
@@ -2799,6 +2800,88 @@ public abstract class Entity
      * The maximum elevation change the entity can cross
      */
     public abstract int getMaxElevationChange();
+
+    /**
+     * Get a list of the class names of the <code>Transporter</code>s
+     * for the given <code>Entity</code>.
+     * <p/>
+     * This method should <strong>only</strong> be used when serializing
+     * the <code>Entity</code>.
+     *
+     * @param   entity - the <code>Entity</code> being serialized.
+     * @return  a <code>String</code> listing the <code>Transporter</code>
+     *          class names of the <code>Entity</code>.  This value will
+     *          be empty ("") if the entity has no <code>Transporter</code>s.
+     */
+    public static String encodeTransporters( Entity entity ) {
+        StringBuffer buffer = new StringBuffer();
+        Enumeration iter = entity.transports.elements();
+        boolean isFirst = true;
+        while ( iter.hasMoreElements() ) {
+
+            // Every entry after the first gets a leading comma.
+            if ( isFirst ) {
+                isFirst = false;
+            } else {
+                buffer.append( ',' );
+            }
+
+            // Add the next Transporter's class name.
+            Transporter transporter = (Transporter) iter.nextElement();
+            buffer.append( transporter.getClass().getName() );
+
+            // If this is a TroopSpace transporter, get it's capacity.
+            if ( transporter instanceof TroopSpace ) {
+                buffer.append( "=" )
+                    .append( ((TroopSpace)transporter).totalSpace );
+            }
+        }
+        return buffer.toString();
+    }
+
+    /**
+     * Set the <code>Transporter</code>s for the given <code>Entity</code>.
+     * <p/>
+     * This method should <strong>only</strong> be used when deserializing
+     * the <code>Entity</code>.
+     *
+     * @param   entity - the <code>Entity</code> being deserialized.
+     * @param   transporters - the <code>String</code> listing the class
+     *          names of the <code>Transporter</code>s to be set for the
+     *          <code>Entity</code>, separated by commas.  This value can
+     *          be <code>null</code> or empty ("").
+     * @throws  <code>IllegalStateException</code> if any error occurs.
+     */
+    public static void decodeTransporters( Entity entity,
+                                           String transporters )
+        throws IllegalStateException {
+
+        // Split the string on the commas, and add transporters to the Entity.
+        Enumeration iter = StringUtil.splitString( transporters,
+                                                   "," ).elements();
+        while ( iter.hasMoreElements() ) {
+            try {
+                String name = (String) iter.nextElement();
+                Class transporter = Class.forName( name );
+                Object object = null;
+                if ( TroopSpace.class.getName().equals( name ) ) {
+                    // Get the tonnage of space.
+                    int tonnage = Integer.parseInt
+                        ( name.substring(name.lastIndexOf("=")) );
+                    object = new TroopSpace( tonnage );
+                } else {
+                    object = transporter.newInstance();
+                }
+                entity.addTransporter( (Transporter) object );
+            }
+            catch ( Exception err ) {
+                err.printStackTrace();
+                throw new IllegalStateException( err.getMessage() );
+            }
+
+        } // Handle the next transporter
+            
+    }
 
     /**
      * Add a transportation component to this Entity. Please note, this

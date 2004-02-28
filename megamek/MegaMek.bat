5dd3641c68da89364d826c9a70ea5ab6dcf29a8b
oscarmm <oscarmm@users.sourceforge.net>
2004-02-28 03:50:21 +0000

    Megamek.bat improved, defaults to Sun JVM

--- megamek/MegaMek.bat
+++ megamek/MegaMek.bat
@@ -1,21 +1,41 @@
 @echo off
 echo Starting MegaMek...
-echo If you get a message like, "Cannot find the file 'wjview'", please
-echo open the file named "readme.txt" and refer to the sections named
-echo "RUNNING MEGAMEK" and "INSTALLING OR UPDATING YOUR JAVA RUNTIME".
+echo.
 
-if "%JAVA_HOME%"=="" goto MicrosoftJRE
 rem Uncomment the next two lines to profile memory use.
 rem java -Xrunprof:depth=12 -jar MegaMek.jar
 rem goto done
+
 rem Uncomment the next two lines to run the packet tool.
 rem java -classpath MegaMek.jar megamek.test.PacketTool
 rem goto done
-java -jar MegaMek.jar
+
+java
+if not errorlevel 2 goto sunjvm
+echo.
+echo Failed to find Sun JVM.  Trying Microsoft JVM instead...
+jview
+if not errorlevel 2 goto msjvm
+goto failure
+
+:msjvm
+start wjview /vst /cp collections.jar;TinyXML.jar;MegaMek.jar megamek/MegaMek
 goto done
 
-:MicrosoftJRE
-start wjview /vst /cp collections.jar;TinyXML.jar;MegaMek.jar megamek/MegaMek
+:sunjvm
+start javaw -jar MegaMek.jar
+goto done
+
+:failure
+echo.
+echo.
+echo -----------------------------------------------------------------
+echo Failed to find a Java Virtual Machine.  Please press any key and
+echo read the sections titled:
+echo "RUNNING MEGAMEK" and "INSTALLING OR UPDATING YOUR JAVA RUNTIME".
+pause
+start notepad readme.txt
 
 :done
+cls
 @echo on

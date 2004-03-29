c74b2486e2a172ea3a46f60c67b3c99396dcd07c
suvarov454 <suvarov454@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2004-03-29 12:18:28 +0000

    Development snapshot 2004-03-29

--- trunk/megamek/src/megamek/MegaMek.java
+++ trunk/megamek/src/megamek/MegaMek.java
@@ -29,7 +29,7 @@ import megamek.server.*;
 public class MegaMek
     implements ActionListener
 {
-    public static String    VERSION = "0.29.30-dev";
+    public static String    VERSION = "0.29.30";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
 
     public Frame            frame;

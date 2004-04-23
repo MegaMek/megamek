c46697993f0e2cfb05f879be77b27421cf989e68
endersdouble <endersdouble@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2004-04-23 03:31:30 +0000

    Spelling fix.

--- megamek/src/megamek/client/bot/TestBot.java
+++ megamek/src/megamek/client/bot/TestBot.java
@@ -1,11 +1,11 @@
 /*
  * MegaMek - Copyright (C) 2000-2003 Ben Mazur (bmazur@sev.org)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
@@ -116,7 +116,7 @@ public class TestBot extends BotClient {
                 if (!game.getTurn().isValidEntity(entity, game)) {
                     continue;
                 }
-                System.out.println("Contimplating movement of " + entity.getShortName() + " " + entity.getId());
+                System.out.println("Contemplating movement of " + entity.getShortName() + " " + entity.getId());
 
                 MoveOption[] result = calculateMove(entity);
 

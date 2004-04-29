af24e6eb00ecbcebd900188e860cd570e685a747
hawkprime <hawkprime@users.sourceforge.net>
2004-04-29 14:26:50 +0000

    changed botgui to just use its own frame

--- megamek/src/megamek/client/bot/ui/AWT/BotGUI.java
+++ megamek/src/megamek/client/bot/ui/AWT/BotGUI.java
@@ -1,6 +1,10 @@
 package megamek.client.bot;
 
-import megamek.MegaMek;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.io.File;
+
+import megamek.client.CommonHelpDialog;
 import megamek.client.ConfirmDialog;
 import megamek.client.GameEvent;
 import megamek.client.GameListener;
@@ -9,15 +13,14 @@ import megamek.common.Settings;
 
 public class BotGUI implements GameListener {
 
-    private MegaMek megamek;
     private BotClient bot;
+    private Frame frame = new Frame();
     private static boolean WarningShown;
-    
-    public BotGUI(MegaMek megamek, BotClient bot) {
-        this.megamek = megamek;
+
+    public BotGUI(BotClient bot) {
         this.bot = bot;
     }
-    
+
     public void gameBoardChanged(GameEvent e) {
         ;
     }
@@ -39,32 +42,38 @@ public class BotGUI implements GameListener {
     }
 
     public void gamePhaseChange(GameEvent e) {
-		if (bot.game.getPhase() == Game.PHASE_LOUNGE || bot.game.getPhase() == Game.PHASE_STARTING_SCENARIO) {
-		    notifyOfBot();
-		}
+        if (bot.game.getPhase() == Game.PHASE_LOUNGE || bot.game.getPhase() == Game.PHASE_STARTING_SCENARIO) {
+            notifyOfBot();
+        }
     }
-    
-	public void notifyOfBot() {
-		if (Settings.nagForBotReadme && !WarningShown) {
-		    WarningShown = true;
-			String title = "Please read the ai-readme.txt";
-			String body =
-				"The bot does not work with all units or game options.\n"
-					+ "Please read the ai-readme.txt file before using the bot.\n"
-					+ " \nWould you like to read the AI documentation now?\n";
-			ConfirmDialog confirm = new ConfirmDialog(megamek.frame, title, body, true);
-			confirm.show();
 
-			if (!confirm.getShowAgain()) {
-				Settings.nagForBotReadme = false;
-				Settings.save();
-			}
+    public void notifyOfBot() {
+        if (Settings.nagForBotReadme && !WarningShown) {
+            WarningShown = true;
+            String title = "Please read the ai-readme.txt";
+            String body =
+                "The bot does not work with all units or game options.\n"
+                    + "Please read the ai-readme.txt file before using the bot.\n"
+                    + " \nWould you like to read the AI documentation now?\n";
+            Dimension screenSize = frame.getToolkit().getScreenSize();
+            frame.pack();
+            frame.setLocation(
+                screenSize.width / 2 - frame.getSize().width / 2,
+                screenSize.height / 2 - frame.getSize().height / 2);
+			ConfirmDialog confirm = new ConfirmDialog(frame, title, body, true);
+            confirm.show();
 
-			if (confirm.getAnswer()) {
-				megamek.showHelp("ai-readme.txt");
-			}
-		}
-	}
+            if (!confirm.getShowAgain()) {
+                Settings.nagForBotReadme = false;
+                Settings.save();
+            }
+
+            if (confirm.getAnswer()) {
+                File helpfile = new File("ai-readme.txt");
+                new CommonHelpDialog(frame, helpfile).show();
+            }
+        }
+    }
 
     public void gamePlayerChat(GameEvent e) {
         ;
@@ -79,6 +88,10 @@ public class BotGUI implements GameListener {
     }
 
     public void gameTurnChange(GameEvent e) {
+        ;
+    }
+
+    public void gameMapQuery(GameEvent e) {
         ;
     }
 

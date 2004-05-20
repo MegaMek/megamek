2359ec26f40a4c7174f66a4ac62b704288a5280b
bmazur <bmazur@users.sourceforge.net>
2004-05-20 17:44:20 +0000

    release v0.29.35 (2004-05-20, 2pm)

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -29,7 +29,7 @@ import megamek.server.*;
 import megamek.test.*;
 
 public class MegaMek implements ActionListener {
-    public static String VERSION = "0.29.35-dev";
+    public static String VERSION = "0.29.35";
     public static long TIMESTAMP = new File("timestamp").lastModified();
 
     private static final NumberFormat commafy = NumberFormat.getInstance();

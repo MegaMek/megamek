8cb84944af3b1c96654e557e3218f929557ce779
suvarov454 <suvarov454@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2004-07-11 22:10:33 +0000

    Stable release 2004-07-11.

--- branches/rel-0-29-stable/megamek/src/megamek/MegaMek.java
+++ branches/rel-0-29-stable/megamek/src/megamek/MegaMek.java
@@ -27,7 +27,7 @@ import megamek.server.*;
 public class MegaMek
     implements WindowListener, ActionListener
 {
-    public static String    VERSION = "0.29-stable-9 (CVS)";
+    public static String    VERSION = "0.29-stable-9";
     public static long      TIMESTAMP = new File("timestamp").lastModified();
 
     public Frame            frame;

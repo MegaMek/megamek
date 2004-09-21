c45a7dee3d758d19f656a19fbec8aedfff611ebf
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2004-09-21 17:35:23 +0000

    typo fixing

--- megamek/src/megamek/common/GameOptions.java
+++ megamek/src/megamek/common/GameOptions.java
@@ -97,8 +97,8 @@ public class GameOptions extends Options implements Serializable {
         addOption(ruleBreakers, new GameOption("no_clan_physical", "No physical attacks for the clans", "If checked, clan Meks may not make physical attacks.\n\nUnchecked by default.", false));
         addOption(ruleBreakers, new GameOption("no_hover_charge", "No charge attacks for Hover vehicles", "If checked, Hover vehicles may not make rams/charges.\n\nUnchecked by default.", false));
         addOption(ruleBreakers, new GameOption("woods_burn_down", "Woods have a chance to burn down", "If checked, woods will burn down as if cleared on a roll of 11+.\n\nUnchecked by default.", false));
-        addOption(ruleBreakers, new GameOption("vechicles_safe_from_infernos", "Vehicles may not be the target of an Inferno missle attack.", "If checked, Vehicles cannot be the target of an Inferno SRM attack.\n\nUnchecked by default.", false));
-        addOption(ruleBreakers, new GameOption("protos_safe_from_infernos", "Protomechs may not be the target of an Inferno missle attack.", "If checked, Protomechs cannot be the target of an Inferno SRM attack.\n\nUnchecked by default.", false));
+        addOption(ruleBreakers, new GameOption("vehicles_safe_from_infernos", "Vehicles may not be the target of an Inferno missile attack.", "If checked, Vehicles cannot be the target of an Inferno SRM attack.\n\nUnchecked by default.", false));
+        addOption(ruleBreakers, new GameOption("protos_safe_from_infernos", "Protomechs may not be the target of an Inferno missile attack.", "If checked, Protomechs cannot be the target of an Inferno SRM attack.\n\nUnchecked by default.", false));
         addOption(ruleBreakers, new GameOption("check_leg_breaches", "Check for Leg breaches in Level 1 water", "If checked, a Mek standing in Level 1 water will check for hull breaches (which is **against** the rules on page 94 of the BMRr).\n\nUnchecked by default.", false));
     }
 

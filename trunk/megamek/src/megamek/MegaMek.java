af10f85030624ccd0d206fa33162cf814c40dbd4
suvarov454 <suvarov454@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2004-10-11 01:21:42 +0000

    Ready to develop v0.29.53

--- trunk/megamek/src/megamek/MegaMek.java
+++ trunk/megamek/src/megamek/MegaMek.java
@@ -30,7 +30,7 @@ import megamek.server.*;
 import megamek.test.*;
 
 public class MegaMek implements ActionListener {
-    public static String VERSION = "0.29.52";
+    public static String VERSION = "0.29.53-dev";
     public static long TIMESTAMP = new File("timestamp").lastModified();
 
     private static final NumberFormat commafy = NumberFormat.getInstance();

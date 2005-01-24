358ba157c2e4b69eae5dcce976cd3657d1d0d0e5
beerockxs <sebastian@brocks.cc>
2005-01-24 15:14:42 +0000

    + Bug 1089323: Atillery Freezes Game if destroyed/fled

--- megamek/src/megamek/common/WeaponResult.java
+++ megamek/src/megamek/common/WeaponResult.java
@@ -28,4 +28,5 @@ implements Serializable {
     public int[] amsShotDown = new int[0];
     public int amsShotDownTotal = 0;
     public Coords artyAttackerCoords=null; //HACK.  Someone do something much neater, PLEASE.
+    public Entity artyEntity=null; // Just as HACKish, but I don't see a different way.
 }

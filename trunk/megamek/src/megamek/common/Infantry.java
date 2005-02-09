71ad6c1b30d99294589f26501b38f2bbb2d282c7
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2005-02-09 19:09:59 +0000

    + Bug 1112541: PSR reporting when autoejecting in wrong place

--- trunk/megamek/src/megamek/common/Infantry.java
+++ trunk/megamek/src/megamek/common/Infantry.java
@@ -247,8 +247,8 @@ public class Infantry
      * Infantry have no piloting skill (set to 5 for BV purposes)
      */
     public void setCrew(Pilot p) {
-        super.setCrew(p);
-        this.getCrew().setPiloting(5);
+        super.setCrew(new Pilot(p.getName(), p.getGunnery(), 5));
+        this.getCrew().setHits(p.getHits());
     }
 
     /**

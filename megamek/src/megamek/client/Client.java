7f021714f9924d3b25aad71497f346d29863b2bd
beerockxs <sebastian@brocks.cc>
2005-03-07 21:53:28 +0000

    *** empty log message ***

--- megamek/src/megamek/client/Client.java
+++ megamek/src/megamek/client/Client.java
@@ -1,6 +1,7 @@
 /*
- * MegaMek - Copyright (C) 2000,2001,2002,2003,2004 Ben Mazur (bmazur@sev.org)
- *
+ * MegaMek -
+ * Copyright (C) 2000,2001,2002,2003,2004,2005 Ben Mazur (bmazur@sev.org)
+ * 
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by the Free
  *  Software Foundation; either version 2 of the License, or (at your option)

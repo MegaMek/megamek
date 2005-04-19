0afd28e2363c3b9891caa5bdba1cb9b0a2867ff0
mev <mev@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2005-04-19 23:31:36 +0000

    Implemented storing/reading the user defined locale for ResourceBundles in the MegaMek.cfg

--- trunk/megamek/src/megamek/common/options/Messages.java
+++ trunk/megamek/src/megamek/common/options/Messages.java
@@ -18,11 +18,13 @@ import java.text.MessageFormat;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
+import megamek.common.Settings;
+
 class Messages {
     private static final String BUNDLE_NAME = "megamek.common.options.messages";//$NON-NLS-1$
 
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-            .getBundle(BUNDLE_NAME);
+        .getBundle(BUNDLE_NAME, Settings.getLocale());
 
     private Messages() {
     }

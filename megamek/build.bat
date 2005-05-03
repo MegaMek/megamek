68f1422fb502082e7aa974792209f00e159c66d6
beerockxs <sebastian@brocks.cc>
2005-05-03 21:12:35 +0000

    + Patch 1194720, RFE 1050894: Alternate Artillery Scatter Option also updated build.bat to work with java 1.5

--- megamek/build.bat
+++ megamek/build.bat
@@ -1,3 +1,3 @@
 @if not exist classes mkdir classes
-javac -target 1.1 -d classes -O -classpath .;collections.jar;TinyXML.jar;Ostermiller.jar;PngEncoder.jar;TabPanel.jar megamek/*.java
+javac -target 1.1 -source 1.2 -d classes -O -classpath .;collections.jar;TinyXML.jar;Ostermiller.jar;PngEncoder.jar;TabPanel.jar megamek/*.java
 jar cmf manifest.txt MegaMek.jar -C classes .

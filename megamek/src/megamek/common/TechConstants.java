7628a2869f3896d57f4cbdb2540ac26b964803ed
endersdouble <endersdouble@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2005-05-29 02:03:37 +0000

    Patch 1209460:  Level 3 'Mechs, Protected by Option

--- megamek/src/megamek/common/TechConstants.java
+++ megamek/src/megamek/common/TechConstants.java
@@ -31,23 +31,43 @@ public class TechConstants {
     /*
      * These can apply to entities or individual pieces of equipment
      */
-    public static final int         T_IS_LEVEL_1    = 0;
-    public static final int         T_IS_LEVEL_2    = 1;
-    public static final int         T_CLAN_LEVEL_2  = 2;
+    public static final int         T_IS_LEVEL_1        = 0;
+    public static final int         T_IS_LEVEL_2        = 1;
+    public static final int         T_CLAN_LEVEL_2      = 2;
+
+    public static final int         T_IS_LEVEL_2_ALL    = 3;
+    public static final int         T_LEVEL_2_ALL       = 4;
+
+    public static final int         T_IS_LEVEL_3        = 5;
+    public static final int         T_CLAN_LEVEL_3      = 6;
 
     /*
      * These apply to entities only
      */
     //An entity with an Inner Sphere chassis that mounts some Clan equipment
-    public static final int         T_MIXED_BASE_IS_LEVEL_2 = 3;
+    public static final int         T_MIXED_BASE_IS_LEVEL_2 = 7;
     //An entity with a Clan chassis that mounts some Inner Sphere equipment
-    public static final int         T_MIXED_BASE_CLAN_LEVEL_2 = 4;
+    public static final int         T_MIXED_BASE_CLAN_LEVEL_2 = 8;
+
+    public static final int         T_MIXED_ALL         = 9;
+    public static final int         T_ALL               = 10;
     
     // this made public because MekWars accesses it
-    public static final String[]    T_NAMES = {"IS level 1", "IS level 2", 
-        "Clan level 2", "Mixed (IS) level 2", "Mixed (Clan) level 2"};
+    // It must match the index to the constant's value.
+    public static final String[]    T_NAMES = {"IS level 1",
+                                               "IS level 2",
+                                               "Clan level 2",
+                                               "IS level 1 and 2",
+                                               "All level 2",
+                                               "IS level 3",
+                                               "Clan level 3",
+                                               "Mixed (IS) level 2",
+                                               "Mixed (Clan) level 2",
+                                               "Mixed (All)",
+                                               "All"};
 
-    public static final int SIZE = T_NAMES.length;
+    public static final int SIZE_LEVEL_2    = 5;
+    public static final int SIZE            = T_NAMES.length;
     
     public static String getLevelName(int level) {
         if (level >= 0 && level < SIZE) {

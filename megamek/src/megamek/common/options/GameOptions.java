8c977665d49fe108bb935be36439e82344600b89
fastsammy <fastsammy@users.sourceforge.net>
2005-06-06 17:44:50 +0000

    Added Acid-head warheads.  Also put in beginnings of more efficient armor type detection (non-exhaustive-search).

--- megamek/src/megamek/common/options/GameOptions.java
+++ megamek/src/megamek/common/options/GameOptions.java
@@ -75,6 +75,7 @@ public class GameOptions extends AbstractOptions implements Serializable {
 
         OptionGroup level3 = addGroup("level3"); //$NON-NLS-1$
         addOption(level3,"allow_level_3_units", false);
+        addOption(level3,"allow_level_3_ammo", false);
         addOption(level3,"double_blind", false); //$NON-NLS-1$
         addOption(level3,"team_vision", true); //$NON-NLS-1$
         addOption(level3,"floating_crits", false); //$NON-NLS-1$

8e2daffa8da233de4a3c2cd7ec869b24c090a4e1
fastsammy <fastsammy@users.sourceforge.net>
2005-06-07 01:33:27 +0000

    Fixed compile error in Server.java

--- megamek/src/megamek/server/Server.java
+++ megamek/src/megamek/server/Server.java
@@ -9383,7 +9383,7 @@ implements Runnable, ConnectionHandler {
                 && (te.getArmor(hit) > 0)
                 && ((te.getArmorType() == EquipmentType.T_ARMOR_FERRO_FIBROUS)
                     || (te.getArmorType() == EquipmentType.T_ARMOR_LIGHT_FERRO)
-                    || (te.getArmorType() == EquipmentType.T_ARMOR_HEAVY_FERRO))
+                    || (te.getArmorType() == EquipmentType.T_ARMOR_HEAVY_FERRO)))
             isFerroFibrousTarget = true;
         boolean wasDamageIS = false;
         IHex te_hex = null;

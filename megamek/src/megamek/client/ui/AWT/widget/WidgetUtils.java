e5d4491fb4fe0653264496c83c10715291699718
schoelling <schoelling@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2005-06-20 11:55:04 +0000

    Should have changed copyright message...

--- megamek/src/megamek/client/ui/AWT/widget/WidgetUtils.java
+++ megamek/src/megamek/client/ui/AWT/widget/WidgetUtils.java
@@ -1,8 +1,15 @@
-/*
- * Created on 20.06.2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+/**
+ * MegaMek - Copyright (C) 2003 Ben Mazur (bmazur@sev.org)
+ * 
+ *  This program is free software; you can redistribute it and/or modify it 
+ *  under the terms of the GNU General Public License as published by the Free 
+ *  Software Foundation; either version 2 of the License, or (at your option) 
+ *  any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *  for more details.
  */
 package megamek.client.util.widget;
 

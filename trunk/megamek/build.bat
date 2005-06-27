4d5de2a1f97f44244039e011ddead79079f3ca95
oscarmm <oscarmm@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2005-06-27 16:35:15 +0000

    Fixed build.bat

--- trunk/megamek/build.bat
+++ trunk/megamek/build.bat
@@ -1,3 +1,3 @@
 @if not exist classes mkdir classes
-javac -target 1.1 -source 1.2 -d classes -O -classpath .;collections.jar;TinyXML.jar;Ostermiller.jar;PngEncoder.jar;TabPanel.jar megamek/*.java
-jar cmf manifest.txt MegaMek.jar -C classes .
+javac -target 1.1 -source 1.2 -d classes -O -classpath src;lib/collections.jar;lib/TinyXML.jar;lib/Ostermiller.jar;lib/PngEncoder.jar;lib/TabPanel.jar src/megamek/*.java
+jar cmf lib/manifest.txt MegaMek.jar -C classes . -C l10n .

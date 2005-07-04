7fe905a2e1f07bc83041f8b340e37fd16c4a44ee
beerockxs <sebastian@brocks.cc>
2005-07-04 18:46:47 +0000

    + Bug 1174455: animated movement not working.

--- megamek/src/megamek/server/Server.java
+++ megamek/src/megamek/server/Server.java
@@ -3492,9 +3492,11 @@ implements Runnable, ConnectionHandler {
             }
 
             // What the *heck* is this???
+            // it's a monster hack, and wrong too :)
+            // used for movement animation stuff.
+            // broken for maps larget than 255 hexes across
             movePath.addElement
-                (new Integer(curPos.hashCode() ^ (curFacing << 16)));
-
+                (new Integer(((curPos.x & 0xFF) << 8) | (curPos.y& 0xFF) | (curFacing << 16)));
             // update lastPos, prevStep, prevFacing & prevHex
             lastPos = new Coords(curPos);
             prevStep = step;

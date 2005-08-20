08dec5b887eecaea8559244eda8d5731ef8f865a
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2005-08-20 15:21:47 +0000

    + Fixed build.bat

--- trunk/megamek/build.bat
+++ trunk/megamek/build.bat
@@ -1,4 +1,5 @@
 @if not exist classes mkdir classes
-javac -target 1.5 -source 1.5 -d classes -O -classpath src;lib/collections.jar;lib/TinyXML.jar;lib/Ostermiller.jar;lib/PngEncoder.jar;lib/TabPanel.jar src/megamek/*.java
+javac -target 1.4 -source 1.4 -d classes -O -classpath src;lib/collections.jar;lib/TinyXML.jar;lib/Ostermiller.jar;lib/PngEncoder.jar;lib/TabPanel.jar src/megamek/*.java
 jar cmf lib/manifest.txt MegaMek.jar -C classes .
 jar uf MegaMek.jar -C l10n .
+jar uf MegaMek.jar -C src/megamek/MegaMek.properties .

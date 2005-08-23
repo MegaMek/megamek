954508d62bc2af35da20c4ba01120838585a36fb
beerockxs <sebastian@brocks.cc>
2005-08-23 13:47:11 +0000

    remove unnecessary cast

--- megamek/src/megamek/common/loaders/HmpFile.java
+++ megamek/src/megamek/common/loaders/HmpFile.java
@@ -512,7 +512,7 @@ public class HmpFile
     throws EntityLoadingException {
       // Use pass-by-value in case we need the original criticals
       //  later (getMtf for example).
-      long[] crits = (long[])criticals.clone();
+      long[] crits = criticals.clone();
 
       for (int i = 0; i < mech.getNumberOfCriticals(location); i++) {
      if (mech.getCritical(location, i) == null) {

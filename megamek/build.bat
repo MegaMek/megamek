66d3b831740d9181e396071e24a29d9c497eb972
mev <mev@users.sourceforge.net>
2005-08-25 18:23:28 +0000

    fixed build.bat to include MegaMek.properties into MegaMek.jar

--- megamek/build.bat
+++ megamek/build.bat
@@ -2,3 +2,4 @@
 javac -target 1.4 -source 1.4 -d classes -O -classpath src;lib/collections.jar;lib/TinyXML.jar;lib/Ostermiller.jar;lib/PngEncoder.jar;lib/TabPanel.jar src/megamek/*.java
 jar cmf lib/manifest.txt MegaMek.jar -C classes .
 jar uf MegaMek.jar -C l10n .
+jar uf MegaMek.jar -C src megamek\MegaMek.properties

75aa55b4df87e4e237fd9388593a390e6a1e3e7e
oscarmm <oscarmm@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2005-09-01 17:38:09 +0000

    Added kludge to fix build.bat.  Not sure what is causing the problem though

--- trunk/megamek/build.bat
+++ trunk/megamek/build.bat
@@ -1,5 +1,8 @@
 @if not exist classes mkdir classes
-javac -target 1.4 -source 1.4 -d classes -O -classpath src;lib/collections.jar;lib/TinyXML.jar;lib/Ostermiller.jar;lib/PngEncoder.jar;lib/TabPanel.jar src/megamek/*.java
+rem The last phrase of the javac command below is a kludge to get around an
+rem odd problem where the AWT subdirectory of the source tree was not being
+rem compiled at all.  I don't know what is actually going on.
+javac -target 1.4 -source 1.4 -d classes -O -classpath src;lib/collections.jar;lib/TinyXML.jar;lib/Ostermiller.jar;lib/PngEncoder.jar;lib/TabPanel.jar src/megamek/*.java src/megamek/client/ui/AWT/*.java
 jar cmf lib/manifest.txt MegaMek.jar -C classes .
 jar uf MegaMek.jar -C l10n .
 jar uf MegaMek.jar -C src megamek\MegaMek.properties

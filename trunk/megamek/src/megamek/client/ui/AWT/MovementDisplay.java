846bf30bf202a8f8bb6459b04281cd9223dffeca
coelocanth <coelocanth@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2005-09-22 22:39:06 +0000

    + Bug 1293793: elevation bug: VTOL wont unload

--- trunk/megamek/src/megamek/client/ui/AWT/MovementDisplay.java
+++ trunk/megamek/src/megamek/client/ui/AWT/MovementDisplay.java
@@ -1162,7 +1162,7 @@ public class MovementDisplay
         if ( !legalGear
                 || loadedUnits.size() == 0 
                 || cen == Entity.NONE
-                || (getCeElevation() > 0
+                || (cmd.getFinalElevation() > 0
                     && !vtolLoaded)) {
             setUnloadEnabled( false );
         } else {
@@ -1879,16 +1879,4 @@ public class MovementDisplay
         clientgui.getBoardView().removeBoardViewListener(this);
     }
 
-    public int getCeElevation() {
-        Entity ce = ce();
-        int retVal = ce.getElevation();
-        for (Enumeration e = cmd.getSteps(); e.hasMoreElements(); ) {
-            MoveStep ms = (MoveStep)e.nextElement();
-            if (ms.getType() == MovePath.STEP_UP)
-                retVal++;
-            else if (ms.getType() == MovePath.STEP_DOWN)
-                retVal--;
-        }
-        return retVal;
-    }
 }

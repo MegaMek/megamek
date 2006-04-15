d09cad69ea7ef314f7296f7e7f38c4a8d99d784e
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2006-04-15 13:36:10 +0000

    + Make tab names in the chatlounge clearer.

--- trunk/megamek/src/megamek/client/ui/swing/ChatLounge.java
+++ trunk/megamek/src/megamek/client/ui/swing/ChatLounge.java
@@ -1,5 +1,6 @@
 /*
- * MegaMek - Copyright (C) 2000,2001,2002,2003,2004 Ben Mazur (bmazur@sev.org)
+ * MegaMek - 
+ *  Copyright (C) 2000,2001,2002,2003,2004,2005,2006 Ben Mazur (bmazur@sev.org)
  * 
  *  This program is free software; you can redistribute it and/or modify it 
  *  under the terms of the GNU General Public License as published by the Free 
@@ -563,7 +564,7 @@ public class ChatLounge
         // Should we display the panels in tabs?
         if (GUIPreferences.getInstance().getChatLoungeTabs()) {
             this.panTabs.add ("Select Units", panMain); //$NON-NLS-1$
-            this.panTabs.add ("Configure", panTop); //$NON-NLS-1$
+            this.panTabs.add ("Configure Game", panTop); //$NON-NLS-1$
         } else {
             c.weighty = 0.0;
             gridbag.setConstraints(panTop, c);

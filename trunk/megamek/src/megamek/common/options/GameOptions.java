63468b5f2459fe3f6dc9221399beac8b5d2c39c4
fastsammy <fastsammy@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2006-08-15 02:49:29 +0000

    + Added optional turn limit.

--- trunk/megamek/src/megamek/common/options/GameOptions.java
+++ trunk/megamek/src/megamek/common/options/GameOptions.java
@@ -71,6 +71,8 @@ public class GameOptions extends AbstractOptions implements Serializable {
         addOption(victory,"bv_destroyed_percent", 100); //$NON-NLS-1$
         addOption(victory,"use_bv_ratio", false); //$NON-NLS-1$
         addOption(victory,"bv_ratio_percent", 300); //$NON-NLS-1$
+        addOption(victory,"use_game_turn_limit", false); //$NON-NLS-1$
+        addOption(victory,"game_turn_limit", 10); //$NON-NLS-1$
 
         OptionGroup level2 = addGroup("level2"); //$NON-NLS-1$
         addOption(level2,"flamer_heat", true); //$NON-NLS-1$

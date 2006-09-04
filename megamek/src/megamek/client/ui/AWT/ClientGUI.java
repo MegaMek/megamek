7cf024e2108c18c8e39c2c5eb352d5230af3eb7d
coelocanth <coelocanth@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2006-09-04 12:22:21 +0000

    + Bug 1478186: Battlefield Screen could be placed offscreen

--- megamek/src/megamek/client/ui/AWT/ClientGUI.java
+++ megamek/src/megamek/client/ui/AWT/ClientGUI.java
@@ -52,6 +52,9 @@ import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.FileDialog;
 import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
@@ -60,6 +63,7 @@ import java.awt.MenuItem;
 import java.awt.Panel;
 import java.awt.Point;
 import java.awt.PopupMenu;
+import java.awt.Rectangle;
 import java.awt.Scrollbar;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
@@ -252,9 +256,35 @@ public class ClientGUI
         this.frame = new Frame(Messages.getString("ClientGUI.title")); //$NON-NLS-1$
         menuBar.setGame(client.game);
         frame.setMenuBar(menuBar);
+        Rectangle virtualBounds = new Rectangle();
+        GraphicsEnvironment ge = GraphicsEnvironment.
+                getLocalGraphicsEnvironment();
+        GraphicsDevice[] gs =
+                ge.getScreenDevices();
+        for (int j = 0; j < gs.length; j++) { 
+            GraphicsDevice gd = gs[j];
+            GraphicsConfiguration[] gc =
+                gd.getConfigurations();
+            for (int i=0; i < gc.length; i++) {
+                virtualBounds =
+                    virtualBounds.union(gc[i].getBounds());
+            }
+        } 
         if (GUIPreferences.getInstance().getWindowSizeHeight() != 0) {
-            frame.setLocation(GUIPreferences.getInstance().getWindowPosX(), GUIPreferences.getInstance().getWindowPosY());
-            frame.setSize(GUIPreferences.getInstance().getWindowSizeWidth(), GUIPreferences.getInstance().getWindowSizeHeight());
+            int x = GUIPreferences.getInstance().getWindowPosX();
+            int y = GUIPreferences.getInstance().getWindowPosY();
+            int w = GUIPreferences.getInstance().getWindowSizeWidth();
+            int h = GUIPreferences.getInstance().getWindowSizeHeight();
+            if (x < virtualBounds.getMinX() || x + w >virtualBounds.getMaxX())
+                x = 0;
+            if (y < virtualBounds.getMinY() || y + h >virtualBounds.getMaxY())
+                y = 0;
+            if(w > virtualBounds.getWidth())
+                w = (int)virtualBounds.getWidth();
+            if(h > virtualBounds.getHeight())
+                h = (int)virtualBounds.getHeight();
+            frame.setLocation(x, y);
+            frame.setSize(w, h);
         } else {
             frame.setSize(800, 600);
         }

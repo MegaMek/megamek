dd877cc769ecb6187f5f7c9dd884b6f41cc4b5f4
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2006-09-06 18:54:35 +0000

    add no-localize marker

--- megamek/src/megamek/common/options/GameOptions.java
+++ megamek/src/megamek/common/options/GameOptions.java
@@ -167,7 +167,7 @@ public class GameOptions extends AbstractOptions implements Serializable {
         addOption(ruleBreakers,"armed_mechwarriors", false); //$NON-NLS-1$
         addOption(ruleBreakers,"auto_abandon_unit", false); //$NON-NLS-1$
         addOption(ruleBreakers,"no_ignite_clear", false); //$NON-NLS-1$
-        addOption(ruleBreakers,"falls_end_movement", false); // What this patch is all about
+        addOption(ruleBreakers,"falls_end_movement", false); //$NON-NLS-1$
     }
 
     public Vector loadOptions() {

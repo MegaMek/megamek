7b6646ca6af4159e547e28198cbdac0cd38fbee6
coelocanth <coelocanth@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2006-10-12 01:35:23 +0000

    + Bug: flak bonus not applied for vtol at elevation 1 over height 2 terrain

--- branches/rel-0-32-0/src/megamek/common/actions/WeaponAttackAction.java
+++ branches/rel-0-32-0/src/megamek/common/actions/WeaponAttackAction.java
@@ -681,7 +681,10 @@ public class WeaponAttackAction extends AbstractAttackAction {
                 && (atype.getAmmoType() == AmmoType.T_AC_LBX
                 || atype.getAmmoType() == AmmoType.T_AC_LBX_THB)
                 && atype.getMunitionType() == AmmoType.M_CLUSTER)
-                && te.getElevation() > game.getBoard().getHex(te.getPosition()).ceiling()) {
+                                && te.getElevation() > 0
+                && te.getElevation() > game.getBoard().getHex(te.getPosition()).terrainLevel(Terrains.BLDG_ELEV)
+                && te.getElevation() != game.getBoard().getHex(te.getPosition()).terrainLevel(Terrains.BRIDGE_ELEV)) {
+
             toHit.addModifier(-3, "flak to-hit modifier");
         } else if (usesAmmo && atype.getToHitModifier() != 0) {
             toHit.addModifier(atype.getToHitModifier(), "ammunition to-hit modifier");

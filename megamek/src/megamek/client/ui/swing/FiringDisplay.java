2d8453a35cf3fef8a66c343f3a0b90ae0d295b42
coelocanth <coelocanth@users.sourceforge.net>
2006-10-24 14:19:05 +0000

    + Change TC aimed shot check to check for F_PULSE flag. moved NarcPod.java to correct place

--- megamek/src/megamek/client/ui/swing/FiringDisplay.java
+++ megamek/src/megamek/client/ui/swing/FiringDisplay.java
@@ -1653,7 +1653,8 @@ public class FiringDisplay
                     }
                     break;
                 case (IAimingModes.AIM_MODE_TARG_COMP):
-                    if (!wtype.hasFlag(WeaponType.F_DIRECT_FIRE)) {
+                    if (!wtype.hasFlag(WeaponType.F_DIRECT_FIRE)
+                            ||wtype.hasFlag(WeaponType.F_PULSE)) {
                         return false;
                     }
                     if(weapon.howManyShots() > 1) {

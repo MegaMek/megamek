8f2549da1b2ada87350ab7937feabab16c2b2327
coelocanth <coelocanth@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2006-10-31 12:11:36 +0000

    0.32.1 release

--- branches/rel-0-32-0/src/megamek/MegaMek.java
+++ branches/rel-0-32-0/src/megamek/MegaMek.java
@@ -36,7 +36,7 @@ import megamek.server.DedicatedServer;
  */
 public class MegaMek {
 
-    public static String VERSION = "0.32.1"; //$NON-NLS-1$
+    public static String VERSION = "0.32.2-dev"; //$NON-NLS-1$
 
     public static long TIMESTAMP = new File(PreferenceManager.getClientPreferences().getLogDirectory() + File.separator + "timestamp").lastModified(); //$NON-NLS-1$
 

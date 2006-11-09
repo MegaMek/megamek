f6934dcd83b80301da765da8faf5a51d55bb26cc
coelocanth <coelocanth@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2006-11-09 13:34:26 +0000

    + Bug 1593129: Clan BA blocking CT weapons

--- megamek/src/megamek/common/ClampMountTank.java
+++ megamek/src/megamek/common/ClampMountTank.java
@@ -30,12 +30,6 @@ package megamek.common;
 
     // Private attributes, constants and helper functions.
 
-    /**
-     * The set of front locations blocked by loaded troopers.
-     */
-    private static final int[] BLOCKED_LOCATIONS = { Tank.LOC_RIGHT,
-                                                     Tank.LOC_LEFT,
-                                                     Tank.LOC_REAR };
 
     /**
      * The set of front locations that load troopers externally.
@@ -45,20 +39,6 @@ package megamek.common;
                                                       Tank.LOC_REAR };
 
     // Protected constructors and methods.
-
-    /**
-     * Get the locations blocked when a squad is loaded.
-     * <p/>
-     * Sub-classes are encouraged to override this method.
-     *
-     * @param   isRear - a <code>boolean</code> value stating if the given
-     *          location is rear facing; if <code>false</code>, the location
-     *          is front facing.
-     * @return  an array of <code>int</code> listing the blocked locations.
-     */
-    protected int[] getBlockedLocs( boolean isRear ) {
-        return ClampMountTank.BLOCKED_LOCATIONS;
-    }
 
     /**
      * Get the exterior locations that a loaded squad covers.

10975a6235a3ef667e8a208294507193776b2dfa
coelocanth <coelocanth@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2006-11-09 13:34:26 +0000

    + Bug 1593129: Clan BA blocking CT weapons

--- trunk/megamek/src/megamek/common/BattleArmorHandlesTank.java
+++ trunk/megamek/src/megamek/common/BattleArmorHandlesTank.java
@@ -5,13 +5,6 @@ public class BattleArmorHandlesTank extends BattleArmorHandles {
     // Private attributes, constants and helper functions.
 
     /**
-     * The set of front locations blocked by loaded troopers.
-     */
-    private static final int[] BLOCKED_LOCATIONS = { Tank.LOC_RIGHT,
-                                                     Tank.LOC_LEFT,
-                                                     Tank.LOC_REAR };
-
-    /**
      * The set of front locations that load troopers externally.
      */
     private static final int[] EXTERIOR_LOCATIONS = { Tank.LOC_RIGHT,
@@ -19,20 +12,6 @@ public class BattleArmorHandlesTank extends BattleArmorHandles {
                                                       Tank.LOC_REAR };
 
     // Protected constructors and methods.
-
-    /**
-     * Get the locations blocked when a squad is loaded.
-     * <p/>
-     * Sub-classes are encouraged to override this method.
-     *
-     * @param   isRear - a <code>boolean</code> value stating if the given
-     *          location is rear facing; if <code>false</code>, the location
-     *          is front facing.
-     * @return  an array of <code>int</code> listing the blocked locations.
-     */
-    protected int[] getBlockedLocs( boolean isRear ) {
-        return BattleArmorHandlesTank.BLOCKED_LOCATIONS;
-    }
 
     /**
      * Get the exterior locations that a loaded squad covers.
@@ -48,4 +27,51 @@ public class BattleArmorHandlesTank extends BattleArmorHandles {
         return BattleArmorHandlesTank.EXTERIOR_LOCATIONS;
     }
 
+    /**
+     * Determine if transported units prevent a weapon in the given location
+     * from firing.
+     * <p>
+     * Sub-classes should override the <code>getBlockedLocs</code> method.
+     *
+     * @param   loc - the <code>int</code> location attempting to fire.
+     * @param   isRear - a <code>boolean</code> value stating if the given
+     *          location is rear facing; if <code>false</code>, the location
+     *          is front facing.
+     * @return  <code>true</code> if a transported unit is in the way, 
+     *          <code>false</code> if the weapon can fire.
+     *
+     * @see     megamek.common.BattleArmorHandles#getBlockedLocs(boolean)
+     */
+    public final boolean isWeaponBlockedAt( int loc, boolean isRear ) {
+        // Assume that the weapon is not blocked.
+        boolean result = false;
+
+        // The weapon can only be blocked if we are carrying troopers.
+        if ( null != this.troopers ) {
+
+            // Is the relevant trooper alive?
+            int tloc = BattleArmor.LOC_SQUAD;
+            int tloc2 = BattleArmor.LOC_SQUAD;
+            switch(loc) {
+            case Tank.LOC_REAR:
+                tloc = BattleArmor.LOC_CLAN_5;
+                tloc2 = BattleArmor.LOC_CLAN_6;
+                break;
+            case Tank.LOC_LEFT:
+                tloc = BattleArmor.LOC_CLAN_3;
+                tloc2 = BattleArmor.LOC_CLAN_4;
+                break;
+            case Tank.LOC_RIGHT:
+                tloc = BattleArmor.LOC_CLAN_1;
+                tloc2 = BattleArmor.LOC_CLAN_2;
+                break;
+            }
+            if((troopers.locations() > tloc && troopers.getInternal(tloc) > 0) ||
+                    (troopers.locations() > tloc2 && troopers.getInternal(tloc2) > 0))
+                result = true;
+        } // End carrying-troopers
+
+        // Return our result.
+        return result;
+    }
 }

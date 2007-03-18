884d35fd13b34cfa39331ac05ac8d45bd1cda0c8
torren <torren@users.sourceforge.net>
2007-03-18 18:06:12 +0000

    + Update to BV 2.0 rules Rounding Jump MP/2 in Speed Factor Formula

--- megamek/src/megamek/common/Protomech.java
+++ megamek/src/megamek/common/Protomech.java
@@ -877,7 +877,7 @@ public class Protomech extends Entity implements Serializable {
         weaponBV += oEquipmentBV;
 
         // adjust further for speed factor
-        double speedFactor = Math.pow(1+(((double)getOriginalRunMP()+((double)jumpMP/2)-5)/10), 1.2);
+        double speedFactor = Math.pow(1+(((double)getOriginalRunMP()+(Math.round((double)jumpMP/2))-5)/10), 1.2);
         speedFactor = Math.round(speedFactor * 100) / 100.0;
         
         obv = weaponBV * speedFactor;

358956c30910d5cf48cc3c6340d5fbb76eb010f3
beerockxs <sebastian@brocks.cc>
2007-11-09 14:32:41 +0000

    ready to develop 0.33.12

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -47,7 +47,7 @@ import megamek.server.DedicatedServer;
  */
 public class MegaMek {
 
-    public static String VERSION = "0.33.11"; //$NON-NLS-1$
+    public static String VERSION = "0.33.12-dev"; //$NON-NLS-1$
 
     public static long TIMESTAMP = new File(PreferenceManager.getClientPreferences().getLogDirectory() + File.separator + "timestamp").lastModified(); //$NON-NLS-1$
 

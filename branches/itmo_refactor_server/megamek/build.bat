a20ce24d63ab027a4a293cbbdc8e7069e9d3af6a
itmo <itmo@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2007-12-08 04:26:51 +0000

    i will branch again from stable

--- branches/itmo_refactor_server/megamek/build.bat
+++
@@ -1,5 +0,0 @@
-@if not exist classes mkdir classes
-javac -target 1.5 -source 1.5 -d classes -O -classpath src;lib/TinyXML.jar;lib/Ostermiller.jar;lib/PngEncoder.jar;lib/TabPanel.jar src/megamek/*.java src/megamek/client/ui/AWT/*.java
-jar cmf lib/manifest.txt MegaMek.jar -C classes .
-jar uf MegaMek.jar -C l10n .
-jar uf MegaMek.jar -C src megamek\MegaMek.properties

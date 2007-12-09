4af1f33f9720f690a831dc6bf884e1d3a5b4602b
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2007-12-09 01:02:03 +0000

    d'oh, forgot to add files

---
+++ branches/refactor_weapons/megamek/src/megamek/common/weapons/EnergyWeaponHandler.java
@@ -0,0 +1,66 @@
+/**
+ * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
+ * 
+ *  This program is free software; you can redistribute it and/or modify it 
+ *  under the terms of the GNU General Public License as published by the Free 
+ *  Software Foundation; either version 2 of the License, or (at your option) 
+ *  any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *  for more details.
+ */
+package megamek.common.weapons;
+
+import megamek.common.BattleArmor;
+import megamek.common.IGame;
+import megamek.common.Infantry;
+import megamek.common.ToHitData;
+import megamek.common.WeaponType;
+import megamek.common.actions.WeaponAttackAction;
+import megamek.common.weapons.WeaponHandler;
+import megamek.server.Server;
+
+public class EnergyWeaponHandler extends WeaponHandler {
+    /**
+     * @param toHit
+     * @param waa
+     * @param g
+     */
+    public EnergyWeaponHandler(ToHitData toHit, WeaponAttackAction waa, IGame g,
+            Server s) {
+        super(toHit, waa, g, s);
+    }
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see megamek.common.weapons.WeaponHandler#calcDamagePerHit()
+     */
+    protected int calcDamagePerHit() {
+        int toReturn;
+        // Check for Altered Damage from Energy Weapons (MTR, pg.22)
+        int nDamPerHit = wtype.getDamage();
+        int nRange = ae.getPosition().distance(target.getPosition());
+        if (game.getOptions().booleanOption("maxtech_altdmg")) {
+            if (nRange <= 1) {
+                nDamPerHit++;
+            } else if (nRange <= wtype.getMediumRange()) {
+                // Do Nothing for Short and Medium Range
+            } else if (nRange <= wtype.getLongRange()) {
+                nDamPerHit--;
+            } else if (nRange <= wtype.getExtremeRange()) {
+                nDamPerHit = (int)Math.floor(nDamPerHit/2.0);
+            }
+        }
+        if (bGlancing) {
+            toReturn = (int)Math.floor(nDamPerHit/2.0);
+        } else {
+            toReturn = nDamPerHit;
+        }
+        return toReturn;
+    }
+        
+
+}
---
+++ branches/refactor_weapons/megamek/src/megamek/common/weapons/ISLightRecoillessRifle.java
@@ -0,0 +1,49 @@
+/**
+ * MegaMek - Copyright (C) 2004,2005 Ben Mazur (bmazur@sev.org)
+ * 
+ *  This program is free software; you can redistribute it and/or modify it 
+ *  under the terms of the GNU General Public License as published by the Free 
+ *  Software Foundation; either version 2 of the License, or (at your option) 
+ *  any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *  for more details.
+ */
+/*
+ * Created on Sep 24, 2004
+ *
+ */
+package megamek.common.weapons;
+
+import megamek.common.AmmoType;
+import megamek.common.TechConstants;
+
+/**
+ * @author Andrew Hunter
+ * 
+ */
+public class ISLightRecoillessRifle extends Weapon {
+    /**
+     * 
+     */
+    public ISLightRecoillessRifle() {
+        super();
+        this.techLevel = TechConstants.T_IS_LEVEL_2;
+        this.name = "Light Recoilless";
+        this.setInternalName(this.name);
+        this.addLookupName("ISLight Recoilless Rifle");
+        this.heat = 0;
+        this.damage = 2;
+        this.ammoType = AmmoType.T_NA;
+        this.shortRange = 2;
+        this.mediumRange = 4;
+        this.longRange = 6;
+        this.extremeRange = 8;
+        this.tonnage = 0.0f;
+        this.criticals = 0;
+        this.bv = 12;
+        this.flags |= F_DIRECT_FIRE | F_NO_FIRES | F_BALLISTIC;
+    }
+}

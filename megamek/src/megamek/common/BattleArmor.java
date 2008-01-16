724493c35d1d3c09c514b7d40a515b863333d89c
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-01-16 18:31:16 +0000

    + Bug 1685545: BA Mine Launcher doesn't work

--- megamek/src/megamek/common/BattleArmor.java
+++ megamek/src/megamek/common/BattleArmor.java
@@ -1036,18 +1036,24 @@ public class BattleArmor
     
     public boolean loadWeapon(Mounted mounted, Mounted mountedAmmo) {
         //BA must carry the ammo in same location as the weapon.
+        // except for mine launcher mines
         //This allows for squad weapons and individual trooper weapons
         //such as NARC and the support weapons in TW/TO 
-        if(mounted.getLocation() != mountedAmmo.getLocation())
+        AmmoType at = (AmmoType)mountedAmmo.getType();
+        if (!(at.getAmmoType() == AmmoType.T_MINE) &&
+                mounted.getLocation() != mountedAmmo.getLocation())
             return false;
         return super.loadWeapon(mounted, mountedAmmo);
     }
 
     public boolean loadWeaponWithSameAmmo(Mounted mounted, Mounted mountedAmmo) {
         //BA must carry the ammo in same location as the weapon.
+        // except for mine launcher mines
         //This allows for squad weapons and individual trooper weapons
-        //such as NARC and the support weapons in TW/TO 
-        if(mounted.getLocation() != mountedAmmo.getLocation())
+        //such as NARC and the support weapons in TW/TO
+        AmmoType at = (AmmoType)mountedAmmo.getType();
+        if (!(at.getAmmoType() == AmmoType.T_MINE) &&
+                mounted.getLocation() != mountedAmmo.getLocation())
             return false;
         return super.loadWeaponWithSameAmmo(mounted, mountedAmmo);
     }

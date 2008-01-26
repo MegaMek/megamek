73bbf750f7e445dce647de5e8d89a5be475844f2
beerockxs <sebastian@brocks.cc>
2008-01-26 18:15:48 +0000

    + Merged refactor server branch: Refactored Victory Conditions, easy to implement new ones     Should work just the same

---
+++ megamek/src/megamek/server/victory/NoodleVictory.java
@@ -0,0 +1,55 @@
+package megamek.server.victory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import megamek.common.IGame;
+
+/**
+ * detailed spaghetti from the old victory code this assumes game options do not
+ * change during game (atleast regarding victory conditions)
+ */
+public class NoodleVictory implements Victory {
+    protected Victory v;
+
+    public NoodleVictory() {
+    }
+
+    protected void construct(IGame game) {
+        ArrayList<Victory> victories = new ArrayList<Victory>();
+
+        // BV related victory conditions
+        if (game.getOptions().booleanOption("use_bv_destroyed")) {
+            victories.add(new BVDestroyedVictory(game.getOptions().intOption(
+                    "bv_destroyed_percent")));
+        }
+        if (game.getOptions().booleanOption("use_bv_ratio")) {
+            victories.add(new BVRatioVictory(game.getOptions().intOption(
+                    "bv_ratio_percent")));
+        }
+
+        // Commander killed victory condition
+        if (game.getOptions().booleanOption("commander_killed")) {
+            victories.add(new LastCmdrStandingVictory());
+        }
+        // use a summing victory target to check if someone is winning
+        v = new SummingThresholdVictory(game.getOptions().intOption(
+                "achieve_conditions"), victories.toArray(new Victory[0]));
+    }
+
+    public Victory.Result victory(IGame game, HashMap<String, Object> ctx) {
+        if (v == null)
+            construct(game);
+        Victory.Result res = v.victory(game, ctx);
+
+        if (res.victory()) {
+            return res;
+        }
+
+        if (!res.victory() && game.gameTimerIsExpired()) {
+            return new SimpleDrawResult();
+        }
+
+        return res;
+    }
+}

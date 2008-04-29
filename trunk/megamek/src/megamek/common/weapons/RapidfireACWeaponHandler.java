2ea07b263abec8b0670dfe35034bdcf7cb854e07
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-04-29 15:57:53 +0000

    + Bug: Rapidfire AC doesn't generate heat

--- trunk/megamek/src/megamek/common/weapons/RapidfireACWeaponHandler.java
+++ trunk/megamek/src/megamek/common/weapons/RapidfireACWeaponHandler.java
@@ -19,12 +19,9 @@ package megamek.common.weapons;
 
 import java.util.Vector;
 
-import megamek.common.BattleArmor;
-import megamek.common.Compute;
 import megamek.common.CriticalSlot;
 import megamek.common.HitData;
 import megamek.common.IGame;
-import megamek.common.Infantry;
 import megamek.common.Mounted;
 import megamek.common.Report;
 import megamek.common.ToHitData;
@@ -40,7 +37,6 @@ public class RapidfireACWeaponHandler extends UltraWeaponHandler {
      * 
      */
     private static final long serialVersionUID = -1770392652874842106L;
-    int howManyShots;
 
     /**
      * @param t
@@ -50,94 +46,6 @@ public class RapidfireACWeaponHandler extends UltraWeaponHandler {
     public RapidfireACWeaponHandler(ToHitData t, WeaponAttackAction w, IGame g,
             Server s) {
         super(t, w, g, s);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see megamek.common.weapons.WeaponHandler#addHeatUseAmmo()
-     */
-    protected void useAmmo() {
-        setDone();
-        checkAmmo();
-        int total = ae.getTotalAmmoOfType(ammo.getType());
-        if (total > 1) {
-            howManyShots = 2;
-        }
-        if (total == 1) {
-            howManyShots = 1;
-        }
-        if (total == 0) {
-            // can't happen?
-
-        }
-        if (ammo.getShotsLeft() == 0) {
-            ae.loadWeapon(weapon);
-            ammo = weapon.getLinked();
-            // there will be some ammo somewhere, otherwise shot will not have
-            // been fired.
-        }
-        if (ammo.getShotsLeft() == 1) {
-            ammo.setShotsLeft(0);
-            ae.loadWeapon(weapon);
-            ammo = weapon.getLinked();
-            // that fired one, do we need to fire another?
-            ammo.setShotsLeft(ammo.getShotsLeft()
-                    - ((howManyShots == 2) ? 1 : 0));
-        } else {
-            ammo.setShotsLeft(ammo.getShotsLeft() - howManyShots);
-        }
-
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see megamek.common.weapons.WeaponHandler#calcHits(java.util.Vector)
-     */
-    protected int calcHits(Vector<Report> vPhaseReport) {
-        // conventional infantry gets hit in one lump
-        // BAs can't mount ACs
-        if (target instanceof Infantry && !(target instanceof BattleArmor)) {
-            return 1;
-        }
-        int shotsHit;
-        switch (howManyShots) {
-            case 1:
-                shotsHit = 1;
-                break;
-            default:
-                shotsHit = allShotsHit() ? howManyShots : Compute
-                        .missilesHit(howManyShots);
-                // report number of shots that hit only when weapon doesn't jam
-                if (!weapon.isJammed()) {
-                    r = new Report(3325);
-                    r.subject = subjectId;
-                    r.add(shotsHit);
-                    r.add(" shot(s) ");
-                    r.add(toHit.getTableDesc());
-                    r.newlines = 0;
-                    vPhaseReport.addElement(r);
-                    r = new Report(3345);
-                    r.subject = subjectId;
-                    r.newlines = 0;
-                    vPhaseReport.addElement(r);
-                }
-                break;
-        }
-        bSalvo = true;
-        return shotsHit;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see megamek.common.weapons.WeaponHandler#addHeat()
-     */
-    protected void addHeat() {// silly hack
-        for (int x = 0; x < howManyShots; x++) {
-            super.addHeat();
-        }
     }
 
     /*

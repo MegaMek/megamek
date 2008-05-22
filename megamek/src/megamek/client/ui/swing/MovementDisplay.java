759d3a0211966e276fd1d6efc2b1e8dc0abbc541
swang30 <swang30@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-05-22 05:45:25 +0000

    Synchronized Swing and AWT as of 5/21/2008

--- megamek/src/megamek/client/ui/swing/MovementDisplay.java
+++ megamek/src/megamek/client/ui/swing/MovementDisplay.java
@@ -1079,10 +1079,11 @@ public class MovementDisplay extends StatusBarPhaseDisplay implements
             if (hex.containsTerrain(Terrains.ICE) && waterLevel > 0) {
                 nagReport.append(Messages
                         .getString("MovementDisplay.IceLanding"));
-            }
-            rollTarget = entity.checkWaterMove(waterLevel);
-            if (rollTarget.getValue() != TargetRoll.CHECK_FALSE) {
-                nagReport.append(addNag(rollTarget));
+            }else if (!(prevStep.climbMode() && hex.containsTerrain(Terrains.BRIDGE))){
+                rollTarget = entity.checkWaterMove(waterLevel);
+                if (rollTarget.getValue() != TargetRoll.CHECK_FALSE) {
+                    nagReport.append(addNag(rollTarget));
+                }
             }
         }
 

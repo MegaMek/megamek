0ace42f7cd1ad2d6c9180bb07be3faa5e8e75f46
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-05-28 21:24:00 +0000

    release 0.33.26

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -46,7 +46,7 @@ import megamek.server.DedicatedServer;
  */
 public class MegaMek {
 
-    public static String VERSION = "0.33.26-dev"; //$NON-NLS-1$
+    public static String VERSION = "0.33.26"; //$NON-NLS-1$
 
     public static long TIMESTAMP = new File(PreferenceManager
             .getClientPreferences().getLogDirectory()

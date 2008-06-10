c09c92f54efda562de284d95e955d426c45be541
bombaijin <bombaijin@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-06-10 04:14:24 +0000

    + Fixed munition reading problem in bay weapon ammo

--- trunk/megamek/src/megamek/common/loaders/BLKDropshipFile.java
+++ trunk/megamek/src/megamek/common/loaders/BLKDropshipFile.java
@@ -285,7 +285,7 @@ public class BLKDropshipFile extends BLKFile implements IMechLoader {
                 }
                 
                 //check for ammo loadouts
-                if(equipName.contains("Ammo:")) {
+                if(equipName.contains(":") && equipName.contains("Ammo")) {
                     //then split by the :
                     String [] temp;
                     temp = equipName.split(":");
--- trunk/megamek/src/megamek/common/loaders/BLKSpaceStationFile.java
+++ trunk/megamek/src/megamek/common/loaders/BLKSpaceStationFile.java
@@ -307,7 +307,7 @@ public class BLKSpaceStationFile extends BLKFile implements IMechLoader {
                 }
                 
                 //check for ammo loadouts
-                if(equipName.contains("Ammo:")) {
+                if(equipName.contains(":") && equipName.contains("Ammo")) {
                     //then split by the :
                     String [] temp;
                     temp = equipName.split(":");

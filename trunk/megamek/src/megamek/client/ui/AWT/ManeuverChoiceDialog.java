97d6d5170ffe9b0167e4327a578552e773555e45
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-06-17 16:51:51 +0000

    ready to develop 0.33.29

--- trunk/megamek/src/megamek/client/ui/AWT/ManeuverChoiceDialog.java
+++ trunk/megamek/src/megamek/client/ui/AWT/ManeuverChoiceDialog.java
@@ -26,7 +26,6 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.Panel;
 import java.awt.ScrollPane;
-import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;

bd526fd771f27089e84451ccec0362a954664a81
beerockxs <sebastian@brocks.cc>
2008-06-17 16:30:05 +0000

    release 0.33.28

--- megamek/src/megamek/client/ui/swing/ManeuverChoiceDialog.java
+++ megamek/src/megamek/client/ui/swing/ManeuverChoiceDialog.java
@@ -27,13 +27,11 @@ import java.awt.event.WindowEvent;
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 import megamek.common.ManeuverType;
 

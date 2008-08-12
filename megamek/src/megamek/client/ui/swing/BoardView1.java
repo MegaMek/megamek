50554d3bd587423512992b99080f79e2729bc426
torren <torren@users.sourceforge.net>
2008-08-12 03:43:18 +0000

    + Bug: PPC Inhibitors not showing if Dial Down Damage rule used + added text "Hull Down" when unit is Hull Down on map

--- megamek/src/megamek/client/ui/swing/BoardView1.java
+++ megamek/src/megamek/client/ui/swing/BoardView1.java
@@ -2574,9 +2574,9 @@ public class BoardView1 extends JPanel implements IBoardView, Scrollable,
             if (entity.isHullDown()) {
                 // draw "D"
                 graph.setColor(Color.darkGray);
-                graph.drawString("D", 40, 71); //$NON-NLS-1$
+                graph.drawString("Hull Down", 40, 71); //$NON-NLS-1$
                 graph.setColor(Color.black);
-                graph.drawString("D", 39, 70); //$NON-NLS-1$                
+                graph.drawString("Hull Down", 39, 70); //$NON-NLS-1$                
             } else if (entity instanceof Infantry) {
                 int dig = ((Infantry) entity).getDugIn();
                 if (dig == Infantry.DUG_IN_COMPLETE) {

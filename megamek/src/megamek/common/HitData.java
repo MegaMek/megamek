9a6a4ec377500a6c73ece1d2439efcf41d741816
torren <torren@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-08-22 19:27:21 +0000

    + Bug: AP ammo and TC Ammo not doing full damage to Hardened Armor

--- megamek/src/megamek/common/HitData.java
+++ megamek/src/megamek/common/HitData.java
@@ -31,6 +31,8 @@ public class HitData {
     public static final int DAMAGE_ENERGY = -3;
     public static final int DAMAGE_MISSILE = -4;
     public static final int DAMAGE_BALLISTIC = -5;
+    public static final int DAMAGE_ARMOR_PIERCING = -6;
+    public static final int DAMAGE_ARMOR_PIERCING_MISSILE = -7;
     
     private int location;
     private boolean rear;

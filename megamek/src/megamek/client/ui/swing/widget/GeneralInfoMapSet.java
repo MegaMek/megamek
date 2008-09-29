7cd7069c833dc65f21b7609ba759a4bf658b3963
coelocanth <coelocanth@users.sourceforge.net>
2008-09-29 23:00:20 +0000

    + Bug 2085049: movement types not localised 	Note german text is machine translated, please correct if you are a native speaker

--- megamek/src/megamek/client/ui/swing/widget/GeneralInfoMapSet.java
+++ megamek/src/megamek/client/ui/swing/widget/GeneralInfoMapSet.java
@@ -368,7 +368,7 @@ public class GeneralInfoMapSet implements DisplayMapSet {
 
         if (en instanceof Tank) {
             movementTypeL.setVisible(true);
-            movementTypeR.setString(en.getMovementModeAsString());
+            movementTypeR.setString(Messages.getString("MovementType."+en.getMovementModeAsString()));
             movementTypeR.setVisible(true);
         } else {
             movementTypeL.setVisible(false);

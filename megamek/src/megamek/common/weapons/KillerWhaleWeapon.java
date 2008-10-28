079e67ace2d41610d75064dd05268824cb4f63e5
Jay Lawson <jaylawson39@gmail.com>
2008-10-28 17:39:23 +0000

    + Fixed: White Shark/Santa Anna appropriate weapon handler

--- megamek/src/megamek/common/weapons/KillerWhaleWeapon.java
+++ megamek/src/megamek/common/weapons/KillerWhaleWeapon.java
@@ -57,7 +57,7 @@ public class KillerWhaleWeapon extends CapitalMissileWeapon {
         this.extAV =4;
         this.maxRange = RANGE_EXT;
     }
-    
+
     /*
      * (non-Javadoc)
      * 
@@ -66,6 +66,13 @@ public class KillerWhaleWeapon extends CapitalMissileWeapon {
      */
     protected AttackHandler getCorrectHandler(ToHitData toHit,
             WeaponAttackAction waa, IGame game, Server server) {
+        AmmoType atype = (AmmoType) game.getEntity(waa.getEntityId()).getEquipment(waa.getWeaponId()).getLinked().getType();
+        if (atype.hasFlag(AmmoType.F_NUCLEAR)) {
+            return new SantaAnnaHandler(toHit, waa, game, server);
+        }
+        if (atype.hasFlag(AmmoType.F_TELE_MISSILE)) {
+            return new TeleMissileHandler(toHit, waa, game, server);
+        }
         return new KillerWhaleHandler(toHit, waa, game, server);
     }
 }

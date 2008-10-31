2bd5e72070628546ea8e182286c2380113ff9243
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-10-31 04:21:12 +0000

    fix unneeded imports delete equipment.txt again, it gets autogenerated when compiling.

--- trunk/megamek/src/megamek/common/weapons/CapitalMissileWeapon.java
+++ trunk/megamek/src/megamek/common/weapons/CapitalMissileWeapon.java
@@ -17,12 +17,6 @@
  */
 package megamek.common.weapons;
 
-import megamek.common.AmmoType;
-import megamek.common.IGame;
-import megamek.common.ToHitData;
-import megamek.common.actions.WeaponAttackAction;
-import megamek.server.Server;
-
 /**
  * @author Jay Lawson
  */

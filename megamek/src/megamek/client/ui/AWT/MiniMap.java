3f77be7c8f8f7d33fdfa41b138c70b7f7ea52be3
beerockxs <sebastian@brocks.cc>
2008-11-02 17:27:23 +0000

    + Bug: 3D boardview not working + Assign more memory to javac when building MM via ant, to prevent OutOfMemory     errors

--- megamek/src/megamek/client/ui/AWT/MiniMap.java
+++ megamek/src/megamek/client/ui/AWT/MiniMap.java
@@ -43,6 +43,7 @@ import megamek.client.Client;
 import megamek.client.event.BoardViewEvent;
 import megamek.client.event.BoardViewListener;
 import megamek.client.event.BoardViewListenerAdapter;
+import megamek.client.ui.IBoardView;
 import megamek.client.ui.AWT.util.PlayerColors;
 import megamek.common.Coords;
 import megamek.common.Entity;

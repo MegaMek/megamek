2642a258d25bdee71d836fbc4bb7280374d3789d
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2008-11-02 10:20:41 +0000

    + Bug: LACs having Rapid fire mode when game option is not set

--- megamek/src/megamek/common/weapons/LACWeapon.java
+++ megamek/src/megamek/common/weapons/LACWeapon.java
@@ -18,13 +18,8 @@
 package megamek.common.weapons;
 
 import megamek.common.AmmoType;
-import megamek.common.IGame;
-import megamek.common.Mounted;
-import megamek.common.ToHitData;
-import megamek.common.actions.WeaponAttackAction;
-import megamek.server.Server;
 
-public abstract class LACWeapon extends AmmoWeapon {
+public abstract class LACWeapon extends ACWeapon {
 
     /**
      * 
@@ -33,40 +28,8 @@ public abstract class LACWeapon extends AmmoWeapon {
 
     public LACWeapon() {
         super();
-        this.flags |= F_DIRECT_FIRE | F_BALLISTIC;
         this.ammoType = AmmoType.T_LAC;
-        String[] modes = { "", "Rapid" };
-        this.setModes(modes);
-        this.explosive = true; // when firing incendiary ammo
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see megamek.common.weapons.Weapon#getCorrectHandler(megamek.common.ToHitData,
-     *      megamek.common.actions.WeaponAttackAction, megamek.common.IGame,
-     *      megamek.server.Server)
-     */
-    protected AttackHandler getCorrectHandler(ToHitData toHit,
-            WeaponAttackAction waa, IGame game, Server server) {
-        AmmoType atype = (AmmoType) game.getEntity(waa.getEntityId())
-                .getEquipment(waa.getWeaponId()).getLinked().getType();
-        Mounted weapon = game.getEntity(waa.getEntityId()).getEquipment(
-                waa.getWeaponId());
-        if (weapon.curMode().equals("Rapid")) {
-            return new RapidfireACWeaponHandler(toHit, waa, game, server);
-        } else if (atype.getMunitionType() == AmmoType.M_ARMOR_PIERCING) {
-            return new ACAPHandler(toHit, waa, game, server);
-        } else if (atype.getMunitionType() == AmmoType.M_FLECHETTE) {
-            return new ACFlechetteHandler(toHit, waa, game, server);
-        } else if (atype.getMunitionType() == AmmoType.M_INCENDIARY_AC) {
-            return new ACIncendiaryHandler(toHit, waa, game, server);
-        } else if (atype.getMunitionType() == AmmoType.M_TRACER) {
-            return new ACTracerHandler(toHit, waa, game, server);
-        } else {
-            return new ACWeaponHandler(toHit, waa, game, server);
-        }
-
-    }
 
 }

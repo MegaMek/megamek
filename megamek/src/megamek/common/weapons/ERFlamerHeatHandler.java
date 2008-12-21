0ac23fc708f6d044f63a2c2f94f31f3dfa175248
beerockxs <sebastian@brocks.cc>
2008-12-21 15:21:10 +0000

    + Added ER Flamers

--- megamek/src/megamek/common/weapons/ERFlamerHeatHandler.java
+++ megamek/src/megamek/common/weapons/ERFlamerHeatHandler.java
@@ -23,8 +23,10 @@ import megamek.common.BattleArmor;
 import megamek.common.Building;
 import megamek.common.Compute;
 import megamek.common.Entity;
+import megamek.common.HitData;
 import megamek.common.IGame;
 import megamek.common.Infantry;
+import megamek.common.Mech;
 import megamek.common.Report;
 import megamek.common.TargetRoll;
 import megamek.common.ToHitData;
@@ -32,22 +34,61 @@ import megamek.common.actions.WeaponAttackAction;
 import megamek.server.Server;
 
 /**
- * @author Sebastian Brocks
+ * @author Andrew Hunter
  */
-public class FlamerHandler extends WeaponHandler {
+public class ERFlamerHeatHandler extends FlamerHeatHandler {
+
     /**
      *
      */
-    private static final long serialVersionUID = -7348456582587703751L;
+    private static final long serialVersionUID = -7015983426485759999L;
 
     /**
      * @param toHit
      * @param waa
      * @param g
      */
-    public FlamerHandler(ToHitData toHit, WeaponAttackAction waa, IGame g,
+    public ERFlamerHeatHandler(ToHitData toHit, WeaponAttackAction waa, IGame g,
             Server s) {
         super(toHit, waa, g, s);
+    }
+
+    @Override
+    protected void handleEntityDamage(Entity entityTarget,
+            Vector<Report> vPhaseReport, Building bldg, int hits, int nCluster,
+            int nDamPerHit, int bldgAbsorbs) {
+        if (entityTarget instanceof Mech
+                && game.getOptions().booleanOption("flamer_heat")) {
+            // heat
+            HitData hit = entityTarget.rollHitLocation(toHit.getHitTable(),
+                    toHit.getSideTable(), waa.getAimedLocation(), waa
+                            .getAimingMode());
+
+            if (entityTarget.removePartialCoverHits(hit.getLocation(), toHit
+                    .getCover(), Compute.targetSideTable(ae, entityTarget))) {
+                // Weapon strikes Partial Cover.
+                r = new Report(3460);
+                r.subject = subjectId;
+                r.add(entityTarget.getShortName());
+                r.add(entityTarget.getLocationAbbr(hit));
+                r.newlines = 0;
+                r.indent(2);
+                vPhaseReport.addElement(r);
+                missed = true;
+                return;
+            }
+            r = new Report(3400);
+            r.subject = subjectId;
+            r.indent(2);
+            r.add(2);
+            r.newlines = 0;
+            r.choose(true);
+            vPhaseReport.addElement(r);
+            entityTarget.heatFromExternal += 1;
+        } else {
+            super.handleEntityDamage(entityTarget, vPhaseReport, bldg, hits,
+                    nCluster, nDamPerHit, bldgAbsorbs);
+        }
     }
 
     /*
@@ -62,8 +103,8 @@ public class FlamerHandler extends WeaponHandler {
             if (ae instanceof BattleArmor) {
                 toReturn = Compute.d6(3);
             }
-            toReturn = Compute.d6(4);
-            if ( bDirect ) {
+            toReturn = Compute.d6(2);
+            if (bDirect) {
                 toReturn += toHit.getMoS()/3;
             }
             // pain shunted infantry get half damage
@@ -78,6 +119,7 @@ public class FlamerHandler extends WeaponHandler {
         }
         return toReturn;
     }
+
 
     /**
      * @return a <code>boolean</code> value indicating wether or not this

b9776cfcb431026e69a5d2489c630b9ff7575683
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-01-19 21:16:16 +0000

    + Added Fluid Guns. Only construction data, no rules

--- trunk/megamek/src/megamek/common/weapons/VehicleFlamerWeapon.java
+++ trunk/megamek/src/megamek/common/weapons/VehicleFlamerWeapon.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -28,23 +28,25 @@ import megamek.server.Server;
  */
 public abstract class VehicleFlamerWeapon extends AmmoWeapon {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -8729838198434670197L;
 
     /**
-     * 
+     *
      */
     public VehicleFlamerWeapon() {
         super();
-        this.flags |= F_FLAMER;
-        this.ammoType = AmmoType.T_VEHICLE_FLAMER;
+        flags |= F_FLAMER;
+        ammoType = AmmoType.T_VEHICLE_FLAMER;
+        //TODO: cool should be an ammotype, not a mode
         String modes[] = { "Damage", "Heat", "Cool" };
-        this.setModes(modes);
-        
-        this.atClass = CLASS_POINT_DEFENSE;
+        setModes(modes);
+
+        atClass = CLASS_POINT_DEFENSE;
     }
 
+    @Override
     protected AttackHandler getCorrectHandler(ToHitData toHit,
             WeaponAttackAction waa, IGame game, Server server) {
         if ((game.getEntity(waa.getEntityId()).getEquipment(waa.getWeaponId())
@@ -52,6 +54,7 @@ public abstract class VehicleFlamerWeapon extends AmmoWeapon {
             return new VehicleFlamerHeatHandler(toHit, waa, game, server);
         } else if ((game.getEntity(waa.getEntityId()).getEquipment(
                 waa.getWeaponId()).curMode().equals("Cool"))) {
+            //TODO: cool should be an ammotype, not a mode
             return new VehicleFlamerCoolHandler(toHit, waa, game, server);
         } else {
             return new VehicleFlamerHandler(toHit, waa, game, server);

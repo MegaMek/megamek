1552872d17b9e55d706b213cd98b6e14f75aa4c3
torren <torren@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-01-27 19:37:45 +0000

    + Bug: Clan RAC/2 and RAC/5 had wrong crits/tonnage

--- trunk/megamek/src/megamek/common/weapons/CLRAC2.java
+++ trunk/megamek/src/megamek/common/weapons/CLRAC2.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -24,31 +24,31 @@ import megamek.common.TechConstants;
  */
 public class CLRAC2 extends RACWeapon {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -2134880724662962943L;
 
     /**
-     * 
+     *
      */
     public CLRAC2() {
         super();
-        this.techLevel = TechConstants.T_CLAN_EXPERIMENTAL;
-        this.name = "Rotary AC/2";
-        this.setInternalName("CLRotaryAC2");
-        this.addLookupName("Clan Rotary AC/2");
-        this.addLookupName("Clan Rotary Assault Cannon/2");
-        this.heat = 1;
-        this.damage = 2;
-        this.rackSize = 2;
-        this.minimumRange = 2;
-        this.shortRange = 9;
-        this.mediumRange = 18;
-        this.longRange = 27;
-        this.extremeRange = 36;
-        this.tonnage = 7.0f;
-        this.criticals = 4;
-        this.bv = 185;
-        this.cost = 240000;
+        techLevel = TechConstants.T_CLAN_EXPERIMENTAL;
+        name = "Rotary AC/2";
+        setInternalName("CLRotaryAC2");
+        addLookupName("Clan Rotary AC/2");
+        addLookupName("Clan Rotary Assault Cannon/2");
+        heat = 1;
+        damage = 2;
+        rackSize = 2;
+        minimumRange = 2;
+        shortRange = 9;
+        mediumRange = 18;
+        longRange = 27;
+        extremeRange = 36;
+        tonnage = 8.0f;
+        criticals = 4;
+        bv = 185;
+        cost = 240000;
     }
 }
--- trunk/megamek/src/megamek/common/weapons/CLRAC5.java
+++ trunk/megamek/src/megamek/common/weapons/CLRAC5.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -24,30 +24,30 @@ import megamek.common.TechConstants;
  */
 public class CLRAC5 extends RACWeapon {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -9054458663836717481L;
 
     /**
-     * 
+     *
      */
     public CLRAC5() {
         super();
-        this.techLevel = TechConstants.T_CLAN_EXPERIMENTAL;
-        this.name = "Rotary AC/5";
-        this.setInternalName("CLRotaryAC5");
-        this.addLookupName("Clan Rotary AC/5");
-        this.addLookupName("Clan Rotary Assault Cannon/5");
-        this.heat = 1;
-        this.damage = 5;
-        this.rackSize = 5;
-        this.shortRange = 7;
-        this.mediumRange = 14;
-        this.longRange = 21;
-        this.extremeRange = 28;
-        this.tonnage = 10.0f;
-        this.criticals = 5;
-        this.bv = 360;
-        this.cost = 400000;
+        techLevel = TechConstants.T_CLAN_EXPERIMENTAL;
+        name = "Rotary AC/5";
+        setInternalName("CLRotaryAC5");
+        addLookupName("Clan Rotary AC/5");
+        addLookupName("Clan Rotary Assault Cannon/5");
+        heat = 1;
+        damage = 5;
+        rackSize = 5;
+        shortRange = 7;
+        mediumRange = 14;
+        longRange = 21;
+        extremeRange = 28;
+        tonnage = 10.0f;
+        criticals = 8;
+        bv = 360;
+        cost = 400000;
     }
 }

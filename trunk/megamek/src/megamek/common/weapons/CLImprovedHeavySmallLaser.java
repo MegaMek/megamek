5307492ed22573b63248dfb13562b08a0fda93e7
torren <torren@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-01-29 20:58:57 +0000

    + RFE: Added new internal names for Improved Heavy Medium and Improved Heavy Small Lasers

--- trunk/megamek/src/megamek/common/weapons/CLImprovedHeavySmallLaser.java
+++ trunk/megamek/src/megamek/common/weapons/CLImprovedHeavySmallLaser.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004,2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -24,18 +24,19 @@ import megamek.common.TechConstants;
  */
 public class CLImprovedHeavySmallLaser extends LaserWeapon {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 4467522144065588079L;
 
     /**
-     * 
+     *
      */
     public CLImprovedHeavySmallLaser() {
         super();
         techLevel = TechConstants.T_CLAN_EXPERIMENTAL;
-        name = "Improved Small Heavy Laser";
+        name = "Improved Heavy Small Laser";
         setInternalName("CLImprovedSmallHeavyLaser");
+        addLookupName("CLImprovedHeavySmallLaser");
         addLookupName("Clan Improved Small Heavy Laser");
         heat = 3;
         damage = 6;

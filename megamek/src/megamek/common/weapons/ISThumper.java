4ce9a919209815a9b7d2f47f57f22a7d4359d3bb
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-02-20 17:53:20 +0000

    + Updated Artillery BV values to BV 2.0

--- megamek/src/megamek/common/weapons/ISThumper.java
+++ megamek/src/megamek/common/weapons/ISThumper.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004,2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -26,31 +26,31 @@ import megamek.common.TechConstants;
 public class ISThumper extends ArtilleryWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -3256813053043672610L;
 
     /**
-     * 
+     *
      */
     public ISThumper() {
         super();
-        this.techLevel = TechConstants.T_IS_ADVANCED;
-        this.name = "Thumper";
-        this.setInternalName("ISThumper");
-        this.addLookupName("ISThumperArtillery");
-        this.addLookupName("IS Thumper");
-        this.heat = 5;
-        this.rackSize = 15;
-        this.ammoType = AmmoType.T_THUMPER;
-        this.shortRange = 1;
-        this.mediumRange = 2;
-        this.longRange = 21;
-        this.extremeRange = 21; // No extreme range.
-        this.tonnage = 15f;
-        this.criticals = 15;
-        this.bv = 40;
-        this.cost = 187500;
+        techLevel = TechConstants.T_IS_ADVANCED;
+        name = "Thumper";
+        setInternalName("ISThumper");
+        addLookupName("ISThumperArtillery");
+        addLookupName("IS Thumper");
+        heat = 5;
+        rackSize = 15;
+        ammoType = AmmoType.T_THUMPER;
+        shortRange = 1;
+        mediumRange = 2;
+        longRange = 21;
+        extremeRange = 21; // No extreme range.
+        tonnage = 15f;
+        criticals = 15;
+        bv = 43;
+        cost = 187500;
     }
 
 }

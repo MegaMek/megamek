7a63a9509f8e6a8045778d3dd40e170647761ccc
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-03-02 20:47:29 +0000

    + Bug 1734691: LOS walking "both sides of the fence"

--- megamek/src/megamek/common/LargeSupportTank.java
+++ megamek/src/megamek/common/LargeSupportTank.java
@@ -1,28 +1,29 @@
 /*
  *  MegaMek - Copyright (C) 2000-2003 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * This is a large support vehicle
  * @author beerockxs
  */
 public class LargeSupportTank extends SupportTank implements Serializable {
-    
+
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -3177191060629774478L;
     // locations
@@ -34,25 +35,28 @@ public class LargeSupportTank extends SupportTank implements Serializable {
     public static final int LOC_REARLEFT = 5;
     public static final int LOC_REAR = 6;
     public static final int LOC_TURRET = 7;
-    
+
     protected static String[] LOCATION_ABBRS = { "BD", "FR", "FRRS", "FRLS",
         "RRRS", "RRLS", "RR", "TU" };
-    
+
     public static String[] LOCATION_NAMES = { "Body", "Front", "Front Right",
         "Front Left", "Rear Right", "Rear Left", "Rear", "Turret" };
-    
-    
+
+
+    @Override
     public String[] getLocationAbbrs() {
         return LOCATION_ABBRS;
     }
-    
+
+    @Override
     public String[] getLocationNames() {
         return LOCATION_NAMES;
     }
-    
+
     /**
      * Rolls up a hit location
      */
+    @Override
     public HitData rollHitLocation(int table, int side, int aimedLocation,
             int aimingMode) {
         int nArmorLoc = LOC_FRONT;
@@ -60,7 +64,7 @@ public class LargeSupportTank extends SupportTank implements Serializable {
         boolean bRearSide = false;
         boolean bRear = false;
         int motiveMod = 0;
-        if (side == ToHitData.SIDE_FRONT && isHullDown() && !m_bHasNoTurret) {
+        if ((side == ToHitData.SIDE_FRONT) && isHullDown() && !m_bHasNoTurret) {
             // on a hull down vee, all front hits go to turret if one exists.
             nArmorLoc = LOC_TURRET;
         }
@@ -84,7 +88,7 @@ public class LargeSupportTank extends SupportTank implements Serializable {
             nArmorLoc = LOC_REAR;
             motiveMod = 1;
             bRear = true;
-        }        
+        }
         if(game.getOptions().booleanOption("tacops_vehicle_effective")) {
             motiveMod = 0;
         }
@@ -92,7 +96,7 @@ public class LargeSupportTank extends SupportTank implements Serializable {
         boolean bHitAimed = false;
         if ((aimedLocation != LOC_NONE)
                 && (aimingMode != IAimingModes.AIM_MODE_NONE)) {
-            
+
             int roll = Compute.d6(2);
 
             if ((5 < roll) && (roll < 9)) {
@@ -129,7 +133,7 @@ public class LargeSupportTank extends SupportTank implements Serializable {
                 if (bRear || !(bSide || bRearSide)) {
                     rv.setEffect(HitData.EFFECT_VEHICLE_MOVE_DAMAGED);
                     rv.setMotiveMod(motiveMod);
-                }                
+                }
                 break;
             case 6:
             case 7:
@@ -163,38 +167,71 @@ public class LargeSupportTank extends SupportTank implements Serializable {
                 }
             }
         }
-        if (table == ToHitData.HIT_SWARM)
+        if (table == ToHitData.HIT_SWARM) {
             rv.setEffect(rv.getEffect() | HitData.EFFECT_CRITICAL);
+        }
         return rv;
     }
-    
-    public int sideTable(Coords src) {
-        if (src.equals(position)) {
+
+    @Override
+    public int sideTable(Coords src, boolean usePrior, int face) {
+        Coords effectivePos = position;
+        if (usePrior) {
+            effectivePos = getPriorPosition();
+        }
+        if (src.equals(effectivePos)) {
             // most places handle 0 range explicitly,
             // this is a safe default (calculation gives SIDE_RIGHT)
             return ToHitData.SIDE_FRONT;
         }
         // calculate firing angle
-        int fa = (position.degree(src) + (6 - facing) * 60) % 360;
+        int fa = (effectivePos.degree(src) + (6 - face) * 60) % 360;
 
-        if (fa > 30 && fa <= 90) {
-            return ToHitData.SIDE_FRONTRIGHT;
-        } else if (fa > 90 && fa < 150) {
-            return ToHitData.SIDE_REARRIGHT;
-        } else if (fa >= 150 && fa < 210) {
-            return ToHitData.SIDE_REAR;
-        } else if (fa >= 210 && fa < 270) {
+        boolean leftBetter = true;
+        // if we're right on the line, we need to special case this
+        // defender would choose along which hex the LOS gets drawn, and that
+        // side also determines the side we hit in
+        if (fa % 30 == 0) {
+            LosEffects.AttackInfo ai = LosEffects.buildAttackInfo(src, getPosition(),
+                    1, getElevation(), game.getBoard().getHex(src).floor(),
+                    game.getBoard().getHex(getPosition()).floor());
+            ArrayList<Coords> in = Coords.intervening(ai.attackPos, ai.targetPos,
+                    true);
+            leftBetter = LosEffects.dividedLeftBetter(in, game, ai,
+                    Compute.isInBuilding(game, this), new LosEffects());
+        }
+
+        if ((fa == 330) && !leftBetter) {
+            return ToHitData.SIDE_FRONTLEFT;
+        } else if ((fa == 270) && !leftBetter) {
             return ToHitData.SIDE_REARLEFT;
-        } else if (fa >= 270 && fa < 330) {
+        } else if ((fa == 210) && !leftBetter) {
+            return  ToHitData.SIDE_REAR;
+        } else if ((fa == 150) && !leftBetter) {
+            return ToHitData.SIDE_REARRIGHT;
+        } else if ((fa == 90) && leftBetter) {
+            return ToHitData.SIDE_REARRIGHT;
+        } else if ((fa == 30) && leftBetter) {
+            return ToHitData.SIDE_FRONTRIGHT;
+        } else if ((fa > 30) && (fa <= 90)) {
+            return ToHitData.SIDE_FRONTRIGHT;
+        } else if ((fa > 90) && (fa < 150)) {
+            return ToHitData.SIDE_REARRIGHT;
+        } else if ((fa >= 150) && (fa < 210)) {
             return ToHitData.SIDE_REAR;
+        } else if ((fa >= 210) && (fa < 270)) {
+            return ToHitData.SIDE_REARLEFT;
+        } else if ((fa >= 270) && (fa < 330)) {
+            return ToHitData.SIDE_FRONTLEFT;
         } else {
             return ToHitData.SIDE_FRONT;
         }
     }
-    
+
     /**
      * Returns the number of locations in the entity
      */
+    @Override
     public int locations() {
         return m_bHasNoTurret ? 7 : 8;
     }

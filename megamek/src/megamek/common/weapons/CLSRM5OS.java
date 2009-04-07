a1680c4523c01329f796c5387a4a20c9754a23d4
beerockxs <sebastian@brocks.cc>
2009-04-07 17:50:32 +0000

    + Bug: Clan Medium Bär BA does not have heavy battle claws (magnetic) + Bug: Clan SRM 5 BV wrong

--- megamek/src/megamek/common/weapons/CLSRM5OS.java
+++ megamek/src/megamek/common/weapons/CLSRM5OS.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common.weapons;
@@ -21,24 +21,24 @@ import megamek.common.TechConstants;
 public class CLSRM5OS extends SRMWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 9051765359928076836L;
 
     /**
-     * 
+     *
      */
     public CLSRM5OS() {
         super();
-        this.techLevel = TechConstants.T_CLAN_TW;
-        this.name = "SRM 5 (OS)";
-        this.setInternalName("CLSRM5OS");
-        this.rackSize = 5;
-        this.shortRange = 3;
-        this.mediumRange = 6;
-        this.longRange = 9;
-        this.extremeRange = 12;
-        this.bv = 12;
-        this.flags |= F_NO_FIRES | F_ONESHOT;
+        techLevel = TechConstants.T_CLAN_TW;
+        name = "SRM 5 (OS)";
+        setInternalName("CLSRM5OS");
+        rackSize = 5;
+        shortRange = 3;
+        mediumRange = 6;
+        longRange = 9;
+        extremeRange = 12;
+        bv = 9;
+        flags |= F_NO_FIRES | F_ONESHOT;
     }
 }

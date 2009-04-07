44019b926d27100162137ab9cbed4e9d3b04b80e
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-04-07 17:50:32 +0000

    + Bug: Clan Medium Bär BA does not have heavy battle claws (magnetic) + Bug: Clan SRM 5 BV wrong

--- trunk/megamek/src/megamek/common/weapons/CLSRM5.java
+++ trunk/megamek/src/megamek/common/weapons/CLSRM5.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common.weapons;
@@ -21,27 +21,27 @@ import megamek.common.TechConstants;
 public class CLSRM5 extends SRMWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 9051765359928076836L;
 
     /**
-     * 
+     *
      */
     public CLSRM5() {
         super();
-        this.techLevel = TechConstants.T_CLAN_TW;
-        this.name = "SRM 5";
-        this.setInternalName("CLSRM5");
-        this.addLookupName("Clan SRM-5");
-        this.addLookupName("Clan SRM 5");
-        this.rackSize = 5;
-        this.shortRange = 3;
-        this.mediumRange = 6;
-        this.longRange = 9;
-        this.extremeRange = 12;
-        this.bv = 58;
-        this.flags |= F_NO_FIRES;
-        this.cost = 80000;
+        techLevel = TechConstants.T_CLAN_TW;
+        name = "SRM 5";
+        setInternalName("CLSRM5");
+        addLookupName("Clan SRM-5");
+        addLookupName("Clan SRM 5");
+        rackSize = 5;
+        shortRange = 3;
+        mediumRange = 6;
+        longRange = 9;
+        extremeRange = 12;
+        bv = 47;
+        flags |= F_NO_FIRES;
+        cost = 80000;
     }
 }

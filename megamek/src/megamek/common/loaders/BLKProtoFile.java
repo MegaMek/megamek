39a3039c5cbc9a8addef472972ada6b9b3708e65
Torren <Torren@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-04-13 16:09:09 +0000

    + added the ability to load the source tag for all BLK file loaders

--- megamek/src/megamek/common/loaders/BLKProtoFile.java
+++ megamek/src/megamek/common/loaders/BLKProtoFile.java
@@ -36,152 +36,156 @@ import megamek.common.util.BuildingBlock;
 
 public class BLKProtoFile extends BLKFile implements IMechLoader {
 
-    public BLKProtoFile(BuildingBlock bb) {
-        dataFile = bb;
-    }
+	public BLKProtoFile(BuildingBlock bb) {
+		dataFile = bb;
+	}
 
-    public Entity getEntity() throws EntityLoadingException {
+	public Entity getEntity() throws EntityLoadingException {
 
-        Protomech t = new Protomech();
+		Protomech t = new Protomech();
 
-        if (!dataFile.exists("name"))
-            throw new EntityLoadingException("Could not find name block.");
-        t.setChassis(dataFile.getDataAsString("Name")[0]);
+		if (!dataFile.exists("name")) {
+			throw new EntityLoadingException("Could not find name block.");
+		}
+		t.setChassis(dataFile.getDataAsString("Name")[0]);
 
-        // Model is not strictly necessary.
-        if (dataFile.exists("Model")
-                && dataFile.getDataAsString("Model")[0] != null) {
-            t.setModel(dataFile.getDataAsString("Model")[0]);
-        } else {
-            t.setModel("");
-        }
+		// Model is not strictly necessary.
+		if (dataFile.exists("Model") && dataFile.getDataAsString("Model")[0] != null) {
+			t.setModel(dataFile.getDataAsString("Model")[0]);
+		} else {
+			t.setModel("");
+		}
 
-        if (!dataFile.exists("year"))
-            throw new EntityLoadingException("Could not find year block.");
-        t.setYear(dataFile.getDataAsInt("year")[0]);
+		if (dataFile.exists("source")) {
+			t.setSource(dataFile.getDataAsString("source")[0]);
+		}
 
-        setTechLevel(t);
-        
-        if (!dataFile.exists("tonnage"))
-            throw new EntityLoadingException("Could not find weight block.");
-        t.setWeight(dataFile.getDataAsFloat("tonnage")[0]);
+		if (!dataFile.exists("year")) {
+			throw new EntityLoadingException("Could not find year block.");
+		}
+		t.setYear(dataFile.getDataAsInt("year")[0]);
 
-        /***********************************************************************
-         * 'Protos have only one motion type. if
-         * (!dataFile.exists("motion_type")) throw new
-         * EntityLoadingException("Could not find movement block."); String
-         * sMotion = dataFile.getDataAsString("motion_type")[0]; int nMotion =
-         * -1; for (int x = 0; x < MOVES.length; x++) { if
-         * (sMotion.equals(MOVES[x])) { nMotion = x; break; } } if (nMotion ==
-         * -1) throw new EntityLoadingException("Invalid movment type: " +
-         * sMotion); t.setMovementType(nMotion); 'Protos have only one motion
-         * type. *
-         **********************************************************************/
-        t.setMovementMode(IEntityMovementMode.INF_JUMP);
+		setTechLevel(t);
 
-        if (!dataFile.exists("cruiseMP"))
-            throw new EntityLoadingException("Could not find cruiseMP block.");
-        t.setOriginalWalkMP(dataFile.getDataAsInt("cruiseMP")[0]);
+		if (!dataFile.exists("tonnage")) {
+			throw new EntityLoadingException("Could not find weight block.");
+		}
+		t.setWeight(dataFile.getDataAsFloat("tonnage")[0]);
 
-        if (dataFile.exists("jumpingMP"))
-            t.setOriginalJumpMP(dataFile.getDataAsInt("jumpingMP")[0]);
+		/***********************************************************************
+		 * 'Protos have only one motion type. if
+		 * (!dataFile.exists("motion_type")) throw new
+		 * EntityLoadingException("Could not find movement block."); String
+		 * sMotion = dataFile.getDataAsString("motion_type")[0]; int nMotion =
+		 * -1; for (int x = 0; x < MOVES.length; x++) { if
+		 * (sMotion.equals(MOVES[x])) { nMotion = x; break; } } if (nMotion ==
+		 * -1) throw new EntityLoadingException("Invalid movment type: " +
+		 * sMotion); t.setMovementType(nMotion); 'Protos have only one motion
+		 * type. *
+		 **********************************************************************/
+		t.setMovementMode(IEntityMovementMode.INF_JUMP);
 
-        if (!dataFile.exists("armor"))
-            throw new EntityLoadingException("Could not find armor block.");
+		if (!dataFile.exists("cruiseMP")) {
+			throw new EntityLoadingException("Could not find cruiseMP block.");
+		}
+		t.setOriginalWalkMP(dataFile.getDataAsInt("cruiseMP")[0]);
 
-        int[] armor = dataFile.getDataAsInt("armor");
+		if (dataFile.exists("jumpingMP")) {
+			t.setOriginalJumpMP(dataFile.getDataAsInt("jumpingMP")[0]);
+		}
 
-        boolean hasMainGun = false;
-        if (Protomech.NUM_PMECH_LOCATIONS == armor.length) {
-            hasMainGun = true;
-        } else if (Protomech.NUM_PMECH_LOCATIONS - 1 == armor.length) {
-            hasMainGun = false;
-        } else {
-            throw new EntityLoadingException("Incorrect armor array length");
-        }
+		if (!dataFile.exists("armor")) {
+			throw new EntityLoadingException("Could not find armor block.");
+		}
 
-        t.setHasMainGun(hasMainGun);
+		int[] armor = dataFile.getDataAsInt("armor");
 
-        // add the body to the armor array
-        for (int x = 0; x < armor.length; x++) {
-            t.initializeArmor(armor[x], x);
-        }
+		boolean hasMainGun = false;
+		if (Protomech.NUM_PMECH_LOCATIONS == armor.length) {
+			hasMainGun = true;
+		} else if (Protomech.NUM_PMECH_LOCATIONS - 1 == armor.length) {
+			hasMainGun = false;
+		} else {
+			throw new EntityLoadingException("Incorrect armor array length");
+		}
 
-        t.autoSetInternal();
+		t.setHasMainGun(hasMainGun);
 
-        String[] abbrs = t.getLocationNames();
-        for (int loop = 0; loop < t.locations(); loop++) {
-            loadEquipment(t, abbrs[loop], loop);
-        }
-        return t;
-    }
+		// add the body to the armor array
+		for (int x = 0; x < armor.length; x++) {
+			t.initializeArmor(armor[x], x);
+		}
 
-    private void loadEquipment(Protomech t, String sName, int nLoc)
-            throws EntityLoadingException {
-        String[] saEquip = dataFile.getDataAsString(sName + " Equipment");
-        if (saEquip == null)
-            return;
+		t.autoSetInternal();
 
-        // prefix is "Clan " or "IS "
-        String prefix;
-        if (t.getTechLevel() == TechConstants.T_CLAN_TW) {
-            prefix = "Clan ";
-        } else {
-            prefix = "IS ";
-        }
+		String[] abbrs = t.getLocationNames();
+		for (int loop = 0; loop < t.locations(); loop++) {
+			loadEquipment(t, abbrs[loop], loop);
+		}
+		return t;
+	}
 
-        for (int x = 0; x < saEquip.length; x++) {
-            String equipName = saEquip[x].trim();
+	private void loadEquipment(Protomech t, String sName, int nLoc) throws EntityLoadingException {
+		String[] saEquip = dataFile.getDataAsString(sName + " Equipment");
+		if (saEquip == null) {
+			return;
+		}
 
-            // ProtoMech Ammo comes in non-standard amounts.
-            int ammoIndex = equipName.indexOf("Ammo (");
-            int shotsCount = 0;
-            if (ammoIndex > 0) {
-                // Try to get the number of shots.
-                try {
-                    String shots = equipName.substring(ammoIndex + 6, equipName
-                            .length() - 1);
-                    shotsCount = Integer.parseInt(shots);
-                    if (shotsCount < 0) {
-                        throw new EntityLoadingException(
-                                "Invalid number of shots in: " + equipName
-                                        + ".");
-                    }
-                } catch (NumberFormatException badShots) {
-                    throw new EntityLoadingException(
-                            "Could not determine the number of shots in: "
-                                    + equipName + ".");
-                }
+		// prefix is "Clan " or "IS "
+		String prefix;
+		if (t.getTechLevel() == TechConstants.T_CLAN_TW) {
+			prefix = "Clan ";
+		} else {
+			prefix = "IS ";
+		}
 
-                // Strip the shots out of the ammo name.
-                equipName = equipName.substring(0, ammoIndex + 4);
-            }
-            EquipmentType etype = EquipmentType.get(equipName);
+		for (String element : saEquip) {
+			String equipName = element.trim();
 
-            if (etype == null) {
-                // try w/ prefix
-                etype = EquipmentType.get(prefix + equipName);
-            }
+			// ProtoMech Ammo comes in non-standard amounts.
+			int ammoIndex = equipName.indexOf("Ammo (");
+			int shotsCount = 0;
+			if (ammoIndex > 0) {
+				// Try to get the number of shots.
+				try {
+					String shots = equipName.substring(ammoIndex + 6, equipName.length() - 1);
+					shotsCount = Integer.parseInt(shots);
+					if (shotsCount < 0) {
+						throw new EntityLoadingException("Invalid number of shots in: " + equipName + ".");
+					}
+				} catch (NumberFormatException badShots) {
+					throw new EntityLoadingException("Could not determine the number of shots in: " + equipName + ".");
+				}
 
-            if (etype != null) {
-                try {
-                    // If this is an Ammo slot, only add
-                    // the indicated number of shots.
-                    if (ammoIndex > 0) {
-                        t.addEquipment(etype, nLoc, false, shotsCount);
-                    } else {
-                        t.addEquipment(etype, nLoc);
-                    }
-                } catch (LocationFullException ex) {
-                    throw new EntityLoadingException(ex.getMessage());
-                }
-            }
-            /*******************************************************************
-             * remove this block after TAG and Active Probes are implemented
-             * else if ( !equipName.equals("0") ) { System.err.println("Could
-             * not find " + equipName + " for " + t.getShortName() ); } //killme
-             * remove this block after TAG and Active Probes are implemented *
-             ******************************************************************/
-        }
-    }
+				// Strip the shots out of the ammo name.
+				equipName = equipName.substring(0, ammoIndex + 4);
+			}
+			EquipmentType etype = EquipmentType.get(equipName);
+
+			if (etype == null) {
+				// try w/ prefix
+				etype = EquipmentType.get(prefix + equipName);
+			}
+
+			if (etype != null) {
+				try {
+					// If this is an Ammo slot, only add
+					// the indicated number of shots.
+					if (ammoIndex > 0) {
+						t.addEquipment(etype, nLoc, false, shotsCount);
+					} else {
+						t.addEquipment(etype, nLoc);
+					}
+				} catch (LocationFullException ex) {
+					throw new EntityLoadingException(ex.getMessage());
+				}
+			}
+			/*******************************************************************
+			 * remove this block after TAG and Active Probes are implemented
+			 * else if ( !equipName.equals("0") ) { System.err.println("Could
+			 * not find " + equipName + " for " + t.getShortName() ); } //killme
+			 * remove this block after TAG and Active Probes are implemented *
+			 ******************************************************************/
+		}
+	}
 }

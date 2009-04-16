443feb8d45f0b03cc212ff0bdf9112587cf1af27
beerockxs <sebastian@brocks.cc>
2009-04-16 12:28:19 +0000

    + Bug 2765038: alertdialogs can possibly be larger than the screen

--- megamek/src/megamek/client/ui/AWT/widget/AdvancedLabel.java
+++ megamek/src/megamek/client/ui/AWT/widget/AdvancedLabel.java
@@ -1,14 +1,14 @@
 /*
  * MegaMek - Copyright (C) 2000,2001,2002,2004 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 
@@ -19,6 +19,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.ScrollPane;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
@@ -26,12 +27,12 @@ import java.util.Vector;
  * Author: Ryan McConnell (oscarmm)
  */
 
-public class AdvancedLabel extends Component {
+public class AdvancedLabel extends ScrollPane {
 
     /**
-     * 
+     *
      */
-    private static final long serialVersionUID = -8349184630103360938L;
+    private static final long serialVersionUID = -3424428063742960330L;
     private Vector<String> stringVector = new Vector<String>();
     private Color[] colorArray;
 
@@ -44,61 +45,92 @@ public class AdvancedLabel extends Component {
     private static final int leftMargin = 2;
     private static final int rightMargin = 2;
 
+    private ActualLabel label;
+
+    private class ActualLabel extends Component {
+        /**
+         *
+         */
+        private static final long serialVersionUID = 8474196817663156107L;
+
+        public ActualLabel(String text) {
+            colorArray = null;
+            setText(text);
+        }
+
+        public ActualLabel(String text, Color[] lineColors) {
+            colorArray = lineColors;
+            setText(text);
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            getSizes();
+            for (int i = 0; i < stringVector.size(); i++) {
+                if (colorArray != null) {
+                    g.setColor(colorArray[i]);
+                }
+                g.drawString(stringVector.elementAt(i), leftMargin, lineHeight
+                        * (i + 1));
+            }
+        }
+
+        public void setText(String text) {
+            stringVector.removeAllElements();
+            StringTokenizer st = new StringTokenizer(text, "\r\n"); //$NON-NLS-1$
+            while (st.hasMoreTokens()) {
+                stringVector.addElement(st.nextToken());
+            }
+        }
+
+        /**
+         * Sets important dimension properties for our label.
+         */
+        private void getSizes() {
+            FontMetrics fm = getFontMetrics(getFont());
+            lineHeight = fm.getHeight();
+            for (int i = 0; i < stringVector.size(); i++) {
+                maxLineWidth = Math.max(maxLineWidth, fm.stringWidth(stringVector
+                        .elementAt(i)));
+            }
+            // ascent = fm.getAscent(); //ascent is never used so I removed it.
+            descent = fm.getDescent();
+            sized = true;
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return getMinimumSize();
+        }
+
+        @Override
+        public Dimension getMinimumSize() {
+            if (!sized) {
+                getSizes();
+            }
+            int totalWidth = maxLineWidth + leftMargin + rightMargin;
+            int totalHeight = stringVector.size() * lineHeight
+                    + stringVector.size() * descent;
+            Dimension d = new Dimension(totalWidth, totalHeight);
+            return d;
+        }
+    }
+
     public AdvancedLabel(String text) {
-        colorArray = null;
-        setText(text);
+        super();
+        label = new ActualLabel(text);
+        setSize(600, 400);
+        add(label);
     }
 
     public AdvancedLabel(String text, Color[] lineColors) {
-        colorArray = lineColors;
-        setText(text);
-    }
-
-    public void paint(Graphics g) {
-        getSizes();
-        for (int i = 0; i < stringVector.size(); i++) {
-            if (colorArray != null)
-                g.setColor(colorArray[i]);
-            g.drawString(stringVector.elementAt(i), leftMargin, lineHeight
-                    * (i + 1));
-        }
+        super();
+        label = new ActualLabel(text, lineColors);
+        setSize(600, 400);
+        add(label);
     }
 
     public void setText(String text) {
-        stringVector.removeAllElements();
-        StringTokenizer st = new StringTokenizer(text, "\r\n"); //$NON-NLS-1$
-        while (st.hasMoreTokens()) {
-            stringVector.addElement(st.nextToken());
-        }
-    }
-
-    /*
-     * Sets important dimension properties for our label.
-     */
-    private void getSizes() {
-        FontMetrics fm = getFontMetrics(getFont());
-        lineHeight = fm.getHeight();
-        for (int i = 0; i < stringVector.size(); i++) {
-            maxLineWidth = Math.max(maxLineWidth, fm.stringWidth(stringVector
-                    .elementAt(i)));
-        }
-        // ascent = fm.getAscent(); //ascent is never used so I removed it.
-        descent = fm.getDescent();
-        sized = true;
-    }
-
-    public Dimension getPreferredSize() {
-        return getMinimumSize();
-    }
-
-    public Dimension getMinimumSize() {
-        if (!sized) {
-            getSizes();
-        }
-        int totalWidth = maxLineWidth + leftMargin + rightMargin;
-        int totalHeight = stringVector.size() * lineHeight
-                + stringVector.size() * descent;
-        Dimension d = new Dimension(totalWidth, totalHeight);
-        return d;
+        label.setText(text);
     }
 }

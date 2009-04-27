bf406c88bbf9fe3b6bded49ed76cf69c0fa0f976
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-04-27 20:17:04 +0000

    ready for 0.34.1

--- branches/rel-0-34-0/src/megamek/MegaMek.java
+++ branches/rel-0-34-0/src/megamek/MegaMek.java
@@ -48,7 +48,7 @@ import megamek.server.DedicatedServer;
  */
 public class MegaMek {
 
-    public static String VERSION = "0.34.0"; //$NON-NLS-1$
+    public static String VERSION = "0.34.1-dev"; //$NON-NLS-1$
     public static long TIMESTAMP = new File(PreferenceManager
             .getClientPreferences().getLogDirectory()
             + File.separator + "timestamp").lastModified(); //$NON-NLS-1$

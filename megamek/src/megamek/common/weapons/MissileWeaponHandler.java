af268278d5ec77cbb3e8e5dd943b34067fb1252c
beerockxs <sebastian@brocks.cc>
2009-04-28 19:27:11 +0000

    + Bug 2555773, Patch 2782679: Narc pods working for non-narc ammo

--- megamek/src/megamek/common/weapons/MissileWeaponHandler.java
+++ megamek/src/megamek/common/weapons/MissileWeaponHandler.java
@@ -205,11 +205,11 @@ public class MissileWeaponHandler extends AmmoWeaponHandler {
             bTargetECMAffected = Compute.isAffectedByECM(ae,
                     target.getPosition(), target.getPosition());
             if (((atype.getAmmoType() == AmmoType.T_LRM) ||
-                 (atype.getAmmoType() == AmmoType.T_SRM)) ||
-                 ((atype.getAmmoType() == AmmoType.T_MML)
+                 (atype.getAmmoType() == AmmoType.T_SRM) ||
+                 (atype.getAmmoType() == AmmoType.T_MML))
                     && (atype.getMunitionType() == AmmoType.M_NARC_CAPABLE)
                     && ((weapon.curMode() == null) || !weapon.curMode().equals(
-                            "Indirect")))) {
+                            "Indirect"))) {
                 if (bTargetECMAffected) {
                     // ECM prevents bonus
                     r = new Report(3330);
@@ -740,10 +740,10 @@ public class MissileWeaponHandler extends AmmoWeaponHandler {
                     && (((atype.getAmmoType() == AmmoType.T_ATM) && ((atype
                             .getMunitionType() == AmmoType.M_STANDARD)
                             || (atype.getMunitionType() == AmmoType.M_EXTENDED_RANGE) || (atype
-                            .getMunitionType() == AmmoType.M_HIGH_EXPLOSIVE))) || (((atype
+                            .getMunitionType() == AmmoType.M_HIGH_EXPLOSIVE))) || ((((atype
                             .getAmmoType() == AmmoType.T_LRM) || (atype
                             .getAmmoType() == AmmoType.T_SRM)) && (atype
-                            .getMunitionType() == AmmoType.M_ARTEMIS_CAPABLE) || (atype
+                            .getMunitionType() == AmmoType.M_ARTEMIS_CAPABLE)) || (atype
                             .getMunitionType() == AmmoType.M_ARTEMIS_V_CAPABLE)))) {
                 isNemesisConfusable = true;
             }

a9e8dd051daac729f9ae8def67ba99a66ad4fe97
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-05-21 16:09:03 +0000

    weapons not showing up in MML

--- trunk/megamek/src/megamek/common/weapons/ISPlasmaRifle.java
+++ trunk/megamek/src/megamek/common/weapons/ISPlasmaRifle.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004,2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -30,38 +30,39 @@ import megamek.server.Server;
 public class ISPlasmaRifle extends AmmoWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -7919371014161089388L;
 
     public ISPlasmaRifle() {
-        this.techLevel = TechConstants.T_IS_TW_NON_BOX;
-        this.name = "Plasma Rifle";
-        this.setInternalName("ISPlasmaRifle");
-        this.heat = 10;
-        this.damage = DAMAGE_VARIABLE;
-        this.rackSize = 1;
-        this.ammoType = AmmoType.T_PLASMA;
-        this.minimumRange = WEAPON_NA;
-        this.shortRange = 5;
-        this.mediumRange = 10;
-        this.longRange = 15;
-        this.extremeRange = 20;
-        this.tonnage = 6.0f;
-        this.criticals = 2;
-        this.flags |= F_PLASMA | F_DIRECT_FIRE;
-        this.bv = 210;
-        this.cost = 480000;
-        this.shortAV = 10;
-        this.medAV = 10;
-        this.maxRange = RANGE_MED;
-        this.atClass = CLASS_PLASMA;
+        techLevel = TechConstants.T_IS_TW_NON_BOX;
+        name = "Plasma Rifle";
+        setInternalName("ISPlasmaRifle");
+        heat = 10;
+        damage = DAMAGE_VARIABLE;
+        rackSize = 1;
+        ammoType = AmmoType.T_PLASMA;
+        minimumRange = WEAPON_NA;
+        shortRange = 5;
+        mediumRange = 10;
+        longRange = 15;
+        extremeRange = 20;
+        tonnage = 6.0f;
+        criticals = 2;
+        flags |= F_PLASMA | F_DIRECT_FIRE;
+        flags1 |= F_TANK_WEAPON | F_MECH_WEAPON | F_AERO_WEAPON;
+        bv = 210;
+        cost = 480000;
+        shortAV = 10;
+        medAV = 10;
+        maxRange = RANGE_MED;
+        atClass = CLASS_PLASMA;
 
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.Weapon#getCorrectHandler(megamek.common.ToHitData,
      *      megamek.common.actions.WeaponAttackAction, megamek.common.Game,
      *      megamek.server.Server)

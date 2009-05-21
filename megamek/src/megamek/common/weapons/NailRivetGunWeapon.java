a253b1fc322f983512b520fb3666467e2ea75c3b
beerockxs <sebastian@brocks.cc>
2009-05-21 16:04:17 +0000

    weapons not showing up in MML

--- megamek/src/megamek/common/weapons/NailRivetGunWeapon.java
+++ megamek/src/megamek/common/weapons/NailRivetGunWeapon.java
@@ -36,6 +36,7 @@ public abstract class NailRivetGunWeapon extends AmmoWeapon {
     public NailRivetGunWeapon() {
         super();
         ammoType = AmmoType.T_NAIL_RIVET_GUN;
+        flags |= F_BALLISTIC;
         flags1 |= F_MECH_WEAPON | F_AERO_WEAPON | F_TANK_WEAPON;
         heat = 0;
         damage = 0;

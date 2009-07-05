b47d6a79828faa350dd42692ee4f7843a586273a
beerockxs <sebastian@brocks.cc>
2009-07-05 17:39:54 +0000

    + Bug: Aeros on the ground map being hit by ground artillery + start of Artillery FLAK against Aero units

--- megamek/src/megamek/common/weapons/MicroBombHandler.java
+++ megamek/src/megamek/common/weapons/MicroBombHandler.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004,2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -35,7 +35,7 @@ import megamek.server.Server;
  */
 public class MicroBombHandler extends AmmoWeaponHandler {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -2995118961278208244L;
 
@@ -51,7 +51,7 @@ public class MicroBombHandler extends AmmoWeaponHandler {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.WeaponHandler#specialResolution(java.util.Vector,
      *      megamek.common.Entity, boolean)
      */
@@ -82,7 +82,7 @@ public class MicroBombHandler extends AmmoWeaponHandler {
         int ratedDamage = ba.getShootingStrength();
         server.artilleryDamageArea(coords, ae.getPosition(), (AmmoType) ammo
                 .getType(), subjectId, ae, ratedDamage * 2, ratedDamage, false,
-                0, vPhaseReport);
+                0, vPhaseReport, false);
         return true;
     }
 }

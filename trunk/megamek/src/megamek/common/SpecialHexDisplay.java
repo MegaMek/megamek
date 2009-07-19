85004a1849f3563de30fceec672f3af573f53664
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-07-19 21:19:57 +0000

    + Bug 2823891: Firing bug

--- trunk/megamek/src/megamek/common/SpecialHexDisplay.java
+++ trunk/megamek/src/megamek/common/SpecialHexDisplay.java
@@ -23,7 +23,7 @@ import java.io.Serializable;
 public class SpecialHexDisplay implements Serializable {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 27470795993329492L;
 
@@ -64,7 +64,7 @@ public class SpecialHexDisplay implements Serializable {
             }
         },
         PLAYER_NOTE         (null);
-    
+
         private transient Image defaultImage;
         private final String defaultImagePath;
 
@@ -86,11 +86,11 @@ public class SpecialHexDisplay implements Serializable {
         public void setDefaultImage(Image defaultImage) {
             this.defaultImage = defaultImage;
         }
-        
+
         public boolean drawBefore() {
             return true;
         }
-        
+
         public boolean drawAfter() {
             return false;
         }
@@ -101,16 +101,16 @@ public class SpecialHexDisplay implements Serializable {
     private int round;
 
     private String owner = null;
-    
+
     private boolean obscured = true;
-    
+
     public static int NO_ROUND = -99;
 
     @SuppressWarnings("unused")
     private SpecialHexDisplay() {
         //deserialization use only
     }
-    
+
     public SpecialHexDisplay(Type type) {
         this.type = type;
         round = NO_ROUND;
@@ -134,38 +134,38 @@ public class SpecialHexDisplay implements Serializable {
         this.round = round;
         this.owner = owner;
     }
-    
+
     public SpecialHexDisplay(Type type, int round, String owner, String info, boolean obscured) {
         this.type = type;
         this.info = info;
         this.round = round;
         this.owner = owner;
-        this.obscured = obscured; 
+        this.obscured = obscured;
     }
 
     public boolean thisRound(int testRound) {
-        if (NO_ROUND == this.round) {
+        if (NO_ROUND == round) {
             return true;
         }
-        return testRound == this.round;
+        return testRound == round;
     }
 
     /** Does this SpecialHexDisplayObjet concern a round in the future? */
     public boolean futureRound(int testRound) {
-        if(NO_ROUND == this.round) {
+        if(NO_ROUND == round) {
             return true;
         }
-        return testRound > this.round;
+        return testRound > round;
     }
-    
+
     /** Does this SpecialHexDisplayObjet concern a round in the past? */
     public boolean pastRound(int testRound) {
-        if(NO_ROUND == this.round) {
+        if(NO_ROUND == round) {
             return true;
         }
-        return testRound < this.round;
+        return testRound < round;
     }
-    
+
     public String getInfo() {
         return info;
     }
@@ -212,18 +212,17 @@ public class SpecialHexDisplay implements Serializable {
      * @return
      */
     public boolean drawNow(IGame.Phase phase, int curRound) {
-        boolean shouldDisplay = thisRound(curRound) || 
+        boolean shouldDisplay = thisRound(curRound) ||
             (pastRound(curRound) && type.drawBefore()) ||
             (futureRound(curRound) && type.drawAfter());
-        if(phase.isBefore(IGame.Phase.PHASE_OFFBOARD) && 
-                (type == Type.ARTILLERY_TARGET || type == Type.ARTILLERY_HIT)
-        ) {
+        if(phase.isBefore(IGame.Phase.PHASE_OFFBOARD) &&
+                ((type == Type.ARTILLERY_TARGET) || (type == Type.ARTILLERY_HIT))) {
             //hack to display atry targets the round after the hit.
             shouldDisplay = shouldDisplay || thisRound(curRound-1);
         }
-        
+
         //System.err.println("turn: " + round + " Special type: " + type + " drawing: " + shouldDisplay + " details: " + info);
-            
+
         return shouldDisplay;
     }
 
@@ -232,10 +231,10 @@ public class SpecialHexDisplay implements Serializable {
      * @return
      */
     public boolean isOwner(String toPlayer) {
-        if(owner == null || owner.equals(toPlayer)) {
+        if((owner == null) || owner.equals(toPlayer)) {
             return true;
         }
-        
+
         return false;
     }
 }

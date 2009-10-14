c089ea73356449a59dbd2748b35dbe3f0f762ea7
beerockxs <sebastian@brocks.cc>
2009-10-14 14:29:00 +0000

    + Fix potential report filtering issue

--- megamek/src/megamek/server/Server.java
+++ megamek/src/megamek/server/Server.java
@@ -20860,6 +20860,9 @@ public class Server implements Runnable {
             return r;
         }
         Entity entity = game.getEntity(r.subject);
+        if (entity == null) {
+            entity = game.getOutOfGameEntity(r.subject);
+        }
         Player owner = null;
         if (entity != null) {
             owner = entity.getOwner();

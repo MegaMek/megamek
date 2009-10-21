d665930a9f90c2dd14b0b23799200fdfb064128b
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2009-10-21 12:03:39 +0000

    name PPC PPC, not Partice Cannon

--- trunk/megamek/src/megamek/common/weapons/ISPPC.java
+++ trunk/megamek/src/megamek/common/weapons/ISPPC.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004,2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -28,45 +28,45 @@ import megamek.server.Server;
  */
 public class ISPPC extends PPCWeapon {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 5775665622863346537L;
 
     /**
-     * 
+     *
      */
     public ISPPC() {
         super();
-        this.techLevel = TechConstants.T_INTRO_BOXSET;
-        this.name = "Particle Cannon";
-        this.setInternalName(this.name);
-        this.addLookupName("IS PPC");
-        this.addLookupName("ISPPC");
-        this.heat = 10;
-        this.damage = 10;
-        this.minimumRange = 3;
-        this.shortRange = 6;
-        this.mediumRange = 12;
-        this.longRange = 18;
-        this.extremeRange = 24;
-        this.waterShortRange = 4;
-        this.waterMediumRange = 7;
-        this.waterLongRange = 10;
-        this.waterExtremeRange = 14;
-        this.setModes(new String[] { "Field Inhibitor ON",
+        techLevel = TechConstants.T_INTRO_BOXSET;
+        name = "PPC";
+        setInternalName(name);
+        addLookupName("IS PPC");
+        addLookupName("ISPPC");
+        heat = 10;
+        damage = 10;
+        minimumRange = 3;
+        shortRange = 6;
+        mediumRange = 12;
+        longRange = 18;
+        extremeRange = 24;
+        waterShortRange = 4;
+        waterMediumRange = 7;
+        waterLongRange = 10;
+        waterExtremeRange = 14;
+        setModes(new String[] { "Field Inhibitor ON",
                 "Field Inhibitor OFF" });
-        this.tonnage = 7.0f;
-        this.criticals = 3;
-        this.bv = 176;
-        this.cost = 200000;
-        this.shortAV = 10;
-        this.medAV = 10;
-        this.maxRange = RANGE_MED;
+        tonnage = 7.0f;
+        criticals = 3;
+        bv = 176;
+        cost = 200000;
+        shortAV = 10;
+        medAV = 10;
+        maxRange = RANGE_MED;
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.Weapon#getCorrectHandler(megamek.common.ToHitData,
      *      megamek.common.actions.WeaponAttackAction, megamek.common.Game,
      *      megamek.server.Server)

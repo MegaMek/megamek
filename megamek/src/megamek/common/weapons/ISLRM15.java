94c034bcb86c5f82beb85b2250b549eb8bbe74be
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2010-01-09 01:36:34 +0000

    + Bug 2928258: LRM 15 not loading from TDB .xml files

--- megamek/src/megamek/common/weapons/ISLRM15.java
+++ megamek/src/megamek/common/weapons/ISLRM15.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common.weapons;
@@ -21,35 +21,35 @@ import megamek.common.TechConstants;
 public class ISLRM15 extends LRMWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 603060073432118270L;
 
     /**
-     * 
+     *
      */
     public ISLRM15() {
         super();
-        this.techLevel = TechConstants.T_INTRO_BOXSET;
-        this.name = "LRM 15";
-        this.setInternalName(this.name);
-        this.addLookupName("IS LRM-15");
-        this.addLookupName("ISLRM15");
-        this.addLookupName("IS LRM 5");
-        this.heat = 5;
-        this.rackSize = 15;
-        this.minimumRange = 6;
-        this.shortRange = 7;
-        this.mediumRange = 14;
-        this.longRange = 21;
-        this.extremeRange = 28;
-        this.tonnage = 7.0f;
-        this.criticals = 3;
-        this.bv = 136;
-        this.cost = 175000;
-        this.shortAV = 9;
-        this.medAV = 9;
-        this.longAV = 9;
-        this.maxRange = RANGE_LONG;
+        techLevel = TechConstants.T_INTRO_BOXSET;
+        name = "LRM 15";
+        setInternalName(name);
+        addLookupName("IS LRM-15");
+        addLookupName("ISLRM15");
+        addLookupName("IS LRM 15");
+        heat = 5;
+        rackSize = 15;
+        minimumRange = 6;
+        shortRange = 7;
+        mediumRange = 14;
+        longRange = 21;
+        extremeRange = 28;
+        tonnage = 7.0f;
+        criticals = 3;
+        bv = 136;
+        cost = 175000;
+        shortAV = 9;
+        medAV = 9;
+        longAV = 9;
+        maxRange = RANGE_LONG;
     }
 }

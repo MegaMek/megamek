3935a56efbde76784e18e9c164bbb6b2508e441a
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2010-01-13 15:50:54 +0000

    + Bug 2930686: Plasma rifle vs. fire-resistant armor

--- branches/rel-0-34-0/src/megamek/common/weapons/PlasmaRifleHandler.java
+++ branches/rel-0-34-0/src/megamek/common/weapons/PlasmaRifleHandler.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common.weapons;
@@ -32,7 +32,7 @@ import megamek.server.Server;
 
 public class PlasmaRifleHandler extends AmmoWeaponHandler {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -2092721653693187140L;
 
@@ -45,21 +45,22 @@ public class PlasmaRifleHandler extends AmmoWeaponHandler {
             Server s) {
         super(toHit, waa, g, s);
         generalDamageType = HitData.DAMAGE_ENERGY;
-        
+
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.WeaponHandler#handleEntityDamage(megamek.common.Entity,
      *      java.util.Vector, megamek.common.Building, int, int, int, int)
      */
+    @Override
     protected void handleEntityDamage(Entity entityTarget,
             Vector<Report> vPhaseReport, Building bldg, int hits, int nCluster,
             int nDamPerHit, int bldgAbsorbs) {
         super.handleEntityDamage(entityTarget, vPhaseReport, bldg, hits,
                 nCluster, nDamPerHit, bldgAbsorbs);
-        if (!missed && entityTarget instanceof Mech) {
+        if (!missed && (entityTarget instanceof Mech)) {
             r = new Report(3400);
             r.subject = subjectId;
             r.indent(2);
@@ -74,15 +75,17 @@ public class PlasmaRifleHandler extends AmmoWeaponHandler {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.WeaponHandler#calcDamagePerHit()
      */
+    @Override
     protected int calcDamagePerHit() {
         if (target instanceof Mech) {
             int toReturn = 10;
-            if (bGlancing)
+            if (bGlancing) {
                 toReturn = (int) Math.floor(toReturn / 2.0);
-            if ( game.getOptions().booleanOption("tacops_range") && nRange > wtype.getRanges(weapon)[RangeType.RANGE_LONG] ) {
+            }
+            if ( game.getOptions().booleanOption("tacops_range") && (nRange > wtype.getRanges(weapon)[RangeType.RANGE_LONG]) ) {
                 toReturn = (int) Math.floor(toReturn / 2.0);
                 toReturn -= 1;
             }
@@ -94,20 +97,22 @@ public class PlasmaRifleHandler extends AmmoWeaponHandler {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.WeaponHandler#calcnCluster()
      */
+    @Override
     protected int calcnCluster() {
         if (target instanceof Mech) {
             bSalvo = false;
             return 1;
         } else {
             int toReturn = 5;
-            if (target instanceof Infantry && !(target instanceof BattleArmor))
+            if ((target instanceof Infantry) && !(target instanceof BattleArmor)) {
                 toReturn = Compute.d6(2);
+            }
             bSalvo = true;
             // pain shunted infantry get half damage
-            if (target instanceof Infantry
+            if ((target instanceof Infantry)
                     && ((Entity) target).getCrew().getOptions().booleanOption(
                             "pain_shunt")) {
                 toReturn = Math.max(toReturn / 2, 1);
@@ -118,9 +123,10 @@ public class PlasmaRifleHandler extends AmmoWeaponHandler {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.WeaponHandler#calcHits(java.util.Vector)
      */
+    @Override
     protected int calcHits(Vector<Report> vPhaseReport) {
         int toReturn;
         // against mechs, 1 hit with 10 damage, plus heat
@@ -130,44 +136,49 @@ public class PlasmaRifleHandler extends AmmoWeaponHandler {
             // but fireresistant BA armor gets no damage from heat, and half the
             // normal one, so only 5 damage
         } else {
-            if (target instanceof BattleArmor
-                    && ((BattleArmor) target).isFireResistant())
-                toReturn = 10 / 2;
-            toReturn = 10 + Compute.d6(2);
-            if (bGlancing)
+            if ((target instanceof BattleArmor)
+                    && ((BattleArmor) target).isFireResistant()) {
+                toReturn = 5;
+            } else {
+                toReturn = 10 + Compute.d6(2);
+            }
+            if (bGlancing) {
                 toReturn = (int) Math.floor(toReturn / 2.0);
+            }
         }
         return toReturn;
     }
-    
+
 
     /**
      * @return a <code>boolean</code> value indicating wether or not this
      *         attack needs further calculating, like a missed shot hitting a
      *         building, or an AMS only shooting down some missiles.
      */
+    @Override
     protected boolean handleSpecialMiss(Entity entityTarget,
             boolean targetInBuilding, Building bldg, Vector<Report> vPhaseReport) {
         // Shots that miss an entity can set fires.
         // Buildings can't be accidentally ignited,
         // and some weapons can't ignite fires.
-        if (entityTarget != null
-                && (bldg == null && wtype.getFireTN() != TargetRoll.IMPOSSIBLE)) {
+        if ((entityTarget != null)
+                && ((bldg == null) && (wtype.getFireTN() != TargetRoll.IMPOSSIBLE))) {
             server.tryIgniteHex(target.getPosition(), subjectId, true, false, new TargetRoll(wtype.getFireTN(), wtype.getName()),
                     3, vPhaseReport);
         }
-        
+
         //shots that miss an entity can also potential cause explosions in a heavy industrial hex
         server.checkExplodeIndustrialZone(target.getPosition(), vPhaseReport);
 
         // BMRr, pg. 51: "All shots that were aimed at a target inside
         // a building and miss do full damage to the building instead."
-        if (!targetInBuilding || toHit.getValue() == TargetRoll.AUTOMATIC_FAIL) {
+        if (!targetInBuilding || (toHit.getValue() == TargetRoll.AUTOMATIC_FAIL)) {
             return false;
         }
         return true;
     }
-    
+
+    @Override
     protected void handleIgnitionDamage(Vector<Report> vPhaseReport,
             Building bldg, boolean bSalvo, int hits) {
         if (!bSalvo) {
@@ -184,7 +195,8 @@ public class PlasmaRifleHandler extends AmmoWeaponHandler {
                     true, -1, vPhaseReport);
         }
     }
-    
+
+    @Override
     protected void handleClearDamage(Vector<Report> vPhaseReport,
             Building bldg, int nDamage, boolean bSalvo) {
         if (!bSalvo) {
@@ -206,7 +218,7 @@ public class PlasmaRifleHandler extends AmmoWeaponHandler {
         // Buildings can't be accidentally ignited.
         //TODO: change this for TacOps - now you roll another 2d6 first and on a 5 or less
         //you do a normal ignition as though for intentional fires
-        if (bldg != null
+        if ((bldg != null)
                 && server.tryIgniteHex(target.getPosition(), subjectId, true,false,
                         new TargetRoll(wtype.getFireTN(), wtype.getName()), 5, vPhaseReport)) {
             return;

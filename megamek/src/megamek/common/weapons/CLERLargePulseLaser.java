5dcb1d28372817e5f98d4e2be14a0a137a9d6c24
beerockxs <sebastian@brocks.cc>
2010-02-16 21:36:15 +0000

    + Bug: Clan ER Medium Pulse and Clan ER Large Pulse had wrong BV + Bug: Thunderbolt 15 ammo has wrong BV + Bug: Fluid gun ammo considered explosive + Bug: Reinforced Internal Structure BV modifier missing + Bug: PPC capacitor BV not quite correct + Bug: M-Pods should subtract defensive BV for being explosive

--- megamek/src/megamek/common/weapons/CLERLargePulseLaser.java
+++ megamek/src/megamek/common/weapons/CLERLargePulseLaser.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -24,34 +24,34 @@ import megamek.common.TechConstants;
  */
 public class CLERLargePulseLaser extends PulseLaserWeapon {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -5795252987498124086L;
 
     /**
-     * 
+     *
      */
     public CLERLargePulseLaser() {
         super();
-        this.techLevel = TechConstants.T_CLAN_EXPERIMENTAL;
-        this.name = "ER Large Pulse Laser";
-        this.setInternalName("CLERLargePulseLaser");
-        this.addLookupName("Clan ER Pulse Large Laser");
-        this.addLookupName("Clan ER Large Pulse Laser");
-        this.heat = 13;
-        this.damage = 10;
-        this.toHitModifier = -1;
-        this.shortRange = 7;
-        this.mediumRange = 15;
-        this.longRange = 23;
-        this.extremeRange = 30;
-        this.waterShortRange = 4;
-        this.waterMediumRange = 10;
-        this.waterLongRange = 14;
-        this.waterExtremeRange = 20;
-        this.tonnage = 6.0f;
-        this.criticals = 3;
-        this.bv = 271;
-        this.cost = 400000;
+        techLevel = TechConstants.T_CLAN_EXPERIMENTAL;
+        name = "ER Large Pulse Laser";
+        setInternalName("CLERLargePulseLaser");
+        addLookupName("Clan ER Pulse Large Laser");
+        addLookupName("Clan ER Large Pulse Laser");
+        heat = 13;
+        damage = 10;
+        toHitModifier = -1;
+        shortRange = 7;
+        mediumRange = 15;
+        longRange = 23;
+        extremeRange = 30;
+        waterShortRange = 4;
+        waterMediumRange = 10;
+        waterLongRange = 14;
+        waterExtremeRange = 20;
+        tonnage = 6.0f;
+        criticals = 3;
+        bv = 272;
+        cost = 400000;
     }
 }

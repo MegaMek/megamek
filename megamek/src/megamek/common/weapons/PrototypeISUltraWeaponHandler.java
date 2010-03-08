75582ad2439f8daf07c1ed8a1ee061a9a685c976
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2010-03-08 21:04:55 +0000

    klondike prototype weapons HAG aimed shots

--- megamek/src/megamek/common/weapons/PrototypeISUltraWeaponHandler.java
+++ megamek/src/megamek/common/weapons/PrototypeISUltraWeaponHandler.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -29,10 +29,10 @@ import megamek.server.Server;
 /**
  * @author Andrew Hunter
  */
-public class PrototypeUltraWeaponHandler extends UltraWeaponHandler {
+public class PrototypeISUltraWeaponHandler extends UltraWeaponHandler {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 6441106275439235564L;
 
@@ -41,19 +41,19 @@ public class PrototypeUltraWeaponHandler extends UltraWeaponHandler {
      * @param w
      * @param g
      */
-    public PrototypeUltraWeaponHandler(ToHitData t, WeaponAttackAction w,
+    public PrototypeISUltraWeaponHandler(ToHitData t, WeaponAttackAction w,
             IGame g, Server s) {
         super(t, w, g, s);
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.UltraWeaponHandler#doChecks(java.util.Vector)
      */
     @Override
     protected boolean doChecks(Vector<Report> vPhaseReport) {
-        if (roll <= 4 && howManyShots == 2) {
+        if (((roll <= 4) && (howManyShots == 2)) || ((roll == 2) && (howManyShots == 1))) {
             Report r = new Report();
             r.subject = subjectId;
             weapon.setJammed(true);

0edb01e04f4397a5429ad10f7dea2ae08a6fb26b
beerockxs <sebastian@brocks.cc>
2010-05-12 16:45:35 +0000

    + Bug: Extended and Enhanced LRMs had wrong BV

--- megamek/src/megamek/common/weapons/ISExtendedLRM5.java
+++ megamek/src/megamek/common/weapons/ISExtendedLRM5.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common.weapons;
@@ -21,32 +21,32 @@ import megamek.common.TechConstants;
 public class ISExtendedLRM5 extends ExtendedLRMWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -6153832907941260136L;
 
     /**
-     * 
+     *
      */
     public ISExtendedLRM5() {
         super();
-        this.techLevel = TechConstants.T_IS_EXPERIMENTAL;
-        this.name = "ExtendedLRM 5";
-        this.setInternalName(this.name);
-        this.addLookupName("IS ExtendedLRM-5");
-        this.addLookupName("ISExtendedLRM5");
-        this.addLookupName("IS ExtendedLRM 5");
-        this.addLookupName("ELRM-5 (THB)");
-        this.heat = 3;
-        this.rackSize = 5;
-        this.minimumRange = 10;
-        this.shortRange = 12;
-        this.mediumRange = 22;
-        this.longRange = 38;
-        this.extremeRange = 44;
-        this.tonnage = 6.0f;
-        this.criticals = 1;
-        this.bv = 59;
-        this.cost = 110000;
+        techLevel = TechConstants.T_IS_EXPERIMENTAL;
+        name = "ExtendedLRM 5";
+        setInternalName(name);
+        addLookupName("IS ExtendedLRM-5");
+        addLookupName("ISExtendedLRM5");
+        addLookupName("IS ExtendedLRM 5");
+        addLookupName("ELRM-5 (THB)");
+        heat = 3;
+        rackSize = 5;
+        minimumRange = 10;
+        shortRange = 12;
+        mediumRange = 22;
+        longRange = 38;
+        extremeRange = 44;
+        tonnage = 6.0f;
+        criticals = 1;
+        bv = 367;
+        cost = 110000;
     }
 }

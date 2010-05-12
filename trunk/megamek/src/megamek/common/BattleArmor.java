02dd5fa96c63ccd609d1d2d8571b21c7a07959b1
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2010-05-12 17:11:08 +0000

    + Bug: BA magclamp BV

--- trunk/megamek/src/megamek/common/BattleArmor.java
+++ trunk/megamek/src/megamek/common/BattleArmor.java
@@ -824,7 +824,8 @@ public class BattleArmor extends Infantry {
                 for (Mounted misc : getMisc()) {
                     if ((misc.getLocation() == LOC_SQUAD) || (misc.getLocation() == i)) {
                         if (misc.getType().hasFlag(MiscType.F_MAGNET_CLAW)
-                                || misc.getType().hasFlag(MiscType.F_VIBROCLAW)) {
+                                || misc.getType().hasFlag(MiscType.F_VIBROCLAW)
+                                || misc.getType().hasFlag(MiscType.F_MAGNETIC_CLAMP)) {
                             oBV += misc.getType().getBV(this);
                         }
                     }

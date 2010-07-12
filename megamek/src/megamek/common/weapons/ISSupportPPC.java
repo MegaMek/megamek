4f6c66d12f21755392447c79f9657d765816ac40
beerockxs <sebastian@brocks.cc>
2010-07-12 13:18:12 +0000

    + Added some missing costs

--- megamek/src/megamek/common/weapons/ISSupportPPC.java
+++ megamek/src/megamek/common/weapons/ISSupportPPC.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004,2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -25,27 +25,28 @@ import megamek.common.TechConstants;
  */
 public class ISSupportPPC extends PPCWeapon {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -993141316216102914L;
 
     /**
-     * 
+     *
      */
     public ISSupportPPC() {
         super();
-        this.techLevel = TechConstants.T_IS_TW_NON_BOX;
-        this.name = "Support PPC";
-        this.setInternalName(this.name);
-        this.addLookupName("ISSupportPPC");
-        this.damage = 2;
-        this.ammoType = AmmoType.T_NA;
-        this.shortRange = 2;
-        this.mediumRange = 5;
-        this.longRange = 7;
-        this.extremeRange = 10;
-        this.bv = 14;
-        this.setModes(new String[] { "Field Inhibitor ON",
+        techLevel = TechConstants.T_IS_TW_NON_BOX;
+        name = "Support PPC";
+        setInternalName(name);
+        addLookupName("ISSupportPPC");
+        damage = 2;
+        ammoType = AmmoType.T_NA;
+        shortRange = 2;
+        mediumRange = 5;
+        longRange = 7;
+        extremeRange = 10;
+        bv = 14;
+        setModes(new String[] { "Field Inhibitor ON",
                 "Field Inhibitor OFF" });
+        cost = 14000;
     }
 }

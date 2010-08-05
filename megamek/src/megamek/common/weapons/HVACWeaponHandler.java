434d4deb3d187d320fa32fc3837bdedf6e6a2ea9
beerockxs <sebastian@brocks.cc>
2010-08-05 17:51:13 +0000

    + Bug 3028714: rac against infantry damage wrong + Bug 3011684: non-explosive tank weapons explode on weapon destroyed crit + Bug 2891636: RAC shouldn't be explosive the turn it jammed

--- megamek/src/megamek/common/weapons/HVACWeaponHandler.java
+++ megamek/src/megamek/common/weapons/HVACWeaponHandler.java
@@ -89,7 +89,7 @@ public class HVACWeaponHandler extends ACWeaponHandler {
         if (roll == 2) {
             Report r = new Report(3162);
             r.subject = subjectId;
-            weapon.setJammed(false);
+            weapon.setJammed(true);
             weapon.setHit(true);
             int wlocation = weapon.getLocation();
             for (int i = 0; i < ae.getNumberOfCriticals(wlocation); i++) {

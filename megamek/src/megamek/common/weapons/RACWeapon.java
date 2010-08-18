1766e329c6df8ad1630bc71f58603f51129e5d08
beerockxs <sebastian@brocks.cc>
2010-08-18 23:41:07 +0000

    + Bug: RAC at 1-shot mode not doing enough damage to infantry

--- megamek/src/megamek/common/weapons/RACWeapon.java
+++ megamek/src/megamek/common/weapons/RACWeapon.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2004 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -29,28 +29,28 @@ import megamek.server.Server;
  */
 public abstract class RACWeapon extends UACWeapon {
 
-    
+
     private static final long serialVersionUID = 659000035767322660L;
 
     /**
-     * 
+     *
      */
     public RACWeapon() {
         super();
-        this.ammoType = AmmoType.T_AC_ROTARY;
+        ammoType = AmmoType.T_AC_ROTARY;
         String[] modeStrings = { "Single", "2-shot", "3-shot", "4-shot", "5-shot",
                 "6-shot" };
-        this.setModes(modeStrings);
+        setModes(modeStrings);
         // explosive when jammed
-        this.explosive = true;
-        this.explosionDamage = damage;
-        this.techRating = RATING_E;
-        this.atClass = CLASS_AC;
+        explosive = true;
+        explosionDamage = damage;
+        techRating = RATING_E;
+        atClass = CLASS_AC;
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.Weapon#getCorrectHandler(megamek.common.ToHitData,
      *      megamek.common.actions.WeaponAttackAction, megamek.common.Game,
      *      megamek.server.Server)
@@ -65,10 +65,8 @@ public abstract class RACWeapon extends UACWeapon {
                 || weapon.curMode().equals("4-shot")
                 || weapon.curMode().equals("3-shot")) {
             return new RACHandler(toHit, waa, game, server);
-        } else if (weapon.curMode().equals("2-shot")) {
-            return new UltraWeaponHandler(toHit, waa, game, server);
         } else {
-            return new ACWeaponHandler(toHit, waa, game, server);
+            return new UltraWeaponHandler(toHit, waa, game, server);
         }
     }
 }

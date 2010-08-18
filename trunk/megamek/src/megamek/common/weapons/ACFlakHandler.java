0514e24aa241bc921ad0e4bba9de5c3aa938727c
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2010-08-18 05:14:23 +0000

    + Bug: FLAK ammo halving damage against VTOLs + Bug: FLAK ammo has wrong techlevel

--- trunk/megamek/src/megamek/common/weapons/ACFlakHandler.java
+++ trunk/megamek/src/megamek/common/weapons/ACFlakHandler.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common.weapons;
@@ -19,7 +19,6 @@ import megamek.common.IGame;
 import megamek.common.Infantry;
 import megamek.common.RangeType;
 import megamek.common.ToHitData;
-import megamek.common.VTOL;
 import megamek.common.actions.WeaponAttackAction;
 import megamek.server.Server;
 
@@ -29,7 +28,7 @@ import megamek.server.Server;
 public class ACFlakHandler extends AmmoWeaponHandler {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -7814754695629391969L;
 
@@ -45,18 +44,17 @@ public class ACFlakHandler extends AmmoWeaponHandler {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.WeaponHandler#calcDamagePerHit()
      */
     @Override
     protected int calcDamagePerHit() {
         double toReturn = wtype.getDamage();
-        
-        if ( ((target instanceof VTOL) && !(target instanceof Aero) && !(target instanceof Infantry)) 
-                || target instanceof BattleArmor ) {
+
+        if ( !(target.isAirborneVTOL()) && !(target instanceof Aero && target.isAirborne()) && !(target instanceof Infantry && !(target instanceof BattleArmor))) {
             toReturn /= 2;
         }
-        
+
         // during a swarm, all damage gets applied as one block to one
         // location
         if (ae instanceof BattleArmor && weapon.getLocation() == BattleArmor.LOC_SQUAD && (ae.getSwarmTargetId() == target.getTargetId())) {
@@ -66,12 +64,12 @@ public class ACFlakHandler extends AmmoWeaponHandler {
         if (bGlancing) {
             toReturn = (int) Math.floor(toReturn / 2.0);
         }
-        
+
         if (game.getOptions().booleanOption("tacops_range") && nRange > wtype.getRanges(weapon)[RangeType.RANGE_LONG]) {
             toReturn = (int) Math.floor(toReturn * .75);
         }
 
         return (int) toReturn;
     }
-    
+
 }

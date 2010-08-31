6902e573eeb8d8c586a7e2549c98ae58c6b628d7
torren <torren@users.sourceforge.net>
2010-08-31 04:28:32 +0000

    + RFE: added Light/Medium/Heavy Rifles for IS Mechs/Tanks/Aeros

---
+++ megamek/src/megamek/common/weapons/RifleWeaponHandler.java
@@ -0,0 +1,74 @@
+/**
+ * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
+ * 
+ *  This program is free software; you can redistribute it and/or modify it 
+ *  under the terms of the GNU General Public License as published by the Free 
+ *  Software Foundation; either version 2 of the License, or (at your option) 
+ *  any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *  for more details.
+ */
+package megamek.common.weapons;
+
+import megamek.common.BattleArmor;
+import megamek.common.Compute;
+import megamek.common.IGame;
+import megamek.common.Infantry;
+import megamek.common.RangeType;
+import megamek.common.ToHitData;
+import megamek.common.actions.WeaponAttackAction;
+import megamek.server.Server;
+
+/**
+ * @author Jason Tighe
+ */
+public class RifleWeaponHandler extends AmmoWeaponHandler {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 7468287406174862534L;
+
+    /**
+     * @param t
+     * @param w
+     * @param g
+     * @param s
+     */
+    public RifleWeaponHandler(ToHitData t, WeaponAttackAction w, IGame g, Server s) {
+        super(t, w, g, s);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see megamek.common.weapons.WeaponHandler#calcDamagePerHit()
+     */
+    @Override
+    protected int calcDamagePerHit() {
+        double toReturn = wtype.getDamage();
+        // we default to direct fire weapons for anti-infantry damage
+        if ((target instanceof Infantry) && !(target instanceof BattleArmor)) {
+            toReturn = Compute.directBlowInfantryDamage(toReturn, bDirect ? toHit.getMoS() : 0, Compute.WEAPON_DIRECT_FIRE, ((Infantry) target).isMechanized());
+        } else if (bDirect) {
+            toReturn = Math.min(toReturn + (toHit.getMoS() / 3), toReturn * 2);
+        }
+
+        if (!(ae instanceof BattleArmor) && (!ae.hasBARArmor() || (ae.getBARRating() >= 8))) {
+            toReturn = Math.min(0, toReturn - 3);
+        }
+
+        if (bGlancing) {
+            toReturn = (int) Math.floor(toReturn / 2.0);
+        }
+
+        if (game.getOptions().booleanOption("tacops_range") && (nRange > wtype.getRanges(weapon)[RangeType.RANGE_LONG])) {
+            toReturn = (int) Math.floor(toReturn * .75);
+        }
+
+        return (int) toReturn;
+    }
+}

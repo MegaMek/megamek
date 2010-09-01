765c71282d15f13757c68c794101468b2694e358
jasonsmyr <jasonsmyr@users.sourceforge.net>
2010-09-01 14:45:57 +0000

    Refactored RandomSkillDialog to display cleaner

--- megamek/src/megamek/client/ui/swing/RandomSkillDialog.java
+++ megamek/src/megamek/client/ui/swing/RandomSkillDialog.java
@@ -1,14 +1,14 @@
 /*
  * MegaMek - Copyright (C) 2002, 2003 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 
@@ -16,28 +16,17 @@
  * The random skill dialog allows the player to randomly assign skills to pilots based on overall experience level.
  *
  * @author  Ben
+ * @author  JasonSmyr
  */
 package megamek.client.ui.swing;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.Enumeration;
 import java.util.Iterator;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
 
 import megamek.client.Client;
 import megamek.client.ui.Messages;
@@ -49,55 +38,40 @@ import megamek.common.VTOL;
 public class RandomSkillDialog extends JDialog implements ActionListener,
         ItemListener {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = -2459992981678758743L;
     private Client client;
     private ClientGUI clientgui;
 
-    private JPanel panButtons = new JPanel();
-    private JButton butOkay = new JButton(Messages.getString("Okay")); //$NON-NLS-1$
-    private JButton butCancel = new JButton(Messages.getString("Cancel")); //$NON-NLS-1$
-
-    private JLabel labelMethod = new JLabel(Messages
-            .getString("RandomSkillDialog.labelMethod"), SwingConstants.RIGHT); //$NON-NLS-1$
-    private JComboBox chMethod = new JComboBox();
-    private JLabel labelType = new JLabel(Messages
-            .getString("RandomSkillDialog.labelType"), SwingConstants.RIGHT); //$NON-NLS-1$
-    private JComboBox chType = new JComboBox();
-    private JLabel labelLevel = new JLabel(Messages
-            .getString("RandomSkillDialog.labelLevel"), SwingConstants.RIGHT); //$NON-NLS-1$
-    private JComboBox chLevel = new JComboBox();
-
-    private JLabel labelPlayer = new JLabel(Messages
-            .getString("MechSelectorDialog.m_labelPlayer"), SwingConstants.RIGHT); //$NON-NLS-1$
-    private JComboBox chPlayer = new JComboBox();
-
-    private JTextArea texDesc = new JTextArea(Messages
-            .getString("CustomMechDialog.texDesc"), 3, 35); //$NON-NLS-1$
-
-    private JCheckBox cForceClose = new JCheckBox(Messages
-            .getString("RandomSkillDialog.cForceClose"));
-
-    /** Creates a new instance of StartingPositionDialog */
+    /** Creates new form RandomSkillDialog2 */
     public RandomSkillDialog(ClientGUI clientgui) {
-        super(clientgui.frame,
-                Messages.getString("RandomSkillDialog.title"), true); //$NON-NLS-1$
+        super(clientgui.frame, Messages.getString("RandomSkillDialog.title"), true); //$NON-NLS-1$
+        initComponents();
+
         this.client = clientgui.getClient();
         this.clientgui = clientgui;
 
         updatePlayerChoice();
 
+
+        butOkay.setText(Messages.getString("Okay")); //$NON-NLS-1$
+        butCancel.setText(Messages.getString("Cancel")); //$NON-NLS-1$
+        labelMethod.setText(Messages.getString("RandomSkillDialog.labelMethod")); //$NON-NLS-1$
+        labelType.setText(Messages.getString("RandomSkillDialog.labelType")); //$NON-NLS-1$
+        labelLevel.setText(Messages.getString("RandomSkillDialog.labelLevel")); //$NON-NLS-1$
+        labelPlayer.setText(Messages.getString("MechSelectorDialog.m_labelPlayer")); //$NON-NLS-1$
+        texDesc.setText(Messages.getString("CustomMechDialog.texDesc")); //$NON-NLS-1$
+        cForceClose.setText(Messages.getString("RandomSkillDialog.cForceClose"));
+
+
         cForceClose.setSelected(false);
 
         texDesc.setLineWrap(true);
+
         texDesc.setEnabled(true);
 
         chMethod.addItem(Messages.getString("RandomSkillDialog.MethodTW")); //$NON-NLS-1$
         chMethod.addItem(Messages.getString("RandomSkillDialog.MethodTaharqa")); //$NON-NLS-1$
-        chMethod
-                .addItem(Messages.getString("RandomSkillDialog.MethodConstant")); //$NON-NLS-1$
+        chMethod.addItem(Messages.getString("RandomSkillDialog.MethodConstant")); //$NON-NLS-1$
 
         chMethod.setSelectedIndex(Compute.METHOD_TW);
         chMethod.addItemListener(this);
@@ -116,105 +90,9 @@ public class RandomSkillDialog extends JDialog implements ActionListener,
 
         chLevel.setSelectedIndex(Compute.LEVEL_REGULAR);
 
-        setupButtons();
-
-        // layout
-        GridBagLayout gridbag = new GridBagLayout();
-        GridBagConstraints c = new GridBagConstraints();
-        this.setLayout(gridbag);
-
-        c.fill = GridBagConstraints.BOTH;
-        c.insets = new Insets(4, 4, 4, 4);
-        c.weightx = 1.0;
-        c.weighty = 1.0;
-        c.gridwidth = 1;
-        c.anchor = GridBagConstraints.EAST;
-        gridbag.setConstraints(labelMethod, c);
-        this.add(labelMethod);
-
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.anchor = GridBagConstraints.WEST;
-        gridbag.setConstraints(chMethod, c);
-        this.add(chMethod);
-
-        c.gridwidth = 1;
-        c.anchor = GridBagConstraints.EAST;
-        gridbag.setConstraints(labelType, c);
-        this.add(labelType);
-
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.anchor = GridBagConstraints.WEST;
-        gridbag.setConstraints(chType, c);
-        this.add(chType);
-
-        c.gridwidth = 1;
-        c.anchor = GridBagConstraints.EAST;
-        gridbag.setConstraints(labelLevel, c);
-        this.add(labelLevel);
-
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.anchor = GridBagConstraints.WEST;
-        gridbag.setConstraints(chLevel, c);
-        this.add(chLevel);
-
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.anchor = GridBagConstraints.CENTER;
-        gridbag.setConstraints(cForceClose, c);
-        this.add(cForceClose);
-
-        c.weightx = 1.0;
-        c.weighty = 0.0;
-        gridbag.setConstraints(texDesc, c);
-        this.add(texDesc);
-
-        c.fill = GridBagConstraints.VERTICAL;
-        gridbag.setConstraints(panButtons, c);
-        this.add(panButtons);
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                setVisible(false);
-            }
-        });
-
-        pack();
-        setResizable(false);
-        setLocation(clientgui.frame.getLocation().x
-                + clientgui.frame.getSize().width / 2 - getSize().width / 2,
-                clientgui.frame.getLocation().y
-                        + clientgui.frame.getSize().height / 2
-                        - getSize().height / 2);
-    }
-
-    private void setupButtons() {
         butOkay.addActionListener(this);
         butCancel.addActionListener(this);
 
-        // layout
-        GridBagLayout gridbag = new GridBagLayout();
-        GridBagConstraints c = new GridBagConstraints();
-        panButtons.setLayout(gridbag);
-
-        c.insets = new Insets(5, 5, 0, 0);
-        c.weightx = 1.0;
-        c.weighty = 1.0;
-        c.fill = GridBagConstraints.VERTICAL;
-        c.ipadx = 20;
-        c.ipady = 5;
-        c.gridwidth = 1;
-        gridbag.setConstraints(butOkay, c);
-        panButtons.add(butOkay);
-
-        c.gridwidth = 1;
-        gridbag.setConstraints(butCancel, c);
-        panButtons.add(butCancel);
-        c.gridwidth = 1;
-        gridbag.setConstraints(labelPlayer, c);
-        panButtons.add(labelPlayer);
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        gridbag.setConstraints(chPlayer, c);
-        panButtons.add(chPlayer);
     }
 
     private void updatePlayerChoice() {
@@ -223,16 +101,16 @@ public class RandomSkillDialog extends JDialog implements ActionListener,
         chPlayer.removeAllItems();
         chPlayer.setEnabled(true);
         chPlayer.addItem(clientName);
-        for (Iterator<Client> i = clientgui.getBots().values().iterator(); i
-                .hasNext();) {
+        for (Iterator<Client> i = clientgui.getBots().values().iterator(); i.hasNext();) {
             chPlayer.addItem(i.next().getName());
         }
         if (chPlayer.getItemCount() == 1) {
             chPlayer.setEnabled(false);
         }
         chPlayer.setSelectedItem(lastChoice);
-        if (chPlayer.getSelectedIndex() < 0)
+        if (chPlayer.getSelectedIndex() < 0) {
             chPlayer.setSelectedIndex(0);
+        }
     }
 
     @Override
@@ -255,14 +133,12 @@ public class RandomSkillDialog extends JDialog implements ActionListener,
             if (c == null) {
                 c = client;
             }
-            for (Enumeration<Entity> e = c.game.getEntities(); e
-                    .hasMoreElements();) {
+            for (Enumeration<Entity> e = c.game.getEntities(); e.hasMoreElements();) {
                 Entity ent = e.nextElement();
                 if (ent.getOwnerId() == c.getLocalPlayer().getId()) {
-                    int skills[] = Compute.getRandomSkills(chMethod
-                            .getSelectedIndex(), chType.getSelectedIndex(),
+                    int skills[] = Compute.getRandomSkills(chMethod.getSelectedIndex(), chType.getSelectedIndex(),
                             chLevel.getSelectedIndex(), ent instanceof Tank
-                                    || ent instanceof VTOL);
+                            || ent instanceof VTOL);
                     if (cForceClose.isSelected()) {
                         skills[1] = skills[0] + 1;
                     }
@@ -290,12 +166,10 @@ public class RandomSkillDialog extends JDialog implements ActionListener,
                 texDesc.setText(Messages.getString("RandomSkillDialog.descTW"));
             }
             if (chMethod.getSelectedIndex() == Compute.METHOD_TAHARQA) {
-                texDesc.setText(Messages
-                        .getString("RandomSkillDialog.descTaharqa"));
+                texDesc.setText(Messages.getString("RandomSkillDialog.descTaharqa"));
             }
             if (chMethod.getSelectedIndex() == Compute.METHOD_CONSTANT) {
-                texDesc.setText(Messages
-                        .getString("RandomSkillDialog.descConstant"));
+                texDesc.setText(Messages.getString("RandomSkillDialog.descConstant"));
             }
 
         }
@@ -305,4 +179,124 @@ public class RandomSkillDialog extends JDialog implements ActionListener,
         this.client = client;
     }
 
+
+
+ 
+    private void initComponents() {
+
+        panButtons = new javax.swing.JPanel();
+        butOkay = new javax.swing.JButton();
+        butCancel = new javax.swing.JButton();
+        labelPlayer = new javax.swing.JLabel();
+        chPlayer = new javax.swing.JComboBox();
+        jPanel2 = new javax.swing.JPanel();
+        labelMethod = new javax.swing.JLabel();
+        labelType = new javax.swing.JLabel();
+        labelLevel = new javax.swing.JLabel();
+        chMethod = new javax.swing.JComboBox();
+        chType = new javax.swing.JComboBox();
+        chLevel = new javax.swing.JComboBox();
+        cForceClose = new javax.swing.JCheckBox();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        texDesc = new javax.swing.JTextArea();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        butOkay.setText("Okay");
+        panButtons.add(butOkay);
+
+        butCancel.setText("Cancel");
+        panButtons.add(butCancel);
+
+        labelPlayer.setText("Player:");
+        panButtons.add(labelPlayer);
+
+        chPlayer.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        panButtons.add(chPlayer);
+
+        getContentPane().add(panButtons, java.awt.BorderLayout.PAGE_END);
+
+        labelMethod.setText("Method");
+
+        labelType.setText("Pilot Type");
+
+        labelLevel.setText("Experience");
+
+        cForceClose.setText("Force Piloting to be Gunnery+1");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addGap(84, 84, 84)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(labelLevel)
+                                .addGap(15, 15, 15))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(labelMethod)
+                                    .addComponent(labelType))
+                                .addGap(18, 18, 18)))
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(chMethod, javax.swing.GroupLayout.Alignment.TRAILING, 0, 238, Short.MAX_VALUE)
+                            .addComponent(chType, javax.swing.GroupLayout.Alignment.TRAILING, 0, 238, Short.MAX_VALUE)
+                            .addComponent(chLevel, 0, 238, Short.MAX_VALUE)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(cForceClose)))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(chMethod, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelMethod))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(chType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelType))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(chLevel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelLevel))
+                .addContainerGap(30, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(89, Short.MAX_VALUE)
+                .addComponent(cForceClose)
+                .addContainerGap())
+        );
+
+        getContentPane().add(jPanel2, java.awt.BorderLayout.PAGE_START);
+
+        texDesc.setColumns(20);
+        texDesc.setRows(5);
+        jScrollPane1.setViewportView(texDesc);
+
+        getContentPane().add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }
+
+    private javax.swing.JButton butCancel;
+    private javax.swing.JButton butOkay;
+    private javax.swing.JCheckBox cForceClose;
+    private javax.swing.JComboBox chLevel;
+    private javax.swing.JComboBox chMethod;
+    private javax.swing.JComboBox chPlayer;
+    private javax.swing.JComboBox chType;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel labelLevel;
+    private javax.swing.JLabel labelMethod;
+    private javax.swing.JLabel labelPlayer;
+    private javax.swing.JLabel labelType;
+    private javax.swing.JPanel panButtons;
+    private javax.swing.JTextArea texDesc;
+ 
 }

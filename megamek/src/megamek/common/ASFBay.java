06fdd26550b34363eaaeaf4743bb6077b5003c39
torren <torren@users.sourceforge.net>
2010-09-19 17:42:40 +0000

    + Bug: Bay Size only accepting Ints

--- megamek/src/megamek/common/ASFBay.java
+++ megamek/src/megamek/common/ASFBay.java
@@ -17,7 +17,8 @@ package megamek.common;
 import java.util.Vector;
 
 /**
- * Represtents a volume of space set aside for carrying ASFs and Small Craft aboard DropShips
+ * Represtents a volume of space set aside for carrying ASFs and Small Craft
+ * aboard DropShips
  */
 
 public final class ASFBay extends Bay {
@@ -26,210 +27,208 @@ public final class ASFBay extends Bay {
      * 
      */
     private static final long serialVersionUID = -4110012474950158433L;
-  
+
     /**
      * The default constructor is only for serialization.
      */
     protected ASFBay() {
-        this.totalSpace = 0;
-        this.currentSpace = 0;
+        totalSpace = 0;
+        currentSpace = 0;
     }
 
     // Public constructors and methods.
 
     /**
-     * Create a space for the given tonnage of troops. For this class, only
-     * the weight of the troops (and their equipment) are considered; if
-     * you'd like to think that they are stacked like lumber, be my guest.
-     *
-     * @param   space - The weight of troops (in tons) this space can carry.
+     * Create a space for the given tonnage of troops. For this class, only the
+     * weight of the troops (and their equipment) are considered; if you'd like
+     * to think that they are stacked like lumber, be my guest.
+     * 
+     * @param space
+     *            - The weight of troops (in tons) this space can carry.
      */
-    public ASFBay( int space, int doors ) {
-        this.totalSpace = space;
-        this.currentSpace = space;
+    public ASFBay(double space, int doors) {
+        totalSpace = space;
+        currentSpace = space;
         this.doors = doors;
-        this.doorsNext = doors;
-        this.recoverySlots = initializeRecoverySlots();
+        doorsNext = doors;
+        recoverySlots = initializeRecoverySlots();
     }
 
     /**
-     * Determines if this object can accept the given unit.  The unit may
-     * not be of the appropriate type or there may be no room for the unit.
-     *
-     * @param   unit - the <code>Entity</code> to be loaded.
-     * @return  <code>true</code> if the unit can be loaded,
-     *          <code>false</code> otherwise.
+     * Determines if this object can accept the given unit. The unit may not be
+     * of the appropriate type or there may be no room for the unit.
+     * 
+     * @param unit
+     *            - the <code>Entity</code> to be loaded.
+     * @return <code>true</code> if the unit can be loaded, <code>false</code>
+     *         otherwise.
      */
     @Override
-    public boolean canLoad( Entity unit ) {
+    public boolean canLoad(Entity unit) {
         // Assume that we cannot carry the unit.
         boolean result = false;
 
         // Only ASFs
-        if ( unit instanceof Aero && !(unit instanceof FighterSquadron) && !(unit instanceof SmallCraft) && !(unit instanceof Jumpship)) {
+        if ((unit instanceof Aero) && !(unit instanceof FighterSquadron) && !(unit instanceof SmallCraft) && !(unit instanceof Jumpship)) {
             result = true;
         }
 
-        //System.err.print("Current space to load " + unit.getShortName() + " is " + this.currentSpace + "\n");
-        if ( this.currentSpace < 1 ) {
+        // System.err.print("Current space to load " + unit.getShortName() +
+        // " is " + this.currentSpace + "\n");
+        if (currentSpace < 1) {
             result = false;
         }
 
-//      is there at least one recovery slot available?
-        if(getRecoverySlots() < 1) {
+        // is there at least one recovery slot available?
+        if (getRecoverySlots() < 1) {
             result = false;
         }
-        
+
         // Return our result.
         return result;
     }
-    
+
     /**
-     * Load the given unit.  
-     *
-     * @param   unit - the <code>Entity</code> to be loaded.
+     * Load the given unit.
+     * 
+     * @param unit
+     *            - the <code>Entity</code> to be loaded.
      * @exception - If the unit can't be loaded, an
-     *          <code>IllegalArgumentException</code> exception will be thrown.
+     *            <code>IllegalArgumentException</code> exception will be
+     *            thrown.
      */
     @Override
-    public void load( Entity unit ) throws IllegalArgumentException {
+    public void load(Entity unit) throws IllegalArgumentException {
         // If we can't load the unit, throw an exception.
-        if ( !this.canLoad(unit) ) {
-            throw new IllegalArgumentException( "Can not load " +
-                        unit.getShortName() +
-                        " into this bay. " + this.currentSpace);
+        if (!canLoad(unit)) {
+            throw new IllegalArgumentException("Can not load " + unit.getShortName() + " into this bay. " + currentSpace);
         }
 
-        this.currentSpace -= 1;
-        
+        currentSpace -= 1;
+
         // Add the unit to our list of troops.
-        this.troops.addElement( unit );
+        troops.addElement(unit);
     }
 
-    //Recovery is different from loading in that it uses up a recovery slot
-    //load is only used in deployment phase
-    public void recover( Entity unit ) throws IllegalArgumentException {
+    // Recovery is different from loading in that it uses up a recovery slot
+    // load is only used in deployment phase
+    public void recover(Entity unit) throws IllegalArgumentException {
         // If we can't load the unit, throw an exception.
-        if ( !this.canLoad(unit) ) {
-            throw new IllegalArgumentException( "Can not recover " +
-                        unit.getShortName() +
-                        " into this bay. " + this.currentSpace);
+        if (!canLoad(unit)) {
+            throw new IllegalArgumentException("Can not recover " + unit.getShortName() + " into this bay. " + currentSpace);
         }
 
-        this.currentSpace -= 1;
-        
+        currentSpace -= 1;
 
-        //get the closest open recovery slot and make it unavailable
+        // get the closest open recovery slot and make it unavailable
         closeSlot();
-        
+
         // Add the unit to our list of troops.
-        this.troops.addElement( unit );
+        troops.addElement(unit);
     }
-    
-    
-    
+
     @Override
     public String getUnusedString() {
-        return "Aerospace Fighter - " + this.currentSpace + " units (" + getRecoverySlots() + " recovery open)";
+        return "Aerospace Fighter - " + currentSpace + " units (" + getRecoverySlots() + " recovery open)";
     }
 
     @Override
     public String getType() {
         return "Fighter";
     }
-    
-//  update the time remaining in recovery slots
+
+    // update the time remaining in recovery slots
     public void updateSlots() {
-        if(this.recoverySlots.size() < 1) {
+        if (recoverySlots.size() < 1) {
             return;
         }
-        
-        for(int i = this.recoverySlots.size() - 1; i >= 0; i--) {
-             if(this.recoverySlots.elementAt(i) > 0) {
-                 int temp = this.recoverySlots.elementAt(i) - 1;
-                 this.recoverySlots.remove(i);
-                 this.recoverySlots.add(temp);
-             }
+
+        for (int i = recoverySlots.size() - 1; i >= 0; i--) {
+            if (recoverySlots.elementAt(i) > 0) {
+                int temp = recoverySlots.elementAt(i) - 1;
+                recoverySlots.remove(i);
+                recoverySlots.add(temp);
+            }
         }
     }
-    
+
     public Vector<Integer> initializeRecoverySlots() {
-        
+
         Vector<Integer> slots = new Vector<Integer>();
- 
-        for(int i = 0; i < this.doors; i++) {
+
+        for (int i = 0; i < doors; i++) {
             slots.add(0);
             slots.add(0);
         }
-         
+
         return slots;
     }
-    
-    //check how many available slots we have  
+
+    // check how many available slots we have
     public int getRecoverySlots() {
-        //a zero means it is available
+        // a zero means it is available
         int avail = 0;
-        if(null == this.recoverySlots) {
+        if (null == recoverySlots) {
             return avail;
-        }        
-        for(int i = 0; i < this.recoverySlots.size(); i++) {
-             if(this.recoverySlots.elementAt(i) == 0) {
-                 avail++;
-             }
+        }
+        for (int i = 0; i < recoverySlots.size(); i++) {
+            if (recoverySlots.elementAt(i) == 0) {
+                avail++;
+            }
         }
         return avail;
     }
-    
+
     public void closeSlot() {
-        for(int i = 0; i < this.recoverySlots.size(); i++) {
-             if(this.recoverySlots.elementAt(i) == 0) {
-                 this.recoverySlots.remove(i);
-                 this.recoverySlots.add(5);
-                 break;
-             }
+        for (int i = 0; i < recoverySlots.size(); i++) {
+            if (recoverySlots.elementAt(i) == 0) {
+                recoverySlots.remove(i);
+                recoverySlots.add(5);
+                break;
+            }
         }
     }
-    
-    //  destroy a door
+
+    // destroy a door
     @Override
     public void destroyDoorNext() {
-        
+
         setDoorsNext(getDoorsNext() - 1);
-        
-        //get rid of two empty recovery slots
-        //it doesn't matter which ones
-        if(this.recoverySlots.size() > 0) {
-            this.recoverySlots.remove(0);
+
+        // get rid of two empty recovery slots
+        // it doesn't matter which ones
+        if (recoverySlots.size() > 0) {
+            recoverySlots.remove(0);
         }
-        if(this.recoverySlots.size() > 0) {
-            this.recoverySlots.remove(0);
+        if (recoverySlots.size() > 0) {
+            recoverySlots.remove(0);
         }
     }
-    
-    //  destroy a door
+
+    // destroy a door
     @Override
     public void destroyDoor() {
-        
-        this.doors -= 1;
-        
-        //get rid of two empty recovery slots
-        //it doesn't matter which ones
-        if(this.recoverySlots.size() > 0) {
-            this.recoverySlots.remove(0);
+
+        doors -= 1;
+
+        // get rid of two empty recovery slots
+        // it doesn't matter which ones
+        if (recoverySlots.size() > 0) {
+            recoverySlots.remove(0);
         }
-        if(this.recoverySlots.size() > 0) {
-            this.recoverySlots.remove(0);
+        if (recoverySlots.size() > 0) {
+            recoverySlots.remove(0);
         }
     }
-  
-    //get doors should be different - first I must subtract the number of active recoveries
+
+    // get doors should be different - first I must subtract the number of
+    // active recoveries
     @Override
     public int getDoors() {
-        
-        //just take the available recovery slots, divided by two
-        return (int)Math.floor(getRecoverySlots()/2.0);
-        
-        
+
+        // just take the available recovery slots, divided by two
+        return (int) Math.floor(getRecoverySlots() / 2.0);
+
     }
-    
-} 
+
+}
--- megamek/src/megamek/common/InfantryBay.java
+++ megamek/src/megamek/common/InfantryBay.java
@@ -14,9 +14,9 @@
 
 package megamek.common;
 
-
 /**
- * Represtents a volume of space set aside for carrying ASFs and Small Craft aboard DropShips
+ * Represtents a volume of space set aside for carrying ASFs and Small Craft
+ * aboard DropShips
  */
 
 public final class InfantryBay extends Bay {
@@ -32,52 +32,54 @@ public final class InfantryBay extends Bay {
      * The default constructor is only for serialization.
      */
     protected InfantryBay() {
-        this.totalSpace = 0;
-        this.currentSpace = 0;
+        totalSpace = 0;
+        currentSpace = 0;
     }
 
     // Public constructors and methods.
 
     /**
-     * Create a space for the given tonnage of troops. For this class, only
-     * the weight of the troops (and their equipment) are considered; if
-     * you'd like to think that they are stacked like lumber, be my guest.
-     *
-     * @param   space - The weight of troops (in tons) this space can carry.
+     * Create a space for the given tonnage of troops. For this class, only the
+     * weight of the troops (and their equipment) are considered; if you'd like
+     * to think that they are stacked like lumber, be my guest.
+     * 
+     * @param space
+     *            - The weight of troops (in tons) this space can carry.
      */
-    public InfantryBay( int space, int doors ) {
-        this.totalSpace = space;
-        this.currentSpace = space;
+    public InfantryBay(double space, int doors) {
+        totalSpace = space;
+        currentSpace = space;
         this.doors = doors;
-        this.doorsNext = doors;
+        doorsNext = doors;
     }
-  
+
     /**
-     * Determines if this object can accept the given unit.  The unit may
-     * not be of the appropriate type or there may be no room for the unit.
-     *
-     * @param   unit - the <code>Entity</code> to be loaded.
-     * @return  <code>true</code> if the unit can be loaded,
-     *          <code>false</code> otherwise.
+     * Determines if this object can accept the given unit. The unit may not be
+     * of the appropriate type or there may be no room for the unit.
+     * 
+     * @param unit
+     *            - the <code>Entity</code> to be loaded.
+     * @return <code>true</code> if the unit can be loaded, <code>false</code>
+     *         otherwise.
      */
     @Override
-    public boolean canLoad( Entity unit ) {
+    public boolean canLoad(Entity unit) {
         // Assume that we cannot carry the unit.
         boolean result = false;
 
         // Only smallcraft
-        if ( unit instanceof Infantry ) {
+        if (unit instanceof Infantry) {
             result = true;
         }
 
         // We must have enough space for the new troops.
         // POSSIBLE BUG: we may have to take the Math.ceil() of the weight.
-        if ( this.currentSpace < 1 ) {
+        if (currentSpace < 1) {
             result = false;
         }
-        
-        //is the door functional
-        if( this.doors < 1 ) {
+
+        // is the door functional
+        if (doors < 1) {
             result = false;
         }
 
@@ -87,12 +89,12 @@ public final class InfantryBay extends Bay {
 
     @Override
     public String getUnusedString() {
-        return "Infantry - " + this.currentSpace + " platoons";
+        return "Infantry - " + currentSpace + " platoons";
     }
-    
+
     @Override
     public String getType() {
         return "Infantry";
     }
-    
+
 } // End package class TroopSpace implements Transporter

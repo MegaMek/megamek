a8d66d45430721ef99fd8a433711730f25fb9d8e
Torren <Torren@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2010-10-24 16:51:06 +0000

    + Changed Text on ASFBay.getUnused()

--- megamek/src/megamek/common/ASFBay.java
+++ megamek/src/megamek/common/ASFBay.java
@@ -1,15 +1,15 @@
 /*
  * MegaMek - Copyright (C) 2003, 2004 Ben Mazur (bmazur@sev.org)
  * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
- *  any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
- *  for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  */
 
 package megamek.common;
@@ -129,7 +129,9 @@ public final class ASFBay extends Bay {
 
     @Override
     public String getUnusedString() {
-        return "Aerospace Fighter - " + String.format("%1$,.0f", currentSpace) + " units (" + getRecoverySlots() + " recovery open)";
+        // "Aerospace Fighter - " + String.format("%1$,.0f", currentSpace) +
+        // " units (" + getRecoverySlots() + " recovery open)";
+        return String.format("Aerospace Fighter - %1$,.0f units", currentSpace);
     }
 
     @Override

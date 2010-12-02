6246c634e22e1742b178b68eb494b2b9db53b3d2
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2010-12-02 22:25:57 +0000

    + Bug: Vibroblades counting twice for BV

--- trunk/megamek/src/megamek/common/Mech.java
+++ trunk/megamek/src/megamek/common/Mech.java
@@ -1,12 +1,12 @@
 /*
  * MegaMek - Copyright (C) 2000,2001,2002,2003,2004,2005 Ben Mazur
  * (bmazur@sev.org)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
@@ -281,7 +281,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Damage the cowl. Returns amount of excess damage
-     * 
+     *
      * @param amount
      * @return
      */
@@ -333,7 +333,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * find the least restrictive location of the two locations passed in
-     * 
+     *
      * @param location1
      * @param location2
      * @return
@@ -371,7 +371,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Get the number of turns MASC has been used continuously.
      * <p/>
      * This method should <strong>only</strong> be used during serialization.
-     * 
+     *
      * @return the <code>int</code> number of turns MASC has been used.
      */
     public int getMASCTurns() {
@@ -382,7 +382,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Set the number of turns MASC has been used continuously.
      * <p/>
      * This method should <strong>only</strong> be used during deserialization.
-     * 
+     *
      * @param turns
      *            The <code>int</code> number of turns MASC has been used.
      */
@@ -394,7 +394,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Determine if MASC has been used this turn.
      * <p/>
      * This method should <strong>only</strong> be used during serialization.
-     * 
+     *
      * @return <code>true</code> if MASC has been used.
      */
     public boolean isMASCUsed() {
@@ -405,7 +405,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Set whether MASC has been used.
      * <p/>
      * This method should <strong>only</strong> be used during deserialization.
-     * 
+     *
      * @param used
      *            The <code>boolean</code> whether MASC has been used.
      */
@@ -419,7 +419,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * This function cheks for masc failure.
-     * 
+     *
      * @param md
      *            the movement path.
      * @param vDesc
@@ -449,7 +449,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * check one masc system for failure
-     * 
+     *
      * @param masc
      * @param vDesc
      * @param vCriticals
@@ -688,7 +688,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * This function returns true iff the system is in perfect condition.
-     * 
+     *
      * @param system
      *            the system to check
      * @return false if the system is damaged.
@@ -737,7 +737,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech have composite internal structure?
-     * 
+     *
      * @return
      */
     public boolean hasCompositeStructure() {
@@ -746,7 +746,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech have reinforced internal structure?
-     * 
+     *
      * @return
      */
     public boolean hasReinforcedStructure() {
@@ -755,7 +755,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech mount MASC?
-     * 
+     *
      * @return
      */
     public boolean hasMASC() {
@@ -788,7 +788,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech have working jump boosters?
-     * 
+     *
      * @return
      */
     public boolean hasJumpBoosters() {
@@ -810,7 +810,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * get non-supercharger MASC mounted on this mech
-     * 
+     *
      * @return
      */
     public Mounted getMASC() {
@@ -825,7 +825,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * get a supercharger mounted on this mech
-     * 
+     *
      * @return
      */
     public Mounted getSuperCharger() {
@@ -903,7 +903,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech have industrial TSM=
-     * 
+     *
      * @return
      */
     public boolean hasIndustrialTSM() {
@@ -917,7 +917,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech have a null-sig-system?
-     * 
+     *
      * @return
      */
     public boolean hasNullSig() {
@@ -933,7 +933,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech have a void-sig-system?
-     * 
+     *
      * @return
      */
     public boolean hasVoidSig() {
@@ -949,7 +949,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech have tracks?
-     * 
+     *
      * @return
      */
     public boolean hasTracks() {
@@ -965,7 +965,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * does this mech have a chameleon light polarization shield?
-     * 
+     *
      * @return
      */
     public boolean hasChameleonShield() {
@@ -991,7 +991,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * set this mech's <code>Engine</code>
-     * 
+     *
      * @param e
      *            the <code>Engine</code> to set
      */
@@ -1004,7 +1004,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Used to set this Mech's original walk mp
-     * 
+     *
      * @return this units calculated walking speed, dependent on engine rating
      *         and weight
      */
@@ -1252,7 +1252,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Gives the bonus to Jump MP conferred by a mech partial wing.
-     * 
+     *
      * @param mount
      *            The mounted location of the Wing
      * @return The Jump MP bonus conferred by the wing
@@ -1326,7 +1326,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Gives the heat capacity bonus conferred by a mech partial wing.
-     * 
+     *
      * @return the heat capacity bonus provided by the wing
      */
     private int getPartialWingHeatBonus() {
@@ -1508,7 +1508,7 @@ public abstract class Mech extends Entity implements Serializable {
     /**
      * base for adding engine sinks. Newer method allows externals to say how
      * much are engine HS.
-     * 
+     *
      * @param totalSinks
      *            the amount of heatsinks to add to the engine
      * @param sinkName
@@ -1522,7 +1522,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * add heat sinks into the engine
-     * 
+     *
      * @param sinkName
      *            the <code>String</code> determining the type of heatsink to
      *            add. must be a lookupname of a heatsinktype
@@ -2440,7 +2440,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Sets the internal structure for the mech.
-     * 
+     *
      * @param head
      *            head
      * @param ct
@@ -3870,6 +3870,10 @@ public abstract class Mech extends Entity implements Serializable {
             if (mounted.isDestroyed()) {
                 continue;
             }
+            // vibroblades have been counted under weapons
+            if ((mounted.getType() instanceof MiscType) && ((MiscType) mounted.getType()).hasFlag(MiscType.F_CLUB) && ((MiscType) mounted.getType()).isVibroblade()) {
+                continue;
+            }
 
             if (mtype.hasFlag(MiscType.F_ECM) || mtype.hasFlag(MiscType.F_BAP) || mtype.hasFlag(MiscType.F_AP_POD) || mtype.hasFlag(MiscType.F_MASS)
             // not yet coded: || etype.hasFlag(MiscType.F_BRIDGE_LAYING)
@@ -4236,7 +4240,7 @@ public abstract class Mech extends Entity implements Serializable {
     /**
      * Calculate the C-bill cost of the mech. Passing null as the argument will
      * skip the detailed report processing.
-     * 
+     *
      * @param detail
      *            buffer to append the detailed cost report to
      * @return The cost in C-Bills of the 'Mech in question.
@@ -4538,7 +4542,7 @@ public abstract class Mech extends Entity implements Serializable {
      * can be active and not working when under ECCM)
      * <p/>
      * Sub-classes are encouraged to override this method.
-     * 
+     *
      * @return <code>true</code> if this unit has a stealth system that is
      *         currently active, <code>false</code> if there is no stealth
      *         system or if it is inactive.
@@ -4566,7 +4570,7 @@ public abstract class Mech extends Entity implements Serializable {
      * can be active and not working when under ECCM)
      * <p/>
      * Sub-classes are encouraged to override this method.
-     * 
+     *
      * @return <code>true</code> if this unit has a stealth system that is
      *         currently active, <code>false</code> if there is no stealth
      *         system or if it is inactive.
@@ -4642,7 +4646,7 @@ public abstract class Mech extends Entity implements Serializable {
      * <code>IllegalArgumentException</code> will be thrown.
      * <p/>
      * Sub-classes are encouraged to override this method.
-     * 
+     *
      * @param range
      *            - an <code>int</code> value that must match one of the
      *            <code>Compute</code> class range constants.
@@ -4749,7 +4753,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Determine if the unit can be repaired, or only harvested for spares.
-     * 
+     *
      * @return A <code>boolean</code> that is <code>true</code> if the unit can
      *         be repaired (given enough time and parts); if this value is
      *         <code>false</code>, the unit is only a source of spares.
@@ -5313,7 +5317,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for a standard cockpit. Note: This is
      * part of the mek creation public API, and might not be referenced by any
      * MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addCockpit() {
@@ -5333,7 +5337,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for an industrial cockpit. Note: This is
      * part of the mek creation public API, and might not be referenced by any
      * MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addIndustrialCockpit() {
@@ -5353,7 +5357,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for an industrial cockpit. Note: This is
      * part of the mek creation public API, and might not be referenced by any
      * MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addPrimitiveCockpit() {
@@ -5373,7 +5377,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for an industrial primitive cockpit.
      * Note: This is part of the mek creation public API, and might not be
      * referenced by any MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addIndustrialPrimitiveCockpit() {
@@ -5393,7 +5397,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for a small cockpit. Note: This is part
      * of the mek creation public API, and might not be referenced by any
      * MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addSmallCockpit() {
@@ -5440,7 +5444,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for a torso-mounted cockpit. Note: This
      * is part of the mek creation public API, and might not be referenced by
      * any MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addTorsoMountedCockpit() {
@@ -5476,7 +5480,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for a standard gyro. Also set the gyro
      * type variable. Note: This is part of the mek creation public API, and
      * might not be referenced by any MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addGyro() {
@@ -5494,7 +5498,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for a compact gyro. Also set the gyro
      * type variable. Note: This is part of the mek creation public API, and
      * might not be referenced by any MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addCompactGyro() {
@@ -5511,7 +5515,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for an extra-light gyro. Also set the
      * gyro type variable. Note: This is part of the mek creation public API,
      * and might not be referenced by any MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addXLGyro() {
@@ -5532,7 +5536,7 @@ public abstract class Mech extends Entity implements Serializable {
      * Add the critical slots necessary for a heavy-duty gyro. Also set the gyro
      * type variable. Note: This is part of the mek creation public API, and
      * might not be referenced by any MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addHeavyDutyGyro() {
@@ -5548,7 +5552,7 @@ public abstract class Mech extends Entity implements Serializable {
      * method before setting a mek's engine object will result in a NPE. Note:
      * This is part of the mek creation public API, and might not be referenced
      * by any MegaMek code.
-     * 
+     *
      * @return false if insufficient critical space
      */
     public boolean addEngineCrits() {
@@ -5715,7 +5719,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Does this mech have an undamaged HarJel system in this location?
-     * 
+     *
      * @param loc
      *            the <code>int</code> location to check
      * @return a <code>boolean</code> value indicating a present HarJel system
@@ -5908,7 +5912,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Is this an Industrial Mech?
-     * 
+     *
      * @return if this mech has an industrial inner structure
      */
     public boolean isIndustrial() {
@@ -5918,7 +5922,7 @@ public abstract class Mech extends Entity implements Serializable {
     /**
      * set if this mech just moved into water that would kill it because of the
      * lack of environmental sealing
-     * 
+     *
      * @param moved
      */
     public void setJustMovedIntoIndustrialKillingWater(boolean moved) {
@@ -5928,7 +5932,7 @@ public abstract class Mech extends Entity implements Serializable {
     /**
      * did this mech just moved into water that would kill it because we lack
      * environmental sealing?
-     * 
+     *
      * @return
      */
     public boolean isJustMovedIntoIndustrialKillingWater() {
@@ -5939,7 +5943,7 @@ public abstract class Mech extends Entity implements Serializable {
      * should this mech die at the end of turn because it's an IndustrialMech
      * without environmental sealing that moved into water last round and stayed
      * there?
-     * 
+     *
      * @return
      */
     public boolean shouldDieAtEndOfTurnBecauseOfWater() {
@@ -5949,7 +5953,7 @@ public abstract class Mech extends Entity implements Serializable {
     /**
      * Set if this Mech's ICE Engine is stalled or not should only be used for
      * industrial mechs carrying an ICE engine
-     * 
+     *
      * @param stalled
      */
     public void setStalled(boolean stalled) {
@@ -6074,7 +6078,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Is this a primitive Mech?
-     * 
+     *
      * @return
      */
     public boolean isPrimitive() {
@@ -6137,7 +6141,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * how many levels did this mech fall this turn?
-     * 
+     *
      * @return
      */
     public int getLevelsFallen() {
@@ -6154,7 +6158,7 @@ public abstract class Mech extends Entity implements Serializable {
 
     /**
      * Is the passed in location an arm?
-     * 
+     *
      * @param loc
      * @return
      */

7231bed98e1232fd8aff53c06e781e7001129b4d
bombaijin <bombaijin@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2010-12-08 21:26:55 +0000

    + if all landing hexes are paved/road, assume this is an "airfield"

--- megamek/src/megamek/common/Aero.java
+++ megamek/src/megamek/common/Aero.java
@@ -2563,9 +2563,11 @@ public class Aero extends Entity {
             }
         }
         //terrain mods
+        //lets assume that if all hexes are paved and/or road, then we have an "airstrip"
         boolean lightWoods = false;
         boolean rough = false;
         boolean heavyWoods = false;
+        boolean airstrip = true;
         //dropships need a a landing strip three hexes wide
         Vector<Coords> startingPos =  new Vector<Coords>();
         startingPos.add(currentPos);
@@ -2577,6 +2579,9 @@ public class Aero extends Entity {
             for (int i = 0; i < getLandingLength(); i++) {
                 pos = pos.translated(face);
                 IHex hex = game.getBoard().getHex(pos);
+                if(airstrip && !hex.containsTerrain(Terrains.PAVEMENT) && !hex.containsTerrain(Terrains.ROAD)) {
+                    airstrip = false;
+                }
                 if(!rough && hex.containsTerrain(Terrains.ROUGH) || hex.containsTerrain(Terrains.RUBBLE)) {
                     rough = true;
                 }
@@ -2587,6 +2592,9 @@ public class Aero extends Entity {
                     heavyWoods = true;
                 }
             }
+        }
+        if(airstrip) {
+            roll.addModifier(-1,"landing on airfield");
         }
         if(rough) {
             roll.addModifier(+3, "rough/rubble in landing path");

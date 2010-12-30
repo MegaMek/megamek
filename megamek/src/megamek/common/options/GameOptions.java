48c5e51cf195ac15b37fc9002c81a43e2ed33cbd
Jay Lawson <jaylawson39@gmail.com>
2010-12-30 01:08:19 +0000

    + TacOps Advanced Determining Critical Hits option

--- megamek/src/megamek/common/options/GameOptions.java
+++ megamek/src/megamek/common/options/GameOptions.java
@@ -132,6 +132,7 @@ public class GameOptions extends AbstractOptions {
         IBasicOptionGroup advancedCombat = addGroup("advancedCombat"); //$NON-NLS-1$
         addOption(advancedCombat, "tacops_ams", false); //$NON-NLS-1$
         addOption(advancedCombat, "floating_crits", false); //$NON-NLS-1$
+        addOption(advancedCombat, "tacops_crit_roll", false); //$NON-NLS-1$
         addOption(advancedCombat, "tacops_engine_explosions", false); //$NON-NLS-1$
         addOption(advancedCombat, "tacops_called_shots", false); //$NON-NLS-1$
         addOption(advancedCombat, "tacops_prone_fire", false); //$NON-NLS-1$

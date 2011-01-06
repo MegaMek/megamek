6238a7732376f024197e503d53cbadf8cc09a297
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-01-06 23:23:44 +0000

    + Bug: LAC 2 and 5 missing aero values + Bug: LAC 10 and 20 should be unofficial

--- trunk/megamek/src/megamek/common/weapons/ISLAC20.java
+++ trunk/megamek/src/megamek/common/weapons/ISLAC20.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -22,29 +22,29 @@ import megamek.common.TechConstants;
 public class ISLAC20 extends LACWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 7135078308771443835L;
 
     public ISLAC20() {
         super();
-        this.techLevel = TechConstants.T_IS_EXPERIMENTAL;
-        this.name = "Light Auto Cannon/20";
-        this.setInternalName(this.name);
-        this.addLookupName("IS Light AutoCannon/20");
-        this.addLookupName("ISLAC20");
-        this.addLookupName("IS Light Autocannon/20");
-        this.heat = 7;
-        this.damage = 20;
-        this.rackSize = 20;
-        this.shortRange = 2;
-        this.mediumRange = 4;
-        this.longRange = 6;
-        this.extremeRange = 8;
-        this.tonnage = 9.0f;
-        this.criticals = 6;
-        this.bv = 118;
-        this.cost = 325000;
-        this.explosionDamage = damage;
+        techLevel = TechConstants.T_IS_UNOFFICIAL;
+        name = "Light Auto Cannon/20";
+        setInternalName(name);
+        addLookupName("IS Light AutoCannon/20");
+        addLookupName("ISLAC20");
+        addLookupName("IS Light Autocannon/20");
+        heat = 7;
+        damage = 20;
+        rackSize = 20;
+        shortRange = 2;
+        mediumRange = 4;
+        longRange = 6;
+        extremeRange = 8;
+        tonnage = 9.0f;
+        criticals = 6;
+        bv = 118;
+        cost = 325000;
+        explosionDamage = damage;
     }
 }

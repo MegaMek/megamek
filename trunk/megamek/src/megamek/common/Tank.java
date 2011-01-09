b93848969d9d67081ea52faa9ab545c37af95321
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-01-09 22:46:17 +0000

    + Added drone carrier control system construction data

--- trunk/megamek/src/megamek/common/Tank.java
+++ trunk/megamek/src/megamek/common/Tank.java
@@ -1835,7 +1835,9 @@ public class Tank extends Entity {
 
     @Override
     public void addEquipment(Mounted mounted, int loc, boolean rearMounted) throws LocationFullException {
-        super.addEquipment(mounted, loc, rearMounted);
+        if (getEquipmentNum(mounted) == -1) {
+            super.addEquipment(mounted, loc, rearMounted);
+        }
         // Add the piece equipment to our slots.
         addCritical(loc, new CriticalSlot(CriticalSlot.TYPE_EQUIPMENT, getEquipmentNum(mounted), true, mounted));
     }

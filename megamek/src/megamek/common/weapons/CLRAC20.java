56facf2a2e8f479e08848d028224492e9c6f908e
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-01-20 16:04:37 +0000

    + Bug 2982926: unable to load CASE II from HMP + Bug 2980124: MFUK RAC/20 should be splitable

--- megamek/src/megamek/common/weapons/CLRAC20.java
+++ megamek/src/megamek/common/weapons/CLRAC20.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 /*
@@ -24,31 +24,32 @@ import megamek.common.TechConstants;
  */
 public class CLRAC20 extends RACWeapon {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -6413635622423390328L;
 
     /**
-     * 
+     *
      */
     public CLRAC20() {
         super();
-        this.techLevel = TechConstants.T_CLAN_UNOFFICIAL;
-        this.name = "Rotary AC/20";
-        this.setInternalName("CLRotaryAC20");
-        this.addLookupName("Clan Rotary AC/20");
-        this.addLookupName("Clan Rotary Assault Cannon/20");
-        this.heat = 7;
-        this.damage = 20;
-        this.rackSize = 20;
-        this.shortRange = 4;
-        this.mediumRange = 8;
-        this.longRange = 12;
-        this.extremeRange = 16;
-        this.tonnage = 16.0f;
-        this.criticals = 10;
-        this.bv = 823;
-        this.cost = 960000;
-        this.techRating = RATING_F;
+        techLevel = TechConstants.T_CLAN_UNOFFICIAL;
+        name = "Rotary AC/20";
+        setInternalName("CLRotaryAC20");
+        addLookupName("Clan Rotary AC/20");
+        addLookupName("Clan Rotary Assault Cannon/20");
+        heat = 7;
+        damage = 20;
+        rackSize = 20;
+        shortRange = 4;
+        mediumRange = 8;
+        longRange = 12;
+        extremeRange = 16;
+        tonnage = 16.0f;
+        criticals = 10;
+        bv = 823;
+        cost = 960000;
+        techRating = RATING_F;
+        flags = flags.or(F_SPLITABLE);
     }
 }

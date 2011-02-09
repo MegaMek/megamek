9f77fda1498b9dae097bc7546f018e774b9e44bc
torren <torren@users.sourceforge.net>
2011-02-09 05:20:45 +0000

    + Bug: ERLRM's did not have Aero damage

--- megamek/src/megamek/common/weapons/ISExtendedLRM15.java
+++ megamek/src/megamek/common/weapons/ISExtendedLRM15.java
@@ -1,15 +1,15 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *  for more details.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  */
 package megamek.common.weapons;
 
@@ -48,5 +48,11 @@ public class ISExtendedLRM15 extends ExtendedLRMWeapon {
         criticals = 6;
         bv = 200;
         cost = 350000;
+        shortAV = 9;
+        medAV = 9;
+        longAV = 9;
+        extAV = 9;
+        maxRange = RANGE_EXT;
+
     }
 }
--- megamek/src/megamek/common/weapons/ISExtendedLRM20.java
+++ megamek/src/megamek/common/weapons/ISExtendedLRM20.java
@@ -1,15 +1,15 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *  for more details.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  */
 package megamek.common.weapons;
 
@@ -51,6 +51,7 @@ public class ISExtendedLRM20 extends ExtendedLRMWeapon {
         shortAV = 12;
         medAV = 12;
         longAV = 12;
-        maxRange = RANGE_LONG;
+        extremeRange = 12;
+        maxRange = RANGE_EXT;
     }
 }

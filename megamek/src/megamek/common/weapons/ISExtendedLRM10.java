28363f4f04df98a525b6100a4c70fa9804a25616
Torren <Torren@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-02-09 05:20:45 +0000

    + Bug: ERLRM's did not have Aero damage

--- megamek/src/megamek/common/weapons/ISExtendedLRM10.java
+++ megamek/src/megamek/common/weapons/ISExtendedLRM10.java
@@ -1,15 +1,15 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *  for more details.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  */
 package megamek.common.weapons;
 
@@ -48,5 +48,10 @@ public class ISExtendedLRM10 extends ExtendedLRMWeapon {
         criticals = 4;
         bv = 133;
         cost = 225000;
+        shortAV = 6;
+        medAV = 6;
+        longAV = 6;
+        extAV = 6;
+        maxRange = RANGE_EXT;
     }
 }

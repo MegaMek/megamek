cedbde9ff860cb929758b44c0cc7bbf3eb53ecdc
beerockxs <sebastian@brocks.cc>
2011-03-20 17:49:37 +0000

    + Bug: moving backwards over elevation change under water didn't cause PSR

--- megamek/src/megamek/server/Server.java
+++ megamek/src/megamek/server/Server.java
@@ -6315,7 +6315,7 @@ public class Server implements Runnable {
             // moving backwards over elevation change
             if (((step.getType() == MoveStepType.BACKWARDS)
                     || (step.getType() == MoveStepType.LATERAL_LEFT_BACKWARDS) || (step.getType() == MoveStepType.LATERAL_RIGHT_BACKWARDS))
-                    && (game.getBoard().getHex(lastPos).getElevation() != curHex.getElevation())
+                    && (lastHex.getElevation() - lastHex.depth() != curHex.getElevation() - curHex.depth())
                     && !(entity instanceof VTOL)
                     && !(md.getFinalClimbMode() && curHex.containsTerrain(Terrains.BRIDGE) && (curHex.terrainLevel(Terrains.BRIDGE_ELEV) + curHex.getElevation() == (prevHex.getElevation() + (prevHex.containsTerrain(Terrains.BRIDGE)?prevHex.terrainLevel(Terrains.BRIDGE_ELEV):0))))) {
 

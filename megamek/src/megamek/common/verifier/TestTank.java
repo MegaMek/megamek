8e5ca1c352b818c8e883aa75efc7da26dc4736be
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-03-29 21:24:33 +0000

    + Bug: plasma weapons should need heat sinks on tanks

--- megamek/src/megamek/common/verifier/TestTank.java
+++ megamek/src/megamek/common/verifier/TestTank.java
@@ -144,7 +144,7 @@ public class TestTank extends TestEntity {
         int heat = 0;
         for (Mounted m : tank.getWeaponList()) {
             WeaponType wt = (WeaponType) m.getType();
-            if (wt.hasFlag(WeaponType.F_LASER) || wt.hasFlag(WeaponType.F_PPC)) {
+            if (wt.hasFlag(WeaponType.F_LASER) || wt.hasFlag(WeaponType.F_PPC) || wt.hasFlag(WeaponType.F_PLASMA) || wt.hasFlag(WeaponType.F_PLASMA_MFUK)) {
                 heat += wt.getHeat();
             }
             // laser insulator reduce heat by 1, to a minimum of 1

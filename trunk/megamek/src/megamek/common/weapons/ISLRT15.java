692ba6c19f180ca59c98e0fd14637ffccc5cf4ff
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-04-06 21:03:39 +0000

    + Bug 3277802: incorrect LRT 15 alias

--- trunk/megamek/src/megamek/common/weapons/ISLRT15.java
+++ trunk/megamek/src/megamek/common/weapons/ISLRT15.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common.weapons;
@@ -21,32 +21,32 @@ import megamek.common.TechConstants;
 public class ISLRT15 extends LRTWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -6506265507271800879L;
 
     /**
-     * 
+     *
      */
     public ISLRT15() {
         super();
-        this.techLevel = TechConstants.T_IS_TW_NON_BOX;
-        this.name = "LRT 15";
-        this.setInternalName(this.name);
-        this.addLookupName("IS LRT-15");
-        this.addLookupName("ISLRTorpedo15");
-        this.addLookupName("IS LRT 5");
-        this.addLookupName("ISLRT5");
-        this.heat = 5;
-        this.rackSize = 15;
-        this.minimumRange = 6;
-        this.waterShortRange = 7;
-        this.waterMediumRange = 14;
-        this.waterLongRange = 21;
-        this.waterExtremeRange = 28;
-        this.tonnage = 7.0f;
-        this.criticals = 3;
-        this.bv = 136;
-        this.cost = 175000;
+        techLevel = TechConstants.T_IS_TW_NON_BOX;
+        name = "LRT 15";
+        setInternalName(name);
+        addLookupName("IS LRT-15");
+        addLookupName("ISLRTorpedo15");
+        addLookupName("IS LRT 15");
+        addLookupName("ISLRT15");
+        heat = 5;
+        rackSize = 15;
+        minimumRange = 6;
+        waterShortRange = 7;
+        waterMediumRange = 14;
+        waterLongRange = 21;
+        waterExtremeRange = 28;
+        tonnage = 7.0f;
+        criticals = 3;
+        bv = 136;
+        cost = 175000;
     }
 }

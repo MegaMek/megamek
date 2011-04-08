89d680e6934227955365551a907588d17a0e017a
torren <torren@users.sourceforge.net>
2011-04-08 03:25:54 +0000

    + Bug: Heavy Flamer was 1.0 tons now 1.5 Tons

--- megamek/src/megamek/common/weapons/ISHeavyFlamer.java
+++ megamek/src/megamek/common/weapons/ISHeavyFlamer.java
@@ -1,19 +1,18 @@
 /**
  * MegaMek - Copyright (C) 2004,2005 Ben Mazur (bmazur@sev.org)
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *  for more details.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  */
 /*
  * Created on Sep 24, 2004
- *
  */
 package megamek.common.weapons;
 
@@ -48,7 +47,7 @@ public class ISHeavyFlamer extends VehicleFlamerWeapon {
         mediumRange = 4;
         longRange = 6;
         extremeRange = 8;
-        tonnage = 1.0f;
+        tonnage = 1.5f;
         criticals = 1;
         bv = 20;
         cost = 20000;

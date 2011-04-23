29ec1dce19acf5f11f8a2dccf9c28f55baa19b41
khm19_de <khm19_de@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-04-23 13:19:39 +0000

    + Bug: TacOps enhanced missile defense was being applied even to non-AMS cluster penalties

--- megamek/src/megamek/common/weapons/ATMHandler.java
+++ megamek/src/megamek/common/weapons/ATMHandler.java
@@ -297,10 +297,10 @@ public class ATMHandler extends MissileWeaponHandler {
             if (ae instanceof BattleArmor) {
                 missilesHit = Compute.missilesHit(wtype.getRackSize()
                         * ((BattleArmor) ae).getShootingStrength(),
-                        nMissilesModifier, weapon.isHotLoaded());
+                        nMissilesModifier, weapon.isHotLoaded(), false, advancedAMS && amsEnganged);
             } else {
                 missilesHit = Compute.missilesHit(wtype.getRackSize(),
-                        nMissilesModifier, weapon.isHotLoaded());
+                        nMissilesModifier, weapon.isHotLoaded(), false, advancedAMS && amsEnganged);
             }
         }
 

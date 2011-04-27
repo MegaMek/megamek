92b808e8281ba6c419bfc70be716e11ab0df1632
Torren <Torren@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-04-27 04:05:35 +0000

    + Bug: Equipment in Turrets for Large Support Vehicles loading in Tank Turret Location

--- megamek/src/megamek/common/loaders/BLKLargeSupportTankFile.java
+++ megamek/src/megamek/common/loaders/BLKLargeSupportTankFile.java
@@ -1,11 +1,11 @@
 /*
  * MegaMek - Copyright (C) 2000-2002 Ben Mazur (bmazur@sev.org)
- *
+ * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
@@ -14,12 +14,12 @@
 
 /*
  * BLkFile.java
- *
+ * 
  * Created on April 6, 2002, 2:06 AM
  */
 
 /**
- *
+ * 
  * @author njrkrynn
  * @version
  */
@@ -30,7 +30,6 @@ import megamek.common.Entity;
 import megamek.common.EntityMovementMode;
 import megamek.common.EquipmentType;
 import megamek.common.LargeSupportTank;
-import megamek.common.Tank;
 import megamek.common.util.BuildingBlock;
 
 public class BLKLargeSupportTankFile extends BLKFile implements IMechLoader {
@@ -88,8 +87,8 @@ public class BLKLargeSupportTankFile extends BLKFile implements IMechLoader {
             throw new EntityLoadingException("Could not find cruiseMP block.");
         }
         int engineRating = dataFile.getDataAsInt("cruiseMP")[0] * (int) t.getWeight() - t.getSuspensionFactor();
-        if (engineRating%5 > 0 ) {
-            engineRating += (5-engineRating%5);
+        if (engineRating % 5 > 0) {
+            engineRating += (5 - engineRating % 5);
         }
         t.setEngine(new Engine(engineRating, BLKFile.translateEngineCode(engineCode), engineFlags));
         t.setOriginalWalkMP(dataFile.getDataAsInt("cruiseMP")[0]);
@@ -114,9 +113,9 @@ public class BLKLargeSupportTankFile extends BLKFile implements IMechLoader {
             t.setBARRating(dataFile.getDataAsInt("barrating")[0]);
         } else {
             for (int i = 1; i < t.locations(); i++) {
-                t.setArmorType(dataFile.getDataAsInt(t.getLocationName(i)+"_armor_type")[0], i);
-                t.setArmorTechLevel(dataFile.getDataAsInt(t.getLocationName(i)+"_armor_type")[0], i);
-                t.setBARRating(dataFile.getDataAsInt(t.getLocationName(i)+"_barrating")[0], i);
+                t.setArmorType(dataFile.getDataAsInt(t.getLocationName(i) + "_armor_type")[0], i);
+                t.setArmorTechLevel(dataFile.getDataAsInt(t.getLocationName(i) + "_armor_type")[0], i);
+                t.setBARRating(dataFile.getDataAsInt(t.getLocationName(i) + "_barrating")[0], i);
             }
         }
         if (dataFile.exists("internal_type")) {
@@ -154,9 +153,9 @@ public class BLKLargeSupportTankFile extends BLKFile implements IMechLoader {
         loadEquipment(t, "Rear Left", LargeSupportTank.LOC_REARLEFT);
         loadEquipment(t, "Rear", LargeSupportTank.LOC_REAR);
         if (!t.hasNoTurret()) {
-            loadEquipment(t, "Turret", Tank.LOC_TURRET);
+            loadEquipment(t, "Turret", LargeSupportTank.LOC_TURRET);
         }
-        loadEquipment(t, "Body", Tank.LOC_BODY);
+        loadEquipment(t, "Body", LargeSupportTank.LOC_BODY);
 
         if (dataFile.exists("omni")) {
             t.setOmni(true);

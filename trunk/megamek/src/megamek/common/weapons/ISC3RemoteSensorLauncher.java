869277847376e0f44dcf863a8b23d7db9872f0b9
Torren <Torren@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-06-05 03:56:19 +0000

    + Misspelled Name for Clan Drone Operating System + Bug: No Name or lookup name or internal name for ISC3RemoteSensorLauncher

--- trunk/megamek/src/megamek/common/weapons/ISC3RemoteSensorLauncher.java
+++ trunk/megamek/src/megamek/common/weapons/ISC3RemoteSensorLauncher.java
@@ -1,15 +1,15 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *  for more details.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  */
 package megamek.common.weapons;
 
@@ -35,6 +35,9 @@ public class ISC3RemoteSensorLauncher extends MissileWeapon {
      */
     public ISC3RemoteSensorLauncher() {
         super();
+        name = "C3 Remote Sensor Lanucher";
+        setInternalName("ISC3RemoteSensorLauncher");
+        addLookupName("C3RemoteSensorLauncher");
         flags = flags.or(F_NO_FIRES);
         ammoType = AmmoType.T_C3_REMOTE_SENSOR;
         cost = 400000;
@@ -43,9 +46,10 @@ public class ISC3RemoteSensorLauncher extends MissileWeapon {
         techRating = RATING_E;
         techLevel = TechConstants.T_IS_EXPERIMENTAL;
         bv = 30;
-        availRating = new int[]{RATING_X, RATING_X, RATING_F};
+        availRating = new int[]
+            { RATING_X, RATING_X, RATING_F };
         flags = flags.or(F_MECH_WEAPON).or(F_TANK_WEAPON);
-        //suppveeslots = 3;
+        // suppveeslots = 3;
     }
 
     /*
@@ -56,9 +60,8 @@ public class ISC3RemoteSensorLauncher extends MissileWeapon {
      *      megamek.server.Server)
      */
     @Override
-    protected AttackHandler getCorrectHandler(ToHitData toHit,
-            WeaponAttackAction waa, IGame game, Server server) {
+    protected AttackHandler getCorrectHandler(ToHitData toHit, WeaponAttackAction waa, IGame game, Server server) {
         return super.getCorrectHandler(toHit, waa, game, server);
-        //FIXME: Implement handler
+        // FIXME: Implement handler
     }
 }

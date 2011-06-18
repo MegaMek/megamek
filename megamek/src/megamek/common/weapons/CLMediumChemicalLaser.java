91ec33eceeedf5750fe190828b23015319f60b7a
khm19_de <khm19_de@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-06-18 16:50:47 +0000

    + Bug 3321794: Medium chemical laser range too short + Chemical laser cost and underwater range fixes

--- megamek/src/megamek/common/weapons/CLMediumChemicalLaser.java
+++ megamek/src/megamek/common/weapons/CLMediumChemicalLaser.java
@@ -33,20 +33,21 @@ public class CLMediumChemicalLaser extends CLChemicalLaserWeapon {
         setInternalName("CLMediumChemicalLaser");
         setInternalName("CLMediumChemLaser");
         heat = 2;
+        damage = 5;
         rackSize = 5;
-        shortRange = 2;
-        mediumRange = 4;
-        longRange = 6;
-        extremeRange = 8;
-        waterShortRange = 1;
-        waterMediumRange = 2;
-        waterLongRange = 3;
-        waterExtremeRange = 3;
+        shortRange = 3;
+        mediumRange = 6;
+        longRange = 9;
+        extremeRange = 12;
+        waterShortRange = 2;
+        waterMediumRange = 4;
+        waterLongRange = 6;
+        waterExtremeRange = 8;
         tonnage = 1.0f;
         criticals = 1;
         bv = 37;
-        cost = 300000;
-        shortAV = 7;
+        cost = 30000;
+        shortAV = 5;
         maxRange = RANGE_SHORT;
     }
 }

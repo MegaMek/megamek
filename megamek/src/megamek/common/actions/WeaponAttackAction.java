d39bd3b56ab771e11aee601c77ee01dd84de8c29
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2011-10-09 12:44:01 +0000

    + Don't allow the zero-distance hex to be the closest hex for ground-to-air fire     using advanced AA rules from SO

--- megamek/src/megamek/common/actions/WeaponAttackAction.java
+++ megamek/src/megamek/common/actions/WeaponAttackAction.java
@@ -612,10 +612,10 @@ public class WeaponAttackAction extends AbstractAttackAction implements Serializ
 
         if(Compute.isGroundToAir(ae, target) && game.getOptions().booleanOption("stratops_aa_fire")
                 && (null != te) && (te instanceof Aero)) {
-        	int vMod = ((Aero)te).getCurrentVelocity();
-        	if(game.getOptions().booleanOption("aa_move_mod")) {
-        		vMod = Math.min(vMod/2, 4);
-        	}
+            int vMod = ((Aero)te).getCurrentVelocity();
+            if(game.getOptions().booleanOption("aa_move_mod")) {
+                vMod = Math.min(vMod/2, 4);
+            }
             toHit.addModifier(vMod, "velocity");
         }
 

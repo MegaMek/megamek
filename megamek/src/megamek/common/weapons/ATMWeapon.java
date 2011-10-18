76c8fa803126cf76a01b01850040a4dc2fd49586
beerockxs <sebastian@brocks.cc>
2011-10-18 17:26:57 +0000

    + Added quarters/seating

--- megamek/src/megamek/common/weapons/ATMWeapon.java
+++ megamek/src/megamek/common/weapons/ATMWeapon.java
@@ -1,14 +1,14 @@
 /**
  * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
- * 
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License as published by the Free 
- *  Software Foundation; either version 2 of the License, or (at your option) 
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
- * 
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
 package megamek.common.weapons;
@@ -25,19 +25,19 @@ import megamek.server.Server;
 public abstract class ATMWeapon extends MissileWeapon {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -1735365348213073649L;
 
     public ATMWeapon() {
         super();
-        this.ammoType = AmmoType.T_ATM;       
-        this.atClass = CLASS_ATM;
+        ammoType = AmmoType.T_ATM;
+        atClass = CLASS_ATM;
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see megamek.common.weapons.Weapon#getCorrectHandler(megamek.common.ToHitData,
      *      megamek.common.actions.WeaponAttackAction, megamek.common.Game,
      *      megamek.server.Server)

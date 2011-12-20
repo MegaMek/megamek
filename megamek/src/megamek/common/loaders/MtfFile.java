635d582449042c6ef0ecc6d1be1fde0a20e038e9
beerockxs <sebastian@brocks.cc>
2011-12-20 13:30:35 +0000

    manual BV fixes

--- megamek/src/megamek/common/loaders/MtfFile.java
+++ megamek/src/megamek/common/loaders/MtfFile.java
@@ -1,11 +1,11 @@
 /*
  * MegaMek - Copyright (C) 2000-2002 Ben Mazur (bmazur@sev.org)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
@@ -14,7 +14,7 @@
 
 /*
  * MtfFile.java
- * 
+ *
  * Created on April 7, 2002, 8:47 PM
  */
 
@@ -444,7 +444,7 @@ public class MtfFile implements IMechLoader {
             mech.getFluff().setHistory(history);
             mech.getFluff().setMMLImagePath(imagePath);
 
-            if (bv > 0) {
+            if (bv != 0) {
                 mech.setUseManualBV(true);
                 mech.setManualBV(bv);
             }
@@ -881,7 +881,7 @@ public class MtfFile implements IMechLoader {
         }
 
         if (line.trim().toLowerCase().startsWith("bv:")) {
-            bv = Integer.parseInt(line.substring("imagefile:".length()));
+            bv = Integer.parseInt(line.substring("bv:".length()));
             return true;
         }
 

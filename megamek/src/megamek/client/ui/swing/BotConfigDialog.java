5660acbc97e7934a0a59d486243781a1a974a62a
beerockxs <sebastian@brocks.cc>
2012-01-08 23:40:06 +0000

    + Bug 3463526: bot prevents new game starting

--- megamek/src/megamek/client/ui/swing/BotConfigDialog.java
+++ megamek/src/megamek/client/ui/swing/BotConfigDialog.java
@@ -45,10 +45,11 @@ import megamek.client.ui.Messages;
 import megamek.common.Coords;
 
 /**
- * BotConfigDialog is a dialog box that configures bot properties
- * TODO add appropriate configuration parameters for each bot
+ * BotConfigDialog is a dialog box that configures bot properties TODO add
+ * appropriate configuration parameters for each bot
  */
-public class BotConfigDialog extends JDialog implements ActionListener, KeyListener {
+public class BotConfigDialog extends JDialog implements ActionListener,
+        KeyListener {
 
     /**
      *
@@ -56,24 +57,24 @@ public class BotConfigDialog extends JDialog implements ActionListener, KeyListe
     private static final long serialVersionUID = -544663266637225925L;
     private JRadioButton testbot_radiobutton;
     private JRadioButton princess_radiobutton;
-    private ButtonGroup selectbot_group=new ButtonGroup();
-    
-    //Items for princess config here
-    JComboBox princess_verbosity;
-    JTextField princess_target_hex_num_x;    
+    private ButtonGroup selectbot_group = new ButtonGroup();
+
+    // Items for princess config here
+    JComboBox<String> princess_verbosity;
+    JTextField princess_target_hex_num_x;
     JButton princess_addtarget_button;
-    JList princess_targets_list;
-    DefaultListModel princess_targets_list_model=new DefaultListModel();
+    JList<String> princess_targets_list;
+    DefaultListModel<String> princess_targets_list_model = new DefaultListModel<String>();
     JCheckBox princess_forcedwithdrawal;
 
     private JTextField namefield;
-    private boolean custom_name=false; //did user not use default name?
-    public boolean dialog_aborted=true; //did user not click Ok button?
+    private boolean custom_name = false; // did user not use default name?
+    public boolean dialog_aborted = true; // did user not click Ok button?
 
     private JButton butOK = new JButton(Messages.getString("Okay")); //$NON-NLS-1$
 
     JPanel botspecificcards;
-    
+
     public BotConfigDialog(JFrame parent) {
         super(parent, "Configure Bot", true);
         super.setResizable(false);
@@ -82,126 +83,134 @@ public class BotConfigDialog extends JDialog implements ActionListener, KeyListe
 
         butOK.addActionListener(this);
 
-        testbot_radiobutton=new JRadioButton("TestBot");
+        testbot_radiobutton = new JRadioButton("TestBot");
         testbot_radiobutton.addActionListener(this);
         testbot_radiobutton.setSelected(true);
         selectbot_group.add(testbot_radiobutton);
-        princess_radiobutton=new JRadioButton("Princess");
+        princess_radiobutton = new JRadioButton("Princess");
         princess_radiobutton.addActionListener(this);
         selectbot_group.add(princess_radiobutton);
 
-        JPanel namepanel=new JPanel(new FlowLayout());
+        JPanel namepanel = new JPanel(new FlowLayout());
         namepanel.add(new JLabel("Name: "));
-        namefield=new JTextField();
+        namefield = new JTextField();
         namefield.setColumns(12);
         namefield.setText("TestBot");
         namefield.addKeyListener(this);
         namepanel.add(namefield);
-        
-        JPanel testbotconfigcard=new JPanel();
-        
-        //JPanel princessconfigcard=new JPanel(new FlowLayout());
-        JPanel princessconfigcard=new JPanel(new GridBagLayout());
-        GridBagConstraints c=new GridBagConstraints();
-        c.gridy=0;
-        princessconfigcard.add(new JLabel("Verbosity"),c);
-        String[] verbosity_options={"0","1"};
-        princess_verbosity=new JComboBox(verbosity_options);
+
+        JPanel testbotconfigcard = new JPanel();
+
+        // JPanel princessconfigcard=new JPanel(new FlowLayout());
+        JPanel princessconfigcard = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridy = 0;
+        princessconfigcard.add(new JLabel("Verbosity"), c);
+        String[] verbosity_options = { "0", "1" };
+        princess_verbosity = new JComboBox<String>(verbosity_options);
         princess_verbosity.setSelectedIndex(1);
-        princessconfigcard.add(princess_verbosity,c);
-        c.gridy=1;
-        princessconfigcard.add(new JLabel("Strategic Targets"),c);
-        princess_target_hex_num_x=new JTextField();        
+        princessconfigcard.add(princess_verbosity, c);
+        c.gridy = 1;
+        princessconfigcard.add(new JLabel("Strategic Targets"), c);
+        princess_target_hex_num_x = new JTextField();
         princess_target_hex_num_x.setColumns(4);
-        c.gridy=2;
-        princessconfigcard.add(princess_target_hex_num_x,c);        
-        princess_addtarget_button=new JButton("Add Strategic Target");
+        c.gridy = 2;
+        princessconfigcard.add(princess_target_hex_num_x, c);
+        princess_addtarget_button = new JButton("Add Strategic Target");
         princess_addtarget_button.addActionListener(this);
-        princessconfigcard.add(princess_addtarget_button,c);
-        princess_targets_list=new JList(princess_targets_list_model);
-        princess_targets_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        princessconfigcard.add(princess_addtarget_button, c);
+        princess_targets_list = new JList<String>(princess_targets_list_model);
+        princess_targets_list
+                .setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         princess_targets_list.setLayoutOrientation(JList.VERTICAL);
-        c.gridy=3;        
-        JScrollPane ptlscroller=new JScrollPane(princess_targets_list);
+        c.gridy = 3;
+        JScrollPane ptlscroller = new JScrollPane(princess_targets_list);
         ptlscroller.setAlignmentX(LEFT_ALIGNMENT);
-        princessconfigcard.add(ptlscroller,c);
-        princess_forcedwithdrawal=new JCheckBox("Forced Withdrawal");
+        princessconfigcard.add(ptlscroller, c);
+        princess_forcedwithdrawal = new JCheckBox("Forced Withdrawal");
         princess_forcedwithdrawal.setSelected(true);
         princessconfigcard.add(princess_forcedwithdrawal);
-        
-        
-        botspecificcards=new JPanel(new CardLayout());
-        botspecificcards.add(testbotconfigcard,"testbot_config");
-        botspecificcards.add(princessconfigcard,"princess_config");
 
-        JPanel toppanel=new JPanel(new GridLayout(1,0));
-        
-        JPanel selectbotpanel=new JPanel(new GridLayout(0,1));
+        botspecificcards = new JPanel(new CardLayout());
+        botspecificcards.add(testbotconfigcard, "testbot_config");
+        botspecificcards.add(princessconfigcard, "princess_config");
+
+        JPanel toppanel = new JPanel(new GridLayout(1, 0));
+
+        JPanel selectbotpanel = new JPanel(new GridLayout(0, 1));
         selectbotpanel.add(testbot_radiobutton);
         selectbotpanel.add(princess_radiobutton);
         selectbotpanel.add(namepanel);
         selectbotpanel.add(butOK);
         toppanel.add(selectbotpanel);
         toppanel.add(botspecificcards);
-        //add(selectbotpanel);
         add(toppanel);
         pack();
     }
 
     public void actionPerformed(ActionEvent e) {
-    	CardLayout cardlayout = (CardLayout)(botspecificcards.getLayout());
-        if(e.getSource()==testbot_radiobutton) {
-            if(!custom_name) {
+        CardLayout cardlayout = (CardLayout) (botspecificcards.getLayout());
+        if (e.getSource() == testbot_radiobutton) {
+            if (!custom_name) {
                 namefield.setText("TestBot");
             }
             cardlayout.show(botspecificcards, "testbot_config");
 
-        } else if(e.getSource()==princess_radiobutton) {
-            if(!custom_name) {
+        } else if (e.getSource() == princess_radiobutton) {
+            if (!custom_name) {
                 namefield.setText("Princess");
             }
             cardlayout.show(botspecificcards, "princess_config");
 
-        } else if(e.getSource()==butOK) {
-            dialog_aborted=false;
+        } else if (e.getSource() == butOK) {
+            dialog_aborted = false;
             setVisible(false);
-        } else if(e.getSource()==princess_addtarget_button) {
-        	princess_targets_list_model.addElement(princess_target_hex_num_x.getText());
+        } else if (e.getSource() == princess_addtarget_button) {
+            princess_targets_list_model.addElement(princess_target_hex_num_x
+                    .getText());
         }
     }
 
     public void keyTyped(KeyEvent e) {
-        custom_name=true;
+        custom_name = true;
     }
 
-    public void keyReleased(KeyEvent e) {};
-    public void keyPressed(KeyEvent e) {};
+    public void keyReleased(KeyEvent e) {
+    };
 
+    public void keyPressed(KeyEvent e) {
+    };
 
     /**
      * gets the selected, configured bot from the dialog
+     *
      * @param host
      * @param port
      * @return
      */
-    BotClient getSelectedBot(String host,int port) {
-        if(testbot_radiobutton.isSelected()) {
+    BotClient getSelectedBot(String host, int port) {
+        if (testbot_radiobutton.isSelected()) {
             return new TestBot(getBotName(), host, port);
-        } else if(princess_radiobutton.isSelected()) {
-            Princess toreturn=new Princess(getBotName(),host,port);        	
-            toreturn.verbosity=princess_verbosity.getSelectedIndex();
-            //Add targets, adjusting hexes appropriately
-            for(int i=0;i<princess_targets_list_model.getSize();i++) {
-                int xpos=Integer.parseInt(((String)princess_targets_list_model.get(i)).substring(0, 2))-1;
-                int ypos=Integer.parseInt(((String)princess_targets_list_model.get(i)).substring(2, 4))-1;
-                System.err.println("adding "+Integer.toString(xpos)+" , "+Integer.toString(ypos)+" to strategic targets list");
-                toreturn.strategic_targets.add(new Coords(xpos,ypos));                
+        } else if (princess_radiobutton.isSelected()) {
+            Princess toreturn = new Princess(getBotName(), host, port);
+            toreturn.verbosity = princess_verbosity.getSelectedIndex();
+            // Add targets, adjusting hexes appropriately
+            for (int i = 0; i < princess_targets_list_model.getSize(); i++) {
+                int xpos = Integer.parseInt(princess_targets_list_model.get(i)
+                        .substring(0, 2)) - 1;
+                int ypos = Integer.parseInt(princess_targets_list_model.get(i)
+                        .substring(2, 4)) - 1;
+                System.err
+                        .println("adding " + Integer.toString(xpos) + " , "
+                                + Integer.toString(ypos)
+                                + " to strategic targets list");
+                toreturn.strategic_targets.add(new Coords(xpos, ypos));
             }
-            //do forced withdrawal?
-            toreturn.forced_withdrawal=princess_forcedwithdrawal.isSelected();
+            // do forced withdrawal?
+            toreturn.forced_withdrawal = princess_forcedwithdrawal.isSelected();
             return toreturn;
         }
-        return null;  //shouldn't happen
+        return null; // shouldn't happen
 
     }
 

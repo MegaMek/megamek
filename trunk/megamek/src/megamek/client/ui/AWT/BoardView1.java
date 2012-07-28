ab4abe0b1662f46596a623eb8c5423bb844408f7
ralgith-erian <ralgith-erian@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2012-07-28 01:08:32 +0000

    + RFE: Shutdown indicator. Red for automatic shutdown, Yellow for manual shutdown.

--- trunk/megamek/src/megamek/client/ui/AWT/BoardView1.java
+++ trunk/megamek/src/megamek/client/ui/AWT/BoardView1.java
@@ -3599,6 +3599,21 @@ public class BoardView1 extends Canvas implements IBoardView, BoardListener,
                         Messages.getString("BoardView1.LOCKED"), 21, 38); //$NON-NLS-1$
             }
 
+            // If this unit is shutdown, say so.
+            if (entity.isManualShutdown()) {
+                // draw "SHUTDOWN" for manual
+                graph.setColor(Color.darkGray);
+                graph.drawString(Messages.getString("BoardView1.SHUTDOWN"), 50, 71); //$NON-NLS-1$
+                graph.setColor(Color.yellow);
+                graph.drawString(Messages.getString("BoardView1.SHUTDOWN"), 49, 70); //$NON-NLS-1$
+            } else if (entity.isShutDown()) {
+                // draw "SHUTDOWN" for manual
+                graph.setColor(Color.darkGray);
+                graph.drawString(Messages.getString("BoardView1.SHUTDOWN"), 50, 71); //$NON-NLS-1$
+                graph.setColor(Color.red);
+                graph.drawString(Messages.getString("BoardView1.SHUTDOWN"), 49, 70); //$NON-NLS-1$
+            }
+
             // If this unit is being swarmed or is swarming another, say so.
             if (Entity.NONE != entity.getSwarmAttackerId()) {
                 // draw "SWARMED"

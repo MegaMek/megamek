c65e285e516978b0abf7124bcb5b78bed4e6f223
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2013-01-23 17:36:03 +0000

    + Bug: Firing arcs for large support vees and super heavy vehicles wrong

--- trunk/megamek/src/megamek/common/SuperHeavyTank.java
+++ trunk/megamek/src/megamek/common/SuperHeavyTank.java
@@ -262,4 +262,71 @@ public class SuperHeavyTank extends Tank {
         return NUM_OF_SLOTS;
     }
 
+    /**
+     * Returns the Compute.ARC that the weapon fires into.
+     */
+    @Override
+    public int getWeaponArc(int wn) {
+        final Mounted mounted = getEquipment(wn);
+
+        // B-Pods need to be special-cased, the have 360 firing arc
+        if ((mounted.getType() instanceof WeaponType) && mounted.getType().hasFlag(WeaponType.F_B_POD)) {
+            return Compute.ARC_360;
+        }
+        switch (mounted.getLocation()) {
+            case LOC_BODY:
+                // Body mounted C3Ms fire into the front arc,
+                // per
+                // http://forums.classicbattletech.com/index.php/topic,9400.0.html
+            case LOC_FRONT:
+                if (mounted.isPintleTurretMounted()) {
+                    return Compute.ARC_PINTLE_TURRET_FRONT;
+                }
+                if (game.getOptions().booleanOption("tacops_vehicle_arcs")) {
+                    return Compute.ARC_NOSE;
+                }
+            case LOC_TURRET:
+                if (game.getOptions().booleanOption("tacops_vehicle_arcs")) {
+                    return Compute.ARC_TURRET;
+                }
+                return Compute.ARC_FORWARD;
+            case LOC_TURRET_2:
+                return Compute.ARC_FORWARD;
+            case LOC_FRONTRIGHT:
+            case LOC_REARRIGHT:
+                if (mounted.isSponsonTurretMounted()) {
+                    return Compute.ARC_SPONSON_TURRET_RIGHT;
+                }
+                if (mounted.isPintleTurretMounted()) {
+                    return Compute.ARC_PINTLE_TURRET_RIGHT;
+                }
+                if (game.getOptions().booleanOption("tacops_vehicle_arcs")) {
+                    return Compute.ARC_RIGHT_BROADSIDE;
+                }
+                return Compute.ARC_RIGHTSIDE;
+            case LOC_FRONTLEFT:
+            case LOC_REARLEFT:
+                if (mounted.isSponsonTurretMounted()) {
+                    return Compute.ARC_SPONSON_TURRET_LEFT;
+                }
+                if (mounted.isPintleTurretMounted()) {
+                    return Compute.ARC_PINTLE_TURRET_LEFT;
+                }
+                if (game.getOptions().booleanOption("tacops_vehicle_arcs")) {
+                    return Compute.ARC_LEFT_BROADSIDE;
+                }
+                return Compute.ARC_LEFTSIDE;
+            case LOC_REAR:
+                if (mounted.isPintleTurretMounted()) {
+                    return Compute.ARC_PINTLE_TURRET_REAR;
+                }
+                if (game.getOptions().booleanOption("tacops_vehicle_arcs")) {
+                    return Compute.ARC_AFT;
+                }
+                return Compute.ARC_REAR;
+            default:
+                return Compute.ARC_360;
+        }
+    }
+
 }

87493f808a61cea193e641ead4fd86d4b3889dc8
arlith <Arlith@users.noreply.github.com>
2013-05-18 22:48:12 +0000

    + Updated ImageCache MAX_SIZE

--- megamek/src/megamek/client/ui/swing/util/ImageCache.java
+++ megamek/src/megamek/client/ui/swing/util/ImageCache.java
@@ -27,7 +27,7 @@ import java.util.List;
  */
 public class ImageCache<K, V> {
 
-    public static int MAX_SIZE = 2000;
+    public static int MAX_SIZE = 20000;
     private int maxSize;
     private Hashtable<K, V> cache;
     private LinkedList<K> lru = new LinkedList<K>();

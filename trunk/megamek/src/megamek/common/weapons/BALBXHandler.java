d527391aaed5175b1b8db358308391c6dbcbfd80
beerockxs <beerockxs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2013-08-11 19:04:43 +0000

    constructio data for BA BLX

---
+++ trunk/megamek/src/megamek/common/weapons/BALBXHandler.java
@@ -0,0 +1,55 @@
+/**
+ * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *  for more details.
+ */
+package megamek.common.weapons;
+
+import megamek.common.BattleArmor;
+import megamek.common.Compute;
+import megamek.common.IGame;
+import megamek.common.Infantry;
+import megamek.common.ToHitData;
+import megamek.common.actions.WeaponAttackAction;
+import megamek.server.Server;
+
+public class BALBXHandler extends SRMHandler implements AttackHandler {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -6378056043285522609L;
+
+    public BALBXHandler(ToHitData t, WeaponAttackAction w, IGame g, Server s) {
+        super(t, w, g, s, 0);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see megamek.common.weapons.WeaponHandler#calcDamagePerHit()
+     */
+    @Override
+    protected int calcDamagePerHit() {
+        if ((target instanceof Infantry) && !(target instanceof BattleArmor)) {
+            double toReturn = Compute.directBlowInfantryDamage(
+                    wtype.getRackSize() * 2, bDirect ? toHit.getMoS() / 3 : 0,
+                    wtype.getInfantryDamageClass(),
+                    ((Infantry) target).isMechanized());
+            if (bGlancing) {
+                toReturn /= 2;
+            }
+            return (int) Math.floor(toReturn);
+        }
+        return 1;
+    }
+
+}

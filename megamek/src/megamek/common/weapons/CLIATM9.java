312f9e8ec7304ca2f07c1fa382b614910366a237
arlith <Arlith@users.noreply.github.com>
2013-11-20 17:53:09 +0000

    Forgot to add new files from the WoR equipment patch

---
+++ megamek/src/megamek/common/weapons/CLIATM9.java
@@ -0,0 +1,55 @@
+/**
+ * MegaMek - Copyright (C) 2005 Ben Mazur (bmazur@sev.org)
+ * 
+ *  This program is free software; you can redistribute it and/or modify it 
+ *  under the terms of the GNU General Public License as published by the Free 
+ *  Software Foundation; either version 2 of the License, or (at your option) 
+ *  any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *  for more details.
+ */
+package megamek.common.weapons;
+
+import megamek.common.TechConstants;
+
+/**
+ * @author Sebastian Brocks, edited by Greg
+ */
+public class CLIATM9 extends CLIATMWeapon {
+
+    /**
+     * I think i can just assign 1? I don't think SVUIDs conflict with those from other classes
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 
+     */
+    public CLIATM9() {
+        super();
+        techLevel.put(3070, TechConstants.T_CLAN_EXPERIMENTAL);
+        this.name = "iATM 9";
+        this.setInternalName("CLiATM9");
+        this.addLookupName("Clan iATM-9");
+        this.heat = 6;
+        this.rackSize = 9;
+        this.minimumRange = 4;
+        this.shortRange = 5;
+        this.mediumRange = 10;
+        this.longRange = 15;
+        this.extremeRange = 20;
+        this.tonnage = 5.0f;
+        this.criticals = 4;
+        this.bv = 231; // Ammo BV is 54
+        this.cost = 450000;
+        this.shortAV = 18;
+        this.medAV = 18;
+        this.maxRange = RANGE_MED;
+        introDate = 3070;
+        availRating = new int[]{RATING_X,RATING_X,RATING_F};
+        techRating = RATING_F;
+    }
+}

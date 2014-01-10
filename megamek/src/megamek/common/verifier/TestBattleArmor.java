c5adb246766d810832081f20836030e845c65bca
arlith <Arlith@users.noreply.github.com>
2014-01-10 17:02:15 +0000

    + Added a BAArmor enum to TestBattleArmor.  This works like the AeroArmor enum in TestAero and is used to define what subset of all armors are legal for BattleArmor.

--- megamek/src/megamek/common/verifier/TestBattleArmor.java
+++ megamek/src/megamek/common/verifier/TestBattleArmor.java
@@ -27,9 +27,66 @@ import megamek.common.EquipmentType;
 
 
 public class TestBattleArmor extends TestEntity {
+    
+    /**
+     * An enumeration that keeps track of the legal armors for BattleArmor.  
+     * Each entry consists of the type, which 
+     * corresponds to the types defined in <code>EquipmentType</code>.
+     * 
+     * @author arlith
+     *
+     */
+    public static enum BAArmor{
+        STANDARD(EquipmentType.T_ARMOR_STANDARD,false),   
+        CLAN_STANDARD(EquipmentType.T_ARMOR_STANDARD,true),
+        STANDARD_PROTOTYPE(EquipmentType.T_ARMOR_BA_STANDARD_PROTOTYPE,false),
+        STANDARD_ADVANCED(EquipmentType.T_ARMOR_BA_STANDARD_ADVANCED,false),
+        STEALTH_BASIC(EquipmentType.T_ARMOR_BA_STEALTH_BASIC,false),
+        STEALTH(EquipmentType.T_ARMOR_BA_STEALTH,false),
+        STEALTH_IMPROVED(EquipmentType.T_ARMOR_BA_STEALTH_IMP,false),
+        STEALTH_PROTOTYPE(EquipmentType.T_ARMOR_BA_STEALTH_PROTOTYPE,false),
+        FIRE_RESISTANT(EquipmentType.T_ARMOR_BA_FIRE_RESIST,false),
+        MIMETIC(EquipmentType.T_ARMOR_BA_MIMETIC,false);
+
+        /**
+         * The type, corresponding to types defined in 
+         * <code>EquipmentType</code>.
+         */
+        public int type;
+        
+        /**
+         * Denotes whether this armor is Clan or not.
+         */
+        public boolean isClan;
+        
+        BAArmor(int t, boolean c){
+            type = t;
+            isClan = c;
+        }
+        
+        /**
+         * Given an armor type, return the <code>AeroArmor</code> instance that
+         * represents that type.
+         * 
+         * @param t  The armor type.
+         * @param c  Whether this armor type is Clan or not.
+         * @return   The <code>AeroArmor</code> that correspondes to the given 
+         *              type or null if no match was found.
+         */
+        public static BAArmor getArmor(int t, boolean c){
+            for (BAArmor a : values()){
+                if (a.type == t && a.isClan == c){
+                    return a;
+                }
+            }
+            return null;
+        }
+    }
+    
     private BattleArmor ba;
     
-    public TestBattleArmor(BattleArmor armor, TestEntityOption option, String fileString) {
+    public TestBattleArmor(BattleArmor armor, TestEntityOption option,
+            String fileString) {
         super(option, null, null, null);
         this.ba = armor;
         this.fileString = fileString;
@@ -156,7 +213,9 @@ public class TestBattleArmor extends TestEntity {
     
     @Override
     public float getWeightArmor() {
-        return ba.getOArmor(1) * EquipmentType.getBaArmorWeightPerPoint(ba.getArmorType(1), ba.isClan());
+        return ba.getOArmor(1)
+                * EquipmentType.getBaArmorWeightPerPoint(ba.getArmorType(1),
+                        ba.isClan());
     }
     
     @Override

977872cc2f2198e8fc2275d803919e74e334d7f6
hammer-gs <hammer-gs@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2014-02-04 03:30:54 +0000

    + Bug BA Weapon fixes + Data some new and corrected units.

--- trunk/megamek/src/megamek/common/weapons/battlearmor/CLBASRM5.java
+++ trunk/megamek/src/megamek/common/weapons/battlearmor/CLBASRM5.java
@@ -49,8 +49,8 @@ public class CLBASRM5 extends SRMWeapon {
         cost = 25000;
         shortAV = 4;
         maxRange = RANGE_SHORT;
-        introDate = 2824;
-        techLevel.put(2824, techLevel.get(3071));
+        introDate = 2868;
+        techLevel.put(2868, techLevel.get(3071));
         availRating = new int[] { RATING_X, RATING_X, RATING_D };
         techRating = RATING_F;
     }

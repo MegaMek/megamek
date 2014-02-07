7cf9ae6b88a389ae0d2cd2ec592fbbc63893d6a0
arlith <Arlith@users.noreply.github.com>
2014-02-07 03:08:08 +0000

    + Added code to MechView so that BA armors don't show in equipment

--- megamek/src/megamek/common/MechView.java
+++ megamek/src/megamek/common/MechView.java
@@ -684,6 +684,11 @@ public class MechView {
                     || (name.indexOf("Endo Steel") != -1) //$NON-NLS-1$
                     || (name.indexOf("Ferro-Fibrous") != -1) //$NON-NLS-1$
                     || (name.indexOf("Reactive") != -1) //$NON-NLS-1$
+                    || (name.indexOf("BA Stealth") != -1) //$NON-NLS-1$
+                    || (name.indexOf("BA Fire Resistant") != -1) //$NON-NLS-1$
+                    || (name.indexOf("BA Mimetic") != -1) //$NON-NLS-1$
+                    || (name.indexOf("BA Standard") != -1) //$NON-NLS-1$
+                    || (name.indexOf("BA Advanced") != -1) //$NON-NLS-1$
                     || (name.indexOf("Reflective") != -1) //$NON-NLS-1$
                     || (name.indexOf("Ferro-Lamellor") != -1)) { //$NON-NLS-1$
                 // These items are displayed elsewhere, so skip them here.

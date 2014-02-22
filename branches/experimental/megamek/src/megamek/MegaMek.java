d073cd8c45bbe72903d6a41009c8eafaba329958
ralgith-erian <ralgith-erian@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2014-02-22 23:06:09 +0000

    + experimental snapshot <0.37.8-exp>

--- branches/experimental/megamek/src/megamek/MegaMek.java
+++ branches/experimental/megamek/src/megamek/MegaMek.java
@@ -56,7 +56,7 @@ import megamek.server.DedicatedServer;
  */
 public class MegaMek {
 
-    public static String VERSION = "0.37.8-exp-dev"; //$NON-NLS-1$
+    public static String VERSION = "0.37.8-exp"; //$NON-NLS-1$
     public static long TIMESTAMP = new File(PreferenceManager
             .getClientPreferences().getLogDirectory()
             + File.separator

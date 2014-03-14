MegaMek/megamek

commit 0abc742c9a0d74054bd612d47f220b71c716b0cf

+ Bug [#2244]: ECM in Double Blind games -> wrong hit roll reporting
git-svn-id: svn://svn.code.sf.net/p/megamek/code/trunk@10660 c34074a7-3587-4b08-b2b8-7c33201a20e7

megamek/src/megamek/client/ui/swing/BoardView1.java
@@ -2417,6 +2417,10 @@ public class BoardView1 extends JPanel implements IBoardView, Scrollable,
             if (entity.getPosition() == null) {
                 continue;
             }
+            if (entity.getOwner().isEnemyOf(localPlayer) 
+                    && !entity.isSeenByEnemy()){
+                continue;
+            }
             if (entity.getSecondaryPositions().isEmpty()) {
                 EntitySprite sprite = new EntitySprite(entity, -1);
                 newSprites.add(sprite);

megamek/src/megamek/client/ui/swing/FiringDisplay.java
@@ -475,7 +475,7 @@ public class FiringDisplay extends StatusBarPhaseDisplay implements
                     "friendly_fire"); //$NON-NLS-1$
             if ((target.getId() != cen)
                     && (friendlyFire || (target.getOwnerId() != ownerId))
-                    && target.isTargetable()) {
+                    && target.isVisibleToEnemy() && target.isTargetable()) {
                 ToHitData thd = WeaponAttackAction.toHit(game, cen, target);
                 thd.setLocation(target.getPosition());
                 thd.setRange(ce().getPosition().distance(target.getPosition()));

megamek/src/megamek/server/Server.java
@@ -25578,6 +25578,22 @@ public class Server implements Runnable {
         if (doBlind()) {
             Vector<IPlayer> playersVector = game.getPlayersVector();
             Vector<IPlayer> vCanSee = whoCanSee(eTarget);
+            
+            // If this unit has ECM, players with units effected by the ECM will
+            //  need to know about this entity, even if they can't see it.
+            //  Otherwise, the client can't properly report things like to-hits.
+            if (eTarget.getECMRange() > 0 && eTarget.getPosition() != null){
+                int ecmRange = eTarget.getECMRange();
+                Coords pos = eTarget.getPosition();
+                for (Entity ent : game.getEntitiesVector()){
+                    if (ent.getPosition() != null
+                            && pos.distance(ent.getPosition()) <= ecmRange) {
+                        if (!vCanSee.contains(ent.getOwner())){
+                            vCanSee.add(ent.getOwner());
+                        }
+                    }
+                }
+            }
 
             // send an entity update to everyone who can see
             Packet pack = createEntityPacket(nEntityID, movePath);
@@ -25809,6 +25825,19 @@ public class Server implements Runnable {
                 if (Compute.canSee(game, spotter, e)) {
                     addVisibleEntity(vCanSee, e);
                     break;
+                }
+                
+                // If this unit has ECM, players with units effected by the ECM
+                //  will need to know about this entity, even if they can't see
+                //  it.  Otherwise, the client can't properly report things 
+                //  like to-hits.
+                if (e.getECMRange() > 0 && e.getPosition() != null &&
+                        spotter.getPosition() != null){
+                    int ecmRange = e.getECMRange();
+                    Coords pos = e.getPosition();
+                    if (pos.distance(spotter.getPosition()) <= ecmRange){
+                        addVisibleEntity(vCanSee, e);
+                    }
                 }
             }
         }

commit 5f8e3db1f88e3c97c9ba642a246c6048d7602999

+ Bug: Fixed a bug introduced in [r10660] (when fixing [#2244]): the code to     determine if an entity should be drawn was using isEverSeenByEnemy instead     of isVisibleToEntity + Bug: Los darkening for sensor ranges was using a >= comparison for min sensor     range instead of >.  The min range is not inclusive as witnessed in     Compute.inSensorRange + Bug [#4068]: In double-blind, units detected by sensors only should not be     aware + Renamed Entity.isSeenByEnemy to Entity.isEverSeenByEnemy, to reduce confusion     with Entity.isVisibleByEnemy

megamek/src/megamek/client/Client.java
@@ -925,8 +925,9 @@ public class Client implements IClientCommandHandler {
     protected void receiveEntityVisibilityIndicator(Packet packet) {
         Entity e = game.getEntity(packet.getIntValue(0));
         if (e != null) { // we may not have this entity due to double blind
-            e.setSeenByEnemy(packet.getBooleanValue(1));
+            e.setEverSeenByEnemy(packet.getBooleanValue(1));
             e.setVisibleToEnemy(packet.getBooleanValue(2));
+            e.setDetectedByEnemy(packet.getBooleanValue(3));
             // this next call is only needed sometimes, but we'll just
             // call it everytime
             game.processGameEvent(new GameEntityChangeEvent(this, e));

megamek/src/megamek/client/ui/swing/BoardView1.java
@@ -1823,7 +1823,7 @@ public class BoardView1 extends JPanel implements IBoardView, Scrollable,
                         if (darken) {
                             if (game.getOptions().booleanOption(
                                     "tacops_sensors")
-                                    && (dist >= minSensorRange)
+                                    && (dist > minSensorRange)
                                     && (dist <= maxSensorRange)) {
                                 drawHexLayer(p, boardGraph,
                                         transparent_light_gray);
@@ -2419,7 +2419,8 @@ public class BoardView1 extends JPanel implements IBoardView, Scrollable,
             }
             if ((localPlayer != null) 
                     && entity.getOwner().isEnemyOf(localPlayer) 
-                    && !entity.isSeenByEnemy()){
+                    && !entity.isVisibleToEnemy()
+                    && !entity.isDetectedByEnemy()){
                 continue;
             }
             if (entity.getSecondaryPositions().isEmpty()) {
@@ -4566,7 +4567,7 @@ public class BoardView1 extends JPanel implements IBoardView, Scrollable,
 
                 // If this unit is currently unknown to the enemy, say so.
                 if (trackThisEntitiesVisibilityInfo(entity)) {
-                    if (!entity.isSeenByEnemy()) {
+                    if (!entity.isEverSeenByEnemy()) {
                         // draw "U"
                         graph.setColor(Color.darkGray);
                         graph.drawString("U", 30, 71); //$NON-NLS-1$

megamek/src/megamek/client/ui/swing/FiringDisplay.java
@@ -476,7 +476,8 @@ public class FiringDisplay extends StatusBarPhaseDisplay implements
             boolean enemyTarget = target.getOwner().isEnemyOf(ce().getOwner());
             if ((target.getId() != cen)
                     && (friendlyFire || enemyTarget)
-                    && (!enemyTarget || target.isVisibleToEnemy()) 
+                    && (!enemyTarget || target.isVisibleToEnemy() 
+                            || target.isDetectedByEnemy()) 
                     && target.isTargetable()) {
                 ToHitData thd = WeaponAttackAction.toHit(game, cen, target);
                 thd.setLocation(target.getPosition());

megamek/src/megamek/common/Compute.java
@@ -3516,10 +3516,19 @@ public class Compute {
     }
 
     /**
-     * Slightly misnamed. Checks to see if the target is visible to the unit,
-     * either visually or through sensors
+     * Checks to see if the target is visible to the unit, always considering 
+     * sensors.
      */
     public static boolean canSee(IGame game, Entity ae, Targetable target) {
+        return canSee(game, ae, target, true);
+    }
+    
+    /**
+     * Checks to see if the target is visible to the unit, if the sensor flag
+     * is true then sensors are checked as well.
+     */
+    public static boolean canSee(IGame game, Entity ae, Targetable target, 
+            boolean useSensors) {
 
         if (!ae.getCrew().isActive()) {
             return false;
@@ -3527,10 +3536,14 @@ public class Compute {
         if (target.isOffBoard()) {
             return false;
         }
-
-        return (LosEffects.calculateLos(game, ae.getId(), target).canSee() && Compute
-                .inVisualRange(game, ae, target))
-                || Compute.inSensorRange(game, ae, target);
+        
+        LosEffects los = LosEffects.calculateLos(game, ae.getId(), target);
+        boolean isVisible = los.canSee() 
+                && Compute.inVisualRange(game, ae, target);
+        if (useSensors){
+            isVisible = isVisible || Compute.inSensorRange(game, ae, target);
+        }
+        return isVisible;
     }
 
     /**

megamek/src/megamek/common/Entity.java
@@ -472,13 +472,19 @@ public abstract class Entity extends TurnOrdered implements Transporter,
      * Indicates whether this entity has been seen by the enemy during the
      * course of this game. Used in double-blind.
      */
-    private boolean seenByEnemy = false;
+    private boolean everSeenByEnemy = false;
 
     /**
      * Indicates whether this entity can currently be seen by the enemy. Used in
      * double-blind.
      */
     private boolean visibleToEnemy = false;
+    
+    /**
+     * Flag that indicates whether this entity has been detected by sensors by
+     * an enemy.
+     */
+    private boolean detectedByEnemy;
 
     /**
      * Check to see who has seen this Entity Used for Double Blind Reports.
@@ -7867,12 +7873,12 @@ public abstract class Entity extends TurnOrdered implements Transporter,
                                                   .getHeight() - 1)));
     }
 
-    public void setSeenByEnemy(boolean b) {
-        seenByEnemy = b;
+    public void setEverSeenByEnemy(boolean b) {
+        everSeenByEnemy = b;
     }
 
-    public boolean isSeenByEnemy() {
-        return seenByEnemy;
+    public boolean isEverSeenByEnemy() {
+        return everSeenByEnemy;
     }
 
     public void setVisibleToEnemy(boolean b) {
@@ -7881,6 +7887,14 @@ public abstract class Entity extends TurnOrdered implements Transporter,
 
     public boolean isVisibleToEnemy() {
         return visibleToEnemy;
+    }
+    
+    public void setDetectedByEnemy(boolean b){
+        detectedByEnemy = b;
+    }
+    
+    public boolean isDetectedByEnemy(){
+        return detectedByEnemy;
     }
 
     public void addBeenSeenBy(IPlayer p) {

megamek/src/megamek/server/Server.java
@@ -25706,9 +25706,22 @@ public class Server implements Runnable {
     }
 
     /**
-     * Returns a vector of which players can see this entity.
+     * Returns a vector of which players can see this entity, always allowing
+     * for sensor detections.
      */
     private Vector<IPlayer> whoCanSee(Entity entity) {
+        return whoCanSee(entity, true);
+    }
+        
+    /**
+     * Returns a vector of which players can see the given entity, optionally
+     * allowing for sensors to count.
+     * 
+     * @param entity        The entity to check visiblity for
+     * @param useSensors    A flag that determines whether sensors are allowed
+     * @return              A vector of the players who can see the entity
+     */
+    private Vector<IPlayer> whoCanSee(Entity entity, boolean useSensors) {
 
         // Some times Null entities are sent to this
         if (entity == null) {
@@ -25733,8 +25746,7 @@ public class Server implements Runnable {
             }
         }
 
-        // If the entity is hidden, skip this; noone else will be able to see
-        // it.
+        // If the entity is hidden, skip; noone else will be able to see it.
         if (!entity.isHidden()) {
             for (int i = 0; i < vEntities.size(); i++) {
                 Entity e = vEntities.elementAt(i);
@@ -25746,7 +25758,7 @@ public class Server implements Runnable {
                 if (e.isOffBoard()) {
                     continue;
                 }
-                if (Compute.canSee(game, e, entity)) {
+                if (Compute.canSee(game, e, entity, useSensors)) {
                     if (!vCanSee.contains(e.getOwner())){
                         vCanSee.addElement(e.getOwner());
                     }
@@ -25759,6 +25771,40 @@ public class Server implements Runnable {
         }
 
         return vCanSee;
+    }
+    
+    private Vector<IPlayer> whoCanDetect(Entity entity) {
+
+        boolean bTeamVision = game.getOptions().booleanOption("team_vision");
+        Vector<Entity> vEntities = game.getEntitiesVector();
+
+        Vector<IPlayer> vCanDetect = new Vector<IPlayer>();
+
+        // If the entity is hidden, skip; noone else will be able to detect it
+        if (!entity.isHidden()) {
+            for (int i = 0; i < vEntities.size(); i++) {
+                Entity e = vEntities.elementAt(i);
+                if (vCanDetect.contains(e.getOwner()) || !e.isActive()) {
+                    continue;
+                }
+
+                // Off board units should not spot on board units
+                if (e.isOffBoard()) {
+                    continue;
+                }
+                if (Compute.inSensorRange(game, e, entity)) {
+                    if (!vCanDetect.contains(e.getOwner())){
+                        vCanDetect.addElement(e.getOwner());
+                    }
+                    if (bTeamVision) {
+                        addTeammates(vCanDetect, e.getOwner());
+                    }
+                    addObservers(vCanDetect);
+                }
+            }
+        }
+
+        return vCanDetect;
     }
 
     /**
@@ -26104,22 +26150,31 @@ public class Server implements Runnable {
         for (int x = 0; x < vAllEntities.size(); x++) {
             Entity e = vAllEntities.elementAt(x);
             boolean previousVisibleValue = e.isVisibleToEnemy();
-            boolean previousSeenValue = e.isSeenByEnemy();
+            boolean previousSeenValue = e.isEverSeenByEnemy();
+            boolean previousDetectedValue = e.isDetectedByEnemy();
             e.setVisibleToEnemy(false);
-            Vector<IPlayer> vCanSee = whoCanSee(e);
-            for (int y = 0; y < vCanSee.size(); y++) {
-                IPlayer p = vCanSee.elementAt(y);
+            e.setDetectedByEnemy(false);
+            Vector<IPlayer> vCanSee = whoCanSee(e, false);
+            for (IPlayer p : vCanSee) {
                 if (e.getOwner().isEnemyOf(p) && !p.isObserver()) {
                     e.setVisibleToEnemy(true);
-                    e.setSeenByEnemy(true);
+                    e.setEverSeenByEnemy(true);
+                }
+            }
+            Vector<IPlayer> vCanDetect = whoCanDetect(e);
+            for (IPlayer p : vCanDetect) {
+                if (e.getOwner().isEnemyOf(p) && !p.isObserver()) {
+                    e.setDetectedByEnemy(true);
                 }
             }
             // If this unit wasn't previously visible and is now visible, it's
             // possible that the enemy's client doesn't know about the unit
-            if (!previousVisibleValue && e.isVisibleToEnemy()){
+            if ((!previousVisibleValue && e.isVisibleToEnemy())
+                    || (!previousDetectedValue && e.isDetectedByEnemy())){
                 entityUpdate(e.getId());
             } else if ((previousVisibleValue != e.isVisibleToEnemy())
-                    || (previousSeenValue != e.isSeenByEnemy())) {
+                    || (previousSeenValue != e.isEverSeenByEnemy())
+                    || (previousDetectedValue != e.isDetectedByEnemy())) {
                 sendVisibilityIndicator(e);
             }
         }
@@ -27226,10 +27281,11 @@ public class Server implements Runnable {
     }
 
     public void sendVisibilityIndicator(Entity e) {
-        final Object[] data = new Object[3];
+        final Object[] data = new Object[4];
         data[0] = new Integer(e.getId());
-        data[1] = Boolean.valueOf(e.isSeenByEnemy());
+        data[1] = Boolean.valueOf(e.isEverSeenByEnemy());
         data[2] = Boolean.valueOf(e.isVisibleToEnemy());
+        data[3] = Boolean.valueOf(e.isDetectedByEnemy());
         send(new Packet(Packet.COMMAND_ENTITY_VISIBILITY_INDICATOR, data));
     }
 
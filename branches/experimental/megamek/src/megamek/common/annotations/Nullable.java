5bb5c0490090b2cbae204f9daec2fec14556b26b
ralgith-erian <ralgith-erian@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2014-03-29 23:16:23 +0000

    + [experimental] merged in trunk

--- branches/experimental/megamek/src/megamek/common/annotations/Nullable.java
+++ branches/experimental/megamek/src/megamek/common/annotations/Nullable.java
@@ -16,8 +16,6 @@ package megamek.common.annotations;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**

6bfedb759335abeb87cc61483aa88ce7279a1fe6
dericpage <dericpage@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2014-05-15 19:37:00 +0000

    Feature Request [#1188]: RATs calling other RATs.  Added code to recognize the "@" symbol as a reference to anther RAT to roll on.  Updated the EXAMPLE RAT FILE.txt file to reflect this information and also copied it into the docs folder to make it easier to find.

--- branches/experimental/megamek/src/megamek/client/RandomUnitGenerator.java
+++ branches/experimental/megamek/src/megamek/client/RandomUnitGenerator.java
@@ -63,7 +63,7 @@ public class RandomUnitGenerator implements Serializable {
     // and the vectors just contain the unit names listed a number of times
     // equal to
     // the frequency
-    Map<String, RatEntry> rats;
+    private Map<String, RatEntry> rats;
     private static RandomUnitGenerator rug;
     private static boolean interrupted = false;
     private static boolean dispose = false;
@@ -81,7 +81,7 @@ public class RandomUnitGenerator implements Serializable {
     public static class RatTreeNode implements Comparable<RatTreeNode> {
         public RatTreeNode(String name) {
             this.name = name;
-            children = new Vector<RatTreeNode>();
+            children = new Vector<>();
         }
 
         public int compareTo(RatTreeNode rtn) {
@@ -99,13 +99,29 @@ public class RandomUnitGenerator implements Serializable {
      * @author arlith
      *
      */
-    private class RatEntry {
-        public Vector<String> units;
-        public Vector<Float> weights;
+    protected class RatEntry {
+        private Vector<String> units;
+        private Vector<Float> weights;
         
         RatEntry(){
-            units = new Vector<String>();
-            weights = new Vector<Float>();
+            setUnits(new Vector<String>());
+            setWeights(new Vector<Float>());
+        }
+
+        public Vector<String> getUnits() {
+            return units;
+        }
+
+        public void setUnits(Vector<String> units) {
+            this.units = units;
+        }
+
+        public Vector<Float> getWeights() {
+            return weights;
+        }
+
+        public void setWeights(Vector<Float> weights) {
+            this.weights = weights;
         }
     }
 
@@ -116,12 +132,20 @@ public class RandomUnitGenerator implements Serializable {
 
     public RandomUnitGenerator() {
         chosenRAT = "TW Heavy Mech (Kurita)";
-        listeners = new ArrayList<ActionListener>();
+        listeners = new ArrayList<>();
+    }
+
+    protected void initRats() {
+        rats = new HashMap<>();
+    }
+
+    protected void initRatTree() {
+        ratTree = new RatTreeNode("Random Assignment Tables");
     }
 
     public synchronized void populateUnits() {
-        rats = new HashMap<String, RatEntry>();
-        ratTree = new RatTreeNode("Random Assignment Tables");
+        initRats();
+        initRatTree();
 
         loadRatsFromDirectory(Configuration.armyTablesDir());
         if (!interrupted) {
@@ -138,7 +162,9 @@ public class RandomUnitGenerator implements Serializable {
     public synchronized void registerListener(ActionListener l){
         listeners.add(l);
     }
-    
+
+    @SuppressWarnings("UnusedDeclaration")
+    // todo Not being used.  Is this really needed?
     public synchronized void removeListener(ActionListener l){
         listeners.remove(l);
     }
@@ -153,6 +179,10 @@ public class RandomUnitGenerator implements Serializable {
                         this,ActionEvent.ACTION_PERFORMED,"rugInitialized"));
             }
         }
+    }
+
+    protected void addRat(String ratName, RatEntry ratEntry) {
+        rats.put(ratName, ratEntry);
     }
 
     private void loadRatsFromDirectory(File dir) {
@@ -170,29 +200,27 @@ public class RandomUnitGenerator implements Serializable {
             return;
         }
 
-        RatTreeNode oldParentNode = null;
+        RatTreeNode oldParentNode;
         if (null == currentNode) {
             currentNode = ratTree;
-            oldParentNode = currentNode;
         }
 
-        Scanner input = null;
+        Scanner input;
 
-        for (File file : files) {
+        for (File ratFile : files) {
             // Check to see if we've been interrupted
             if (interrupted) {
                 return;
             }
 
             // READ IN RATS
-            if (file.isDirectory()) {
-                if (file.getName().toLowerCase().equals("_svn")
-                        || file.getName().toLowerCase().equals(".svn")) {
+            if (ratFile.isDirectory()) {
+                if (ratFile.getName().toLowerCase().equals("_svn") || ratFile.getName().toLowerCase().equals(".svn")) {
                     // This is a Subversion work directory. Lets ignore it.
                     continue;
                 }
 
-                RatTreeNode newNode = new RatTreeNode(file.getName());
+                RatTreeNode newNode = new RatTreeNode(ratFile.getName());
                 oldParentNode = currentNode;
                 currentNode = newNode;
 
@@ -202,10 +230,9 @@ public class RandomUnitGenerator implements Serializable {
                 }
 
                 // recursion is fun
-                loadRatsFromDirectory(file);
+                loadRatsFromDirectory(ratFile);
 
-                // Prune empty nodes (this removes the "Unofficial" place
-                // holder)
+                // Prune empty nodes (this removes the "Unofficial" place holder)
                 if (currentNode.children.size() == 0) {
                     oldParentNode.children.remove(currentNode);
                 }
@@ -213,14 +240,14 @@ public class RandomUnitGenerator implements Serializable {
                 currentNode = oldParentNode;
                 continue;
             }
-            if (!file.getName().toLowerCase().endsWith(".txt")) {
+            if (!ratFile.getName().toLowerCase().endsWith(".txt")) {
                 continue;
             }
-            FileInputStream ratst = null;
+            FileInputStream ratInputStream = null;
             try {
-                ratst = new FileInputStream(file);
-                input = new Scanner(ratst, "UTF-8");
-                int linen = 0;
+                ratInputStream = new FileInputStream(ratFile);
+                input = new Scanner(ratInputStream, "UTF-8");
+                int lineNumber = 0;
                 String key = "Huh";
                 RatEntry re = new RatEntry();
                 while (input.hasNextLine()) {
@@ -231,45 +258,45 @@ public class RandomUnitGenerator implements Serializable {
                     if (line.startsWith("#")) {
                         continue;
                     }
-                    linen++;
-                    if (linen == 1) {
+                    lineNumber++;
+                    if (lineNumber == 1) {
                         key = line;
                     } else {
                         String[] values = line.split(",");
                         if (values.length < 2) {
-                            System.err.println("Not enough fields in "
-                                    + file.getName() + " on " + linen);
+                            System.err.println("Not enough fields in " + ratFile.getName() + " on " + lineNumber);
                             continue;
                         }
                         String name = values[0];
-                        float weight = 0;
+                        float weight;
                         try {
                             weight = Integer.parseInt(values[1].trim());
                         } catch (NumberFormatException nef) {
-                            System.err
-                                    .println("the frequency field could not be interpreted on line "
-                                            + linen + " of " + file.getName());
+                            System.err.println("the frequency field could not be interpreted on line "
+                                               + lineNumber + " of " + ratFile.getName());
                             continue;
                         }
-                        MechSummary unit = MechSummaryCache.getInstance()
-                                .getMech(name);
-                        if (null == unit) {
-                            System.err.println("The unit " + name
-                                    + " could not be found in the " + key
-                                    + " RAT");
+
+                        // The @ symbol denotes a reference to another RAT rather than a unit.
+                        MechSummary unit = null;
+                        if (!name.startsWith("@")) {
+                            unit = MechSummaryCache.getInstance().getMech(name);
+                        }
+                        if ((null == unit) && !name.startsWith("@")) {
+                            System.err.println("The unit " + name + " could not be found in the " + key + " RAT");
                         } else {
-                            re.units.add(name);
-                            re.weights.add(weight);
+                            re.getUnits().add(name);
+                            re.getWeights().add(weight);
                         }
                     }
                 }
-                if (re.units.size() > 0) {
+                if (re.getUnits().size() > 0) {
                     float sum = 0;
-                    for (int i = 0; i < re.weights.size(); i++){
-                        sum += re.weights.get(i);
+                    for (int i = 0; i < re.getWeights().size(); i++) {
+                        sum += re.getWeights().get(i);
                     }
-                    for (int i = 0; i < re.weights.size(); i++){
-                        re.weights.set(i, re.weights.get(i)/sum);
+                    for (int i = 0; i < re.getWeights().size(); i++) {
+                        re.getWeights().set(i, re.getWeights().get(i) / sum);
                     }  
                     rats.put(key, re);
                     if (null != currentNode) {
@@ -277,11 +304,11 @@ public class RandomUnitGenerator implements Serializable {
                     }
                 }
             } catch (FileNotFoundException fne) {
-                System.err.println("Unable to find " + file.getName());
+                System.err.println("Unable to find " + ratFile.getName());
             } finally {
-                if (ratst != null){
+                if (ratInputStream != null) {
                     try {
-                        ratst.close();
+                        ratInputStream.close();
                     } catch (Exception e){
                         // Nothing to do...
                     }
@@ -293,33 +320,60 @@ public class RandomUnitGenerator implements Serializable {
     }
 
     /**
-     * Generate a single random name
+     * Generate a list of units from the RAT
      * 
      * @return - a string giving the name
      */
-    public ArrayList<MechSummary> generate(int numRolls) {
-        ArrayList<MechSummary> units = new ArrayList<MechSummary>();
+    public ArrayList<MechSummary> generate(int numRolls, String ratName) {
+        ArrayList<MechSummary> units = new ArrayList<>();
 
-        if (null != rats) {
-            RatEntry re = rats.get(chosenRAT);
-            if ((null != re) && (re.units.size() > 0)) {
-                for (int roll = 0; roll < numRolls; roll++) {
-                    double rand = Math.random();
-                    int i = 0;
-                    while (i < re.weights.size() && rand > re.weights.get(i)) {
-                        i++;
-                        rand -= re.weights.get(i);
-                    }
-                    String name = re.units.get(i);
-                    MechSummary unit = MechSummaryCache.getInstance().getMech(
-                            name);
-                    if (null != unit) {
-                        units.add(unit);
+        try {
+            Map<String, RatEntry> ratMap = getRatMap();
+            if (null != ratMap) {
+                RatEntry re = ratMap.get(ratName);
+                if ((null != re) && (re.getUnits().size() > 0)) {
+                    for (int roll = 0; roll < numRolls; roll++) {
+                        double rand = getRandom();
+                        int i = 0;
+                        while (i < re.getWeights().size() && rand > re.getWeights().get(i)) {
+                            rand -= re.getWeights().get(i);
+                            i++;
+                        }
+                        String name = re.getUnits().get(i);
+
+                        // If this is a RAT reference, roll the unit on the referenced RAT.
+                        if (name.startsWith("@")) {
+                            units.addAll(generate(1, name.replaceFirst("@", "")));
+                            continue;
+                        }
+
+                        MechSummary unit = getMechByName(name);
+                        if (null != unit) {
+                            units.add(unit);
+                        }
                     }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return units;
+    }
+
+    protected MechSummary getMechByName(String name) {
+        return MechSummaryCache.getInstance().getMech(name);
+    }
+
+    protected double getRandom() {
+        return Math.random();
+    }
+
+    public ArrayList<MechSummary> generate(int numRolls) {
+        return generate(numRolls, getChosenRAT());
+    }
+
+    public Map<String, RatEntry> getRatMap() {
+        return new HashMap<>(rats);
     }
 
     public String getChosenRAT() {

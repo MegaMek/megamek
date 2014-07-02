4f49c4c72ca40454fd5195405a9d5b5690aea64b
ralgith-erian <ralgith-erian@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2014-07-02 21:07:43 +0000

    + [experimental] merged in trunk

--- branches/experimental/megamek/src/megamek/client/bot/princess/Princess.java
+++ branches/experimental/megamek/src/megamek/client/bot/princess/Princess.java
@@ -558,7 +558,6 @@ public class Princess extends BotClient {
         return !entity.isImmobile() && wantsToFallBack(entity);
     }
 
-    @SuppressWarnings("RedundantIfStatement")
     protected boolean mustFleeBoard(Entity entity) {
         if (!isFallingBack(entity)) {
             return false;

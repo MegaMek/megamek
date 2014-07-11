223edc2544288bbc6759cfd78ae1c4120d1018fe
ralgith-erian <ralgith-erian@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2014-07-11 21:03:32 +0000

    + Warning cleanup

--- trunk/megamek/src/megamek/client/bot/MoveOption.java
+++ trunk/megamek/src/megamek/client/bot/MoveOption.java
@@ -20,7 +20,6 @@ import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Vector;
 
 import megamek.client.ui.SharedUtility;
 import megamek.common.Compute;

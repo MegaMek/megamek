d0bfccea64598d7463536611265da17e0ee27fb4
arlith <arlith@c34074a7-3587-4b08-b2b8-7c33201a20e7>
2014-09-09 01:43:28 +0000

    + Bug: VGLs mounted in the front right and front left on vehicles would receive     a 'not in arc' message without TO vehicle arc rules on

--- trunk/megamek/src/megamek/common/Tank.java
+++ trunk/megamek/src/megamek/common/Tank.java
@@ -655,6 +655,10 @@ public class Tank extends Entity {
                 && mounted.getType().hasFlag(WeaponType.F_B_POD)) {
             return Compute.ARC_360;
         }
+        // VGLs always be considered forward, since arc is set by VGL facing
+        if (mounted.getType().hasFlag(WeaponType.F_VGL)) {
+            return Compute.ARC_FORWARD;
+        }
         switch (mounted.getLocation()) {
             case LOC_BODY:
                 // Body mounted C3Ms fire into the front arc,

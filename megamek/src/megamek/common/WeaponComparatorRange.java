800ea5dc7643d426a471e8e95e63160950b24b1e
arlith <Arlith@users.noreply.github.com>
2014-09-15 18:16:09 +0000

    + Feature [#1291]: Renamed WeaponComparator to WeaponComparatorBV and added range and damage

---
+++ megamek/src/megamek/common/WeaponComparatorRange.java
@@ -0,0 +1,72 @@
+/*
+ * MegaMek -
+ * Copyright (C) 2007 Ben Mazur (bmazur@sev.org)
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *  for more details.
+ */
+package megamek.common;
+
+import java.util.Comparator;
+
+/**
+ * Comparator for sorting Weapons (Mounteds that have WeaponTypes) by Range.
+ * 
+ * @author arlith
+ */
+public class WeaponComparatorRange implements Comparator<Mounted> {
+
+    /**
+     * Value used to change order from ascending to descending.  If descending,
+     * value will be -1 and orders will be multiplied by -1.
+     */
+    private int ascending = 1;
+    
+    public WeaponComparatorRange(boolean ascending) {
+        if (!ascending)
+            this.ascending = -1;
+    }
+
+    public int compare(Mounted obj1, Mounted obj2) {
+        if (obj1.getType() instanceof WeaponType
+                && obj2.getType() instanceof WeaponType) {
+            WeaponType weap1 = (WeaponType) obj1.getType();
+            WeaponType weap2 = (WeaponType) obj2.getType();
+            
+            // If types are equal, pick front facing first
+            if (weap1 == weap2) {
+                if (obj1.isRearMounted())
+                    return -1 * ascending;
+                else if (obj2.isRearMounted())
+                    return 1 * ascending;
+                return 0;
+            }
+            int ranges1[] = weap1.getRanges(obj1);
+            int ranges2[] = weap2.getRanges(obj2);
+            // Pick the weapon with the smallest
+            for (int r = 0; r < ranges1.length; r++) {
+                if (ranges1[r] < ranges2[r]) {
+                    return -1 * ascending;
+                } else if (ranges1[r] > ranges2[r]) {
+                    return 1 * ascending;
+                }
+            }
+            // If we get here, all ranges are equals, arbitrate with heat
+            if (weap1.getHeat() > weap2.getHeat()) {
+                return 1 * ascending;
+            } else if (weap1.getHeat() < weap2.getHeat()) {
+                return -1 * ascending;
+            } else {
+                return 0;
+            }
+        }
+        throw new ClassCastException("Passed Mounteds are not Weapons");
+    }
+}

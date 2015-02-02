eb78758595dcf5dbb3d09bad48ab7e23caf05559
arlith <Arlith@users.noreply.github.com>
2015-02-02 03:24:32 +0000

    + Bug: Updated A* heuristic for aerodyne ASFs: paths shoulder be shorter now
    Given the restrictiosn for aerodyne ASF movement on ground maps, there's no
    good admissible heuristic (that I know of at least).  So, the A* comparator
    defaults to ranking paths based on their MP used and arbitrating ties with
    the path that has the most hexes moved.  This makes sense for ground units
    where the more hexes moved could mean a better target movement modifier,
    however it doesn't make sense for ASF, and can lead to unintuitive paths,
    so I switched the fall-back to pick paths with fewer hexes moved, which
    will lead to shorter paths for ASF, and is more likely to be inline with
    what the user has in mind.

--- megamek/src/megamek/common/pathfinder/ShortestPathFinder.java
+++ megamek/src/megamek/common/pathfinder/ShortestPathFinder.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import megamek.common.Aero;
 import megamek.common.Coords;
-import megamek.common.Dropship;
 import megamek.common.IGame;
 import megamek.common.MovePath;
 import megamek.common.MovePath.MoveStepType;
@@ -157,10 +156,19 @@ public class ShortestPathFinder extends MovePathFinder<MovePath> {
         public int compare(MovePath first, MovePath second) {
 
             int h1 = 0, h2 = 0;
+            // We cannot estimate the needed cost for aeros
+            // However, Dropships basically follow ground movement rules
             if ((first.getEntity() instanceof Aero) 
                     && !((Aero)first.getEntity()).isSpheroid()) {
-                //we cannot estimate the needed cost for aeros - maybe a facing change cost would be apropiate
-                // However, Dropships basically follow ground movement rules
+                // We want to pick paths that use fewer MP, and are also shorter
+                // unlike ground units which could benefit from better target
+                // movement modifiers for longer paths
+                int dd = (first.getMpUsed() + h1) - (second.getMpUsed() + h2);
+                if (dd != 0) {
+                    return dd;
+                } else {
+                    return (first.getHexesMoved() - second.getHexesMoved());
+                }
             }else if(first.getEntity().getWalkMP()==0) {
                 //current implementation of movement cost allows a 0mp moves for units with 0 mp.
             }else{

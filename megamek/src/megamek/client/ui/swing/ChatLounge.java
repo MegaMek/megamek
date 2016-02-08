b38330aa54e037ba8ae6cdd38ad2c8338c995883
arlith <arlith@users.noreply.github.com>
2016-02-07 19:57:54 -0600

    + Updated ChatLounge to display hidden unit status

--- megamek/src/megamek/client/ui/swing/ChatLounge.java
+++ megamek/src/megamek/client/ui/swing/ChatLounge.java
@@ -1793,6 +1793,10 @@ public class ChatLounge extends AbstractPhaseDisplay implements ActionListener,
             value += ", aboard " + loader.getShortName() + "";
         }
 
+        if (entity.isHidden()) {
+            value += " (" + Messages.getString("ChatLounge.hidden") + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        }
+        
         if (entity.isOffBoard()) {
             value += " (" + Messages.getString("ChatLounge.deploysOffBoard") + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         } else if (entity.getDeployRound() > 0) {
@@ -1895,8 +1899,13 @@ public class ChatLounge extends AbstractPhaseDisplay implements ActionListener,
             }
 
         }
+
+        if (entity.isHidden()) {
+            value += Messages.getString("ChatLounge.hidden") + "<br>"; //$NON-NLS-1$ ; //$NON-NLS-1$
+        }
+
         if (entity.isOffBoard()) {
-            value += Messages.getString("ChatLounge.deploysOffBoard"); //$NON-NLS-1$ //$NON-NLS-2$
+            value += Messages.getString("ChatLounge.deploysOffBoard"); //$NON-NLS-1$
         } else if (entity.getDeployRound() > 0) {
             value += Messages.getString("ChatLounge.deploysAfterRound") + entity.getDeployRound(); //$NON-NLS-1$
             if (entity.getStartingPos(false) != Board.START_NONE) {
@@ -1983,6 +1992,8 @@ public class ChatLounge extends AbstractPhaseDisplay implements ActionListener,
                     + Messages.getString("ChatLounge.pquirk") : ""); //$NON-NLS-1$ //$NON-NLS-2$
             String negQuirks = (negQuirkCount > 0 ? " <" + negQuirkCount //$NON-NLS-1$
                     + Messages.getString("ChatLounge.nquirk") : ""); //$NON-NLS-1$
+            String hidden = ((entity.isHidden()) ? Messages
+                    .getString("ChatLounge.hidden") : ""); //$NON-NLS-1$
             String offBoard = ((entity.isOffBoard()) ? Messages
                     .getString("ChatLounge.deploysOffBoard") : ""); //$NON-NLS-1$
             String deployRound = ((entity.getDeployRound() > 0) ? Messages
@@ -1991,7 +2002,7 @@ public class ChatLounge extends AbstractPhaseDisplay implements ActionListener,
             value = Messages.getString("ChatLounge.EntityListEntry1", //$NON-NLS-1$
                     new Object[] { entity.getOwner().getName(), gunnery,
                             piloting, advantages, maneiDomini, unitClass,
-                            posQuirks, negQuirks, offBoard, deployRound });
+                            posQuirks, negQuirks, offBoard, deployRound, hidden });
         } else {
             Integer piloting = new Integer(entity.getCrew().getPiloting());
             String advantages = (crewAdvCount > 0 ? " <" + crewAdvCount //$NON-NLS-1$
@@ -2003,6 +2014,8 @@ public class ChatLounge extends AbstractPhaseDisplay implements ActionListener,
             String negQuirks = (negQuirkCount > 0 ? " <" + negQuirkCount //$NON-NLS-1$
                     + Messages.getString("ChatLounge.nquirk") : ""); //$NON-NLS-1$
             Integer battleValue = new Integer(entity.calculateBattleValue());
+            String hidden = ((entity.isHidden()) ? Messages
+                    .getString("ChatLounge.hidden") : ""); //$NON-NLS-1$
             String offBoard = ((entity.isOffBoard()) ? Messages
                     .getString("ChatLounge.deploysOffBoard") : ""); //$NON-NLS-1$ //$NON-NLS-2$
             String deployRound = ((entity.getDeployRound() > 0) ? Messages
@@ -2016,7 +2029,7 @@ public class ChatLounge extends AbstractPhaseDisplay implements ActionListener,
                             entity.getDisplayName(), gunnery, piloting,
                                     advantages, maneiDomini, posQuirks,
                                     negQuirks, battleValue, strTreeView,
-                                    offBoard, deployRound, valid });
+                                    offBoard, deployRound, hidden, valid });
         }
         return value;
     }

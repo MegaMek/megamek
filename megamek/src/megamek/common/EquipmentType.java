306141a69fa7a4bf8a08e1920187c4d7e18a3f1f
Akjosch <development@akjosch.de>
2016-03-19 09:09:57 +0100

    Null guard in EquipmentType.getArmorType()

--- megamek/src/megamek/common/EquipmentType.java
+++ megamek/src/megamek/common/EquipmentType.java
@@ -582,6 +582,9 @@ public class EquipmentType {
     }
 
     public static int getArmorType(EquipmentType et) {
+        if (null == et) {
+            return T_ARMOR_UNKNOWN;
+        }
         for (int x = 0; x < armorNames.length; x++) {
             // Some armor names (Industrial), have a space in the name, so trim
             if (armorNames[x].trim().equals(et.getName().trim())) {

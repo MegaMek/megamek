044bcf83fa78d055e34d9fca83a3470896a4666a
Akjosch <development@akjosch.de>
2016-06-21 17:11:10 +0200

    Stop cleaning up the random name generator's internal data.

--- megamek/src/megamek/client/ui/swing/MegaMekGUI.java
+++ megamek/src/megamek/client/ui/swing/MegaMekGUI.java
@@ -51,7 +51,6 @@ import javax.swing.filechooser.FileFilter;
 
 import megamek.MegaMek;
 import megamek.client.Client;
-import megamek.client.RandomNameGenerator;
 import megamek.client.bot.BotClient;
 import megamek.client.bot.TestBot;
 import megamek.client.bot.princess.Princess;
@@ -511,7 +510,6 @@ public class MegaMekGUI implements IMegaMekGUI {
         // This normally happens in the deployment phase in Client, but
         // if we are loading a game, this phase may not be reached
         MechFileParser.dispose();
-        RandomNameGenerator.getInstance().dispose();
         // We must do this last, as the name and unit generators can create
         // a new instance if they are running
         MechSummaryCache.dispose();

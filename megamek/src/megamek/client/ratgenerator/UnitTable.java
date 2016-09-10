fe24e28f3d64f35ca271da662499b38e971f71f5
Carl Spain <neoancient.bt@gmail.com>
2016-09-10 12:33:24 -0500

    +Added ability to use custom filter on entries in generated RAT.

--- megamek/src/megamek/client/ratgenerator/UnitTable.java
+++ megamek/src/megamek/client/ratgenerator/UnitTable.java
@@ -18,6 +18,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 import megamek.common.Compute;
 import megamek.common.MechSummary;
@@ -29,6 +31,12 @@ import megamek.common.MechSummary;
  *
  */
 public class UnitTable {
+	
+	@FunctionalInterface
+	public interface UnitFilter {
+		boolean include(MechSummary ms);
+	}
+	
 	private List<TableEntry> table = new ArrayList<TableEntry>();
 	private FactionRecord faction;
 	private int unitType;
@@ -42,6 +50,18 @@ public class UnitTable {
 	private FactionRecord deployingFaction;
 	private HashMap<String,UnitTable> salvageCache =
 			new HashMap<String,UnitTable>();
+
+	/* Key is cumulative weight. This is usually more efficient than a weighted list
+	 * for large lists. Salvage and units are kept separate so that salvage totals can be
+	 * adjusted to maintain the same proportion when the units have a filter applied.
+	 */
+	private NavigableMap<Integer,FactionRecord> salvageMap;
+	private NavigableMap<Integer,MechSummary> unitMap;
+	int salvageTotal;
+	int unitTotal;
+	/* Filtering can reduce the total weight of the units. Calculate the salvage pct when
+	 * creating the table to maintain the same proportion. */
+	int salvagePct;
 	
 	public UnitTable(FactionRecord faction, int unitType, int year,
 			String rating, Collection<Integer> weightClasses, int networkMask, Collection<String> subtypes,
@@ -67,6 +87,10 @@ public class UnitTable {
 				roles, roleStrictness, faction);
 	}
 	
+	/**
+	 * Generate the RAT, then go through it to build the NavigableMaps that
+	 * will be used for random selection.
+	 */
 	private void generateTable() {
 		if (!faction.isActiveInYear(year)) {
 			return;
@@ -75,6 +99,23 @@ public class UnitTable {
 				unitType, year, rating, weightClasses, networkMask, subtypes,
 				roles, roleStrictness, deployingFaction);
 		Collections.sort(table);
+		
+		salvageMap = new TreeMap<Integer,FactionRecord>();
+		unitMap = new TreeMap<Integer,MechSummary>();
+		salvageTotal = 0;
+		unitTotal = 0;
+		for (TableEntry entry : table) {
+			if (entry.isUnit()) {
+				unitMap.put(unitTotal, entry.getUnitEntry());
+				unitTotal += entry.weight;
+			} else {
+				salvageMap.put(salvageTotal, entry.getSalvageFaction());
+				salvageTotal += entry.weight;
+			}
+		}
+		if (salvageTotal + unitTotal > 0) {
+			salvagePct = salvageTotal * 100 / (salvageTotal + unitTotal);
+		}
 	}
 	
 	public int getNumEntries() {
@@ -121,38 +162,117 @@ public class UnitTable {
 		return false;
 	}
 	
-	public ArrayList<MechSummary> generateUnits(int num) {
-		ArrayList<MechSummary> retVal = new ArrayList<MechSummary>();
-		ArrayList<TableEntry> weightedList = new ArrayList<TableEntry>();
-		for (TableEntry entry : table) {
-			for (int i = 0; i < entry.weight; i++) {
-				weightedList.add(entry);
+	/**
+	 * 
+	 * @return A single MechSummary selected from the full table.
+	 */
+	public MechSummary generateUnit() {
+		return generateUnit(null);
+	}
+	
+	/**
+	 * 
+	 * @param filter Only units that return true when the filter method is applied will be included
+	 *               in the results. If null, all are included.
+	 * @return A single MechSummary selected from the filtered table.
+	 */
+	public MechSummary generateUnit(UnitFilter filter) {
+		int roll = Compute.randomInt(100);
+		if (roll < salvagePct) {
+			MechSummary ms = generateSalvage(filter);
+			if (ms != null) {
+				return ms;
 			}
 		}
-		while (retVal.size() < num && weightedList.size() > 0) {
-			TableEntry entry = weightedList.get(Compute.randomInt(weightedList.size()));
-			if (entry.isUnit()) {
-				retVal.add(entry.getUnitEntry());
-			} else {
-				UnitTable salvage = salvageCache.get(entry.getSalvageFaction().getKey());
-				if (salvage == null) {
-					salvage = new UnitTable(entry.getSalvageFaction(),
-							unitType, year - 5, rating, weightClasses, networkMask, subtypes,
-							roles, roleStrictness, faction);
-					if (salvage.hasUnits()) {
-						salvageCache.put(entry.getSalvageFaction().getKey(), salvage);
-					} else {
-						table.remove(entry);
-						while (weightedList.remove(entry));
-						continue;
-					}
+		NavigableMap<Integer,MechSummary> useUnitMap = unitMap;
+		int unitMapSize = unitTotal;
+		if (filter != null) {
+			useUnitMap = new TreeMap<>();
+			unitMapSize = 0;
+			for (TableEntry entry : table) {
+				if (entry.isUnit() && filter.include(entry.getUnitEntry())) {
+					useUnitMap.put(unitMapSize, entry.getUnitEntry());
+					unitMapSize += entry.weight;
 				}
-				retVal.addAll(salvage.generateUnits(1));
+			}
+		}
+		
+		if (salvageTotal + unitMapSize == 0) {
+			return null;
+		}
+		roll = Compute.randomInt(unitMapSize);
+		return useUnitMap.floorEntry(roll).getValue();
+	}
+	
+	/**
+	 * 
+	 * @param num The number of units to be generated.
+	 * @return A list of randomly generated units
+	 */
+	public ArrayList<MechSummary> generateUnits(int num) {
+		return generateUnits(num, null);
+	}
+	
+	/**
+	 * 
+	 * @param num The number of units to be generated.
+	 * @param filter Only units that return true when the filter method is applied will be included
+	 *               in the results. If null, all are included.
+	 * @return A list of randomly generated units
+	 */
+	public ArrayList<MechSummary> generateUnits(int num, UnitFilter filter) {
+		ArrayList<MechSummary> retVal = new ArrayList<>();
+		for (int i = 0; i < num; i++) {
+			MechSummary ms = generateUnit(filter);
+			if (ms != null) {
+				retVal.add(ms);
 			}
 		}
 		return retVal;
+	}	
+	
+	/**
+	 * Selects a faction from the salvage list and generates a table using the same parameters
+	 * as this table, but from five years earlier. Generated tables are cached for later use.
+	 * If the generated table contains no units, it is discarded and the selected entry is deleted.
+	 * This continues until either a unit is generated or there are no remaining entries.
+	 * 
+	 * @param filter Passed to generateUnit() in the generated table.
+	 * @return A unit generated from another faction, or null if none of the factions in
+	 * 	       the salvage list contain any units that meet the parameters.
+	 */
+	private MechSummary generateSalvage(UnitFilter filter) {
+		while (salvageTotal > 0) {
+			int roll = Compute.randomInt(salvageTotal);
+			FactionRecord fRec = salvageMap.floorEntry(roll).getValue();
+			UnitTable salvage = salvageCache.get(fRec.getKey());
+			if (salvage == null) {
+				salvage = new UnitTable(fRec,
+						unitType, year - 5, rating, weightClasses, networkMask, subtypes,
+						roles, roleStrictness, faction);
+			}
+			if (salvage.hasUnits()) {
+				salvageCache.put(fRec.getKey(), salvage);
+				return salvage.generateUnit(filter);
+			} else {
+				salvageMap.remove(roll);
+				/* Rebuild the table */
+				salvageMap.clear();
+				salvageTotal = 0;
+				for (TableEntry entry : table) {
+					if (!entry.isUnit()) {
+						salvageMap.put(salvageTotal, entry.getSalvageFaction());
+						salvageTotal += entry.weight;
+					}
+				}
+			}					
+		}
+		assert(salvageMap.isEmpty() && salvageTotal == 0);
+		return null;
 	}
 	
+	/* A tuple that contains either a salvage or a faction entry along with its relative weight.
+	 * in the table. */
 	public static class TableEntry implements Comparable<TableEntry> {
 		int weight;
 		Object entry;

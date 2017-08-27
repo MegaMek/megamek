33045c6810540438e271cbb53047f44c0eeee5ea
Joshua Bartz <28548170+Bonepart@users.noreply.github.com>
2017-08-27 16:11:07 -0500

    Modify BombChoicePanel to enable integration with MekHQ

--- megamek/src/megamek/client/ui/swing/BombChoicePanel.java
+++ megamek/src/megamek/client/ui/swing/BombChoicePanel.java
@@ -44,16 +44,30 @@ public class BombChoicePanel extends JPanel implements Serializable, ItemListene
     private int maxPoints = 0;
     private int maxSize = 0;
     private int maxRows = (int) Math.ceil(BombType.B_NUM / 2.0);
+    
+    //Variable for MekHQ functionality
+    private int[] typeMax;
 
     //private BombChoicePanel m_bombs;
     //private JPanel panBombs = new JPanel();
 
-    @SuppressWarnings("unchecked")
     public BombChoicePanel(IBomber bomber, boolean at2Nukes, boolean allowAdvancedAmmo) {
         this.bomber = bomber;
         this.at2Nukes = at2Nukes;
         this.allowAdvancedAmmo = allowAdvancedAmmo;
-
+        initPanel();
+    }
+    //Constructor to call from MekHQ to pass in typeMax
+    public BombChoicePanel(IBomber bomber, boolean at2Nukes, boolean allowAdvancedAmmo, int[] typeMax) {
+        this.bomber = bomber;
+        this.at2Nukes = at2Nukes;
+        this.allowAdvancedAmmo = allowAdvancedAmmo;
+        this.typeMax = typeMax;
+        initPanel();
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void initPanel() {
         maxPoints = bomber.getMaxBombPoints();
         maxSize = bomber.getMaxBombSize();
         int[] bombChoices = bomber.getBombChoices();
@@ -82,6 +96,11 @@ public class BombChoicePanel extends JPanel implements Serializable, ItemListene
             if (BombType.getBombCost(type) > maxSize) {
                 maxNumBombs = 0;
             }
+            
+            if(typeMax != null) {
+                if (maxNumBombs > 0 && maxNumBombs > typeMax[type]) maxNumBombs = typeMax[type];
+            }
+            
             for (int x = 0; x <= maxNumBombs; x++) {
                 b_choices[type].addItem(Integer.toString(x));
             }
@@ -90,12 +109,10 @@ public class BombChoicePanel extends JPanel implements Serializable, ItemListene
             b_labels[type].setText(BombType.getBombName(type));
             b_choices[type].addItemListener(this);
 
-            if ((type == BombType.B_ALAMO)
-                && !at2Nukes) {
+            if ((type == BombType.B_ALAMO) && !at2Nukes) {
                 b_choices[type].setEnabled(false);
             }
-            if ((type > BombType.B_TAG)
-                && !allowAdvancedAmmo) {
+            if ((type > BombType.B_TAG) && !allowAdvancedAmmo) {
                 b_choices[type].setEnabled(false);
             }
             if ((type == BombType.B_ASEW) || (type == BombType.B_ALAMO)) {
@@ -140,6 +157,9 @@ public class BombChoicePanel extends JPanel implements Serializable, ItemListene
             int maxNumBombs = Math.round(availBombPoints
                     / BombType.getBombCost(type))
                     + current[type];
+            if(typeMax != null) {
+                if (maxNumBombs > 0 && maxNumBombs > typeMax[type]) maxNumBombs = typeMax[type];
+            }
             for (int x = 0; x <= maxNumBombs; x++) {
                 b_choices[type].addItem(Integer.toString(x));
             }
@@ -155,17 +175,21 @@ public class BombChoicePanel extends JPanel implements Serializable, ItemListene
         }
 
         bomber.setBombChoices(choices);
-
+    }
+    public int[] getChoice() {
+        int[] choices = new int[BombType.B_NUM];
+        for (int type = 0; type < BombType.B_NUM; type++) {
+            choices[type] = b_choices[type].getSelectedIndex();
+        }
+        return choices;
     }
 
     @Override
     public void setEnabled(boolean enabled) {
         for (int type = 0; type < BombType.B_NUM; type++) {
-            if ((type == BombType.B_ALAMO)
-                && !at2Nukes) {
+            if ((type == BombType.B_ALAMO) && !at2Nukes) {
                 b_choices[type].setEnabled(false);
-            } else if ((type > BombType.B_TAG)
-                       && !allowAdvancedAmmo) {
+            } else if ((type > BombType.B_TAG) && !allowAdvancedAmmo) {
                 b_choices[type].setEnabled(false);
             } else if ((type == BombType.B_ASEW)
                        || (type == BombType.B_ALAMO)

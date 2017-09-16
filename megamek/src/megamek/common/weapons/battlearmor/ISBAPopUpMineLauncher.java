2669e2124f1c123765d2f913aec4b94852fe96b7
Carl Spain <neoancient@megamek.org>
2017-09-16 09:46:02 -0500

    Added 5-shot and 6-shot modes to ISBAPopUpMineLauncher to allow them to work with any squad size.

--- megamek/src/megamek/common/weapons/battlearmor/ISBAPopUpMineLauncher.java
+++ megamek/src/megamek/common/weapons/battlearmor/ISBAPopUpMineLauncher.java
@@ -58,7 +58,7 @@ public class ISBAPopUpMineLauncher extends Weapon {
         criticals = 1;
         cost = 2500;
         bv = 6;
-        String[] modeStrings = { "Single", "2-shot", "3-shot", "4-shot" };
+        String[] modeStrings = { "Single", "2-shot", "3-shot", "4-shot", "5-shot", "6-shot" };
         setModes(modeStrings);
         flags = flags.or(F_DIRECT_FIRE).or(F_SOLO_ATTACK).or(F_BA_WEAPON).or(F_ONESHOT);
         introDate = 3050;

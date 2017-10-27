02b3af22ff0cf9b5b539e166652d32c51a78dc2d
MKerensky <magnusmd@hotmail.com>
2017-10-27 09:37:53 -0400

    Rebuild of nuclear weapon revamp code

--- megamek/src/megamek/common/weapons/CapitalMissileBayHandler.java
+++ megamek/src/megamek/common/weapons/CapitalMissileBayHandler.java
@@ -82,13 +82,15 @@ public class CapitalMissileBayHandler extends AmmoBayWeaponHandler {
                 double current_av = 0;
 
                 AmmoType atype = (AmmoType) bayWAmmo.getType();
-                if (bayWType.getAtClass() == (20)
-                		&& atype.hasFlag(AmmoType.F_AR10_KILLER_WHALE)) {
+                if (bayWType.getAtClass() == (WeaponType.CLASS_AR10)
+                		&& (atype.hasFlag(AmmoType.F_AR10_KILLER_WHALE)
+                		        || atype.hasFlag(AmmoType.F_PEACEMAKER))) {
                 	weaponarmor = 40;
-                } else if (bayWType.getAtClass() == (20)
-                		&& atype.hasFlag(AmmoType.F_AR10_WHITE_SHARK)) {
+                } else if (bayWType.getAtClass() == (WeaponType.CLASS_AR10)
+                		&& (atype.hasFlag(AmmoType.F_AR10_WHITE_SHARK)
+                                || atype.hasFlag(AmmoType.F_SANTA_ANNA))) {
                 	weaponarmor = 30;
-                } else if (bayWType.getAtClass() == (20)
+                } else if (bayWType.getAtClass() == (WeaponType.CLASS_AR10)
                 		&& atype.hasFlag(AmmoType.F_AR10_BARRACUDA)) {
                 	weaponarmor = 20;
                 } else {
@@ -220,11 +222,15 @@ public class CapitalMissileBayHandler extends AmmoBayWeaponHandler {
                 current_av = 4;
             } else if (atype.hasFlag(AmmoType.F_AR10_WHITE_SHARK)) {
                 current_av = 3;
+            } else if (atype.hasFlag(AmmoType.F_PEACEMAKER)) {
+                current_av = 1000;
+            } else if (atype.hasFlag(AmmoType.F_SANTA_ANNA)) {
+                current_av = 100;
             } else {
                 current_av = 2;
             }
         }
-    	//Nuclear Warheads
+    	//Nuclear Warheads for non-AR10 missiles
     	if (atype.hasFlag(AmmoType.F_SANTA_ANNA)) {
     	    current_av = 100;
     	} else if (atype.hasFlag(AmmoType.F_PEACEMAKER)) {

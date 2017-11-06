b918d21e4d0fa94aa325ab88462b3670b37cb3af
MKerensky <magnusmd@hotmail.com>
2017-11-06 09:52:42 -0500

    Change logging to log4j format

--- megamek/src/megamek/common/weapons/ArtilleryBayWeaponIndirectFireHandler.java
+++ megamek/src/megamek/common/weapons/ArtilleryBayWeaponIndirectFireHandler.java
@@ -42,6 +42,9 @@ import megamek.common.ToHitData;
 import megamek.common.VTOL;
 import megamek.common.actions.ArtilleryAttackAction;
 import megamek.common.actions.WeaponAttackAction;
+import megamek.common.logging.LogLevel;
+import megamek.common.logging.DefaultMmLogger;
+import megamek.common.logging.MMLogger;
 import megamek.common.options.OptionsConstants;
 import megamek.server.Server;
 
@@ -55,12 +58,32 @@ public class ArtilleryBayWeaponIndirectFireHandler extends AmmoBayWeaponHandler 
      */
     private static final long serialVersionUID = -1277649123562229298L;
     boolean handledAmmoAndReport = false;
+    private MMLogger logger = null;
 
     /**
      * This consructor may only be used for deserialization.
      */
     protected ArtilleryBayWeaponIndirectFireHandler() {
         super();
+    }
+    
+    /**
+     * Write debug information to the logs.
+     *
+     * @param methodName Name of the method logging is coming from
+     * @param message Message to log
+     */
+    @SuppressWarnings("unused")
+    private void logDebug(String methodName, String message) {
+        getLogger().log(getClass(), methodName, LogLevel.DEBUG, message);
+    }
+    
+    private MMLogger getLogger() {
+        if (null == logger) {
+            logger = DefaultMmLogger.getInstance();
+        }
+
+        return logger;
     }
 
     /**
@@ -89,6 +112,7 @@ public class ArtilleryBayWeaponIndirectFireHandler extends AmmoBayWeaponHandler 
         
     @Override
     protected void useAmmo() {
+        final String METHOD_NAME = "useAmmo()";
         for (int wId : weapon.getBayWeapons()) {
             Mounted bayW = ae.getEquipment(wId);
             // check the currently loaded ammo
@@ -96,7 +120,7 @@ public class ArtilleryBayWeaponIndirectFireHandler extends AmmoBayWeaponHandler 
 
             if (bayWAmmo == null) {// Can't happen. w/o legal ammo, the weapon
                 // *shouldn't* fire.
-                System.out.println("Handler can't find any ammo!  Oh no!");
+                logDebug(METHOD_NAME, "Handler can't find any ammo! Oh no!");
             }
 
             int shots = bayW.getCurrentShots();

fa2e9a1c959620fe7eddc70f383649895860d194
HammerGS <drivingguy@hotmail.com>
2017-12-17 16:24:54 -0700

    Development Snapshot <0.43.5>

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -75,7 +75,7 @@ public class MegaMek {
 
     private static final MMLogger logger = DefaultMmLogger.getInstance();
 
-    public static String VERSION = "0.43.5-git"; //$NON-NLS-1$
+    public static String VERSION = "0.43.5"; //$NON-NLS-1$
     public static long TIMESTAMP = new File(PreferenceManager
             .getClientPreferences().getLogDirectory()
             + File.separator

20889a58354dcbb6157e0ebc2b289c5f33c4c6f4
Carl Spain <neoancient@megamek.org>
2018-03-26 08:53:37 -0500

    Extracted TransportBay enum from TestAero for more general use.
    Added method to determine whether bay is valid for a given unit.

--- megamek/src/megamek/common/verifier/TestAero.java
+++ megamek/src/megamek/common/verifier/TestAero.java
@@ -24,40 +24,24 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Vector;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
-import megamek.common.ASFBay;
 import megamek.common.Aero;
 import megamek.common.AmmoType;
-import megamek.common.BattleArmorBay;
 import megamek.common.Bay;
-import megamek.common.CargoBay;
 import megamek.common.CrewQuartersCargoBay;
 import megamek.common.CriticalSlot;
 import megamek.common.Engine;
 import megamek.common.Entity;
 import megamek.common.EquipmentType;
 import megamek.common.FirstClassQuartersCargoBay;
-import megamek.common.HeavyVehicleBay;
 import megamek.common.ITechManager;
 import megamek.common.ITechnology;
-import megamek.common.InfantryBay;
-import megamek.common.InsulatedCargoBay;
-import megamek.common.LightVehicleBay;
-import megamek.common.LiquidCargoBay;
-import megamek.common.LivestockCargoBay;
-import megamek.common.MechBay;
 import megamek.common.MiscType;
 import megamek.common.Mounted;
-import megamek.common.ProtomechBay;
-import megamek.common.RefrigeratedCargoBay;
 import megamek.common.SecondClassQuartersCargoBay;
 import megamek.common.SmallCraft;
-import megamek.common.SmallCraftBay;
 import megamek.common.SteerageQuartersCargoBay;
-import megamek.common.SuperHeavyVehicleBay;
-import megamek.common.TechAdvancement;
 import megamek.common.WeaponType;
 import megamek.common.annotations.Nullable;
 import megamek.common.util.StringUtil;
@@ -178,137 +162,6 @@ public class TestAero extends TestEntity {
             }
         }
         return retVal;
-    }
-    
-    public enum TransportBay {
-        MECH ("Mech", 150.0, 2, MechBay.techAdvancement(),
-                (size, num) -> new MechBay(size, 1, num)),
-        PROTOMECH ("Protomech", 10.0, 6, ProtomechBay.techAdvancement(),
-                (size, num) -> new ProtomechBay(size, 1, num)),
-        VEHICLE_HEAVY ("Heavy Vehicle", 100.0, 8, HeavyVehicleBay.techAdvancement(),
-                (size, num) -> new HeavyVehicleBay(size, 1, num)),
-        VEHICLE_LIGHT ("Light Vehicle", 50.0, 5, LightVehicleBay.techAdvancement(),
-                (size, num) -> new LightVehicleBay(size, 1, num)),
-        VEHICLE_SH ("Superheavy Vehicle", 200.0, 15, SuperHeavyVehicleBay.techAdvancement(),
-                (size, num) -> new SuperHeavyVehicleBay(size, 1, num)),
-        INFANTRY_FOOT ("Infantry (Foot)", 5.0, 0, InfantryBay.techAdvancement(),
-                (size, num) -> new InfantryBay(size, 1, num, InfantryBay.PlatoonType.FOOT)),
-        INFANTRY_JUMP ("Infantry (Jump)", 6.0, 0, InfantryBay.techAdvancement(),
-                (size, num) -> new InfantryBay(size, 1, num, InfantryBay.PlatoonType.JUMP)),
-        INFANTRY_MOTORIZED ("Infantry (Motorized)", 7.0, 0, InfantryBay.techAdvancement(),
-                (size, num) -> new InfantryBay(size, 1, num, InfantryBay.PlatoonType.MOTORIZED)),
-        INFANTRY_MECHANIZED ("Infantry (Mech. Squad)", 8.0, 0, InfantryBay.techAdvancement(),
-                (size, num) -> new InfantryBay(size, 1, num, InfantryBay.PlatoonType.MECHANIZED)),
-        IS_BATTLE_ARMOR ("BattleArmor (IS)", 8.0, 6, BattleArmorBay.techAdvancement(),
-                (size, num) -> new BattleArmorBay(size, 1, num, false, false)),
-        CLAN_BATTLE_ARMOR ("BattleArmor (Clan)", 10.0, 6, BattleArmorBay.techAdvancement(),
-                (size, num) -> new BattleArmorBay(size, 1, num, true, false)),
-        CS_BATTLE_ARMOR ("BattleArmor (CS)", 12.0, 6, BattleArmorBay.techAdvancement(),
-                (size, num) -> new BattleArmorBay(size, 1, num, false, true)),
-        FIGHTER ("Fighter", 150.0, 2, ASFBay.techAdvancement(),
-                (size, num) -> new ASFBay(size, 1, num)),
-        SMALL_CRAFT ("Small Craft", 200.0, 5, SmallCraftBay.techAdvancement(),
-                (size, num) -> new SmallCraftBay(size, 1, num)),
-        CARGO ("Cargo", 1.0, 0, CargoBay.techAdvancement(),
-                (size, num) -> new CargoBay(size, 1, num)),
-        LIQUID_CARGO ("Cargo (Liquid)", 1/0.91, 0, CargoBay.techAdvancement(),
-                (size, num) -> new LiquidCargoBay(size, 1, num)),
-        REFRIGERATED_CARGO ("Cargo (Refrigerated)", 1/0.87, 0, CargoBay.techAdvancement(),
-                (size, num) -> new RefrigeratedCargoBay(size, 1, num)),
-        INSULATED_CARGO ("Cargo (Insulated)", 1/0.87, 0, CargoBay.techAdvancement(),
-                (size, num) -> new InsulatedCargoBay(size, 1, num)),
-        LIVESTOCK_CARGO ("Cargo Livestock)", 1/0.83, 0, CargoBay.techAdvancement(),
-                (size, num) -> new LivestockCargoBay(size, 1, num));
-        
-        private String name;
-        private double weight;
-        private int personnel;
-        private TechAdvancement techAdvancement;
-        private BiFunction<Double,Integer,Bay> init;
-        
-        TransportBay(String name, double weight, int personnel,
-                TechAdvancement techAdvancement, BiFunction<Double,Integer,Bay> init) {
-            this.name = name;
-            this.weight = weight;
-            this.personnel = personnel;
-            this.techAdvancement = techAdvancement;
-            this.init = init;
-        }
-        
-        public String getDisplayName() {
-            return name;
-        }
-
-        public double getWeight() {
-            return weight;
-        }
-        
-        public int getPersonnel() {
-            return personnel;
-        }
-
-        public Bay newBay(double size, int bayNum) {
-            return init.apply(size, bayNum);
-        }
-        
-        public TechAdvancement getTechAdvancement() {
-            return techAdvancement;
-        }
-
-        public static @Nullable TransportBay getBayType(Bay bay) {
-            if (bay instanceof MechBay) {
-                return MECH;
-            } else if (bay instanceof ProtomechBay) {
-                return PROTOMECH;
-            } else if (bay instanceof HeavyVehicleBay) {
-                return VEHICLE_HEAVY;
-            } else if (bay instanceof LightVehicleBay) {
-                return VEHICLE_LIGHT;
-            } else if (bay instanceof SuperHeavyVehicleBay) {
-                return VEHICLE_SH;
-            } else if (bay instanceof InfantryBay) {
-                switch (((InfantryBay) bay).getPlatoonType()) {
-                    case JUMP:
-                        return INFANTRY_JUMP;
-                    case MECHANIZED:
-                        return INFANTRY_MECHANIZED;
-                    case MOTORIZED:
-                        return INFANTRY_MOTORIZED;
-                    case FOOT:
-                    default:
-                        return INFANTRY_FOOT;
-                    
-                }
-            } else if (bay instanceof BattleArmorBay) {
-                if (bay.getWeight() / bay.getCapacity() == 12) {
-                    return CS_BATTLE_ARMOR;
-                } else if (bay.getWeight() / bay.getCapacity() == 10) {
-                    return CLAN_BATTLE_ARMOR;
-                }
-                return IS_BATTLE_ARMOR;
-            } else if (bay instanceof ASFBay) {
-                return FIGHTER;
-            } else if (bay instanceof SmallCraftBay) {
-                return SMALL_CRAFT;
-            } else if (bay instanceof LiquidCargoBay) {
-                return LIQUID_CARGO;
-            } else if (bay instanceof LivestockCargoBay) {
-                return LIVESTOCK_CARGO;
-            } else if (bay instanceof RefrigeratedCargoBay) {
-                return REFRIGERATED_CARGO;
-            } else if (bay instanceof InsulatedCargoBay) {
-                return INSULATED_CARGO;
-            } else if (bay instanceof CargoBay) {
-                return CARGO;
-            } else {
-                // Crew quarters are implemented as bays and should not be mixed with transport bays
-                return null;
-            }
-        }
-        
-        public boolean isCargoBay() {
-            return ordinal() >= CARGO.ordinal();
-        }
     }
     
     /**

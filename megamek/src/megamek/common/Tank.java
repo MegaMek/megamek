99104ba7f989427b7d41e3e05d03d149b63b6e5c
Magnus Kerensky <magnusmd@hotmail.com>
2018-06-30 15:01:40 -0400

    Add trailer hitch to canTow check

--- megamek/src/megamek/common/Tank.java
+++ megamek/src/megamek/common/Tank.java
@@ -4114,7 +4114,7 @@ public class Tank extends Entity {
      * @return
      */
     public boolean canTowTrailers() {
-        if (hasMisc(MiscType.F_TRACTOR_MODIFICATION)) {
+        if (hasMisc(MiscType.F_TRACTOR_MODIFICATION) && hasWorkingMisc(MiscType.F_HITCH)) {
             return true;
         }
         return false;

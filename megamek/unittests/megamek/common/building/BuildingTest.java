5802dd5eb0af67ed38d411a00025ba0939e6bb28
Giorgio Gallo <giorgio.gallo@bitnic.it>
2018-08-18 21:45:58 +0200

    License headers added

--- megamek/unittests/megamek/common/building/BuildingTest.java
+++ megamek/unittests/megamek/common/building/BuildingTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2018 The MegaMek Team. All rights reserved.
+ *
+ * This file is part of MegaMek.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package megamek.common.building;
 
 import static org.junit.Assert.assertEquals;

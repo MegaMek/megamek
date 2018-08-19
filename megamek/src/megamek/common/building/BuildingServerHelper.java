24b823468db5617882bd9b89150e0b8bc45be5ca
Giorgio Gallo <giorgio.gallo@bitnic.it>
2018-08-19 16:42:33 +0200

    BuildingServerHelper cleaned up

--- megamek/src/megamek/common/building/BuildingServerHelper.java
+++ megamek/src/megamek/common/building/BuildingServerHelper.java
@@ -19,6 +19,7 @@
 package megamek.common.building;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import megamek.common.Compute;
 import megamek.common.Coords;
@@ -26,6 +27,7 @@ import megamek.common.IBoard;
 import megamek.common.IHex;
 import megamek.common.Report;
 import megamek.common.Terrains;
+import megamek.common.logging.DefaultMmLogger;
 
 /**
  * This class collects functionality originally in Building that is exclusively
@@ -37,26 +39,34 @@ public class BuildingServerHelper {
         // no instances
     }
 
-    public static void collapseBasement(Building b, Coords coords, IBoard board, List<Report> vPhaseReport) {
-        BuildingSection bs = b.sectionAt(coords).get();
+    /** @deprecated use {@link #collapseBasement(Building, IHex, Consumer)} instead */
+    @Deprecated public static void collapseBasement(Building b, Coords coords, IBoard board, List<Report> vPhaseReport) {
+        collapseBasement(b, board.getHex(coords), vPhaseReport::add);
+    }
+
+    public static void collapseBasement(Building b, IHex hex, Consumer<Report> disseminator) {
+        BuildingSection bs = b.sectionAt(hex.getCoords()).get();
 
         if (bs.getBasementType() == BasementType.NONE || bs.getBasementType() == BasementType.ONE_DEEP_NORMALINFONLY) {
-            System.err.println("hex has no basement to collapse"); //$NON-NLS-1$
-            return;
+
+            String msg = String.format("Basement at %s cannot collapse (basement typet: %s)", hex.getCoords().getBoardNum(), bs.getBasementType()); //$NON-NLS-1$
+            DefaultMmLogger.getInstance().warning(BuildingServerHelper.class, "collapseBasement", msg); //$NON-NLS-1$
+
+        } else if (bs.isBasementCollapsed()) {
+
+            String msg = String.format("Basement at %s already collapsed (basement typet: %s)", hex.getCoords().getBoardNum(), bs.getBasementType()); //$NON-NLS-1$
+            DefaultMmLogger.getInstance().warning(BuildingServerHelper.class, "collapseBasement", msg); //$NON-NLS-1$
+
+        } else {
+
+            String msg = String.format("Basement at %s is collapsing (basement type: %s)", hex.getCoords().getBoardNum(), bs.getBasementType()); //$NON-NLS-1$
+            DefaultMmLogger.getInstance().info(BuildingServerHelper.class, "collapseBasement", msg); //$NON-NLS-1$
+
+            disseminator.accept((newHexCollapsedReport(b, bs)));
+            hex.addTerrain(Terrains.getTerrainFactory().createTerrain(Terrains.BLDG_BASE_COLLAPSED, 1));
+            bs.setBasementCollapsed(true);
+
         }
-        if (b.getBasementCollapsed(coords)) {
-            System.err.println("hex has basement that already collapsed"); //$NON-NLS-1$
-            return;
-        }
-        Report r = new Report(2112, Report.PUBLIC);
-        r.add(b.getName());
-        r.add(coords.getBoardNum());
-        vPhaseReport.add(r);
-        System.err.println("basement " + bs.getBasementType() + "is collapsing, hex:" //$NON-NLS-1$ //$NON-NLS-2$
-                + coords.toString() + " set terrain!"); //$NON-NLS-1$
-        board.getHex(coords).addTerrain(Terrains.getTerrainFactory().createTerrain(
-                Terrains.BLDG_BASE_COLLAPSED, 1));
-        b.setBasementCollapsed(coords, true);
     }
 
     /**
@@ -64,26 +74,43 @@ public class BuildingServerHelper {
      * @param coords the <code>Coords</code> of theb building to roll for
      * @param vPhaseReport the <code>Vector<Report></code> containing the phasereport
      * @return a <code>boolean</code> indicating wether the hex and building was changed or not
+     * 
+     * @deprecated use {@link #rollBasement(Building, IHex, Consumer)} instead
      */
-    public static boolean rollBasement(Building b, Coords coords, IBoard board, List<Report> vPhaseReport) {
-        BuildingSection bs = b.sectionAt(coords).get();
-        if (bs.getBasementType() != BasementType.UNKNOWN) return false;
+    @Deprecated public static boolean rollBasement(Building b, Coords coords, IBoard board, List<Report> vPhaseReport) {
+        return rollBasement(b, board.getHex(coords), vPhaseReport::add);
+    }
 
-        IHex hex = board.getHex(coords);
+    public static boolean rollBasement(Building b, IHex hex, Consumer<Report> disseminator) {
+        BuildingSection bs = b.sectionAt(hex.getCoords()).get();
+        if (bs.getBasementType() != BasementType.UNKNOWN) {
+            return false;
+        } else {
+            int roll = Compute.d6(2);
+            BasementType bt = BasementType.basementsTable(roll);
+            bs.setBasementType(bt);
+            hex.addTerrain(Terrains.getTerrainFactory().createTerrain(Terrains.BLDG_BASEMENT_TYPE, bt.getId()));
+            disseminator.accept(newBasementRolledReport(b, bs, roll, bt));
+            return true;
+        }
+    }
+
+    // LATER these need to be moved in some ReportFactory of sorts
+
+    private static Report newHexCollapsedReport(Building b, BuildingSection bs) {
+        Report r = new Report(2112, Report.PUBLIC);
+        r.add(b.getName());
+        r.add(bs.getCoordinates().getBoardNum());
+        return r;
+    }
+
+    private static Report newBasementRolledReport(Building b, BuildingSection bs, int basementRoll, BasementType newType) {
         Report r = new Report(2111, Report.PUBLIC);
         r.add(b.getName());
-        r.add(coords.getBoardNum());
-
-        int basementRoll = Compute.d6(2);
+        r.add(bs.getCoordinates().getBoardNum());
         r.add(basementRoll);
-
-        BasementType newType = BasementType.basementsTable(basementRoll);
-        bs.setBasementType(newType);
-        hex.addTerrain(Terrains.getTerrainFactory().createTerrain(Terrains.BLDG_BASEMENT_TYPE, newType.getId()));
         r.add(newType.getDesc());
-
-        vPhaseReport.add(r);
-        return true;
+        return r;
     }
 
 }

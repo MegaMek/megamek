b445f7d025883f28500ac6fb13d5ff4e37b391a9
Carl Spain <neoancient@megamek.org>
2018-10-08 11:19:55 -0500

    Restrict protomech TAG to Clan.

--- megamek/src/megamek/common/weapons/tag/TAGWeapon.java
+++ megamek/src/megamek/common/weapons/tag/TAGWeapon.java
@@ -36,7 +36,7 @@ public abstract class TAGWeapon extends Weapon {
 
     public TAGWeapon() {
         super();
-        flags = flags.or(F_MECH_WEAPON).or(F_PROTO_WEAPON).or(F_TANK_WEAPON).andNot(F_AERO_WEAPON)
+        flags = flags.or(F_MECH_WEAPON).or(F_TANK_WEAPON).andNot(F_AERO_WEAPON)
                 .or(F_TAG).or(F_NO_FIRES);
         setModes(new String[] { "1-shot", "2-shot", "3-shot", "4-shot" });
     }

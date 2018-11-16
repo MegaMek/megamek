b5c42c9e635ceae32b2721f1bf56a2efd1eb8d59
Carl Spain <neoancient@megamek.org>
2018-11-16 09:47:01 -0600

    Whitespace cleanup

--- megamek/src/megamek/common/EntityFluff.java
+++ megamek/src/megamek/common/EntityFluff.java
@@ -22,9 +22,9 @@ import java.util.stream.Collectors;
 import megamek.common.annotations.Nullable;
 
 /**
- * Extracted from Entity
- * 
- * @author cwspain
+ * Tracks fluff details for entities.
+ *
+ * @author Neoancient
  *
  */
 public class EntityFluff implements Serializable {
@@ -32,22 +32,22 @@ public class EntityFluff implements Serializable {
      *
      */
     private static final long serialVersionUID = -8018098140016149185L;
-    
-    public enum System {
-    	CHASSIS, ENGINE, ARMOR, JUMPJET, COMMUNICATIONS, TARGETING;
 
-		public static @Nullable System parse(String string) {
-			if (null != string) {
-				for (System c : values()) {
-					if (c.toString().equals(string.toUpperCase())) {
-						return c;
-					}
-				}
-			}
-			return null;
-		}
+    public enum System {
+        CHASSIS, ENGINE, ARMOR, JUMPJET, COMMUNICATIONS, TARGETING;
+
+        public static @Nullable System parse(String string) {
+            if (null != string) {
+                for (final System c : values()) {
+                    if (c.toString().equals(string.toUpperCase())) {
+                        return c;
+                    }
+                }
+            }
+            return null;
+        }
     }
-    
+
     private String capabilities = "";
     private String overview = "";
     private String deployment = "";
@@ -55,12 +55,12 @@ public class EntityFluff implements Serializable {
 
     private String manufacturer = "";
     private String primaryFactory = "";
-    private Map<System, String> systemManufacturers = new EnumMap<>(System.class);
-    private Map<System, String> systemModels = new EnumMap<>(System.class);
-    
+    private final Map<System, String> systemManufacturers = new EnumMap<>(System.class);
+    private final Map<System, String> systemModels = new EnumMap<>(System.class);
+
     private String mmlImageFilePath = "";
     private String notes = "";
-    
+
     // For aerospace vessels
     private String use = "";
     private String length = "";
@@ -76,11 +76,11 @@ public class EntityFluff implements Serializable {
     }
 
     public void setCapabilities(String newCapabilities) {
-    	if (null != newCapabilities) {
-    		capabilities = newCapabilities;
-    	} else {
-    		capabilities = "";
-    	}
+        if (null != newCapabilities) {
+            capabilities = newCapabilities;
+        } else {
+            capabilities = "";
+        }
     }
 
     public String getOverview() {
@@ -88,11 +88,11 @@ public class EntityFluff implements Serializable {
     }
 
     public void setOverview(String newOverview) {
-    	if (null != newOverview) {
-    		overview = newOverview;
-    	} else {
-    		overview = "";
-    	}
+        if (null != newOverview) {
+            overview = newOverview;
+        } else {
+            overview = "";
+        }
     }
 
     public String getDeployment() {
@@ -100,95 +100,105 @@ public class EntityFluff implements Serializable {
     }
 
     public void setDeployment(String newDeployment) {
-    	if (null != newDeployment) {
-    		deployment = newDeployment;
-    	} else {
-    		deployment = "";
-    	}
+        if (null != newDeployment) {
+            deployment = newDeployment;
+        } else {
+            deployment = "";
+        }
     }
 
     public void setHistory(String newHistory) {
-    	if (null != newHistory) {
-    		history = newHistory;
-    	} else {
-    		history = "";
-    	}
+        if (null != newHistory) {
+            history = newHistory;
+        } else {
+            history = "";
+        }
     }
 
     public String getHistory() {
         return history;
     }
-    
+
     public String getManufacturer() {
-    	return manufacturer;
+        return manufacturer;
     }
-    
+
     public void setManufacturer(String manufacturer) {
-    	if (null != manufacturer) {
-    		this.manufacturer = manufacturer;
-    	} else {
-    		this.manufacturer = "";
-    	}
+        if (null != manufacturer) {
+            this.manufacturer = manufacturer;
+        } else {
+            this.manufacturer = "";
+        }
     }
-    
+
     public String getPrimaryFactory() {
-    	return primaryFactory;
+        return primaryFactory;
     }
-    
+
     public void setPrimaryFactory(String primaryFactory) {
-    	if (null != primaryFactory) {
-    		this.primaryFactory = primaryFactory;
-    	} else {
-    		this.primaryFactory = "";
-    	}
+        if (null != primaryFactory) {
+            this.primaryFactory = primaryFactory;
+        } else {
+            this.primaryFactory = "";
+        }
     }
-    
+
     /**
      * Retrieves the manufacturer of particular system component
-     * 
-     * @param system The system component
-     * @return       The name of the manufacturer, or an empty string if it has not been set.
+     *
+     * @param system
+     *            The system component
+     * @return The name of the manufacturer, or an empty string if it has not been
+     *         set.
      */
     public String getSystemManufacturer(System system) {
-    	return systemManufacturers.getOrDefault(system, "");
+        return systemManufacturers.getOrDefault(system, "");
     }
-    
+
     /**
      * Sets the name of the manufacturer of a particular system component.
-     * 
-     * @param system The system component
-     * @param manu The name of the manufacturer, or {@code null} or an empty string to remove the entry.
+     *
+     * @param system
+     *            The system component
+     * @param manu
+     *            The name of the manufacturer, or {@code null} or an empty string
+     *            to remove the entry.
      */
     public void setSystemManufacturer(System system, @Nullable String manu) {
-    	if ((null != manu) && (manu.length() > 0)) {
-    		systemManufacturers.put(system, manu);
-    	} else {
-    		systemManufacturers.remove(system);
-    	}
+        if ((null != manu) && (manu.length() > 0)) {
+            systemManufacturers.put(system, manu);
+        } else {
+            systemManufacturers.remove(system);
+        }
     }
-    
+
     /**
      * Retrieves the manufacturer of particular system component
-     * 
-     * @param system The system component
-     * @return       The name of the manufacturer, or an empty string if it has not been set.
+     *
+     * @param system
+     *            The system component
+     * @return The name of the manufacturer, or an empty string if it has not been
+     *         set.
      */
     public String getSystemModel(System system) {
-    	return systemModels.getOrDefault(system, "");
+        return systemModels.getOrDefault(system, "");
     }
-    
+
     /**
      * Sets the model name of a particular system component.
-     * 
-     * @param system  The system component
-     * @param model   The model name, or {@code null} or an empty string to remove the entry.
+     *
+     * @param system
+     *            The system component
+     * @param model
+     *            The model name, or {@code null} or an empty string to remove the
+     *            entry.
      */
     public void setSystemModel(System system, @Nullable String model) {
-    	if ((null != model) && (model.length() > 0)) {
-    		systemModels.put(system, model);
-    	} else {
-    		systemModels.remove(system);
-    	}
+        if ((null != model) && (model.length() > 0)) {
+            systemModels.put(system, model);
+        } else {
+            systemModels.remove(system);
+        }
     }
 
     public String getMMLImagePath() {
@@ -196,95 +206,90 @@ public class EntityFluff implements Serializable {
     }
 
     public void setMMLImagePath(String filePath) {
-    	if (null != filePath) {
-    		mmlImageFilePath = filePath;
-    	} else {
-    		mmlImageFilePath = "";
-    	}
+        if (null != filePath) {
+            mmlImageFilePath = filePath;
+        } else {
+            mmlImageFilePath = "";
+        }
     }
 
-	public String getNotes() {
-		return notes;
-	}
-	
-	public void setNotes(String notes) {
-    	if (null != notes) {
-    		this.notes = notes;
-    	} else {
-    		this.notes = "";
-    	}
-	}
-	
-	public String getUse() {
-		return use;
-	}
-	
-	public void setUse(String use) {
-    	if (null != use) {
-    		this.use = use;
-    	} else {
-    		this.use = "";
-    	}
-	}
-	
-	public String getLength() {
-		return length;
-	}
-	
-	public void setLength(String length) {
-    	if (null != length) {
-    		this.length = length;
-    	} else {
-    		this.length = "";
-    	}
-	}
-	
-	public String getWidth() {
-		return width;
-	}
-	
-	public void setWidth(String width) {
-    	if (null != width) {
-    		this.width = width;
-    	} else {
-    		this.width = "";
-    	}
-	}
-	
-	public String getHeight() {
-		return height;
-	}
-	
-	public void setHeight(String height) {
-    	if (null != height) {
-    		this.height = height;
-    	} else {
-    		this.height = "";
-    	}
-	}
-	
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        if (null != notes) {
+            this.notes = notes;
+        } else {
+            this.notes = "";
+        }
+    }
+
+    public String getUse() {
+        return use;
+    }
+
+    public void setUse(String use) {
+        if (null != use) {
+            this.use = use;
+        } else {
+            this.use = "";
+        }
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        if (null != length) {
+            this.length = length;
+        } else {
+            this.length = "";
+        }
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        if (null != width) {
+            this.width = width;
+        } else {
+            this.width = "";
+        }
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        if (null != height) {
+            this.height = height;
+        } else {
+            this.height = "";
+        }
+    }
+
     /**
      * Used for writing the system manufacturers to a unit file
-     * 
+     *
      * @return A list of all system manufacturers formatted as "system:manufacturer"
      */
     public List<String> createSystemManufacturersList() {
-    	return systemManufacturers.entrySet().stream()
-    			.filter(e -> e.getValue().length() > 0)
-    			.map(e -> e.getKey().toString() + ":" + e.getValue())
-    			.collect(Collectors.toList());
+        return systemManufacturers.entrySet().stream().filter(e -> e.getValue().length() > 0)
+                .map(e -> e.getKey().toString() + ":" + e.getValue()).collect(Collectors.toList());
     }
 
     /**
      * Used for writing the system models to a unit file
-     * 
+     *
      * @return A list of all system models formatted as "system:model"
      */
     public List<String> createSystemModelsList() {
-    	return systemModels.entrySet().stream()
-    			.filter(e -> e.getValue().length() > 0)
-    			.map(e -> e.getKey().toString() + ":" + e.getValue())
-    			.collect(Collectors.toList());
+        return systemModels.entrySet().stream().filter(e -> e.getValue().length() > 0)
+                .map(e -> e.getKey().toString() + ":" + e.getValue()).collect(Collectors.toList());
     }
 }
-

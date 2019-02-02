ece5fb28268c17333a0a86861c1f211bf4c509e9
Christopher Watford <christopher.watford@gmail.com>
2019-02-02 17:47:11 -0500

    Address array bounds bugs (lgtm)

--- megamek/src/megamek/common/CompositeTechLevel.java
+++ megamek/src/megamek/common/CompositeTechLevel.java
@@ -426,7 +426,7 @@ public class CompositeTechLevel implements ITechnology, Serializable {
 
     @Override
     public int getBaseAvailability(int era) {
-        if ((era < 0) || (era > availability.length)
+        if ((era < 0) || (era >= availability.length)
                 || (ITechnology.getTechEra(introYear) > era)) {
             return RATING_X;
         }

be8683fa2c016956d2c4f1f1260ace749386a432
MKerensky <magnusmd@hotmail.com>
2019-06-17 15:34:55 -0400

    Use artillery bay ammo slot passed in from the aaa

--- megamek/src/megamek/common/weapons/ArtilleryBayWeaponIndirectFireHandler.java
+++ megamek/src/megamek/common/weapons/ArtilleryBayWeaponIndirectFireHandler.java
@@ -178,6 +178,11 @@ public class ArtilleryBayWeaponIndirectFireHandler extends AmmoBayWeaponHandler 
             System.err.println("Artillery Entity is null!");
             return true;
         }
+        
+        Mounted ammoUsed = ae.getEquipment(aaa.getAmmoId());
+        final AmmoType atype = ammoUsed == null ? null : (AmmoType) ammoUsed
+                .getType();
+        
         // Are there any valid spotters?
         if ((null != spottersBefore) && !isFlak) {
             // fetch possible spotters now
@@ -400,53 +405,48 @@ public class ArtilleryBayWeaponIndirectFireHandler extends AmmoBayWeaponHandler 
                 return !bMissed;
             }
         }
-        for (int wId : weapon.getBayWeapons()) {
-            Mounted bayW = ae.getEquipment(wId);
-            Mounted bayWAmmo = bayW.getLinked();
-            AmmoType bayAmmoType = (AmmoType) bayWAmmo.getType();
-            if (bayAmmoType.getMunitionType() == AmmoType.M_FLARE) {
-                int radius;
-                if (bayAmmoType.getAmmoType() == AmmoType.T_ARROW_IV) {
-                    radius = 4;
-                } else if (bayAmmoType.getAmmoType() == AmmoType.T_LONG_TOM) {
-                    radius = 3;
-                } else if (bayAmmoType.getAmmoType() == AmmoType.T_SNIPER) {
-                    radius = 2;
-                } else {
-                    radius = 1;
-                }
-                server.deliverArtilleryFlare(targetPos, radius);
-                return false;
+        if (atype.getMunitionType() == AmmoType.M_FLARE) {
+            int radius;
+            if (atype.getAmmoType() == AmmoType.T_ARROW_IV) {
+                radius = 4;
+            } else if (atype.getAmmoType() == AmmoType.T_LONG_TOM) {
+                radius = 3;
+            } else if (atype.getAmmoType() == AmmoType.T_SNIPER) {
+                radius = 2;
+            } else {
+                radius = 1;
             }
-            if (bayAmmoType.getMunitionType() == AmmoType.M_DAVY_CROCKETT_M) {
-                // The appropriate term here is "Bwahahahahaha..."
-                server.doNuclearExplosion(targetPos, 1, vPhaseReport);
-                return false;
-            }
-            if (bayAmmoType.getMunitionType() == AmmoType.M_FASCAM) {
-                server.deliverFASCAMMinefield(targetPos, ae.getOwner().getId(),
-                        bayAmmoType.getRackSize(), ae.getId());
-                return false;
-            }
-            if (bayAmmoType.getMunitionType() == AmmoType.M_INFERNO_IV) {
-                server.deliverArtilleryInferno(targetPos, ae, subjectId,
-                        vPhaseReport);
-                return false;
-            }
-            if (bayAmmoType.getMunitionType() == AmmoType.M_VIBRABOMB_IV) {
-                server.deliverThunderVibraMinefield(targetPos, ae.getOwner()
-                        .getId(), bayAmmoType.getRackSize(), waa.getOtherAttackInfo(), ae
-                        .getId());
-                return false;
-            }
-            if (bayAmmoType.getMunitionType() == AmmoType.M_SMOKE) {
-                server.deliverArtillerySmoke(targetPos, vPhaseReport);
-                return false;
-            }
-            if (bayAmmoType.getMunitionType() == AmmoType.M_LASER_INHIB) {
-                server.deliverLIsmoke(targetPos, vPhaseReport);
-                return false;
-            }
+            server.deliverArtilleryFlare(targetPos, radius);
+            return false;
+        }
+        if (atype.getMunitionType() == AmmoType.M_DAVY_CROCKETT_M) {
+            // The appropriate term here is "Bwahahahahaha..."
+            server.doNuclearExplosion(targetPos, 1, vPhaseReport);
+            return false;
+        }
+        if (atype.getMunitionType() == AmmoType.M_FASCAM) {
+            server.deliverFASCAMMinefield(targetPos, ae.getOwner().getId(),
+                    atype.getRackSize(), ae.getId());
+            return false;
+        }
+        if (atype.getMunitionType() == AmmoType.M_INFERNO_IV) {
+            server.deliverArtilleryInferno(targetPos, ae, subjectId,
+                    vPhaseReport);
+            return false;
+        }
+        if (atype.getMunitionType() == AmmoType.M_VIBRABOMB_IV) {
+            server.deliverThunderVibraMinefield(targetPos, ae.getOwner()
+                    .getId(), atype.getRackSize(), waa.getOtherAttackInfo(), ae
+                    .getId());
+            return false;
+        }
+        if (atype.getMunitionType() == AmmoType.M_SMOKE) {
+            server.deliverArtillerySmoke(targetPos, vPhaseReport);
+            return false;
+        }
+        if (atype.getMunitionType() == AmmoType.M_LASER_INHIB) {
+            server.deliverLIsmoke(targetPos, vPhaseReport);
+            return false;
         }
         int altitude = 0;
         if (isFlak) {

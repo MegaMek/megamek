6401c711cd793cb60f49a671a04ca9bb7e74cfbc
binaryspica <47907439+binaryspica@users.noreply.github.com>
2019-06-27 21:10:24 +0900

    Update PhysicalAttackAction.java

--- megamek/src/megamek/common/actions/PhysicalAttackAction.java
+++ megamek/src/megamek/common/actions/PhysicalAttackAction.java
@@ -215,7 +215,7 @@ public class PhysicalAttackAction extends AbstractAttackAction {
             //but if it spots by TAG it doesn't.
             boolean isSpotByTag = false;//temporal boolean flag
             
-            //search through tagInfoForTurn and see if the 
+            //search through tagInfoForTurn and see if the entity has successfuly designated TAG on something.
             Vector<TagInfo> tmpTagInfoForTurn = game.getTagInfo();
             for (int i = 0; i < tmpTagInfoForTurn.size(); i++){
                 TagInfo tmpInfo = tmpTagInfoForTurn.elementAt(i);

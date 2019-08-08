cb51ddc5dbf3b324b82a3cfffb29ff125ec3f4d6
Carl Spain <neoancient@megamek.org>
2019-08-08 15:49:09 -0500

    Added unit test to verify that all lookup key constants are valid.

--- megamek/src/megamek/common/EquipmentTypeLookup.java
+++ megamek/src/megamek/common/EquipmentTypeLookup.java
@@ -18,6 +18,11 @@
  */
 package megamek.common;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Constants that can be used as lookup keys for {@link EquipmentType#get}. The constant should be used
  * as the internal name when creating the {@link EquipmentType}.
@@ -30,85 +35,169 @@ package megamek.common;
  */
 public class EquipmentTypeLookup {
 
+    /**
+     * Static fields in this class annotated with {code }@EquipmentName} will be checked by the unit tests
+     * to verify they are valid {@link EquipmentType} lookup keys.
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    public @interface EquipmentName{}
+
+    @EquipmentName
     public static final String JUMP_JET = "Jump Jet";
+    @EquipmentName
     public static final String IMPROVED_JUMP_JET = "Improved Jump Jet";
+    @EquipmentName
     public static final String PROTOTYPE_JUMP_JET = "ISPrototypeJumpJet";
+    @EquipmentName
     public static final String PROTOTYPE_IMPROVED_JJ = "ISPrototypeImprovedJumpJet";
+    @EquipmentName
     public static final String MECH_UMU = "UMU";
+    @EquipmentName
     public static final String MECH_JUMP_BOOSTER = "MechanicalJumpBooster";
+    @EquipmentName
     public static final String VEHICLE_JUMP_JET = "VehicleJumpJet";
+    @EquipmentName
     public static final String PROTOMECH_JUMP_JET = "ProtomechJumpJet";
+    @EquipmentName
     public static final String EXTENDED_JUMP_JET_SYSTEM = "ExtendedJumpJetSystem";
+    @EquipmentName
     public static final String PROTOMECH_UMU = "ProtomechUMU";
+    @EquipmentName
     public static final String PROTOMECH_MYOMER_BOOSTER = "CLMyomerBooster";
+    @EquipmentName
     public static final String BA_JUMP_JET = "BAJumpJet";
+    @EquipmentName
     public static final String BA_VTOL = "BAVTOL";
+    @EquipmentName
     public static final String BA_UMU = "BAUMU";
+    @EquipmentName
     public static final String BA_MYOMER_BOOSTER = "CLBAMyomerBooster";
+    @EquipmentName
     public static final String BA_PARTIAL_WING = "BAPartialWing";
+    @EquipmentName
     public static final String BA_JUMP_BOOSTER = "BAJumpBooster";
+    @EquipmentName
     public static final String BA_MECHANICAL_JUMP_BOOSTER = "BAMechanicalJumpBooster";
 
+    @EquipmentName
     public static final String SINGLE_HS = "Heat Sink";
+    @EquipmentName
     public static final String IS_DOUBLE_HS = "ISDoubleHeatSink";
+    @EquipmentName
     public static final String CLAN_DOUBLE_HS = "CLDoubleHeatSink";
+    @EquipmentName
     public static final String LASER_HS = "Laser Heat Sink";
+    @EquipmentName
     public static final String COMPACT_HS_1 = "1 Compact Heat Sink";
+    @EquipmentName
     public static final String COMPACT_HS_2 = "2 Compact Heat Sinks";
+    @EquipmentName
     public static final String IS_DOUBLE_HS_PROTOTYPE = "ISDoubleHeatSinkPrototype";
+    @EquipmentName
     public static final String IS_DOUBLE_HS_FREEZER = "ISDoubleHeatSinkFreezer";
 
+    @EquipmentName
     public static final String HITCH = "Hitch";
+    @EquipmentName
     public static final String CLAN_CASE = "CLCASE";
+    @EquipmentName
     public static final String COOLANT_POD = "Coolant Pod";
+    @EquipmentName
     public static final String MECH_TRACKS = "Tracks";
+    @EquipmentName
     public static final String QUADVEE_WHEELS = "QuadVee Wheels";
+    @EquipmentName
     public static final String IM_EJECTION_SEAT = "Ejection Seat (Industrial Mech)";
+    @EquipmentName
     public static final String TSM = "Triple Strength Myomer";
+    @EquipmentName
     public static final String SCM = "ISSuperCooledMyomer";
+    @EquipmentName
     public static final String ITSM = "Industrial Triple Strength Myomer";
+    @EquipmentName
     public static final String IS_MASC = "ISMASC";
+    @EquipmentName
     public static final String CLAN_MASC = "CLMASC";
+    @EquipmentName
     public static final String SPONSON_TURRET = "SponsonTurret";
+    @EquipmentName
     public static final String PINTLE_TURRET = "PintleTurret";
 
+    @EquipmentName
     public static final String LIMB_CLUB = "Limb Club";
+    @EquipmentName
     public static final String GIRDER_CLUB = "Girder Club";
+    @EquipmentName
     public static final String TREE_CLUB = "Tree Club";
 
+    @EquipmentName
     public static final String INFANTRY_ASSAULT_RIFLE = "InfantryAssaultRifle";
+    @EquipmentName
     public static final String INFANTRY_TAG = "InfantryTAG";
+    @EquipmentName
     public static final String VIBRO_SHOVEL = "Vibro-Shovel";
+    @EquipmentName
     public static final String DEMOLITION_CHARGE = "Demolition Charge";
 
+    @EquipmentName
     public static final String AC_BAY = "AC Bay";
+    @EquipmentName
     public static final String AMS_BAY = "AMS Bay";
+    @EquipmentName
     public static final String ARTILLERY_BAY = "Artillery Bay";
+    @EquipmentName
     public static final String AR10_BAY = "AR10 Bay";
+    @EquipmentName
     public static final String ATM_BAY = "ATM Bay";
+    @EquipmentName
     public static final String CAPITAL_AC_BAY = "Capital AC Bay";
+    @EquipmentName
     public static final String CAPITAL_GAUSS_BAY = "Capital Gauss Bay";
+    @EquipmentName
     public static final String CAPITAL_LASER_BAY = "Capital Laser Bay";
+    @EquipmentName
     public static final String CAPITAL_MASS_DRIVER_BAY = "Capital Mass Driver Bay";
+    @EquipmentName
     public static final String CAPITAL_MISSILE_BAY = "Capital Missile Bay";
+    @EquipmentName
     public static final String CAPITAL_PPC_BAY = "Capital PPC Bay";
+    @EquipmentName
     public static final String GAUSS_BAY = "Gauss Bay";
+    @EquipmentName
     public static final String LASER_BAY = "Laser Bay";
+    @EquipmentName
     public static final String LBX_AC_BAY = "LBX AC Bay";
+    @EquipmentName
     public static final String LRM_BAY = "LRM Bay";
+    @EquipmentName
     public static final String MISC_BAY = "Misc Bay";
+    @EquipmentName
     public static final String MML_BAY = "MML Bay";
+    @EquipmentName
     public static final String MRM_BAY = "MRM Bay";
+    @EquipmentName
     public static final String PLASMA_BAY = "Plasma Bay";
+    @EquipmentName
     public static final String POINT_DEFENSE_BAY = "Point Defense Bay";
+    @EquipmentName
     public static final String PPC_BAY = "PPC Bay";
+    @EquipmentName
     public static final String PULSE_LASER_BAY = "Pulse Laser Bay";
+    @EquipmentName
     public static final String ROCKET_LAUNCHER_BAY = "Rocket Launcher Bay";
+    @EquipmentName
     public static final String SCC_BAY = "Sub-Capital Cannon Bay";
+    @EquipmentName
     public static final String SCL_BAY = "Sub-Capital Laser Bay";
+    @EquipmentName
     public static final String SC_MISSILE_BAY = "Sub-Capital Missile Bay";
+    @EquipmentName
     public static final String SCREEN_LAUNCHER_BAY = "Screen Launcher Bay";
+    @EquipmentName
     public static final String SRM_BAY = "SRM Bay";
+    @EquipmentName
     public static final String TELE_CAPITAL_MISSILE_BAY = "Tele-Operated Capital Missile Bay";
+    @EquipmentName
     public static final String THUNDERBOLT_BAY = "Thunderbolt Bay";
 }

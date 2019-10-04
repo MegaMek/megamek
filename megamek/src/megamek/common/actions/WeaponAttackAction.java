36f9d49cfb50b072600727d7d2c92ac9cf607348
MKerensky <magnusmd@hotmail.com>
2019-10-04 16:16:31 -0400

    Add rules ref for cap missile artillery

--- megamek/src/megamek/common/actions/WeaponAttackAction.java
+++ megamek/src/megamek/common/actions/WeaponAttackAction.java
@@ -2866,6 +2866,7 @@ public class WeaponAttackAction extends AbstractAttackAction implements Serializ
         }
         
         // Capital missiles used for surface to surface artillery attacks
+        // See SO p110
         if ((wtype.getAtClass() == WeaponType.CLASS_AR10 || wtype.getAtClass() == WeaponType.CLASS_CAPITAL_MISSILE)
                 && Compute.isGroundToGround(ae, target)) {
             toHit.addModifier(2, Messages.getString("WeaponAttackAction.SubCapArtillery"));

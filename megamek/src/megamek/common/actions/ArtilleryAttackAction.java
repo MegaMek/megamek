1aae77584a7fc5d1491d4b253ae9f0e9f7718a6e
MKerensky <magnusmd@hotmail.com>
2019-10-08 12:42:25 -0400

    Update flight time calc for bearings only missiles

--- megamek/src/megamek/common/actions/ArtilleryAttackAction.java
+++ megamek/src/megamek/common/actions/ArtilleryAttackAction.java
@@ -55,6 +55,7 @@ public class ArtilleryAttackAction extends WeaponAttackAction implements
         distance = (int)Math.floor((double)distance/game.getPlanetaryConditions().getGravity());
         EquipmentType eType = getEntity(game).getEquipment(weaponId).getType();
         WeaponType wType = (WeaponType) eType;
+        Mounted mounted = getEntity(game).getEquipment(weaponId);
         if (getEntity(game).usesWeaponBays() && wType.getAtClass() == WeaponType.CLASS_ARTILLERY) {
             for (int wId : game.getEntity(entityId).getEquipment(weaponId).getBayWeapons()) {
                 Mounted bayW = game.getEntity(entityId).getEquipment(wId);
@@ -89,10 +90,8 @@ public class ArtilleryAttackAction extends WeaponAttackAction implements
         }
         //Capital missiles fired at bearings-only ranges will act like artillery and use this aaa.
         //An aaa will only be returned if the weapon is set to the correct mode
-        if (((wType.getAtClass() == WeaponType.CLASS_AR10)
-                || (wType.getAtClass() == WeaponType.CLASS_TELE_MISSILE)
-                || (wType.getAtClass() == WeaponType.CLASS_CAPITAL_MISSILE))
-                && (distance >= RangeType.RANGE_BEARINGS_ONLY_MINIMUM)) {
+        if (mounted.isInBearingsOnlyMode()
+                && distance >= RangeType.RANGE_BEARINGS_ONLY_MINIMUM) {
             this.launchVelocity = game.getOptions().intOption(OptionsConstants.ADVAERORULES_STRATOPS_BEARINGS_ONLY_VELOCITY);
             turnsTilHit = (int) (distance / launchVelocity);
             return;

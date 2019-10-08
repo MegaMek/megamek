89dbfa692ed4face0449a2aa389bb83939b003d0
MKerensky <magnusmd@hotmail.com>
2019-10-08 10:15:02 -0400

    Fix handler return logic

--- megamek/src/megamek/common/weapons/capitalweapons/CapitalMissileWeapon.java
+++ megamek/src/megamek/common/weapons/capitalweapons/CapitalMissileWeapon.java
@@ -17,15 +17,12 @@
  */
 package megamek.common.weapons.capitalweapons;
 
-import megamek.common.Dropship;
 import megamek.common.Entity;
 import megamek.common.IGame;
 import megamek.common.Mounted;
 import megamek.common.RangeType;
 import megamek.common.ToHitData;
 import megamek.common.actions.WeaponAttackAction;
-import megamek.common.options.GameOptions;
-import megamek.common.options.OptionsConstants;
 import megamek.common.weapons.AmmoWeapon;
 import megamek.common.weapons.ArtilleryWeaponIndirectFireHandler;
 import megamek.common.weapons.AttackHandler;
@@ -62,8 +59,8 @@ public abstract class CapitalMissileWeapon extends AmmoWeapon {
                 .getEquipment(waa.getWeaponId());
         Entity attacker = game.getEntity(waa.getEntityId());
         int rangeToTarget = attacker.getPosition().distance(waa.getTarget(game).getPosition());
-        // Capital missiles work like artillery for grounded dropships
-        if (attacker instanceof Dropship && !attacker.isAirborne()) {
+        // Capital missiles work like artillery for grounded units
+        if (!attacker.isAirborne()) {
             return new ArtilleryWeaponIndirectFireHandler(toHit, waa, game, server);
         }
         if (weapon.isInBearingsOnlyMode()

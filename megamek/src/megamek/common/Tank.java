f9c3c641f0e558683104242135baef110d827f6c
Carl Spain <neoancient@megamek.org>
2020-02-19 21:57:51 -0600

    Account for heavier structure in non-naval superheavy vehicles when calculating cost.

--- megamek/src/megamek/common/Tank.java
+++ megamek/src/megamek/common/Tank.java
@@ -2677,8 +2677,13 @@ public class Tank extends Entity {
             double techRatingMultiplier = 0.5 + (getStructuralTechRating() * 0.25);
             costs[structCostIdx] *= techRatingMultiplier;
         } else {
-            // IS has no variations, no Endo etc.
-            costs[i++] = RoundWeight.nextHalfTon(weight / 10.0) * 10000;
+            // IS has no variations, no Endo etc., but non-naval superheavies have heavier structure
+            if (!isSuperHeavy() || getMovementMode().equals(EntityMovementMode.NAVAL)
+                    || getMovementMode().equals(EntityMovementMode.SUBMARINE)) { // There are no superheavy hydrofoils
+                costs[i++] = RoundWeight.nextHalfTon(weight / 10.0) * 10000;
+            } else {
+                costs[i++] = RoundWeight.nextHalfTon(weight / 5.0) * 10000;
+            }
             double controlWeight = hasNoControlSystems() ? 0.0 : RoundWeight.nextHalfTon(weight * 0.05); // ?
             // should be rounded up to nearest half-ton
             costs[i++] = 10000 * controlWeight;

4de038374da6bceec66f0e16f8eb60d48539113c
Windchild292 <jrbowen24@gmail.com>
2020-02-29 16:03:52 -0500

    Implementing the MegaMekFile changes in BoardsValidator

--- megamek/src/megamek/utils/BoardsValidator.java
+++ megamek/src/megamek/utils/BoardsValidator.java
@@ -1,7 +1,7 @@
 /*
  * MegaMek - Copyright (C) 2000,2001,2002,2003,2004 Ben Mazur (bmazur@sev.org)
  * Copyright © 2013 Nicholas Walczak (walczak@cs.umn.edu)
- *
+ *  Copyright (c) 2020 - The MegaMek team
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by the Free
  *  Software Foundation; either version 2 of the License, or (at your option)
@@ -12,16 +12,16 @@
  *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  *  for more details.
  */
-
 package megamek.utils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
 
+import megamek.MegaMek;
 import megamek.common.Board;
 import megamek.common.Configuration;
+import megamek.common.IBoard;
+import megamek.common.util.MegaMekFile;
 
 /**
  * This class provides a utility to read in all of the boards and check their validity.
@@ -30,74 +30,61 @@ import megamek.common.Configuration;
  *
  */
 public class BoardsValidator {
-    
     int numBoardErrors = 0;
     
     public BoardsValidator() {
-        
+
     }
 
     /**
-     * Recursively scans the supplied File for any boards and validates them.  If the supplied File is a directory, then
-     * all files in that directory are recursively scanned.
      *
-     * @param file
-     * @throws IOException
-     */
-    private void scanForBoards(File file) throws IOException {
-        if (file.isDirectory()) {
-            String fileList[] = file.list();
-            for (String filename : fileList) {
-                File filepath = new File(file, filename);
-                if (filepath.isDirectory()) {
-                    scanForBoards(new File(file, filename));
-                } else {
-                    validateBoard(filepath);
-                }
-            }
-        } else {
-            validateBoard(file);
-        }
-    }
-
-    /**
-     * Check whether the supplied file is a valid board file or not.  Ignores files that don't end in .board.  Any
-     * errors are logged to System.out.
-     * 
-     * @param boardFile
-     * @throws FileNotFoundException
-     */
-    private void validateBoard(File boardFile) throws FileNotFoundException {
-        // If this isn't a board, ignore it
-        if (!boardFile.toString().endsWith(".board")) {
-            return;
-        }
-        
-        java.io.InputStream is = new FileInputStream(boardFile);
-        StringBuffer errBuff = new StringBuffer();
-        Board b = new Board();
-        b.load(is, errBuff, false);
-        if (errBuff.length() > 0) {
-            System.out.println("Found Invalid Board! Board: " + boardFile);
-            System.out.println(errBuff);
-            numBoardErrors++;
-        }
-    }
-
-    /**
-     * 
-     * @param args
+     * @param args the arguments to apply to the BoardValidator (ignored)
      */
     public static void main(String[] args) {
-        try {
-            File boardDir = Configuration.boardsDir();
-            BoardsValidator validator = new BoardsValidator();
-            
-            validator.scanForBoards(boardDir);
-            System.out.println("Found " + validator.numBoardErrors + " boards with errors!");
-        }catch (IOException e){
-            System.out.println("IOException!");
-            e.printStackTrace();
+        BoardsValidator validator = new BoardsValidator();
+
+        List<File> fileList = new MegaMekFile(Configuration.boardsDir().getName()).getFiles();
+        for (File file : fileList) {
+            validator.validateBoard(file);
+        }
+        if (validator.numBoardErrors > 0) {
+            MegaMek.getLogger().warning(BoardsValidator.class, "main",
+                    "Found " + validator.numBoardErrors + " boards with errors!");
+        } else {
+            MegaMek.getLogger().info(BoardsValidator.class, "main",
+                    "No boards with errors found in board validation.");
+        }
+    }
+
+    /**
+     * Check whether the supplied file is a valid board file or not. It ignores any files that do
+     * not end with
+     * errors are logged to System.out.
+     * 
+     * @param boardFile the board file to check
+     */
+    private void validateBoard(File boardFile) {
+        // If this isn't a board, ignore it
+        if (!boardFile.toString().endsWith(IBoard.BOARD_FILE_EXTENSION)) {
+            return;
+        }
+
+        try (InputStream is = new FileInputStream(boardFile)) {
+            StringBuffer errBuff = new StringBuffer();
+            Board board = new Board();
+            board.load(is, errBuff, false);
+
+            if (errBuff.length() > 0) {
+                MegaMek.getLogger().error(BoardsValidator.class, "validateBoard",
+                        "Found Invalid Board! Board: " + boardFile.toString());
+                MegaMek.getLogger().error(BoardsValidator.class, "validateBoard",
+                        errBuff.toString());
+                numBoardErrors++;
+            }
+        } catch (Exception e) {
+            MegaMek.getLogger().error(BoardsValidator.class, "main",
+                    "Error in loading board " + boardFile);
+            MegaMek.getLogger().error(BoardsValidator.class, "validateBoard", e);
         }
     }
 }

f98a1d353d4f0ccba2f4601c5501c13b96c5853e
Carl Spain <neoancient@megamek.org>
2020-04-21 18:46:20 -0500

    Round munitions bv and cost to three decimal places to eliminate floating point errors.

--- megamek/src/megamek/common/AmmoType.java
+++ megamek/src/megamek/common/AmmoType.java
@@ -15832,8 +15832,8 @@ public class AmmoType extends EquipmentType {
                     && (munition.getMunitionType() == AmmoType.M_COOLANT)) {
                 cost = 3000;
             }
-            munition.bv = bv;
-            munition.cost = cost;
+            munition.bv = Math.round(bv * 1000.0) / 1000.0;
+            munition.cost = Math.round(cost * 1000.0) / 1000.0;
 
             // Copy over all other values.
             munition.damagePerShot = base.damagePerShot;

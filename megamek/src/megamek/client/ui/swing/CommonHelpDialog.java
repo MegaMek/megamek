9799c85788e0bef96d99790860c85ebeecf8d9d7
Simon <juliez@outlook.de>
2020-05-03 17:30:38 +0200

    Refactor CommonHelpDialog

--- megamek/src/megamek/client/ui/swing/CommonHelpDialog.java
+++ megamek/src/megamek/client/ui/swing/CommonHelpDialog.java
@@ -1,5 +1,6 @@
 /*
  * MegaMek - Copyright (C) 2003 Ben Mazur (bmazur@sev.org)
+ * MegaMek - Copyright (C) 2020 - The MegaMek Team  
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by the Free
@@ -18,21 +19,13 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import java.io.File;
-import java.net.URL;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.InputMap;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
-import javax.swing.KeyStroke;
 
 import megamek.client.ui.Messages;
 
@@ -42,62 +35,42 @@ import megamek.client.ui.Messages;
 public class CommonHelpDialog extends JDialog {
 
     private static final long serialVersionUID = 5189627839475444823L;
-    private static final String CLOSEACTION = "CloseAction"; //$NON-NLS-1$
 
     /**
      * Create a help dialog for the given <code>parentFrame</code> by reading
      * from the indicated <code>File</code>.
      */
     public CommonHelpDialog(JFrame parentFrame, File helpfile) {
-        // Construct the superclass.
         super(parentFrame);
 
-        // Load the help file
         setLayout(new BorderLayout());
-        JTextPane helpPane = new JTextPane();
+        JEditorPane helpPane = new JEditorPane();
         helpPane.setEditable(false);
 
+        // Get the help content file if possible
         try {
-            URL url = helpfile.toURI().toURL();
-            helpPane.setPage(url);
+            helpPane.setPage(helpfile.toURI().toURL());
             setTitle(Messages.getString("CommonHelpDialog.helpFile") + helpfile.getName()); //$NON-NLS-1$
-        } catch (Exception e1) {
+        } catch (Exception exc) {
             helpPane.setText(Messages.getString("CommonHelpDialog.errorReading") //$NON-NLS-1$
-                    + e1.getMessage());
+                    + exc.getMessage());
             setTitle(Messages.getString("CommonHelpDialog.noHelp.title")); //$NON-NLS-1$
-            e1.printStackTrace();
+            exc.printStackTrace();
         }
 
-        // Add a "Close" button.
-        Action closeAction = new AbstractAction() {
-            private static final long serialVersionUID = 1680850851585381148L;
+        // Close Button
+        JButton butClose = new ButtonEsc(new CloseAction(this));
 
-            public void actionPerformed(ActionEvent e) {
-                setVisible(false);
-            }
-        };
-        JButton butClose = new JButton(closeAction);
-        butClose.setText(Messages.getString("Close")); //$NON-NLS-1$
-
-        InputMap imap = butClose.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-        imap.put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0), CLOSEACTION);
-        imap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), CLOSEACTION);
-        butClose.getActionMap().put(CLOSEACTION, closeAction);
-
-        // Add to the frame
-        JScrollPane scroll = new JScrollPane(helpPane);
-        getContentPane().add(scroll, BorderLayout.CENTER);
+        // Add all to the dialog
+        getContentPane().add(new JScrollPane(helpPane), BorderLayout.CENTER);
         getContentPane().add(butClose, BorderLayout.SOUTH);
-
-        // Make the window half the screensize by default.
+        
+        // Make the window half the screensize and center on screen
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         Dimension windowSize = new Dimension(gd.getDisplayMode().getWidth() / 2,
                 gd.getDisplayMode().getHeight() / 2);
         pack();
         setSize(windowSize);
-
-        // Place this dialog on middle of screen.
         setLocationRelativeTo(null);
     }
-
 }

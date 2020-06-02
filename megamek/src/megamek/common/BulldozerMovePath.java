0fe86664e9ecefe90b61e35dabc871bb0578188d
NickAragua <a_pokrovski@hotmail.com>
2020-06-01 20:27:51 -0400

    Update megamek/src/megamek/common/BulldozerMovePath.java
    Co-authored-by: Justin Bowen <39067288+Windchild292@users.noreply.github.com>

--- megamek/src/megamek/common/BulldozerMovePath.java
+++ megamek/src/megamek/common/BulldozerMovePath.java
@@ -31,9 +31,6 @@ import megamek.common.pathfinder.BoardClusterTracker.MovementType;
  * @author NickAragua
  */
 public class BulldozerMovePath extends MovePath {
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1346716014573707012L;
 
     public static final int CANNOT_LEVEL = -1;

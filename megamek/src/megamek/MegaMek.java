c53025896d7bc07f55135e4e2fdc2cc04d1d8b96
HammerGS <drivingguy@hotmail.com>
2020-11-15 14:18:59 -0700

    Ready to Develop <v0.47.15>

--- megamek/src/megamek/MegaMek.java
+++ megamek/src/megamek/MegaMek.java
@@ -63,7 +63,7 @@ import megamek.utils.RATGeneratorEditor;
 public class MegaMek {
     private static MMLogger logger = null;
 
-    public static String VERSION = "0.47.14";
+    public static String VERSION = "0.47.15-SNAPSHOT";
     public static long TIMESTAMP = new File(PreferenceManager.getClientPreferences().getLogDirectory()
             + File.separator + "timestamp").lastModified();
 

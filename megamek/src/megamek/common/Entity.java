c6c633c6846b2b27a1a2612413db28c088eafeb2
NickAragua <a_pokrovski@hotmail.com>
2021-01-22 22:47:06 -0500

    more thorough destroyed location equipment destruction

--- megamek/src/megamek/common/Entity.java
+++ megamek/src/megamek/common/Entity.java
@@ -11462,19 +11462,10 @@ public abstract class Entity extends TurnOrdered implements Transporter, Targeta
                 setArmor(IArmorState.ARMOR_DOOMED, loc, true);
             }
         }
-        // equipment marked missing
-        for (Mounted mounted : getEquipment()) {
-            if (((mounted.getLocation() == loc) && mounted.getType()
-                                                          .isHittable())
-                || (mounted.isSplit() && (mounted.getSecondLocation() == loc))) {
-                if (blownOff) {
-                    mounted.setMissing(true);
-                } else {
-                    mounted.setHit(true);
-                }
-            }
-        }
+
         // all critical slots set as missing
+        // while we're here, if something is mounted in those crits, set it as hit, 
+        // instead of looping through all equipment in the unit
         for (int i = 0; i < getNumberOfCriticals(loc); i++) {
             final CriticalSlot cs = getCritical(loc, i);
             if (cs != null) {
@@ -11484,13 +11475,32 @@ public abstract class Entity extends TurnOrdered implements Transporter, Targeta
                     && !cs.isDamaged()) {
                     engineHitsThisPhase++;
                 }
+                
                 if (blownOff) {
                     cs.setMissing(true);
+                    
+                    if (cs.getMount() != null) {
+                        cs.getMount().setMissing(true);
+                    }
+                    
+                    if (cs.getMount2() != null) {
+                        cs.getMount2().setMissing(true);
+                    }
+                    
                 } else {
                     cs.setHit(true);
+                    
+                    if (cs.getMount() != null) {
+                        cs.getMount().setHit(true);
+                    }
+                    
+                    if (cs.getMount2() != null) {
+                        cs.getMount2().setHit(true);
+                    }
                 }
             }
         }
+        
         // dependent locations destroyed, unless they are already destroyed
         if ((getDependentLocation(loc) != Entity.LOC_NONE)
             && !(getInternal(getDependentLocation(loc)) < 0)) {

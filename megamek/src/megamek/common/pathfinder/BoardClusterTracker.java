776cc246619bb3db7c755a29ffed138a0566ae8b
NickAragua <a_pokrovski@hotmail.com>
2021-01-28 13:26:32 -0500

    prevent phantom buildings from crashing bot deployment

--- megamek/src/megamek/common/pathfinder/BoardClusterTracker.java
+++ megamek/src/megamek/common/pathfinder/BoardClusterTracker.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import java.util.Set;
 
 import megamek.client.bot.princess.CardinalEdge;
+import megamek.common.Building;
 import megamek.common.BulldozerMovePath;
 import megamek.common.Coords;
 import megamek.common.Entity;
@@ -365,6 +366,12 @@ public class BoardClusterTracker {
         if (!hex.containsTerrain(Terrains.BLDG_CF) && !hex.containsExit(Terrains.FUEL_TANK_CF)) {
             return false;
         } else if (relevantMovementType == MovementType.Walker) {
+            Building building = board.getBuildingAt(coords);
+            
+            if (building == null) {
+                return false;
+            }
+            
             int buildingCF = board.getBuildingAt(coords).getCurrentCF(coords);
             
             return entity.getWeight() > buildingCF;            

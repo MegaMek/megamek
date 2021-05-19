c4e6d56dde29876a5ae91f1efdee398acf11b5c9
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-05-19 14:53:32 +0200

    Add basic structure for VictoryResultTest

---
+++ megamek/unittests/megamek/server/victory/VictoryResultTest.java
@@ -0,0 +1,13 @@
+package megamek.server.victory;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+
+
+@RunWith(JUnit4.class)
+public class VictoryResultTest {
+
+}

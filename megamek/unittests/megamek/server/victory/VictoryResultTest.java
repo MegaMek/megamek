486f4f685904540e1f9c3447a14dcd64f977e330
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-05-19 15:23:20 +0200

    Add test for getWinningPlayer() method of VictoryResult class

--- megamek/unittests/megamek/server/victory/VictoryResultTest.java
+++ megamek/unittests/megamek/server/victory/VictoryResultTest.java
@@ -1,13 +1,37 @@
 package megamek.server.victory;
 
-import org.junit.Assert;
+import junit.framework.TestCase;
+import megamek.common.Player;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.Mockito;
 
 
 @RunWith(JUnit4.class)
 public class VictoryResultTest {
+
+    @Test
+    public void testGetWinningPlayer() {
+        // Trivial case: no players
+        VictoryResult testResult = new VictoryResult(false);
+        TestCase.assertSame(Player.PLAYER_NONE, testResult.getWinningPlayer());
+
+        // Case with two players
+        int winningPlayer = 0;
+        int losingPlayer = 1;
+
+        testResult.addPlayerScore(winningPlayer, 100);
+        testResult.addPlayerScore(losingPlayer, 40);
+
+        TestCase.assertSame(winningPlayer, testResult.getWinningPlayer());
+
+        // Case with three players and a draw
+        int secondWinningPlayer = 2;
+
+        testResult.addPlayerScore(secondWinningPlayer, 100);
+        TestCase.assertNotSame(secondWinningPlayer, testResult.getWinningPlayer());
+        TestCase.assertNotSame(winningPlayer, testResult.getWinningPlayer());
+        TestCase.assertSame(Player.PLAYER_NONE, testResult.getWinningPlayer());
+    }
 
 }

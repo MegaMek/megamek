4bd3f5502b19e4538fa0b4642cfde88fcb4196c4
robbevandevelde <robbe.vandevelde@student.unatwerpen.be>
2021-05-20 13:23:58 +0200

    Updated test for the victory function within server.java
    added a basic test with mocks that test if the victory function will return false when needed

--- megamek/unittests/megamek/server/victory/VictoryTest.java
+++ megamek/unittests/megamek/server/victory/VictoryTest.java
@@ -1,5 +1,9 @@
 package megamek.server.victory;
 
+import megamek.common.Entity;
+import megamek.common.IPlayer;
+import megamek.common.event.GameListener;
+import megamek.server.Server;
 import org.junit.Assert;
 import junit.framework.TestCase;
 import megamek.common.IGame;
@@ -8,18 +12,30 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
 
+import java.io.IOException;
+import java.util.*;
+
 import static org.mockito.Mockito.doReturn;
 
 
 @RunWith(JUnit4.class)
 public class VictoryTest {
     @Test
-    public void testVictory() {
+    public void testVictory() throws IOException {
+        Server testServer = new Server("test", 123);
         Victory testVictory = Mockito.mock(Victory.class);
-        VictoryResult testVictoryResult = Mockito.mock(VictoryResult.class);
+        VictoryResult testVictoryResult = new VictoryResult(false);
+
         IGame testGame = Mockito.mock(IGame.class);
         Mockito.when(testGame.getVictory()).thenReturn(testVictory);
+        Mockito.when(testGame.getGameListeners()).thenReturn(new Vector<GameListener>());
+        Mockito.when(testGame.getEntities()).thenReturn(Collections.emptyIterator());
+        Mockito.when(testGame.getPlayers()).thenReturn(Collections.emptyEnumeration());
+        Mockito.when(testGame.getAttacks()).thenReturn(Collections.emptyEnumeration());
         Mockito.when(testVictory.checkForVictory(testGame, testGame.getVictoryContext())).thenReturn(testVictoryResult);
+        testServer.setGame(testGame);
+
+        TestCase.assertFalse(testServer.victory());
 
     }
 }

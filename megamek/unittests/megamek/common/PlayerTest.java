e5500bad097efd9441f5c14fb6e0642b350df3f8
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-05-22 14:48:43 +0200

    Create test for getColorForPlayer()

--- megamek/unittests/megamek/common/PlayerTest.java
+++ megamek/unittests/megamek/common/PlayerTest.java
@@ -1,6 +1,7 @@
 package megamek.common;
 
 import junit.framework.TestCase;
+import megamek.client.ui.swing.util.PlayerColour;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -10,6 +11,14 @@ public class PlayerTest {
 
     @Test
     public void testGetColorForPlayer() {
+        String playerName = "jefke";
+        IPlayer player = new Player(0, playerName);
+        TestCase.assertEquals("<B><font color='8080b0'>" + playerName + "</font></B>", player.getColorForPlayer());
+
+        playerName = "Jeanke";
+        IPlayer player2 = new Player(1, playerName);
+        player2.setColour(PlayerColour.FUCHSIA);
+        TestCase.assertEquals("<B><font color='f000f0'>" + playerName + "</font></B>", player2.getColorForPlayer());
 
     }
 

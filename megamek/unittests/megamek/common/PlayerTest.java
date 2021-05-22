4ddab251480575dd8973d0cad38745930cd6274d
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-05-22 14:41:32 +0200

    Create stub test for PlayerTest
    The moved function getColorForPlayer() should be tested

---
+++ megamek/unittests/megamek/common/PlayerTest.java
@@ -0,0 +1,16 @@
+package megamek.common;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class PlayerTest {
+
+    @Test
+    public void testGetColorForPlayer() {
+
+    }
+
+}

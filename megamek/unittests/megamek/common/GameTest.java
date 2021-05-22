3670678b7045b9fcb090c2f7a4eeb6d13077915c
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-05-22 14:22:07 +0200

    Create test for cancelVictory

--- megamek/unittests/megamek/common/GameTest.java
+++ megamek/unittests/megamek/common/GameTest.java
@@ -1,5 +1,6 @@
 package megamek.common;
 
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -9,7 +10,23 @@ public class GameTest {
 
     @Test
     public void testCancelVictory() {
+        // Default test
+        IGame game = new Game();
+        game.cancelVictory();
+        TestCase.assertFalse(game.isForceVictory());
+        TestCase.assertSame(IPlayer.PLAYER_NONE, game.getVictoryPlayerId());
+        TestCase.assertSame(IPlayer.TEAM_NONE, game.getVictoryTeam());
 
+        // Test with members set to specific values
+        IGame game2 = new Game();
+        game2.setVictoryPlayerId(10);
+        game2.setVictoryTeam(10);
+        game2.setForceVictory(true);
+
+        game2.cancelVictory();
+        TestCase.assertFalse(game.isForceVictory());
+        TestCase.assertSame(IPlayer.PLAYER_NONE, game.getVictoryPlayerId());
+        TestCase.assertSame(IPlayer.TEAM_NONE, game.getVictoryTeam());
     }
 
     @Test

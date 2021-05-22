a3da2fb29debef3c3948b864c5aa61c490f97ed7
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-05-22 14:14:42 +0200

    Add stubs for GameTest tests
    The newly created methods cancelVictory() and getVictoryReport() should be tested

---
+++ megamek/unittests/megamek/common/GameTest.java
@@ -0,0 +1,20 @@
+package megamek.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class GameTest {
+
+    @Test
+    public void testCancelVictory() {
+
+    }
+
+    @Test
+    public void testGetVictoryReport() {
+
+    }
+
+}

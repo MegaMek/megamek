e295e2d35c0cf3f85c8395d89eeb245f2bd3c839
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-05-24 10:29:37 +0200

    Replace type specification with diamond operator in getAirborneVTOL
    This was a smell detected by SonarQube

--- megamek/src/megamek/common/Player.java
+++ megamek/src/megamek/common/Player.java
@@ -565,7 +565,7 @@ public final class Player extends TurnOrdered implements IPlayer {
     public Vector<Integer> getAirborneVTOL() {
 
         //a vector of unit ids
-        Vector<Integer> units = new Vector<Integer>();
+        Vector<Integer> units = new Vector<>();
         for (Entity entity : game.getEntitiesVector()) {
             if (entity.getOwner().equals(this)) {
                 if (((entity instanceof VTOL)

5648363e43263865a71a5aa5e92d2b3eda62e83b
unknown <jrbowen24@gmail.com>
2021-09-22 13:10:47 -0400

    Creating base version code for MegaMek

---
+++ megamek/src/megamek/SuiteConstants.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 - The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MegaMek.
+ *
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MegaMek. If not, see <http://www.gnu.org/licenses/>.
+ */
+package megamek;
+
+/**
+ * These are constants that hold across the entire MegaMek Suite.
+ */
+public abstract class SuiteConstants {
+    //region General Constants
+    public static final Version VERSION = new Version("0.49.4-SNAPSHOT");
+    public static final int MAXIMUM_D6_VALUE = 6;
+    //endregion General Constants
+}

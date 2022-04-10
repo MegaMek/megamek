29cf4b402f4f36d6c029129e3d293947713ff07e
emilelegault <e1legault@gmail.com>
2022-04-10 19:55:39 -0400

    Add VictoryResult

--- megamek/unittests/megamek/server/victory/EnemyCommanderDestroyedVictoryTest.java
+++ megamek/unittests/megamek/server/victory/EnemyCommanderDestroyedVictoryTest.java
@@ -5,14 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(value = JUnit4.class)
 public class EnemyCommanderDestroyedVictoryTest {
 
     @Test
-    public void EnemyCommanderDestroyedVictoryTest(){
+    public void WhenEnemyCommanderDestroyedThenVictoryResultIsTrue(){
 
         //arrange
         var EnemyCommanderDestroyedVictory = new EnemyCmdrDestroyedVictory();

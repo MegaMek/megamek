8f1dd8c7e08c6d2333be06000bda43af33273561
kuronekochomusuke <kuronekochomusuke@gmail.com>
2023-04-13 13:55:18 -0400

    add orginal armor and internal structure vaues

--- megamek/src/megamek/client/ui/swing/tooltip/UnitToolTip.java
+++ megamek/src/megamek/client/ui/swing/tooltip/UnitToolTip.java
@@ -1299,8 +1299,8 @@ public final class UnitToolTip {
                 armorType = (entity.isCapitalScale() ? msg_armorcapital + " " : "") + armorType;
                 armorType = " (" + armorType + ")";
             }
-            String armorStr = " " + entity.getTotalArmor() + armorType;
-            String sArmorInternals = addToTT("ArmorInternals", NOBR, armorStr, entity.getTotalInternal()).toString();
+            String armorStr = " " + entity.getTotalArmor() + " / " + entity.getTotalOArmor() + armorType;
+            String sArmorInternals = addToTT("ArmorInternals", NOBR, armorStr, entity.getTotalInternal()).toString() + " / " + entity.getTotalOInternal();
             l4 = "<Li style=\"list-style-type: none; list-style-image: none; margin: 0; padding: 0;\">" + sArmorInternals + "</Li>";
         }
 

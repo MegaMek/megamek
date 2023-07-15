ad3cb99ba19e2420adb9e1e0f00b0087d198682c
kuronekochomusuke <kuronekochomusuke@gmail.com>
2023-07-15 11:44:43 -0400

    allow field of fire and sensors rings to display in indirect attacks phase for tag

--- megamek/src/megamek/client/ui/swing/boardview/BoardView.java
+++ megamek/src/megamek/client/ui/swing/boardview/BoardView.java
@@ -6664,13 +6664,15 @@ public class BoardView extends JPanel implements Scrollable, BoardListener, Mous
     public boolean shouldShowFieldOfFire() {
         return GUIP.getShowFieldOfFire() &&
                 (game.getPhase().isDeployment() || game.getPhase().isMovement()
-                        || game.getPhase().isTargeting() || game.getPhase().isFiring());
+                        || game.getPhase().isTargeting() || game.getPhase().isFiring()
+                        || game.getPhase().isOffboard());
     }
 
     public boolean shouldShowSensorRange() {
         return GUIP.getShowSensorRange() &&
                 (game.getPhase().isDeployment() || game.getPhase().isMovement()
-                        || game.getPhase().isTargeting() || game.getPhase().isFiring());
+                        || game.getPhase().isTargeting() || game.getPhase().isFiring()
+                        || game.getPhase().isOffboard());
     }
 
 }

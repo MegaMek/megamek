83984bf0dbacc2c417bdf14d6d88fa7ea5edda1e
sleet01 <sleet01@hotmail.com>
2024-01-25 12:39:06 -0800

    Add swarming check to see if all rounds hit

--- megamek/src/megamek/common/weapons/battlearmor/BALBXHandler.java
+++ megamek/src/megamek/common/weapons/battlearmor/BALBXHandler.java
@@ -105,4 +105,10 @@ public class BALBXHandler extends WeaponHandler {
         bSalvo = true;
         return missilesHit;
     }
+
+    @Override
+    public boolean allShotsHit() {
+        boolean swarming = (ae.getSwarmTargetId() == target.getId());
+        return (super.allShotsHit() || swarming);
+    }
 }

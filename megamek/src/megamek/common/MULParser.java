8240026a2379fb7a3ee44f89827e3420809cd3e8
Simon <juliez@outlook.de>
2024-02-13 20:14:47 +0100

    Clan personnel aftermerge changes

--- megamek/src/megamek/common/MULParser.java
+++ megamek/src/megamek/common/MULParser.java
@@ -117,6 +117,7 @@ public class MULParser {
     private static final String ATTR_CURRENTSIZE = "currentsize";
     public static final String ATTR_EXT_ID = "externalId";
     public static final String ATTR_PICKUP_ID = "pickUpId";
+    private static final String ATTR_CLANPILOT = "clanpilot";
     public static final String ATTR_NICK = "nick";
     public static final String ATTR_GENDER = "gender";
     public static final String ATTR_CAT_PORTRAIT = "portraitCat";
@@ -1353,8 +1354,8 @@ public class MULParser {
                 crew.setGender(Gender.parseFromString(attributes.get(ATTR_GENDER)), slot);
             }
 
-            if ((attributes.containsKey(CLANPILOT)) && !attributes.get(CLANPILOT).isBlank()) {
-                crew.setClanPilot(Boolean.parseBoolean(attributes.get(CLANPILOT)), slot);
+            if ((attributes.containsKey(ATTR_CLANPILOT)) && !attributes.get(ATTR_CLANPILOT).isBlank()) {
+                crew.setClanPilot(Boolean.parseBoolean(attributes.get(ATTR_CLANPILOT)), slot);
             }
 
             if ((attributes.containsKey(ATTR_CAT_PORTRAIT)) && !attributes.get(ATTR_CAT_PORTRAIT).isBlank()) {

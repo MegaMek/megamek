f7e2651cbebb4d6a767622b11be5629c7d936751
sleet01 <sleet01@hotmail.com>
2024-03-07 12:38:54 -0800

    Replace various list.contains() calls with a function

--- megamek/src/megamek/client/bot/princess/FireControl.java
+++ megamek/src/megamek/client/bot/princess/FireControl.java
@@ -1202,7 +1202,7 @@ public class FireControl {
                 String shootingCheck;
 
                 // Energy / ammo-independent weapons
-                if (Arrays.asList(AmmoType.T_NA, AmmoType.T_INFANTRY).contains(wtype.getAmmoType())) {
+                if (effectivelyAmmoless(wtype)) {
                     shootingCheck = checkGuess(shooter, enemy, weapon, null, game);
                     if (null != shootingCheck) {
                         ret.append(shootingCheck);
@@ -1653,7 +1653,7 @@ public class FireControl {
             WeaponFireInfo bestShoot = null;
 
             // Energy / ammo-independent weapons
-            if (Arrays.asList(AmmoType.T_NA, AmmoType.T_INFANTRY).contains(wtype.getAmmoType())) {
+            if (effectivelyAmmoless(wtype)) {
                 bestShoot = buildWeaponFireInfo(shooter, target, weapon, null, game, true);
             } else {
                 // For certain weapon types, look over all their loaded ammos
@@ -1802,7 +1802,7 @@ public class FireControl {
             WeaponFireInfo bestShoot = null;
 
             // Energy / ammo-independent weapons
-            if (Arrays.asList(AmmoType.T_NA, AmmoType.T_INFANTRY).contains(wtype.getAmmoType())) {
+            if (effectivelyAmmoless(wtype)) {
                 bestShoot = buildWeaponFireInfo(shooter, target, weapon, null, game, false);
             } else {
                 // For certain weapon types, look over all their loaded ammos
@@ -1977,7 +1977,7 @@ public class FireControl {
             WeaponFireInfo bestShoot = null;
 
             // Energy / ammo-independent weapons
-            if (Arrays.asList(AmmoType.T_NA, AmmoType.T_INFANTRY).contains(wtype.getAmmoType())) {
+            if (effectivelyAmmoless(wtype)) {
                 bestShoot = buildWeaponFireInfo(shooter, target, weapon, null, game, false);
             } else {
                 // Check _all_ ammunition for _all_ weapons here.
@@ -2637,7 +2637,7 @@ public class FireControl {
             int bestBracket = RangeType.RANGE_OUT;
 
             // For energy weapons / ammo-independent weapons
-            if (Arrays.asList(AmmoType.T_NA, AmmoType.T_INFANTRY).contains(weaponType.getAmmoType())) {
+            if (effectivelyAmmoless(weaponType)) {
                 bestBracket = RangeType.rangeBracket(range,
                         weaponType.getRanges(weapon),
                         useExtremeRange,
@@ -2722,7 +2722,7 @@ public class FireControl {
             final WeaponType weaponType = (WeaponType) currentWeapon.getType();
 
             // Skip weapons that don't use ammo.
-            if (Arrays.asList(AmmoType.T_NA, AmmoType.T_INFANTRY).contains(weaponType.getAmmoType())) {
+            if (effectivelyAmmoless(weaponType)) {
                 continue;
             }
 
@@ -3604,5 +3604,18 @@ public class FireControl {
         }
 
         return -1;
+    }
+
+    /**
+     *
+     * @param wtype that uses ammo that is not tracked, or not actually ammo
+     * @return true if wtype doesn't actually track ammo
+     */
+    private static boolean effectivelyAmmoless(WeaponType wtype) {
+        List<Integer> atypes = Arrays.asList(
+                AmmoType.T_NA,
+                AmmoType.T_INFANTRY
+        );
+        return atypes.contains(wtype.getAmmoType());
     }
 }

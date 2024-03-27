9a483d52cdbc290a107adcc565636cac26bb5e50
sleet01 <sleet01@hotmail.com>
2024-03-27 09:25:15 -0700

    Initial design pass of step 1 in project

---
+++ megamek/src/megamek/client/generator/TeamLoadoutGenerator.java
@@ -0,0 +1,41 @@
+package megamek.client.generator;
+
+import megamek.client.ui.swing.ClientGUI;
+import megamek.client.ui.swing.dialog.AbstractUnitSelectorDialog;
+import megamek.common.Game;
+import megamek.common.TechConstants;
+import megamek.common.options.GameOptions;
+import megamek.common.options.OptionsConstants;
+
+/**
+ * Notes: checkout
+ * - RATGenerator.java
+ * - ForceDescriptor.java
+ * for era-based search examples
+ */
+
+public class TeamLoadoutGenerator {
+
+    private static ClientGUI cg;
+    private static Game game;
+
+    protected GameOptions gameOptions = null;
+    protected boolean enableYearLimits = false;
+    protected int allowedYear = AbstractUnitSelectorDialog.ALLOWED_YEAR_ANY;
+    protected int gameTechLevel = TechConstants.T_SIMPLE_INTRO;
+    protected boolean eraBasedTechLevel = false;
+
+    TeamLoadoutGenerator(ClientGUI gui){
+        cg = gui;
+        game = cg.getClient().getGame();
+        gameOptions = game.getOptions();
+    }
+
+    public void updateOptionValues() {
+        gameOptions = cg.getClient().getGame().getOptions();
+        enableYearLimits = true;
+        allowedYear = gameOptions.intOption(OptionsConstants.ALLOWED_YEAR);
+        gameTechLevel = TechConstants.getSimpleLevel(gameOptions.stringOption("techlevel"));
+        eraBasedTechLevel = gameOptions.booleanOption(OptionsConstants.ALLOWED_ERA_BASED);
+    }
+}

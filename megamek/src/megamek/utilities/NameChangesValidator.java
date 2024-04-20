fd90e5af3b1729c51d74f86f5d6827fde3a2066e
Simon <juliez@outlook.de>
2024-04-20 15:32:47 +0200

    update to the NameChangesValidator: now validates both sides

--- megamek/src/megamek/utilities/NameChangesValidator.java
+++ megamek/src/megamek/utilities/NameChangesValidator.java
@@ -20,7 +20,6 @@ package megamek.utilities;
 
 import megamek.common.Configuration;
 import megamek.common.MechSummaryCache;
-import megamek.common.util.fileUtils.MegaMekFile;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -32,23 +31,65 @@ import java.nio.charset.StandardCharsets;
  * This tool goes through the name_changes.txt file and finds all lines where the right-side entry (the
  * real and existing unit name) does not actually exist and prints those.
  */
-public class NameChangesValidator implements MechSummaryCache.Listener {
+public class NameChangesValidator {
 
     private static MechSummaryCache mechSummaryCache = null;
     private int errors;
+    private final File lookupNames = new File(Configuration.unitsDir(), MechSummaryCache.FILENAME_LOOKUP);
+    private final File lookupNamesHidden = new File(Configuration.unitsDir(), MechSummaryCache.FILENAME_LOOKUP + ".xxx");
 
     public static void main(String... args) {
         NameChangesValidator validator = new NameChangesValidator();
-        System.out.println("Loading Unit Cache...");
-        mechSummaryCache = MechSummaryCache.getInstance(true);
-        mechSummaryCache.addListener(validator);
+        validator.testLeftSide();
+        validator.testRightSide();
     }
 
-    @Override
-    public void doneLoading() {
-        System.out.println("Comparing name changes...");
-        File lookupNames = new MegaMekFile(Configuration.unitsDir(), MechSummaryCache.FILENAME_LOOKUP).getFile();
+    public void testLeftSide() {
+        System.out.println("Testing name changes...");
+
+        System.out.println("Trying to rename " + lookupNames + " to " + lookupNamesHidden);
+        if (lookupNames.renameTo(lookupNamesHidden) && lookupNamesHidden.exists()) {
+            System.out.println("Loading Unit Cache...");
+            mechSummaryCache = MechSummaryCache.getInstance(true);
+            mechSummaryCache.getAllMechs();
+            System.out.println("Rename successful. Testing lookup names...");
+            try (FileInputStream fis = new FileInputStream(lookupNamesHidden);
+                 InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+                 BufferedReader br = new BufferedReader(isr)) {
+                String line;
+                while (null != (line = br.readLine())) {
+                    if (line.startsWith("#")) {
+                        continue;
+                    }
+                    int index = line.indexOf('|');
+                    if (index > 0) {
+                        String lookupName = line.substring(0, index);
+                        if (mechSummaryCache.getMech(lookupName) != null) {
+                            System.out.println("Lookup name (left side) is an existing unit in line: " + line);
+                            errors++;
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println("Exception " + ex.getMessage());
+                System.exit(64);
+            }
+        }
+
+        System.out.println("Trying to rename " + lookupNamesHidden + " back to " + lookupNames);
+        if (!lookupNamesHidden.renameTo(lookupNames)) {
+            System.out.println("ERROR: Could not rename! Check the files!");
+            System.exit(64);
+        }
+        System.out.println("Rename successful.");
+    }
+
+    private void testRightSide() {
         if (lookupNames.exists()) {
+            System.out.println("Testing actual names...");
+            System.out.println("Reloading Unit Cache...");
+            mechSummaryCache.loadMechData(true);
+            mechSummaryCache.getAllMechs();
             try (FileInputStream fis = new FileInputStream(lookupNames);
                  InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
                  BufferedReader br = new BufferedReader(isr)) {

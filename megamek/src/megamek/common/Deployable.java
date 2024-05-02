8117fb5908587b66a777e93c4d61ec1cad7fd188
Simon <juliez@outlook.de>
2024-05-02 19:59:46 +0200

    Add Deployable interface

---
+++ megamek/src/megamek/common/Deployable.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024 - The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MegaMek.
+ *
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MegaMek. If not, see <http://www.gnu.org/licenses/>.
+ */
+package megamek.common;
+
+/**
+ * This interface is implemented by those units (by InGameObjects) that can be deployed either
+ * offboard or on a board. There are InGameObjects that are only targets (HexTarget) and may thus not
+ * actually be deployable. Deployable objects could theoretically be listed in the lobby's unit list.
+ */
+public interface Deployable {
+
+    /**
+     * Returns true when this unit/object is deployed, i.e. it arrives in the game and may then start
+     * performing actions or be targeted by actions. Usually that means it has a fixed position on a board.
+     * Offboard units also count as undeployed as long as they cannot attack and as deployed when they
+     * can.
+     */
+    boolean isDeployed();
+
+    /**
+     * Returns the round that this unit/object is to be deployed on the board or offboard.
+     */
+    int getDeployRound();
+}

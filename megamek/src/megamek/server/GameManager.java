819ae21997db353ea20f177b6923dfa5b885e560
Simon <juliez@outlook.de>
2024-06-03 20:21:52 +0200

    fix incorrect round counter handling

--- megamek/src/megamek/server/GameManager.java
+++ megamek/src/megamek/server/GameManager.java
@@ -1864,10 +1864,8 @@ public class GameManager extends AbstractGameManager {
                 // commander initiative bonus. Now that initiative is rolled, clear the flag.
                 game.getEntities().forEachRemaining(e -> e.getCrew().resetActedFlag());
 
-                if (!game.shouldDeployThisRound()) {
-                    incrementAndSendGameRound();
-                    asService.performRollingAutosave(this);
-                }
+                incrementAndSendGameRound();
+                asService.performRollingAutosave(this);
 
                 // setIneligible(phase);
                 determineTurnOrder(phase);

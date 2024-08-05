c470153c7fd798b383cb92cf8cd905e3630ba66e
algebro <algebro@tuta.io>
2024-08-05 11:55:18 -0400

    re-add check to see if crew is KO'ed when rerolling pilot KOs

--- megamek/src/megamek/server/GameManager.java
+++ megamek/src/megamek/server/GameManager.java
@@ -20451,7 +20451,8 @@ public class GameManager extends AbstractGameManager {
                     // return true;
                 } // else
                 vDesc.add(r);
-            } while (e.shouldUseEdge(OptionsConstants.EDGE_WHEN_KO)
+            } while ((e.shouldUseEdge(OptionsConstants.EDGE_WHEN_KO)
+                    && e.getCrew().isKoThisRound(crewPos))
                     || e.shouldUseEdge(OptionsConstants.EDGE_WHEN_AERO_KO));
             // end of do-while
             if (e.getCrew().isKoThisRound(crewPos)) {

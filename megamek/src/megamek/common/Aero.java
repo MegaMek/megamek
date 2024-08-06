3519d3762c2ab5c59816d5321ea35f5fdc60f74d
algebro <algebro@tuta.io>
2024-08-06 11:24:45 -0400

    WIP: stub out highest threshold override for LAMs

--- megamek/src/megamek/common/Aero.java
+++ megamek/src/megamek/common/Aero.java
@@ -1609,6 +1609,7 @@ public abstract class Aero extends Entity implements IAero, IBomber {
         return 0;
     }
 
+    @Override
     public int getHighestThresh() {
         int max = damThresh[0];
         for (int i = 1; i < damThresh.length; i++) {

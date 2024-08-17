937a04f1e013870ab00b1b5fc18593f73cee174d
Simon <juliez@outlook.de>
2024-08-17 11:16:48 +0200

    update null guards in getElevation

--- megamek/src/megamek/common/Entity.java
+++ megamek/src/megamek/common/Entity.java
@@ -2158,20 +2158,19 @@ public abstract class Entity extends TurnOrdered implements Transporter, Targeta
     }
 
     /**
-     * Returns the elevation of this entity, relative to the current Hex's
-     * surface
+     * @return The elevation of this Entity, relative to the current Hex's surface. When the unit is
+     * transported, returns the elevation of the carrier.
      */
     @Override
     public int getElevation() {
-        if (Entity.NONE != getTransportId()) {
-            return game.getEntity(getTransportId()).getElevation();
+        if (game != null) {
+            Entity carrier = game.getEntity(getTransportId());
+            if (carrier != null) {
+                return carrier.getElevation();
+            }
         }
 
-        if (isOffBoard()) {
-            return 0;
-        }
-
-        return elevation;
+        return isOffBoard() ? 0 : elevation;
     }
 
     public boolean canGoDown() {

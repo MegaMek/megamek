15d72d0cb81a21bf7a4e0c482bf68c48661af0ad
Richard J Hancock <rjhancock@tapenvy.us>
2024-09-08 22:17:25 -0500

    Compact Mech Quirk Revert

--- megamek/src/megamek/common/options/OptionsConstants.java
+++ megamek/src/megamek/common/options/OptionsConstants.java
@@ -29,7 +29,7 @@ public class OptionsConstants {
     public static final String QUIRK_POS_BATTLE_FIST_RA = "battle_fists_ra";
     public static final String QUIRK_POS_COMBAT_COMPUTER = "combat_computer";
     public static final String QUIRK_POS_COMMAND_MEK = "command_mech";
-    public static final String QUIRK_POS_COMPACT = "compact_mek";
+    public static final String QUIRK_POS_COMPACT = "compact_mech";
     public static final String QUIRK_POS_COWL = "cowl";
     // TODO - Code directional torso mount for play
     public static final String QUIRK_POS_DIRECTIONAL_TORSO_MOUNT = "directional_torso_mount";

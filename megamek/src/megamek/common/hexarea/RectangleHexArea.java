db3216fbaa2a0dab483ff8a616c61ac38f7f998d
Dave N <13752452+HammerGS@users.noreply.github.com>
2024-10-01 19:09:16 -0600

    Update RectangleHexArea.java
    Fixing misformed HTML

--- megamek/src/megamek/common/hexarea/RectangleHexArea.java
+++ megamek/src/megamek/common/hexarea/RectangleHexArea.java
@@ -32,14 +32,15 @@ public class RectangleHexArea extends AbstractHexArea {
     private final int y2;
 
     /**
-     * Creates a rectangle shape. The rectangle includes the corner coordinates. The coordinates do not have to be sorted, i.e. x1 > x2 and
-     * x1 < x2 have the same result. When x1 = x2 and/or y1 = y2, the rectangle consists of a single line or single hex.
+     * Creates a rectangle shape. The rectangle includes the corner coordinates. The coordinates do not have to be sorted, i.e. x1 &gt; x2 and
+     * x1 &lt; x2 have the same result. When x1 = x2 and/or y1 = y2, the rectangle consists of a single line or single hex.
      *
      * @param x1 The first x corner coordinate
      * @param x2 The second x corner coordinate
      * @param y1 The first y corner coordinate
      * @param y2 The second y corner coordinate
      */
+
     public RectangleHexArea(int x1, int y1, int x2, int y2) {
         this.x1 = Math.min(x1, x2);
         this.x2 = Math.max(x1, x2);

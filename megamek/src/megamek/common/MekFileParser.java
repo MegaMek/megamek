9d33bbb16fe3ecf36c250d993a4138ffd5526642
kuronekochomusuke <kuronekochomusuke@gmail.com>
2024-10-05 23:53:55 -0400

    default autoeject to disabled for clan meks

--- megamek/src/megamek/common/MekFileParser.java
+++ megamek/src/megamek/common/MekFileParser.java
@@ -504,10 +504,12 @@ public class MekFileParser {
                 }
             }
 
-            if ((ent instanceof Mek) && (m.getType().hasFlag(MiscType.F_CASE) || m.getType().hasFlag(MiscType.F_CASEII)
-                    || m.getType().hasFlag(MiscType.F_CASEP)
+            boolean hasCase = ent.isClan()
+                || m.getType().hasFlag(MiscType.F_CASE)
+                || m.getType().hasFlag(MiscType.F_CASEII)
+                || m.getType().hasFlag(MiscType.F_CASEP);
 
-            )) {
+            if ((ent instanceof Mek) && hasCase) {
                 ((Mek) ent).setAutoEject(false);
             }
 

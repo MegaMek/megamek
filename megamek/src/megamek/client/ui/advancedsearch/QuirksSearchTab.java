6b0276fe946aca082a24c9e241c5b346c9bbb2b9
Simon <juliez@outlook.de>
2024-10-26 15:19:58 +0200

    update layout in quirkstab

--- megamek/src/megamek/client/ui/advancedsearch/QuirksSearchTab.java
+++ megamek/src/megamek/client/ui/advancedsearch/QuirksSearchTab.java
@@ -47,13 +47,7 @@ class QuirksSearchTab extends JPanel {
 
         listWeaponQuirkType = new TriStateItemList(new WeaponQuirks(), 17);
 
-        GridBagConstraints c = new GridBagConstraints();
-        setLayout(new GridBagLayout());
-
-        c.anchor = GridBagConstraints.NORTHWEST;
-        c.insets = new Insets(20, 10, 0, 0);
-        c.gridy++;
-        JPanel quirkPanel = new JPanel(new BorderLayout());
+        JPanel unitQuirksPanel = new JPanel(new BorderLayout());
         JPanel quirkIEPanel = new JPanel(new FlowLayout());
         quirkIEPanel.add(new JLabel(Messages.getString("MekSelectorDialog.Search.Quirk")));
         quirkIEPanel.add(Box.createHorizontalStrut(15));
@@ -61,9 +55,9 @@ class QuirksSearchTab extends JPanel {
         quirkIEPanel.add(cQuirkInclude);
         quirkIEPanel.add(new JLabel("\u2612"));
         quirkIEPanel.add(cQuirkExclude);
-        quirkPanel.add(quirkIEPanel, BorderLayout.NORTH);
-        quirkPanel.add(new JScrollPane(listQuirkType.getComponent()), BorderLayout.CENTER);
-        add(quirkPanel, c);
+        unitQuirksPanel.add(quirkIEPanel, BorderLayout.NORTH);
+        unitQuirksPanel.add(new JScrollPane(listQuirkType.getComponent()), BorderLayout.CENTER);
+
         JPanel weaponQuirkPanel = new JPanel(new BorderLayout());
         JPanel weaponQuirkIEPanel = new JPanel(new FlowLayout());
         weaponQuirkIEPanel.add(new JLabel(Messages.getString("MekSelectorDialog.Search.WeaponQuirk")));
@@ -74,10 +68,11 @@ class QuirksSearchTab extends JPanel {
         weaponQuirkIEPanel.add(cWeaponQuirkExclude);
         weaponQuirkPanel.add(weaponQuirkIEPanel, BorderLayout.NORTH);
         weaponQuirkPanel.add(new JScrollPane(listWeaponQuirkType.getComponent()), BorderLayout.CENTER);
-        add(weaponQuirkPanel, c);
-        c.weighty = 1;
-        c.gridy++;
-        add(btnQuirksClear, c);
+
+        JPanel innerPanel = new JPanel(new GridLayout(1, 2, 20, 0));
+        innerPanel.add(unitQuirksPanel);
+        innerPanel.add(weaponQuirkPanel);
+        add(innerPanel);
     }
 
     private void loadAndOr(JComboBox<String> cb, int index) {

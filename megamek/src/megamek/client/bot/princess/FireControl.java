77b532ee02d87d23d935fc0f50857318ab37c573
psikomonkie <189469115+psikomonkie@users.noreply.github.com>
2025-02-07 12:41:12 -0500

    Issue 6511: Prevent cast class exception when switching Princess's ammo.

--- megamek/src/megamek/client/bot/princess/FireControl.java
+++ megamek/src/megamek/client/bot/princess/FireControl.java
@@ -2881,8 +2881,14 @@ public class FireControl {
                 continue;
             }
 
-            // If everything looks okay, replace the old WAA with the updated copy
-            info.setAction(cloneWAA);
+            /* If everything looks okay, update the action with the new values
+            *  Don't replace the old action with the clone - the clone doesn't
+            *  consider that the waa might have been a subclass of WeaponAttackAction
+            *  - like ArtilleryAttackAction. So instead update the changed values: */
+            info.getAction().setAmmoId(cloneWAA.getAmmoId());
+            info.getAction().setAmmoMunitionType(cloneWAA.getAmmoMunitionType());
+            info.getAction().setAmmoCarrier(cloneWAA.getAmmoCarrier());
+
             owner.sendAmmoChange(info.getShooter().getId(), shooter.getEquipmentNum(currentWeapon),
                     shooter.getEquipmentNum(mountedAmmo), mountedAmmo.getSwitchedReason());
         }

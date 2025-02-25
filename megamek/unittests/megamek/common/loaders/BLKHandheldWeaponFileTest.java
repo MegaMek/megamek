de27f52b0a573ee877c8732b242d4e0909c555a8
Pavel Braginskiy <pavelbraginskiy@hotmail.com>
2025-02-25 12:46:22 -0800

    License

--- megamek/unittests/megamek/common/loaders/BLKHandheldWeaponFileTest.java
+++ megamek/unittests/megamek/common/loaders/BLKHandheldWeaponFileTest.java
@@ -1,6 +1,19 @@
+/*
+ * MegaMek - Copyright (C) 2025 The MegaMek Team
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
 package megamek.common.loaders;
 
-import megamek.common.Entity;
 import megamek.common.EquipmentType;
 import megamek.common.HandheldWeapon;
 import megamek.common.MekFileParser;
@@ -10,7 +23,6 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class BLKHandheldWeaponFileTest {
 

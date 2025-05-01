83e687d92fe2093af0319a46224ccddc21009cfe
Luana Coppio <lucascoppio@gmail.com>
2025-04-30 21:29:23 -0300

    Update megamek/src/megamek/common/QuadVee.java

--- megamek/src/megamek/common/QuadVee.java
+++ megamek/src/megamek/common/QuadVee.java
@@ -3,7 +3,7 @@
  *
  * This file is part of MegaMek.
  *
- * <Package Name> is free software: you can redistribute it and/or modify
+ * MegaMek is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License (GPL),
  * version 3 or (at your option) any later version,
  * as published by the Free Software Foundation.

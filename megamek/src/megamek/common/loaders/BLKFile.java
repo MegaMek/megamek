655559b5f0252ddf395381d5ecabf5d3097aea3e
Simon <juliez@outlook.de>
2025-06-07 12:58:43 +0200

    allow GE construction in MML

--- megamek/src/megamek/common/loaders/BLKFile.java
+++ megamek/src/megamek/common/loaders/BLKFile.java
@@ -658,7 +658,7 @@ public class BLKFile {
 
         if ((t instanceof Infantry) && ((Infantry) t).getMount() != null) {
             blk.writeBlockData("motion_type", ((Infantry) t).getMount().toString());
-        } else if (!t.isHandheldWeapon()) {
+        } else if (!t.isHandheldWeapon() && !(t instanceof GunEmplacement)) {
             blk.writeBlockData("motion_type", t.getMovementModeAsString());
         }
 
@@ -677,7 +677,7 @@ public class BLKFile {
             blk.writeBlockData("transporters", transporter_array);
         }
 
-        if (!(t.isConventionalInfantry() || t.isHandheldWeapon())) {
+        if (!(t.isConventionalInfantry() || t.isHandheldWeapon() || t instanceof GunEmplacement)) {
             if (t instanceof Aero) {
                 blk.writeBlockData("SafeThrust", t.getOriginalWalkMP());
             } else {
@@ -690,7 +690,7 @@ public class BLKFile {
         }
 
         int numLocs = t.locations();
-        if (!(t instanceof Infantry)) {
+        if (!(t instanceof Infantry || t instanceof GunEmplacement)) {
             if (t instanceof Aero) {
                 if (t.isFighter()) {
                     blk.writeBlockData("cockpit_type", ((Aero) t).getCockpitType());
@@ -717,26 +717,29 @@ public class BLKFile {
 
             // Need to make sure that only "Unknown" armor gets skipped
             // barRating block written out later in the SV-specific section
-            if (!t.hasPatchworkArmor() && (t.getArmorType(0) != ArmorType.T_ARMOR_UNKNOWN)) {
-                blk.writeBlockData("armor_type", t.getArmorType(0));
-                blk.writeBlockData("armor_tech_rating", t.getArmorTechRating().getIndex());
-                blk.writeBlockData("armor_tech_level", t.getArmorTechLevel(0));
-            } else if (t.hasPatchworkArmor()) {
-                blk.writeBlockData("armor_type", EquipmentType.T_ARMOR_PATCHWORK);
-                for (int i = 1; i < t.locations(); i++) {
-                    ArmorType armor = ArmorType.forEntity(t, i);
-                    blk.writeBlockData(t.getLocationName(i) + "_armor_type", armor.getArmorType());
-                    blk.writeBlockData(t.getLocationName(i) + "_armor_tech",
-                          TechConstants.getTechName(t.getArmorTechLevel(i)));
-                    blk.writeBlockData(t.getLocationName(i) + "_armor_tech_rating", armor.getTechRating().getIndex());
-                    if (armor.hasFlag(MiscType.F_SUPPORT_VEE_BAR_ARMOR)) {
-                        blk.writeBlockData(t.getLocationName(i) + "_barrating", armor.getBAR());
+            if (!(t instanceof GunEmplacement)) {
+                if (!t.hasPatchworkArmor() && (t.getArmorType(0) != ArmorType.T_ARMOR_UNKNOWN)) {
+                    blk.writeBlockData("armor_type", t.getArmorType(0));
+                    blk.writeBlockData("armor_tech_rating", t.getArmorTechRating().getIndex());
+                    blk.writeBlockData("armor_tech_level", t.getArmorTechLevel(0));
+                } else if (t.hasPatchworkArmor()) {
+                    blk.writeBlockData("armor_type", EquipmentType.T_ARMOR_PATCHWORK);
+                    for (int i = 1; i < t.locations(); i++) {
+                        ArmorType armor = ArmorType.forEntity(t, i);
+                        blk.writeBlockData(t.getLocationName(i) + "_armor_type", armor.getArmorType());
+                        blk.writeBlockData(t.getLocationName(i) + "_armor_tech",
+                              TechConstants.getTechName(t.getArmorTechLevel(i)));
+                        blk.writeBlockData(t.getLocationName(i) + "_armor_tech_rating",
+                              armor.getTechRating().getIndex());
+                        if (armor.hasFlag(MiscType.F_SUPPORT_VEE_BAR_ARMOR)) {
+                            blk.writeBlockData(t.getLocationName(i) + "_barrating", armor.getBAR());
+                        }
                     }
+                } else {
+                    throw new EntitySavingException("Armor type unknown or not set; aborting save!");
                 }
-            } else {
-                throw new EntitySavingException("Armor type unknown or not set; aborting save!");
             }
-            if (t.getStructureType() != 0) {
+            if (t.getStructureType() != 0 && !(t instanceof GunEmplacement)) {
                 blk.writeBlockData("internal_type", t.getStructureType());
             }
             if (t.isOmni()) {
@@ -973,6 +976,10 @@ public class BLKFile {
             }
             if (!augmentations.isEmpty()) {
                 blk.writeBlockData("augmentation", augmentations.toArray(new String[0]));
+            }
+        } else if (t instanceof GunEmplacement gunEmplacement) {
+            if (!gunEmplacement.hasNoTurret()) {
+                blk.writeBlockData("turret", 1);
             }
         } else {
             blk.writeBlockData("tonnage", t.getWeight());

b1668665a065db8a8734aff77600418ccb0bcf52
Simon <juliez@outlook.de>
2025-06-08 11:20:40 +0200

    issue 7220 - sub capital weapon BV errata

--- megamek/src/megamek/common/weapons/capitalweapons/SubCapMissilePiranhaWeapon.java
+++ megamek/src/megamek/common/weapons/capitalweapons/SubCapMissilePiranhaWeapon.java
@@ -1,15 +1,35 @@
 /*
- * MegaMek - Copyright (C) 2004, 2005 Ben Mazur (bmazur@sev.org)
+ * Copyright (C) 2004, 2005 Ben Mazur (bmazur@sev.org)
+ * Copyright (C) 2025 The MegaMek Team. All Rights Reserved.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * This file is part of MegaMek.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ *
+ * MechWarrior Copyright Microsoft Corporation. MegaMek was created under
+ * Microsoft's "Game Content Usage Rules"
+ * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
+ * affiliated with Microsoft.
  */
 package megamek.common.weapons.capitalweapons;
 
@@ -18,11 +38,14 @@ import megamek.common.Mounted;
 import megamek.common.SimpleTechLevel;
 import megamek.common.alphaStrike.AlphaStrikeElement;
 
+import java.io.Serial;
+
 /**
  * @author Jay Lawson
  * @since Sep 25, 2004
  */
 public class SubCapMissilePiranhaWeapon extends SubCapMissileWeapon {
+    @Serial
     private static final long serialVersionUID = 3827228773282489872L;
 
     public SubCapMissilePiranhaWeapon() {
@@ -30,7 +53,7 @@ public class SubCapMissilePiranhaWeapon extends SubCapMissileWeapon {
         name = "Sub-Capital Missile Launcher (Piranha)";
         setInternalName(name);
         addLookupName("Piranha");
-        this.shortName = "Piranha";
+        shortName = "Piranha";
         heat = 9;
         damage = 3;
         ammoType = AmmoType.AmmoTypeEnum.PIRANHA;
@@ -39,16 +62,16 @@ public class SubCapMissilePiranhaWeapon extends SubCapMissileWeapon {
         longRange = 21;
         extremeRange = 28;
         tonnage = 100.0;
-        bv = 670;
+        bv = 589;
         cost = 75000;
         shortAV = 3;
         medAV = 3;
         longAV = 3;
-        this.missileArmor = 30;
+        missileArmor = 30;
         maxRange = RANGE_LONG;
         flags = flags.or(F_AERO_WEAPON).or(F_MISSILE);
-        this.atClass = CLASS_CAPITAL_MISSILE;
-        rulesRefs = "345, TO";
+        atClass = CLASS_CAPITAL_MISSILE;
+        rulesRefs = "156, TO:AUE";
         // Tech Progression tweaked to combine IntOps with TRO Prototypes/3145 NTNU RS
         techAdvancement.setTechBase(TechBase.ALL)
                 .setIntroLevel(false)

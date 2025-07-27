c123e4e6019cd2c8513a263d62c3528ce6674c56
IllianiCBT <danielfarbrace@icloud.com>
2025-07-27 14:45:51 -0500

    Another attempt to fix the failing javadoc

--- megamek/src/megamek/utilities/MilestoneData.java
+++ megamek/src/megamek/utilities/MilestoneData.java
@@ -44,8 +44,8 @@ import megamek.Version;
  * @param label                Descriptive label for the milestone. Should correspond to the tail of the release's
  *                             GitHub Url.
  * @param version              Version associated with the milestone.
- * @param useFallbackHyperlink {@code true} if {@link MilestoneData#getMegaMekUrl()},
- *                                        {@link MilestoneData#getMegaMekLabUrl()} & {@link MilestoneData#getMekHQUrl()}
+ * @param useFallbackHyperlink {@code true} if {@code MilestoneData#getMegaMekUrl()},
+ *                                        {@code MilestoneData#getMegaMekLabUrl()} & {@code MilestoneData#getMekHQUrl()}
  *                                        should all return their project releases page, instead of a page specific
  *                                        to that Milestone.
  *

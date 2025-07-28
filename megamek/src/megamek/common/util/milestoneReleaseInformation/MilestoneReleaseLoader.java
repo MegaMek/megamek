1c7ecce9aecc6773112b7b737ed65d55fb58b882
IllianiCBT <danielfarbrace@icloud.com>
2025-07-28 14:30:51 -0500

    Added missing licenses

--- megamek/src/megamek/common/util/milestoneReleaseInformation/MilestoneReleaseLoader.java
+++ megamek/src/megamek/common/util/milestoneReleaseInformation/MilestoneReleaseLoader.java
@@ -1,16 +1,48 @@
+/*
+ * Copyright (C) 2025 The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MegaMek.
+ *
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ *
+ * MechWarrior Copyright Microsoft Corporation. MekHQ was created under
+ * Microsoft's "Game Content Usage Rules"
+ * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
+ * affiliated with Microsoft.
+ */
 package megamek.common.util.milestoneReleaseInformation;
 
-import static megamek.SuiteConstants.ALL_MILESTONE_RELEASES;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import megamek.SuiteConstants;
+import megamek.Version;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import megamek.SuiteConstants;
-import megamek.Version;
+import static megamek.SuiteConstants.ALL_MILESTONE_RELEASES;
 
 /**
  * Loads and provides access to milestone release information from a YAML configuration file.

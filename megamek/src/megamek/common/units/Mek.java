1bb0700f9dd8546e75456ad1835138e8a3d719db
James Magnan <james.magnan@gmail.com>
2025-10-05 12:56:29 -0700

    Fix bug #7457

--- megamek/src/megamek/common/units/Mek.java
+++ megamek/src/megamek/common/units/Mek.java
@@ -5305,7 +5305,7 @@ public abstract class Mek extends Entity {
         if (game != null && locationIsLeg(loc) && canFall() && !game.getOptions().booleanOption(OptionsConstants.PLAYTEST_2)) {
             game.addPSR(new PilotingRollData(getId(),
                   TargetRoll.AUTOMATIC_FAIL, 5, "leg destroyed"));
-        } else if (game !=null && game.getOptions().booleanOption(OptionsConstants.PLAYTEST_2)) {
+        } else if (game !=null && locationIsLeg(loc) && canFall() && game.getOptions().booleanOption(OptionsConstants.PLAYTEST_2)) {
             game.addPSR(new PilotingRollData(getId(), TargetRoll.AUTOMATIC_FAIL, 4, "leg destroyed"));
         }
         

bdcfa971c4010d12d7b9a6a286c8873b29c729e3
IllianiBird <danielfarbrace@icloud.com>
2025-10-04 19:32:43 -0500

    Added missing javadocs

---
+++ megamek/src/megamek/common/util/DateUtilities.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2025 The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MegaMek.
+ *
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ *
+ * MechWarrior Copyright Microsoft Corporation. MegaMek was created under
+ * Microsoft's "Game Content Usage Rules"
+ * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
+ * affiliated with Microsoft.
+ */
+package megamek.common.util;
+
+import java.time.LocalDate;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DateUtilities {
+    /**
+     * Returns a random {@link LocalDate} between the given start (inclusive) and end (inclusive) dates.
+     *
+     * <p>This method converts both start and end dates to their epoch day representations, selects a random day
+     * within that range, and constructs a {@code LocalDate} from the random epoch day.</p>
+     *
+     * @param start the earliest date to select (inclusive)
+     * @param end   the latest date to select (inclusive)
+     *
+     * @return a randomly selected {@link LocalDate} between {@code start} and {@code end}, inclusive
+     *
+     * @throws IllegalArgumentException if {@code start} is after {@code end}
+     */
+    public static LocalDate getRandomDateBetween(LocalDate start, LocalDate end) {
+        long startEpochDay = start.toEpochDay();
+        long endEpochDay = end.toEpochDay();
+        long randomDay = ThreadLocalRandom.current().nextLong(startEpochDay, endEpochDay + 1);
+        return LocalDate.ofEpochDay(randomDay);
+    }
+}

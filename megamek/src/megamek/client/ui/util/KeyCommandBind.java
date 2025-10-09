fc8135a196a08e365ce48117ebd946ac70e2f1a4
sleet01 <sleet01@hotmail.com>
2025-10-08 17:49:10 -0700

    Further corrections and fixes from PR comments

--- megamek/src/megamek/client/ui/util/KeyCommandBind.java
+++ megamek/src/megamek/client/ui/util/KeyCommandBind.java
@@ -123,7 +123,7 @@ public enum KeyCommandBind {
     REPORT_KEY_SELECT_NEXT("reportKeySelNext", VK_N, CTRL_DOWN_MASK),
     REPORT_KEY_SELECT_PREVIOUS("reportKeySelPrev", VK_N, CTRL_DOWN_MASK | SHIFT_DOWN_MASK),
     REPORT_FILTER_KEY_SELECT_NEXT("reportFilterKeySelNext", VK_F, CTRL_DOWN_MASK | SHIFT_DOWN_MASK),
-    REPORT_KEY_FILTER("reportKeyFilter", VK_F, SHIFT_DOWN_MASK),
+    REPORT_KEY_FILTER("reportFilterKey", VK_F, SHIFT_DOWN_MASK),
 
     // --------- The following binds are used by the CommonMenuBar:
     // Toggles isometric view on/off

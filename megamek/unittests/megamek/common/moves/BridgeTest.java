ec4b39d0dee474ed2b2be635db6b113e87ecd1c9
psikomonkie <189469115+psikomonkie@users.noreply.github.com>
2025-12-01 18:00:08 -0500

    Issue #7688: Tests for PR #7701

--- megamek/unittests/megamek/common/moves/BridgeTest.java
+++ megamek/unittests/megamek/common/moves/BridgeTest.java
@@ -38,8 +38,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import megamek.common.GameBoardTestCase;
 import megamek.common.enums.MoveStepType;
+import megamek.common.equipment.EquipmentType;
+import megamek.common.equipment.EquipmentTypeLookup;
 import megamek.common.units.BipedMek;
 import megamek.common.units.EntityMovementMode;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class BridgeTest extends GameBoardTestCase {
@@ -47,27 +50,27 @@ public class BridgeTest extends GameBoardTestCase {
     static {
         initializeBoard("BOARD_PLAIN_BRIDGE", """
               size 1 5
-              hex 0101 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
-              hex 0102 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
-              hex 0103 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
-              hex 0104 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
-              hex 0105 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
+              hex 0101 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
+              hex 0102 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
+              hex 0103 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
+              hex 0104 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
+              hex 0105 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
               end"""
         );
 
         initializeBoard("BOARD_SHORT_BRIDGE", """
               size 1 2
               hex 0101 4 "" ""
-              hex 0102 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
+              hex 0102 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
               end"""
         );
 
         initializeBoard("BOARD_BRIDGE_BETWEEN_LAND", """
               size 1 5
               hex 0101 4 "" ""
-              hex 0102 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
-              hex 0103 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
-              hex 0104 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
+              hex 0102 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
+              hex 0103 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
+              hex 0104 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
               hex 0105 4 "" ""
               end"""
         );
@@ -75,9 +78,9 @@ public class BridgeTest extends GameBoardTestCase {
         initializeBoard("BOARD_BRIDGE_BETWEEN_LAND_ELEV_0_BRIDGE", """
               size 1 5
               hex 0101 4 "" ""
-              hex 0102 4 "bridge:1;bridge_cf:100;bridge_elev:0" ""
-              hex 0103 2 "bridge:1;bridge_cf:100;bridge_elev:2" ""
-              hex 0104 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
+              hex 0102 4 "bridge:1:09;bridge_cf:100;bridge_elev:0" ""
+              hex 0103 2 "bridge:1:09;bridge_cf:100;bridge_elev:2" ""
+              hex 0104 0 "bridge:1:09;bridge_cf:100;bridge_elev:4" ""
               hex 0105 4 "" ""
               end"""
         );
@@ -89,6 +92,26 @@ public class BridgeTest extends GameBoardTestCase {
               hex 0103 0 "bridge:1;bridge_cf:100;bridge_elev:4" ""
               hex 0104 0 "" ""
               hex 0105 0 "" ""
+              end"""
+        );
+
+        initializeBoard("BOARD_WALK_UNDER_LOW_BRIDGE", """
+              size 1 5
+              hex 0101 0 "" ""
+              hex 0102 0 "" ""
+              hex 0103 0 "bridge:1;bridge_cf:100;bridge_elev:1" ""
+              hex 0104 0 "" ""
+              hex 0105 0 "" ""
+              end"""
+        );
+
+        initializeBoard("BOARD_WALK_UP_ONTO_LOW_BRIDGE", """
+              size 1 5
+              hex 0101 0 "" ""
+              hex 0103 0 "bridge:1:09;bridge_cf:100;bridge_elev:1" ""
+              hex 0103 0 "bridge:1:09;bridge_cf:100;bridge_elev:1" ""
+              hex 0103 0 "bridge:1:09;bridge_cf:100;bridge_elev:1" ""
+              hex 0105 2 "" ""
               end"""
         );
     }
@@ -163,8 +186,153 @@ public class BridgeTest extends GameBoardTestCase {
               MoveStepType.FORWARDS,
               MoveStepType.FORWARDS,
               MoveStepType.FORWARDS);
-        // Move is not legal, with climb mode on, we can't climb under a bridge
-        assertFalse(movePath.isMoveLegal());
+        // Move is legal, we aren't trying to climb up
+        assertTrue(movePath.isMoveLegal());
         assertMovePathElevations(movePath, 0, 0, 0, 0, 0);
+    }
+
+    @Test
+    void testMovePathBoardWalkUnderLowBridge() {
+        setBoard("BOARD_WALK_UNDER_LOW_BRIDGE");
+        MovePath movePath = getMovePathFor(new BipedMek(), EntityMovementMode.BIPED,
+              MoveStepType.CLIMB_MODE_OFF,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS);
+        assertTrue(movePath.isMoveLegal());
+        assertMovePathElevations(movePath, 0, 0, 0, 0, 0);
+    }
+
+    @Test
+    void testMovePathBoardWalkUnderLowBridgeButTryToClimb() {
+        setBoard("BOARD_WALK_UNDER_LOW_BRIDGE");
+        MovePath movePath = getMovePathFor(new BipedMek(), EntityMovementMode.BIPED,
+              MoveStepType.CLIMB_MODE_ON,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS);
+        // Move is illegal, we can't get onto a bridge without an exit
+        assertFalse(movePath.isMoveLegal());
+        assertMovePathElevations(movePath, 0, 0, 1, 0, 0);
+    }
+
+    @Test
+    void testMovePathBoardTryToClimbOntoWrongWayBridge() {
+        setBoard("BOARD_WALK_UNDER_LOW_BRIDGE");
+        MovePath movePath = getMovePathFor(new BipedMek(), EntityMovementMode.BIPED,
+              MoveStepType.CLIMB_MODE_ON,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS);
+        // Move is illegal, we can't get onto a bridge without an exit
+        assertFalse(movePath.isMoveLegal());
+        assertMovePathElevations(movePath, 0, 0, 1);
+    }
+
+    @Test
+    void testMovePathBoardJumpPastLowBridge() {
+        setBoard("BOARD_WALK_UNDER_LOW_BRIDGE");
+        BipedMek mek = new BipedMek();
+        mek.setOriginalJumpMP(5);
+        EquipmentType equipmentType = EquipmentType.get(EquipmentTypeLookup.JUMP_JET);
+        try {
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+        } catch (Exception ignored) {}
+        MovePath movePath = getMovePathFor(new BipedMek(), EntityMovementMode.BIPED,
+              MoveStepType.CLIMB_MODE_OFF,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS);
+        // Move is legal, we can jump past a bridge
+        assertTrue(movePath.isMoveLegal());
+        assertMovePathElevations(movePath, 0, 0, 0, 0, 0);
+    }
+
+    // FIXME: I think this test should be able to pass, but the user can get the same thing by toggling climb mode off
+    @Test
+    @Disabled
+    void testMovePathBoardJumpOverLowBridge() {
+        setBoard("BOARD_WALK_UNDER_LOW_BRIDGE");
+        BipedMek mek = new BipedMek();
+        mek.setOriginalJumpMP(5);
+        EquipmentType equipmentType = EquipmentType.get(EquipmentTypeLookup.JUMP_JET);
+        try {
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+        } catch (Exception ignored) {}
+        MovePath movePath = getMovePathFor(new BipedMek(), EntityMovementMode.BIPED,
+              MoveStepType.CLIMB_MODE_ON,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS);
+        // Move is legal, we can jump past a bridge
+        assertTrue(movePath.isMoveLegal());
+        assertMovePathElevations(movePath, 0, 0, 1, 0, 0);
+    }
+
+    @Test
+    void testMovePathBoardJumpOntoLowBridge() {
+        setBoard("BOARD_WALK_UNDER_LOW_BRIDGE");
+        BipedMek mek = new BipedMek();
+        mek.setOriginalJumpMP(3);
+        EquipmentType equipmentType = EquipmentType.get(EquipmentTypeLookup.JUMP_JET);
+        try {
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+        } catch (Exception ignored) {}
+        MovePath movePath = getMovePathFor(mek, EntityMovementMode.BIPED,
+              MoveStepType.CLIMB_MODE_ON,
+              MoveStepType.START_JUMP,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS);
+        // Move is legal, we can jump onto a bridge even if the exit is unaligned
+        assertTrue(movePath.isMoveLegal());
+        assertMovePathElevations(movePath, 0, 0, 0, 1);
+    }
+
+    @Test
+    void testMovePathBoardJumpUnderLowBridge() {
+        setBoard("BOARD_WALK_UNDER_LOW_BRIDGE");
+        BipedMek mek = new BipedMek();
+        mek.setOriginalJumpMP(3);
+        EquipmentType equipmentType = EquipmentType.get(EquipmentTypeLookup.JUMP_JET);
+        try {
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+            mek.addEquipment(equipmentType, BipedMek.LOC_CENTER_TORSO);
+        } catch (Exception ignored) {}
+        MovePath movePath = getMovePathFor(mek, EntityMovementMode.BIPED,
+              MoveStepType.CLIMB_MODE_OFF,
+              MoveStepType.START_JUMP,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS);
+        // Move is legal, we can jump onto the ground below the bridge
+        assertTrue(movePath.isMoveLegal());
+        assertMovePathElevations(movePath, 0, 0, 0, 0);
+    }
+
+    @Test
+    void testMovePathBoardWalkUnderUpOntoBridge() {
+        setBoard("BOARD_WALK_UP_ONTO_LOW_BRIDGE");
+        MovePath movePath = getMovePathFor(new BipedMek(), EntityMovementMode.BIPED,
+              MoveStepType.CLIMB_MODE_ON,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS,
+              MoveStepType.FORWARDS);
+        // Move is illegal, we aren't trying to climb up
+        assertTrue(movePath.isMoveLegal());
+        assertMovePathElevations(movePath, 0, 1, 1, 1, 0);
     }
 }

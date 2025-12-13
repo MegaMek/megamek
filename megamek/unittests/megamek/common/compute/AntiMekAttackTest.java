MegaMek/megamek

commit 37bd02f9a294700ecfef8fd3d8b44b8abbace937

Compute, Containers, Copy
Signed-off-by: Richard J Hancock <rjhancock@tapenvy.us>

megamek/src/megamek/client/bot/princess/FireControl.java
@@ -510,9 +510,9 @@ public class FireControl {
         // Check if target is within arc
         final int arc;
         if (PhysicalAttackType.LEFT_PUNCH == attackType) {
-            arc = Compute.ARC_LEFTARM;
+            arc = Compute.ARC_LEFT_ARM;
         } else if (PhysicalAttackType.RIGHT_PUNCH == attackType) {
-            arc = Compute.ARC_RIGHTARM;
+            arc = Compute.ARC_RIGHT_ARM;
         } else {
             arc = Compute.ARC_FORWARD; // assume kick
         }

megamek/src/megamek/client/generator/TeamLoadOutGenerator.java
@@ -42,9 +42,6 @@ import java.util.*;
 
 import megamek.client.ratgenerator.ForceDescriptor;
 import megamek.client.ui.dialogs.unitSelectorDialogs.AbstractUnitSelectorDialog;
-import megamek.common.game.Game;
-import megamek.common.interfaces.ITechnology.Faction;
-import megamek.common.loaders.MapSettings;
 import megamek.common.SimpleTechLevel;
 import megamek.common.Team;
 import megamek.common.TechConstants;
@@ -59,6 +56,9 @@ import megamek.common.equipment.BombType.BombTypeEnum;
 import megamek.common.equipment.EquipmentType;
 import megamek.common.equipment.MiscType;
 import megamek.common.equipment.Mounted;
+import megamek.common.game.Game;
+import megamek.common.interfaces.ITechnology.Faction;
+import megamek.common.loaders.MapSettings;
 import megamek.common.options.GameOptions;
 import megamek.common.options.OptionsConstants;
 import megamek.common.units.BTObject;
@@ -1278,7 +1278,7 @@ public class TeamLoadOutGenerator {
     private void iterativelyLoadAmmo(Entity e, MunitionTree mt, List<AmmoMounted> binList, String binName,
           String techBase, String faction) {
         // Copy counts that we will update, otherwise mt entry gets edited permanently.
-        HashMap<String, Integer> counts = new HashMap<>(mt.getCountsOfAmmosForKey(e.getFullChassis(),
+        HashMap<String, Integer> counts = new HashMap<>(mt.getCountsOfAmmunitionForKey(e.getFullChassis(),
               e.getModel(),
               e.getCrew().getName(0),
               binName));

megamek/src/megamek/common/actions/ClubAttackAction.java
@@ -475,9 +475,9 @@ public class ClubAttackAction extends PhysicalAttackAction {
             clubArc = Compute.ARC_FORWARD;
         } else {
             if (club.getLocation() == Mek.LOC_LARM) {
-                clubArc = Compute.ARC_LEFTARM;
+                clubArc = Compute.ARC_LEFT_ARM;
             } else if (armMounted) {
-                clubArc = Compute.ARC_RIGHTARM;
+                clubArc = Compute.ARC_RIGHT_ARM;
             } else if (club.isRearMounted()) {
                 clubArc = Compute.ARC_REAR;
             } else {

megamek/src/megamek/common/actions/PunchAttackAction.java
@@ -229,8 +229,8 @@ public class PunchAttackAction extends PhysicalAttackAction {
         final int attackerHeight = ae.relHeight() + attHex.getLevel(); // The absolute level of the attacker's arms
         final int targetElevation = target.getElevation()
               + targHex.getLevel(); // The absolute level of the target's arms
-        final int armArc = (arm == PunchAttackAction.RIGHT) ? Compute.ARC_RIGHTARM
-              : Compute.ARC_LEFTARM;
+        final int armArc = (arm == PunchAttackAction.RIGHT) ? Compute.ARC_RIGHT_ARM
+              : Compute.ARC_LEFT_ARM;
 
         ToHitData toHit;
 

megamek/src/megamek/common/compute/ComputeArc.java
@@ -349,12 +349,12 @@ public class ComputeArc {
                         return true;
                     }
                     break;
-                case Compute.ARC_RIGHTARM:
+                case Compute.ARC_RIGHT_ARM:
                     if ((fa >= 300) || (fa <= 120)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_LEFTARM:
+                case Compute.ARC_LEFT_ARM:
                     if ((fa >= 240) || (fa <= 60)) {
                         return true;
                     }
@@ -364,17 +364,17 @@ public class ComputeArc {
                         return true;
                     }
                     break;
-                case Compute.ARC_RIGHTSIDE:
+                case Compute.ARC_RIGHT_SIDE:
                     if ((fa > 60) && (fa <= 120)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_LEFTSIDE:
+                case Compute.ARC_LEFT_SIDE:
                     if ((fa < 300) && (fa >= 240)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_MAINGUN:
+                case Compute.ARC_MAIN_GUN:
                     if ((fa >= 240) || (fa <= 120)) {
                         return true;
                     }
@@ -406,42 +406,42 @@ public class ComputeArc {
                         return true;
                     }
                     break;
-                case Compute.ARC_LWING:
+                case Compute.ARC_LEFT_WING:
                     if ((fa > 300) || (fa <= 0)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_LWING_WPL:
+                case Compute.ARC_LEFT_WING_WPL:
                     if ((fa > 240) || (fa < 60)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_RWING:
+                case Compute.ARC_RIGHT_WING:
                     if ((fa >= 0) && (fa < 60)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_RWING_WPL:
+                case Compute.ARC_RIGHT_WING_WPL:
                     if ((fa > 300) || (fa < 120)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_LWINGA:
+                case Compute.ARC_LEFT_WING_AFT:
                     if ((fa >= 180) && (fa < 240)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_LWINGA_WPL:
+                case Compute.ARC_LEFT_WING_AFT_WPL:
                     if ((fa > 120) && (fa < 300)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_RWINGA:
+                case Compute.ARC_RIGHT_WING_AFT:
                     if ((fa > 120) && (fa <= 180)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_RWINGA_WPL:
+                case Compute.ARC_RIGHT_WING_AFT_WPL:
                     if ((fa > 60) && (fa < 240)) {
                         return true;
                     }
@@ -451,42 +451,42 @@ public class ComputeArc {
                         return true;
                     }
                     break;
-                case Compute.ARC_LEFTSIDE_SPHERE:
+                case Compute.ARC_LEFT_SIDE_SPHERE:
                     if ((fa > 240) || (fa < 0)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_LEFTSIDE_SPHERE_WPL:
+                case Compute.ARC_LEFT_SIDE_SPHERE_WPL:
                     if ((fa > 180) || (fa < 60)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_RIGHTSIDE_SPHERE:
+                case Compute.ARC_RIGHT_SIDE_SPHERE:
                     if ((fa > 0) && (fa < 120)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_RIGHTSIDE_SPHERE_WPL:
+                case Compute.ARC_RIGHT_SIDE_SPHERE_WPL:
                     if ((fa > 300) || (fa < 180)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_LEFTSIDEA_SPHERE:
+                case Compute.ARC_LEFT_SIDE_AFT_SPHERE:
                     if ((fa > 180) && (fa < 300)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_LEFTSIDEA_SPHERE_WPL:
+                case Compute.ARC_LEFT_SIDE_AFT_SPHERE_WPL:
                     if ((fa > 120) && (fa < 360)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_RIGHTSIDEA_SPHERE:
+                case Compute.ARC_RIGHT_SIDE_AFT_SPHERE:
                     if ((fa > 60) && (fa < 180)) {
                         return true;
                     }
                     break;
-                case Compute.ARC_RIGHTSIDEA_SPHERE_WPL:
+                case Compute.ARC_RIGHT_SIDE_AFT_SPHERE_WPL:
                     if ((fa > 0) && (fa < 240)) {
                         return true;
                     }

commit 6fa6eb21e20ac77f02e738d2447205087b8d4c51

Fix swarm attack missing isBurdened() check for Battle Armor
  Fixes a regression introduced in commit 37bd02f9a29 (Aug 18, 2025) where
  the getAntiMekMods() call was accidentally removed from getSwarmMekBaseToHit()
  during a refactoring pass.

  This caused Inner Sphere Battle Armor with body-mounted missile launchers
  to be able to perform swarm attacks when they should be blocked. Per TacOps
  rules, IS BA with body-mounted missiles cannot make anti-mek attacks (both
  leg attacks AND swarm attacks) until the launcher is jettisoned.

  The bug existed since August 2025 - leg attacks correctly blocked burdened
  BA, but swarm attacks did not.

  Root cause: During the "Compute, Containers, Copy" refactoring commit,
  getLegAttackBaseToHit() was updated to call getAntiMekMods() with a new
  instanceof check, but getSwarmMekBaseToHit() had its getAntiMekMods() call
  removed entirely instead of being updated.

  Fix: Restore the getAntiMekMods() call in getSwarmMekBaseToHit() to apply
  shared anti-mek modifiers including the isBurdened() check for BA.

  Adds unit tests verifying both attack types behave consistently for:
  - Burdened IS BA (both attacks impossible)
  - Unburdened IS BA (both attacks possible)
  - Clan BA (both attacks possible, never burdened)
  - Conventional infantry (both attacks possible)

megamek/unittests/megamek/common/compute/AntiMekAttackTest.java
@@ -1,0 +1,493 @@
+/*
+ * Copyright (C) 2025 The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MegaMek.
+ *
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ *
+ * MechWarrior Copyright Microsoft Corporation. MegaMek was created under
+ * Microsoft's "Game Content Usage Rules"
+ * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
+ * affiliated with Microsoft.
+ */
+package megamek.common.compute;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import megamek.common.Player;
+import megamek.common.TechConstants;
+import megamek.common.ToHitData;
+import megamek.common.battleArmor.BattleArmor;
+import megamek.common.board.Coords;
+import megamek.common.equipment.EquipmentType;
+import megamek.common.game.Game;
+import megamek.common.options.GameOptions;
+import megamek.common.options.Option;
+import megamek.common.options.OptionsConstants;
+import megamek.common.rolls.TargetRoll;
+import megamek.common.units.BipedMek;
+import megamek.common.units.Crew;
+import megamek.common.units.CrewType;
+import megamek.common.units.EntityWeightClass;
+import megamek.common.units.Infantry;
+import megamek.common.units.Mek;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for anti-mek attack calculations in {@link Compute}.
+ * <p>
+ * These tests verify that leg attacks and swarm attacks correctly check the isBurdened() status for Battle Armor units
+ * with body-mounted missiles.
+ * <p>
+ * Per TacOps rules, Inner Sphere BA with body-mounted missile launchers cannot make anti-mek attacks until the launcher
+ * is jettisoned.
+ */
+class AntiMekAttackTest {
+
+    private Game game;
+    private GameOptions mockGameOptions;
+    private Player player1;
+    private Player player2;
+
+    @BeforeAll
+    static void initializeEquipment() {
+        EquipmentType.initializeTypes();
+    }
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+        mockGameOptions = mock(GameOptions.class);
+        game.setOptions(mockGameOptions);
+
+        // Set up game options
+        Option mockTrueBoolOpt = mock(Option.class);
+        Option mockFalseBoolOpt = mock(Option.class);
+        when(mockTrueBoolOpt.booleanValue()).thenReturn(true);
+        when(mockFalseBoolOpt.booleanValue()).thenReturn(false);
+        when(mockGameOptions.getOption(anyString())).thenReturn(mockFalseBoolOpt);
+        when(mockGameOptions.booleanOption(eq(OptionsConstants.BASE_FRIENDLY_FIRE))).thenReturn(false);
+
+        // Set up players
+        player1 = new Player(0, "Attacker");
+        player2 = new Player(1, "Defender");
+        game.addPlayer(0, player1);
+        game.addPlayer(1, player2);
+    }
+
+    /**
+     * Creates an Inner Sphere Battle Armor unit. Uses spy to allow mocking isBurdened() for controlled test behavior.
+     */
+    private BattleArmor createISBattleArmor(int troopers, boolean burdened) {
+        BattleArmor battleArmor = spy(new BattleArmor());
+        battleArmor.setGame(game);
+        battleArmor.setId(game.getNextEntityId());
+        battleArmor.setChassis("Test IS BA");
+        battleArmor.setModel(burdened ? "With Missiles" : "Standard");
+        battleArmor.setTroopers(troopers);
+        battleArmor.setWeightClass(EntityWeightClass.WEIGHT_MEDIUM);
+        battleArmor.setTechLevel(TechConstants.T_IS_TW_NON_BOX);
+
+        // Initialize crew with piloting skill
+        Crew crew = new Crew(CrewType.INFANTRY_CREW);
+        crew.setGunnery(4, 0);
+        crew.setPiloting(5, 0);
+        battleArmor.setCrew(crew);
+
+        battleArmor.setOwner(player1);
+
+        // Set armor values for each trooper location
+        for (int i = 1; i <= troopers; i++) {
+            battleArmor.initializeArmor(4, i);
+        }
+        battleArmor.autoSetInternal();
+
+        // Mock isBurdened() to return the desired state
+        when(battleArmor.isBurdened()).thenReturn(burdened);
+
+        return battleArmor;
+    }
+
+    /**
+     * Creates a Clan Battle Armor unit (not affected by isBurdened).
+     */
+    private BattleArmor createClanBattleArmor(int troopers) {
+        BattleArmor battleArmor = spy(new BattleArmor());
+        battleArmor.setGame(game);
+        battleArmor.setId(game.getNextEntityId());
+        battleArmor.setChassis("Test Clan BA");
+        battleArmor.setModel("Elemental");
+        battleArmor.setTroopers(troopers);
+        battleArmor.setWeightClass(EntityWeightClass.WEIGHT_MEDIUM);
+        battleArmor.setTechLevel(TechConstants.T_CLAN_TW);
+
+        // Initialize crew with piloting skill
+        Crew crew = new Crew(CrewType.INFANTRY_CREW);
+        crew.setGunnery(4, 0);
+        crew.setPiloting(5, 0);
+        battleArmor.setCrew(crew);
+
+        battleArmor.setOwner(player1);
+
+        // Set armor values for each trooper location
+        for (int i = 1; i <= troopers; i++) {
+            battleArmor.initializeArmor(4, i);
+        }
+        battleArmor.autoSetInternal();
+
+        // Clan BA is never burdened
+        when(battleArmor.isBurdened()).thenReturn(false);
+
+        return battleArmor;
+    }
+
+    /**
+     * Creates a target Mek.
+     */
+    private Mek createTargetMek() {
+        Mek mek = new BipedMek();
+        mek.setGame(game);
+        mek.setId(game.getNextEntityId());
+        mek.setChassis("Target");
+        mek.setModel("Mek");
+        mek.setWeight(50);
+
+        Crew crew = new Crew(CrewType.SINGLE);
+        mek.setCrew(crew);
+
+        mek.setOwner(player2);
+        mek.autoSetInternal();
+
+        return mek;
+    }
+
+    /**
+     * Creates conventional infantry.
+     */
+    private Infantry createConventionalInfantry(int troopers) {
+        Infantry infantry = new Infantry();
+        infantry.setGame(game);
+        infantry.setId(game.getNextEntityId());
+        infantry.setChassis("Test Platoon");
+        infantry.setModel("Standard");
+
+        Crew crew = new Crew(CrewType.INFANTRY_CREW);
+        crew.setGunnery(4, 0);
+        crew.setPiloting(5, 0);
+        infantry.setCrew(crew);
+
+        infantry.setOwner(player1);
+        infantry.autoSetInternal();
+        infantry.initializeInternal(troopers, Infantry.LOC_INFANTRY);
+
+        return infantry;
+    }
+
+    @Nested
+    @DisplayName("isBurdened() Mock Verification")
+    class IsBurdenedTests {
+
+        @Test
+        @DisplayName("Mock burdened BA returns true for isBurdened()")
+        void mockedBurdenedBaReturnsTrue() {
+            BattleArmor battleArmor = createISBattleArmor(4, true);
+
+            assertTrue(battleArmor.isBurdened(),
+                  "Mocked burdened BA should return true for isBurdened()");
+        }
+
+        @Test
+        @DisplayName("Mock unburdened BA returns false for isBurdened()")
+        void mockedUnburdenedBaReturnsFalse() {
+            BattleArmor battleArmor = createISBattleArmor(4, false);
+
+            assertFalse(battleArmor.isBurdened(),
+                  "Mocked unburdened BA should return false for isBurdened()");
+        }
+
+        @Test
+        @DisplayName("Clan BA always returns false for isBurdened()")
+        void clanBaAlwaysUnburdened() {
+            BattleArmor battleArmor = createClanBattleArmor(5);
+
+            assertFalse(battleArmor.isBurdened(),
+                  "Clan BA should never be burdened");
+        }
+    }
+
+    @Nested
+    @DisplayName("Leg Attack Tests")
+    class LegAttackTests {
+
+        @Test
+        @DisplayName("IS BA without missiles can leg attack")
+        void isBattleArmorWithoutMissilesCanLegAttack() {
+            BattleArmor attacker = createISBattleArmor(4, false);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            ToHitData toHit = Compute.getLegAttackBaseToHit(attacker, defender, game);
+
+            assertNotEquals(TargetRoll.IMPOSSIBLE, toHit.getValue(),
+                  "IS BA without body-mounted missiles should be able to leg attack");
+        }
+
+        @Test
+        @DisplayName("IS BA with body-mounted missiles cannot leg attack")
+        void isBattleArmorWithMissilesCannotLegAttack() {
+            BattleArmor attacker = createISBattleArmor(4, true);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            // Verify the BA is burdened first
+            assertTrue(attacker.isBurdened(), "Test setup: BA should be burdened");
+
+            ToHitData toHit = Compute.getLegAttackBaseToHit(attacker, defender, game);
+
+            assertEquals(TargetRoll.IMPOSSIBLE, toHit.getValue(),
+                  "IS BA with body-mounted missiles should not be able to leg attack");
+            assertTrue(toHit.getDesc().contains("jettison"),
+                  "Reason should mention launcher not jettisoned");
+        }
+
+        @Test
+        @DisplayName("Clan BA can leg attack (never burdened)")
+        void clanBattleArmorCanLegAttack() {
+            BattleArmor attacker = createClanBattleArmor(5);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            // Verify Clan BA is not burdened
+            assertFalse(attacker.isBurdened(), "Test setup: Clan BA should not be burdened");
+
+            ToHitData toHit = Compute.getLegAttackBaseToHit(attacker, defender, game);
+
+            assertNotEquals(TargetRoll.IMPOSSIBLE, toHit.getValue(),
+                  "Clan BA should be able to leg attack (never burdened)");
+        }
+
+        @Test
+        @DisplayName("Conventional infantry can leg attack")
+        void conventionalInfantryCanLegAttack() {
+            Infantry attacker = createConventionalInfantry(22);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            ToHitData toHit = Compute.getLegAttackBaseToHit(attacker, defender, game);
+
+            assertNotEquals(TargetRoll.IMPOSSIBLE, toHit.getValue(),
+                  "Conventional infantry with 22+ troopers should be able to leg attack");
+        }
+    }
+
+    @Nested
+    @DisplayName("Swarm Attack Tests")
+    class SwarmAttackTests {
+
+        @Test
+        @DisplayName("IS BA without missiles can swarm attack")
+        void isBattleArmorWithoutMissilesCanSwarmAttack() {
+            BattleArmor attacker = createISBattleArmor(4, false);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            ToHitData toHit = Compute.getSwarmMekBaseToHit(attacker, defender, game);
+
+            assertNotEquals(TargetRoll.IMPOSSIBLE, toHit.getValue(),
+                  "IS BA without body-mounted missiles should be able to swarm attack");
+        }
+
+        @Test
+        @DisplayName("IS BA with body-mounted missiles cannot swarm attack")
+        void isBattleArmorWithMissilesCannotSwarmAttack() {
+            BattleArmor attacker = createISBattleArmor(4, true);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            // Verify the BA is burdened first
+            assertTrue(attacker.isBurdened(), "Test setup: BA should be burdened");
+
+            ToHitData toHit = Compute.getSwarmMekBaseToHit(attacker, defender, game);
+
+            assertEquals(TargetRoll.IMPOSSIBLE, toHit.getValue(),
+                  "IS BA with body-mounted missiles should not be able to swarm attack");
+            assertTrue(toHit.getDesc().contains("jettison"),
+                  "Reason should mention launcher not jettisoned");
+        }
+
+        @Test
+        @DisplayName("Clan BA can swarm attack (never burdened)")
+        void clanBattleArmorCanSwarmAttack() {
+            BattleArmor attacker = createClanBattleArmor(5);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            // Verify Clan BA is not burdened
+            assertFalse(attacker.isBurdened(), "Test setup: Clan BA should not be burdened");
+
+            ToHitData toHit = Compute.getSwarmMekBaseToHit(attacker, defender, game);
+
+            assertNotEquals(TargetRoll.IMPOSSIBLE, toHit.getValue(),
+                  "Clan BA should be able to swarm attack (never burdened)");
+        }
+
+        @Test
+        @DisplayName("Conventional infantry can swarm attack")
+        void conventionalInfantryCanSwarmAttack() {
+            Infantry attacker = createConventionalInfantry(22);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            ToHitData toHit = Compute.getSwarmMekBaseToHit(attacker, defender, game);
+
+            assertNotEquals(TargetRoll.IMPOSSIBLE, toHit.getValue(),
+                  "Conventional infantry with 22+ troopers should be able to swarm attack");
+        }
+    }
+
+    @Nested
+    @DisplayName("Consistency Tests - Leg and Swarm should behave identically")
+    class ConsistencyTests {
+
+        @Test
+        @DisplayName("Burdened BA: both leg and swarm should be impossible")
+        void burdenedBattleArmorBothAttacksImpossible() {
+            BattleArmor attacker = createISBattleArmor(4, true);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            assertTrue(attacker.isBurdened(), "Test setup: BA should be burdened");
+
+            ToHitData legAttack = Compute.getLegAttackBaseToHit(attacker, defender, game);
+            ToHitData swarmAttack = Compute.getSwarmMekBaseToHit(attacker, defender, game);
+
+            assertEquals(TargetRoll.IMPOSSIBLE, legAttack.getValue(),
+                  "Leg attack should be impossible for burdened BA");
+            assertEquals(TargetRoll.IMPOSSIBLE, swarmAttack.getValue(),
+                  "Swarm attack should be impossible for burdened BA");
+        }
+
+        @Test
+        @DisplayName("Unburdened BA: both leg and swarm should be possible")
+        void unburdenedBattleArmorBothAttacksPossible() {
+            BattleArmor attacker = createISBattleArmor(4, false);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            assertFalse(attacker.isBurdened(), "Test setup: BA should not be burdened");
+
+            ToHitData legAttack = Compute.getLegAttackBaseToHit(attacker, defender, game);
+            ToHitData swarmAttack = Compute.getSwarmMekBaseToHit(attacker, defender, game);
+
+            assertNotEquals(TargetRoll.IMPOSSIBLE, legAttack.getValue(),
+                  "Leg attack should be possible for unburdened BA");
+            assertNotEquals(TargetRoll.IMPOSSIBLE, swarmAttack.getValue(),
+                  "Swarm attack should be possible for unburdened BA");
+        }
+
+        @Test
+        @DisplayName("Clan BA: both leg and swarm should be possible")
+        void clanBattleArmorBothAttacksPossible() {
+            BattleArmor attacker = createClanBattleArmor(5);
+            Mek defender = createTargetMek();
+
+            attacker.setPosition(new Coords(5, 5));
+            defender.setPosition(new Coords(5, 5));
+
+            game.addEntity(attacker);
+            game.addEntity(defender);
+
+            assertFalse(attacker.isBurdened(), "Test setup: Clan BA should not be burdened");
+
+            ToHitData legAttack = Compute.getLegAttackBaseToHit(attacker, defender, game);
+            ToHitData swarmAttack = Compute.getSwarmMekBaseToHit(attacker, defender, game);
+
+            assertNotEquals(TargetRoll.IMPOSSIBLE, legAttack.getValue(),
+                  "Leg attack should be possible for Clan BA");
+            assertNotEquals(TargetRoll.IMPOSSIBLE, swarmAttack.getValue(),
+                  "Swarm attack should be possible for Clan BA");
+        }
+    }
+}
cbaef5f9ffa84f68e26a87136cd0947035e05e88
psikomonkie <189469115+psikomonkie@users.noreply.github.com>
2026-01-02 12:55:48 -0500

    Issue #7855: Fix Victory Report for `AdvancedBuildingEntity`

--- megamek/src/megamek/common/units/AbstractBuildingEntity.java
+++ megamek/src/megamek/common/units/AbstractBuildingEntity.java
@@ -706,7 +706,41 @@ public abstract class AbstractBuildingEntity extends Entity implements IBuilding
      */
     @Override
     public Vector<Report> victoryReport() {
-        return null;
+        Vector<Report> vDesc = new Vector<>();
+
+        Report r = new Report(7025);
+        r.type = Report.PUBLIC;
+        r.addDesc(this);
+        vDesc.addElement(r);
+
+        r = new Report(7036);
+        r.type = Report.PUBLIC;
+        r.newlines = 0;
+        vDesc.addElement(r);
+        vDesc.addAll(getCrew().getDescVector(false));
+        r = new Report(7070, Report.PUBLIC);
+        r.add(getKillNumber());
+        vDesc.addElement(r);
+
+        if (isDestroyed()) {
+            Entity killer = game.getEntity(killerId);
+            if (killer == null) {
+                killer = game.getOutOfGameEntity(killerId);
+            }
+            if (killer != null) {
+                r = new Report(7072, Report.PUBLIC);
+                r.addDesc(killer);
+            } else {
+                r = new Report(7073, Report.PUBLIC);
+            }
+            vDesc.addElement(r);
+        } else if (getCrew().isEjected()) {
+            r = new Report(7071, Report.PUBLIC);
+            vDesc.addElement(r);
+        }
+        r.newlines = 2;
+
+        return vDesc;
     }
 
     /**

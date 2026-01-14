77ab4127cbe1b5c000ab202ab1a88a85e16dd823
sleet01 <sleet01@hotmail.com>
2026-01-13 17:35:24 -0800

    Update copyright year on touched files

--- megamek/src/megamek/common/equipment/Engine.java
+++ megamek/src/megamek/common/equipment/Engine.java
@@ -1,7 +1,7 @@
 /*
 
  * Copyright (C) 2000-2005 Ben Mazur (bmazur@sev.org)
- * Copyright (C) 2005-2025 The MegaMek Team. All Rights Reserved.
+ * Copyright (C) 2005-2026 The MegaMek Team. All Rights Reserved.
  *
  * This file is part of MegaMek.
  *

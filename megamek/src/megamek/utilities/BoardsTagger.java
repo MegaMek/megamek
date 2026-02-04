595ca64516e3b711bae2e95c1c237e3278819b78
HammerGS <drivingguy@hotmail.com>
2026-02-04 09:50:26 -0700

    Feedback on boards tagger.

--- megamek/src/megamek/utilities/BoardsTagger.java
+++ megamek/src/megamek/utilities/BoardsTagger.java
@@ -357,7 +357,11 @@ public class BoardsTagger {
     }
 
     public static void main(String... args) {
-        runBoardTagger();
+        if (args.length > 0 && args[0].equals("--update-headers")) {
+            runCopyrightHeaderUpdater();
+        } else {
+            runBoardTagger();
+        }
     }
 
     public static void runBoardTagger() {

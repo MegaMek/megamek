2bed990f073c6a0b0ac9dde8c3bd230cf0c929ee
James Magnan <james.magnan@gmail.com>
2026-02-11 15:38:29 -0800

    Updating to use the right range checks for out of range, given the use of MMLs

--- megamek/src/megamek/common/weapons/handlers/lrm/LRMSmokeWarheadHandler.java
+++ megamek/src/megamek/common/weapons/handlers/lrm/LRMSmokeWarheadHandler.java
@@ -37,6 +37,7 @@ package megamek.common.weapons.handlers.lrm;
 import java.io.Serial;
 import java.util.Vector;
 
+import megamek.common.RangeType;
 import megamek.common.Report;
 import megamek.common.ToHitData;
 import megamek.common.actions.WeaponAttackAction;
@@ -45,6 +46,7 @@ import megamek.common.compute.Compute;
 import megamek.common.equipment.AmmoType;
 import megamek.common.game.Game;
 import megamek.common.loaders.EntityLoadingException;
+import megamek.common.options.OptionsConstants;
 import megamek.common.units.Entity;
 import megamek.server.SmokeCloud;
 import megamek.server.totalWarfare.TWGameManager;
@@ -64,9 +66,8 @@ public class LRMSmokeWarheadHandler extends LRMHandler {
     @Override
     protected boolean specialResolution(Vector<Report> vPhaseReport, Entity entityTarget) {
         Coords coords = target.getPosition();
-
         AmmoType ammoType = ammo.getType();
-
+        
         if (!bMissed) {
             Report r = new Report(3190);
             r.subject = subjectId;
@@ -75,8 +76,13 @@ public class LRMSmokeWarheadHandler extends LRMHandler {
             vPhaseReport.addElement(r);
         } else {
             // scatterable LRMs scatter like dive bombing
+            int range = RangeType.rangeBracket(coords.distance(attackingEntity.getPosition()),
+                  weaponType.getRanges(weapon, weapon.getLinkedAmmo()),
+                  game.getOptions().booleanOption(OptionsConstants.ADVANCED_COMBAT_TAC_OPS_RANGE), 
+                        game.getOptions().booleanOption(OptionsConstants.ADVANCED_COMBAT_TAC_OPS_LOS_RANGE));
             coords = Compute.scatter(coords, 1);
-            if (game.getBoard().contains(coords)) {
+            
+            if (game.getBoard().contains(coords) && (range != RangeType.RANGE_OUT)) {
                 // misses and scatters to another hex
                 Report r = new Report(3195);
                 r.subject = subjectId;

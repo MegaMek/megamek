913bbf929c5c0a1e3379a797693bb80c36feaa2a
James Magnan <james.magnan@gmail.com>
2026-02-13 11:39:29 -0800

    Updating copyright and license (again)

--- megamek/src/megamek/common/net/upnp/GatewayFinder.java
+++ megamek/src/megamek/common/net/upnp/GatewayFinder.java
@@ -1,37 +1,4 @@
 /*
- * Copyright (C) 2026 The MegaMek Team. All Rights Reserved.
- *
- * This file is part of MegaMek.
- *
- * MegaMek is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License (GPL),
- * version 3 or (at your option) any later version,
- * as published by the Free Software Foundation.
- *
- * MegaMek is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * A copy of the GPL should have been included with this project;
- * if not, see <https://www.gnu.org/licenses/>.
- *
- * NOTICE: The MegaMek organization is a non-profit group of volunteers
- * creating free software for the BattleTech community.
- *
- * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
- * of The Topps Company, Inc. All Rights Reserved.
- *
- * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
- * InMediaRes Productions, LLC.
- *
- * MechWarrior Copyright Microsoft Corporation. MegaMek was created under
- * Microsoft's "Game Content Usage Rules"
- * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
- * affiliated with Microsoft.
- */
-
-/*
  * Copyright (C) 2015 Federico Dossena (adolfintel.com).
  *
  * This library is free software; you can redistribute it and/or
@@ -63,7 +30,7 @@ import java.util.LinkedList;
 
 /**
  * Original Code from https://github.com/adolfintel/WaifUPnP/. Added to Megamek as of Feb 2026 under the GPLv3 license.
- * Change made to package name. Federico Dossena
+ * Change made to package name. Thanks Federico Dossena
  *
  * @author Federico
  */

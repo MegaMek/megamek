14d00197988477a4098a9f00683f4c422f64126c
James Magnan <james.magnan@gmail.com>
2026-02-13 10:49:14 -0800

    Updating copyright and license

--- megamek/src/megamek/common/net/upnp/UPnP.java
+++ megamek/src/megamek/common/net/upnp/UPnP.java
@@ -1,3 +1,35 @@
+/*
+ * Copyright (C) 2026 The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MegaMek.
+ *
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ *
+ * MechWarrior Copyright Microsoft Corporation. MegaMek was created under
+ * Microsoft's "Game Content Usage Rules"
+ * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
+ * affiliated with Microsoft.
+ */
 
 /*
  * Copyright (C) 2015 Federico Dossena (adolfintel.com).
@@ -21,7 +53,9 @@
 package megamek.common.net.upnp;
 
 /**
- * Original Code from https://github.com/adolfintel/WaifUPnP/. Added to Megamek as of Feb 2026 under the GPL license
+ * Original Code from https://github.com/adolfintel/WaifUPnP/. Added to Megamek as of Feb 2026 under the GPLv3 license
+ * Modified to change the package name. Thanks to Federico Dossena.
+ * <p>
  * This class contains static methods that allow quick access to UPnP Port Mapping. Commands will be sent to the default
  * gateway.
  *

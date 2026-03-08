c06dd5e2db07ae214df3bcef8144f84e7b78df93
SJuliez <juliez@outlook.de>
2026-03-08 14:21:55 +0100

    Issue MML2047 javadoc

--- megamek/src/megamek/common/verifier/TestEntity.java
+++ megamek/src/megamek/common/verifier/TestEntity.java
@@ -1027,21 +1027,39 @@ public abstract class TestEntity implements TestEntityOption {
               + printMiscEquip() + printWeapon() + printAmmo();
     }
 
+    /**
+     * Tests the calculated weight of the unit (the sum of the weights of all its components) and the assumed weight
+     * (the fixed tonnage it is constructed with). Returns false if there are errors with the values (e.g. overweight,
+     * underweight if the unit type does not allow it, mek weight must be a multiple of 5t).
+     *
+     * @return true if there are no problems with the unit's assumed and calculated weight values
+     */
+    public final boolean isWeightCorrect() {
+        return correctWeight(new StringBuffer());
+    }
+
+    /**
+     * Tests the calculated weight of the unit (the sum of the weights of all its components) and the assumed weight
+     * (the fixed tonnage it is constructed with). Returns false if there are errors with the values (e.g. overweight,
+     * underweight if the unit type does not allow it, mek weight must be a multiple of 5t). Any error reports are
+     * appended to the given StringBuffer.
+     *
+     * @return true if there are no problems with the unit's assumed and calculated weight values
+     */
     public final boolean correctWeight(StringBuffer buff) {
         return correctWeight(buff, !showOverweightedEntity(), !showUnderweightEntity());
     }
 
     /**
-     * Tests the calculated weight of the unit (the sum of the weights of all its components) against the assumed weight
-     * (the fixed tonnage it is constructed with). Returns false if the calculated weight exceeds the assumed weight or
-     * if the calculated weight is lower than the assumed weight; in both cases a margin can be set by the test options.
-     * This method should be overridden for unit types that require other tests.
+     * Tests the calculated weight of the unit (the sum of the weights of all its components) and the assumed weight
+     * (the fixed tonnage it is constructed with). Returns false if there are errors with the values (e.g. overweight,
+     * underweight if the unit type does not allow it, mek weight must be a multiple of 5t).
      *
      * @param buff              The StringBuffer to append error messages to
      * @param ignoreOverweight  When true, ignore overweight
      * @param ignoreUnderweight When true, ignore underweight
      *
-     * @return true if the unit is considered of allowed weight, false if over or underweight in an unallowed fashion
+     * @return true if there are no problems with the unit's assumed and calculated weight values
      */
     public boolean correctWeight(StringBuffer buff, boolean ignoreOverweight, boolean ignoreUnderweight) {
         double weightSum = calculateWeight();

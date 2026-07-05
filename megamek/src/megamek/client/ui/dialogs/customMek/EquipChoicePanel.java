9a6bf755ce45b33bd71a39a61da56eec4c37927e
James Magnan <james.magnan@gmail.com>
2026-07-05 16:13:29 -0700

    Don't display Rotary Caseless ammo in the choices panel if TWRULES is enabled.

--- megamek/src/megamek/client/ui/dialogs/customMek/EquipChoicePanel.java
+++ megamek/src/megamek/client/ui/dialogs/customMek/EquipChoicePanel.java
@@ -653,6 +653,13 @@ public class EquipChoicePanel extends JPanel {
                     bTechMatch = atCheck.getStaticTechLevel().ordinal() <= legalLevel.ordinal() && canUseThisAmmo;
                 }
 
+                // Check if the ammo type is caseless, and do not include it if it is.
+                if (atCheck.getAmmoType() == AmmoType.AmmoTypeEnum.AC_ROTARY) {
+                    if (gameOpts.booleanOption(OptionsConstants.TWRULES) && atCheck.getMunitionType().contains(AmmoType.Munitions.M_CASELESS)) {
+                        continue;
+                    }
+                }
+
                 // If clan_ignore_eq_limits is unchecked, do NOT allow Clans to use IS-only ammo.
                 EnumSet<AmmoType.Munitions> munitionsTypes = atCheck.getMunitionType();
                 if (!gameOpts.booleanOption(OptionsConstants.ALLOWED_ALL_AMMO_MIXED_TECH) &&

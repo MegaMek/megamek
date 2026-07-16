1bbd755c1dd54293a06a81a47b3af9ef0a8db8ca
James Magnan <james.magnan@gmail.com>
2026-07-15 17:01:18 -0700

    Heat Dissipating Armor takes no heat (from infernos)

--- megamek/src/megamek/server/totalWarfare/TWGameManager.java
+++ megamek/src/megamek/server/totalWarfare/TWGameManager.java
@@ -7091,7 +7091,7 @@ public class TWGameManager extends AbstractGameManager {
                         } else { // No partial cover, missile hits
                             if ((te.getArmor(hit) > 0) &&
                                   (te.getArmorType(hit.getLocation()) == EquipmentType.T_ARMOR_HEAT_DISSIPATING)) {
-                                heatDamage += 1;
+                                heatDamage += Game.rulesManager.getRulesArmor().reduceHeatDamageByArmor(te.getArmorType(hit.getLocation()),heatDamage);
                                 heatReduced = true;
                                 reductionCause = ArmorType.forEntity(te, hit.getLocation()).getName();
                             } else {

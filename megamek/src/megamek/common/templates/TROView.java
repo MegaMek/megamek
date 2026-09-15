a9a80297950d61ff3645c7ae516ffdf61378e1b0
Sean Lai <sean.lai@me.com>
2026-09-15 17:56:55 -0400

    Update summary readout and TRO cost to show two decimal places

--- megamek/src/megamek/common/templates/TROView.java
+++ megamek/src/megamek/common/templates/TROView.java
@@ -177,7 +177,10 @@ public class TROView {
         model.put("techBase", formatTechBase(entity));
         model.put("tonnage", NumberFormat.getInstance().format(entity.getWeight()));
         model.put("battleValue", NumberFormat.getInstance().format(entity.calculateBattleValue()));
-        model.put("cost", NumberFormat.getInstance().format(entity.getCost(false)));
+        NumberFormat costFormatter = NumberFormat.getInstance();
+        costFormatter.setMinimumFractionDigits(2);
+        costFormatter.setMaximumFractionDigits(2);
+        model.put("cost", costFormatter.format(entity.getCost(false)));
 
         final StringJoiner quirksList = getQuirksList(entity);
         if (quirksList.length() > 0) {

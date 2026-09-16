5545350673224252d1c37873c5db0d302fd50234
James Magnan <james.magnan@gmail.com>
2026-09-16 07:45:53 -0700

    Fix NPE around computer movement envelope if the position is null.

--- megamek/src/megamek/client/ui/panels/phaseDisplay/MovementDisplay.java
+++ megamek/src/megamek/client/ui/panels/phaseDisplay/MovementDisplay.java
@@ -33,23 +33,6 @@
  */
 package megamek.client.ui.panels.phaseDisplay;
 
-import static megamek.common.LandingDirection.HORIZONTAL;
-import static megamek.common.LandingDirection.VERTICAL;
-import static megamek.common.bays.Bay.UNSET_BAY;
-import static megamek.common.equipment.MiscType.F_CHAFF_POD;
-import static megamek.common.options.OptionsConstants.ADVANCED_GROUND_MOVEMENT_TAC_OPS_ZIPLINES;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.InputEvent;
-import java.awt.event.MouseEvent;
-import java.io.Serial;
-import java.util.*;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import javax.swing.*;
-
 import megamek.client.event.BoardViewEvent;
 import megamek.client.ui.Messages;
 import megamek.client.ui.SharedUtility;
@@ -135,6 +118,23 @@ import megamek.common.turns.UnloadStrandedTurn;
 import megamek.common.units.*;
 import megamek.common.weapons.TeleMissile;
 import megamek.logging.MMLogger;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+import java.io.Serial;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static megamek.common.LandingDirection.HORIZONTAL;
+import static megamek.common.LandingDirection.VERTICAL;
+import static megamek.common.bays.Bay.UNSET_BAY;
+import static megamek.common.equipment.MiscType.F_CHAFF_POD;
+import static megamek.common.options.OptionsConstants.ADVANCED_GROUND_MOVEMENT_TAC_OPS_ZIPLINES;
 
 public class MovementDisplay extends ActionPhaseDisplay {
 
@@ -6506,6 +6506,15 @@ public class MovementDisplay extends ActionPhaseDisplay {
             movementGear = GEAR_LAND;
         } else {
             entity = suggestion;
+        }
+
+        // This guards against a null position. Go to suggestion if it is null.
+        if (entity.getPosition() == null) {
+            entity = suggestion;
+        }
+        // If the suggestion position is null, exit.
+        if (suggestion.getPosition() == null) {
+            return;
         }
 
         if (entity.isDone()) {

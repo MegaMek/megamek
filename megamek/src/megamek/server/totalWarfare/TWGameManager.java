dd49fd2826751e1851b0e176b91c6593a3faa190
James Magnan <james.magnan@gmail.com>
2026-09-20 14:26:09 -0700

    Critical hits on ACs for aero

--- megamek/src/megamek/server/totalWarfare/TWGameManager.java
+++ megamek/src/megamek/server/totalWarfare/TWGameManager.java
@@ -32,6 +32,21 @@
  */
 package megamek.server.totalWarfare;
 
+import static megamek.common.game.Game.TEAM_HAS_COMBAT_PARALYSIS;
+import static megamek.common.game.Game.TEAM_HAS_COMBAT_SENSE;
+import static megamek.common.game.Game.TEAM_HAS_NO_INITIATIVE_APTITUDE;
+import static megamek.common.options.OptionsConstants.INIT_INITIATIVE_STREAK_COMPENSATION;
+import static megamek.common.options.OptionsConstants.RPG_INDIVIDUAL_INITIATIVE;
+import static megamek.common.weapons.handlers.AreaEffectHelper.calculateDamageFallOff;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import megamek.client.bot.AIType;
 import megamek.client.bot.princess.BehaviorSettings;
 import megamek.client.ui.Messages;
@@ -135,21 +150,6 @@ import megamek.server.UnitOwnershipRules.OwnershipVerdict;
 import megamek.server.commands.*;
 import megamek.server.props.OrbitalBombardment;
 import megamek.server.victory.VictoryResult;
-
-import java.io.File;
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static megamek.common.game.Game.TEAM_HAS_COMBAT_PARALYSIS;
-import static megamek.common.game.Game.TEAM_HAS_COMBAT_SENSE;
-import static megamek.common.game.Game.TEAM_HAS_NO_INITIATIVE_APTITUDE;
-import static megamek.common.options.OptionsConstants.INIT_INITIATIVE_STREAK_COMPENSATION;
-import static megamek.common.options.OptionsConstants.RPG_INDIVIDUAL_INITIATIVE;
-import static megamek.common.weapons.handlers.AreaEffectHelper.calculateDamageFallOff;
 
 /**
  * Manages the Game and processes player actions.
@@ -21601,7 +21601,11 @@ public class TWGameManager extends AbstractGameManager {
                         }
                         Mounted<?> mounted = slot1.getMount();
                         if (mounted.equals(equipmentHit)) {
-                            aero.hitAllCriticalSlots(loc, i);
+                            slot1.setHit(true);
+                            Game.rulesManager.getRulesWeapons().setACHit(slot1, mounted, reports, aero.getId());
+                            if (slot1.isHit()) {
+                                aero.hitAllCriticalSlots(loc, i);
+                            }
                             break;
                         }
                     }
@@ -21618,7 +21622,11 @@ public class TWGameManager extends AbstractGameManager {
                                 }
                                 Mounted<?> mounted = slot1.getMount();
                                 if (mounted.equals(bayWeapon)) {
-                                    aero.hitAllCriticalSlots(loc, i);
+                                    slot1.setHit(true);
+                                    Game.rulesManager.getRulesWeapons().setACHit(slot1, mounted, reports, aero.getId());
+                                    if (slot1.isHit()) {
+                                        aero.hitAllCriticalSlots(loc, i);
+                                    }
                                     break;
                                 }
                             }

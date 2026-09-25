28404db62dc9dccba7a7cf580c04954286419a20
James Magnan <james.magnan@gmail.com>
2026-09-25 13:12:01 -0700

    Moving Unjamming from the general call to its own, and moving to only after firing phase (could have been with offboard or targetting before as well)
    Fixes #9072

--- megamek/src/megamek/server/totalWarfare/TWGameManager.java
+++ megamek/src/megamek/server/totalWarfare/TWGameManager.java
@@ -11099,7 +11099,6 @@ public class TWGameManager extends AbstractGameManager {
                 }
                 case FlipArmsAction faa -> entity.setArmsFlipped(faa.getIsFlipped());
                 case FindClubAction ignored -> resolveFindClub(entity);
-                case UnjamAction ignored -> resolveUnjam(entity);
                 case ClearMinefieldAction clearMinefieldAction ->
                       resolveClearMinefield(entity, clearMinefieldAction.getMinefield());
                 case TriggerAPPodAction tapa -> {
@@ -11160,6 +11159,27 @@ public class TWGameManager extends AbstractGameManager {
                         addReport(r);
                     }
                 }
+                default -> {
+                }
+            }
+        }
+    }
+
+    /**
+     * Called to complete weapon unjams
+     */
+    void resolveUnJams() {
+        // loop through actions and handle everything we expect except attacks
+        for (Enumeration<EntityAction> i = game.getActions(); i.hasMoreElements(); ) {
+            EntityAction ea = i.nextElement();
+            Entity entity = game.getEntity(ea.getEntityId());
+
+            if (entity == null) {
+                continue;
+            }
+
+            switch (ea) {
+                case UnjamAction ignored -> resolveUnjam(entity);
                 default -> {
                 }
             }
